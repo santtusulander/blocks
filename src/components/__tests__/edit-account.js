46293e96882619c3e802d52f064057f28eef50ad
Jenny Steele <jenny.steele@idean.com>
2016-01-05 17:01:17 -0800

    Tests for edit account

--- src/components/__tests__/edit-account.js
+++ src/components/__tests__/edit-account.js
@@ -11,5 +11,25 @@ describe('EditAccount', () => {
       <EditAccount account={Immutable.Map()}/>
     );
     expect(TestUtils.isCompositeComponent(editAccount)).toBeTruthy();
-  });
+  })
+  it('should save changes', () => {
+    let saveChanges = jest.genMockFunction()
+    let editAccount = TestUtils.renderIntoDocument(
+      <EditAccount account={Immutable.Map()} saveChanges={saveChanges}/>
+    )
+    let form = TestUtils.findRenderedDOMComponentWithTag(editAccount, 'form')
+    TestUtils.Simulate.submit(form)
+    expect(saveChanges.mock.calls.length).toEqual(1)
+  })
+  it('should update the account as changes happen', () => {
+    let changeValue = jest.genMockFunction()
+    let editAccount = TestUtils.renderIntoDocument(
+      <EditAccount account={Immutable.Map()} changeValue={changeValue}/>
+    )
+    let inputs = TestUtils.scryRenderedDOMComponentsWithTag(editAccount, 'input')
+    inputs[0].value = 'new'
+    TestUtils.Simulate.change(inputs[0])
+    expect(changeValue.mock.calls[0][0]).toEqual(['name'])
+    expect(changeValue.mock.calls[0][1]).toEqual('new')
+  })
 })
