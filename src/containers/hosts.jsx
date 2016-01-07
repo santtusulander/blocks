1b978a23deb2d065f8f9a045c57e241c51a9a3b4
Jenny Steele <jenny.steele@idean.com>
2016-01-07 15:02:49 -0800

    Add a link to config host instead of a modal like other pages

--- src/containers/hosts.jsx
+++ src/containers/hosts.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router'
 import Immutable from 'immutable'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Table, Modal } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
 import * as hostActionCreators from '../redux/modules/host'
 import AddHost from '../components/add-host'
-// import EditHost from '../components/edit-host'
 
 const Host = host =>
   <tr onClick={host.toggleActive}>
@@ -14,7 +14,9 @@ const Host = host =>
     <td>{host.name}</td>
     <td>{host.description}</td>
     <td>
-      <a href="#" onClick={host.delete}>Delete</a>
+      <Link to={`/configure/${host.brand}/${host.account}/${host.group}/${host.id}`}>
+        Configure
+      </Link> <a href="#" onClick={host.delete}>Delete</a>
     </td>
   </tr>
 Host.displayName = "Host"
@@ -23,10 +25,8 @@ export class Hosts extends React.Component {
   constructor(props) {
     super(props);
 
-    this.changeActiveHostValue = this.changeActiveHostValue.bind(this)
-    this.saveActiveHostChanges = this.saveActiveHostChanges.bind(this)
-    this.toggleActiveHost = this.toggleActiveHost.bind(this)
     this.createNewHost = this.createNewHost.bind(this)
+    this.deleteHost = this.deleteHost.bind(this)
   }
   componentWillMount() {
     this.props.hostActions.startFetching()
@@ -34,34 +34,6 @@ export class Hosts extends React.Component {
       this.props.params.brand,
       this.props.params.account,
       this.props.params.group
-    )
-  }
-  toggleActiveHost(id) {
-    return () => {
-      if(this.props.activeHost && this.props.activeHost.get('host_id') === id){
-        this.props.hostActions.changeActiveHost(null)
-      }
-      else {
-        this.props.hostActions.fetchHost(
-          this.props.params.brand,
-          this.props.params.account,
-          this.props.params.group,
-          id
-        )
-      }
-    }
-  }
-  changeActiveHostValue(valPath, value) {
-    this.props.hostActions.changeActiveHost(
-      this.props.activeHost.setIn(valPath, value)
-    )
-  }
-  saveActiveHostChanges() {
-    this.props.hostActions.updateHost(
-      this.props.params.brand,
-      this.props.params.account,
-      this.props.params.group,
-      this.props.activeHost.toJS()
     )
   }
   createNewHost(id) {
@@ -81,7 +53,6 @@ export class Hosts extends React.Component {
     )
   }
   render() {
-    const activeHost = this.props.activeHost
     return (
       <div className="container">
         <h1 className="page-header">Hosts</h1>
@@ -101,28 +72,16 @@ export class Hosts extends React.Component {
               this.props.hosts.map((host, i) =>
                 <Host key={i} id={host}
                   name="Name" description="Desc"
-                  toggleActive={this.toggleActiveHost(host)}
+                  brand={this.props.params.brand}
+                  account={this.props.params.account}
+                  group={this.props.params.group}
                   delete={(e) => {
-                    e.stopPropagation()
                     e.preventDefault()
                     this.deleteHost(host)
                   }}/>
               )}
           </tbody>
         </Table>
-        {activeHost ?
-          <Modal show={true}
-            onHide={this.toggleActiveHost(activeHost.get('host_id'))}>
-            <Modal.Header closeButton={true}>
-              <Modal.Title>Edit Host</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              {/*<EditHost host={activeHost}
-                changeValue={this.changeActiveHostValue}
-                saveChanges={this.saveActiveHostChanges}/>*/}
-            </Modal.Body>
-          </Modal> : null
-        }
       </div>
     );
   }
@@ -130,7 +89,6 @@ export class Hosts extends React.Component {
 
 Hosts.displayName = 'Hosts'
 Hosts.propTypes = {
-  activeHost: React.PropTypes.instanceOf(Immutable.Map),
   fetching: React.PropTypes.bool,
   hostActions: React.PropTypes.object,
   hosts: React.PropTypes.instanceOf(Immutable.List),
@@ -139,7 +97,6 @@ Hosts.propTypes = {
 
 function mapStateToProps(state) {
   return {
-    activeHost: state.host.get('activeHost'),
     hosts: state.host.get('allHosts'),
     fetching: state.host.get('fetching')
   };
