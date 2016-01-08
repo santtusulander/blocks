41621b68e1346f1ceb825b6f53b797a2609b870d
Mika Paulasaari <mika.paulasaari@idean.com>
2016-01-08 11:09:01 -0800

    Add analysis high level page

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,6 +7,7 @@ import Styleguide from './containers/styleguide'
 import Edge from './containers/configure/edge'
 import Purge from './containers/configure/purge'
 import Login from './containers/login'
+import Analysis from './containers/analysis'
 import Accounts from './containers/accounts'
 import Groups from './containers/groups'
 import Hosts from './containers/hosts'
@@ -18,6 +19,7 @@ module.exports = (
     <Route path="configure/edge" component={Edge}/>
     <Route path="configure/purge" component={Purge}/>
     <Route path="login" component={Login}/>
+    <Route path="analysis" component={Analysis}/>
     <Route path="accounts/:brand" component={Accounts}/>
     <Route path="groups/:brand/:account" component={Groups}/>
     <Route path="hosts/:brand/:account/:group" component={Hosts}/>
