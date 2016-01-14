20010654cdc76ec03e75187959cc8249ec0881b1
Mika Paulasaari <mika.paulasaari@idean.com>
2016-01-14 11:41:49 -0800

    Update content item tests
    Content item list test (the last two) gives errors I can't figure out

--- src/components/__tests__/content-item-chart.js
+++ src/components/__tests__/content-item-chart.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Immutable from 'immutable'
 import TestUtils from 'react-addons-test-utils'
 
-jest.dontMock('../account-chart.jsx')
-const AccountChart = require('../account-chart.jsx')
+jest.dontMock('../content-item-chart.jsx')
+const ContentItemChart = require('../content-item-chart.jsx')
 
 describe('AccountChart', () => {
   it('should exist', () => {
     let account = TestUtils.renderIntoDocument(
-      <AccountChart account={Immutable.Map()}/>
+      <ContentItemChart account={Immutable.Map()}/>
     );
     expect(TestUtils.isCompositeComponent(account)).toBeTruthy();
   })
