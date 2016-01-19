784e4ffeb7906123b5748d01a3b29e808630dfd0
Mika Paulasaari <mika.paulasaari@idean.com>
2016-01-19 10:15:48 -0800

    Add chart components to content item element
    Added two d3 line charts and example of an arc

--- src/components/content-item-chart.jsx
+++ src/components/content-item-chart.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import d3 from 'd3'
 
 class ContentItemChart extends React.Component {
   constructor(props) {
@@ -12,10 +13,91 @@ class ContentItemChart extends React.Component {
     this.props.delete(this.props.id)
   }
   render() {
+    const primaryMax = d3.max(this.props.primaryData, d => d.bytes)
+    const secondaryMax = d3.max(this.props.secondaryData, d => d.bytes)
+    const barMaxHeight = this.props.barMaxHeight;
+    const normalize = d3.scale.linear()
+      .domain([0, Math.max(primaryMax, secondaryMax)])
+      .range([0, barMaxHeight]);
+    const outerRadius = this.props.chartWidth / 2;
+    const innerRadius = (this.props.chartWidth / 2) - this.props.barMaxHeight;
+    const increment = 1.5;
+    var primaryAngle = -90;
+    var secondaryAngle = -90;
+    /* For performance reasons we draw the primary bar chart as a path. We need
+    to add extra points to the array so that the path draws the lines outwards
+    from the center of the graph. Every other value on the array is set to
+    'center', which is translated in the d3 function in to coordinates */
+    const primaryArray = d => {
+      var newArray = [];
+      d.forEach(i => {
+        newArray.push(normalize(i.bytes))
+        newArray.push('center')
+      })
+      return newArray
+    }
+    const primaryLine = d3.svg.line()
+      .x(function(d) {
+        /* If the value is 'center', set the X point to the center of the chart,
+        otherwise the X is calculated with Cos using the known angle, radius
+        (radius of the inner circle + bar height) and X of the circle's center */
+        var x = d === 'center' ? outerRadius :
+          Math.cos(primaryAngle * Math.PI / 180)
+          * (innerRadius + Number(d)) + outerRadius
+        return x
+      })
+      .y(function(d) {
+        /* If the value is 'center', set the Y point to the center of the chart,
+        otherwise the Y is calculated with Sin using the known angle, radius
+        (radius of the inner circle + bar height) and Y of the circle's center */
+        var y = d === 'center' ? outerRadius :
+          Math.sin(primaryAngle * Math.PI / 180)
+          * (innerRadius + Number(d)) + outerRadius
+        // Increment the angle for the next point
+        primaryAngle = primaryAngle + (increment / 2)
+        return y
+      })
+      .interpolate('linear')
+    const secondaryLine = d3.svg.line()
+      .x(function(d) {
+        /* Calculate the X point with Cos using the known angle, radius (radius
+        of the inner circle + bar height) and X of the circle's center */
+        var x = Math.cos(secondaryAngle * Math.PI / 180)
+          * (innerRadius + Number(normalize(d.bytes))) + outerRadius
+        return x
+      })
+      .y(function(d) {
+        /* Calculate the Y point with Sin using the known angle, radius (radius
+        of the inner circle + bar height) and Y of the circle's center */
+        var y = Math.sin(secondaryAngle * Math.PI / 180)
+          * (innerRadius + Number(normalize(d.bytes))) + outerRadius
+        // Increment the angle for the next point
+        secondaryAngle = secondaryAngle + increment
+        return y
+      })
+      .interpolate('basis')
+    const differenceArc = d3.svg.arc()
+      .innerRadius(innerRadius - 10)
+      .outerRadius(innerRadius)
+      .startAngle(0)
+      .endAngle(45 * Math.PI / 180)
     return (
-      <div className="content-item-chart" onClick={this.props.toggleActive}>
+      <div className="content-item-chart" onClick={this.props.toggleActive}
+        style={{width: outerRadius * 2, height: outerRadius * 2}}>
         <div className="glow"></div>
-        <div className="circle-base">
+        <svg className="content-item-chart-svg secondary-data">
+          {/* Add center point as last coordinate to close the path */}
+          <path className="content-item-chart-line"
+            d={secondaryLine(this.props.secondaryData)
+              + 'L' + outerRadius + ' ' + outerRadius} />
+        </svg>
+        <svg className="content-item-chart-svg primary-data">
+          <path className="content-item-chart-line"
+            d={primaryLine(primaryArray(this.props.primaryData))} />
+        </svg>
+        <div className="circle-base"
+          style={{width: innerRadius * 2, height: innerRadius * 2,
+          marginTop: -innerRadius, marginLeft: -innerRadius}}>
           <div className="circle-gradient"></div>
           <div className="text-content">
             <div className="content-item-traffic">
@@ -33,6 +115,12 @@ class ContentItemChart extends React.Component {
             <a href="#" onClick={this.deleteAccount}>Delete</a>
           </div>
         </div>
+        <svg className="content-item-chart-svg difference-arc">
+          <g>
+            <path className="content-item-chart-line"
+              d={differenceArc()} />
+          </g>
+        </svg>
       </div>
     )
   }
@@ -40,10 +128,15 @@ class ContentItemChart extends React.Component {
 
 ContentItemChart.displayName = 'ContentItemChart'
 ContentItemChart.propTypes = {
+  barMaxHeight: React.PropTypes.string,
+  barWidth: React.PropTypes.string,
+  chartWidth: React.PropTypes.string,
   delete: React.PropTypes.func,
   description: React.PropTypes.string,
   id: React.PropTypes.string,
   name: React.PropTypes.string,
+  primaryData: React.PropTypes.array,
+  secondaryData: React.PropTypes.array,
   toggleActive: React.PropTypes.func
 }
 
