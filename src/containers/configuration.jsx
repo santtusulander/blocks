65996fa757a28f3525ca3ec16b63113c4d3e593d
Jenny Steele <jenny.steele@idean.com>
2016-01-21 16:23:58 -0800

    Remove test code since api works now

--- src/containers/configuration.jsx
+++ src/containers/configuration.jsx
@@ -33,174 +33,33 @@ export class Configuration extends React.Component {
       this.props.params.host
     )
   }
+  getActiveConfig() {
+    return this.props.activeHost.get('services').get(0).get('configurations').find(
+      config => {
+        return config.get('config_id') === this.props.params.version
+      }
+    )
+  }
   changeValue(path, value) {
-    let activeHost = this.props.activeHost
+    const hostIndex = this.props.activeHost.get('services').get(0).get('configurations').findIndex(
+      config => {
+        return config.get('config_id') === this.props.params.version
+      }
+    )
+
     this.props.hostActions.changeActiveHost(
-      activeHost.setIn(path, value)
+      this.props.activeHost.setIn(
+        ['services', 0, 'configurations', hostIndex],
+        this.getActiveConfig().setIn(path, value)
+      )
     )
   }
   saveActiveHostChanges() {
-    // TODO: This is test code while trying to get saves working
-    const activeConfig = Immutable.fromJS({
-      "config_id": "1",
-      "configuration_status": {
-        "last_edited_by": "Stan Laurel",
-        "last_edited": "10 Jan 2016 - 10:52"
-      },
-      "edge_configuration": {
-        "published_name": "example.com"
-      },
-      "default_policies": [
-        {
-          "set": {
-            "cache_control": {
-              "honor_origin": true,
-              "check_etag": "weak"
-            }
-          }
-        },
-        {
-          "set": {
-            "cache_name": {
-              "ignore_case": false
-            }
-          }
-        }
-      ],
-      "request_policies": [
-        {
-          "match": {
-            "field": "request_path",
-            "cases": [
-              [
-                "/videos/(.*)\\.mp4",
-                [
-                  {
-                    "set": {
-                      "cache_name": {
-                        "name": [
-                          {
-                            "field": "text",
-                            "field_detail": "content/"
-                          },
-                          {
-                            "field": "request_query_arg",
-                            "field_detail": "itag"
-                          },
-                          {
-                            "field": "text",
-                            "field_detail": "/"
-                          },
-                          {
-                            "field": "group",
-                            "field_detail": "1"
-                          }
-                        ]
-                      }
-                    }
-                  }
-                ]
-              ],
-              [
-                "(.*)\\.m3u8",
-                [
-                  {
-                    "set": {
-                      "cache_control": {
-                        "max-age": 10
-                      }
-                    }
-                  }
-                ]
-              ]
-            ],
-            "default": [
-              {
-                "set": {
-                  "cache_control": {
-                    "no-store": true
-                  }
-                }
-              }
-            ]
-          }
-        },
-        {
-          "match": {
-            "field": "request_cookie",
-            "field_detail": "client_type",
-            "cases": [
-              [
-                "mobile",
-                [
-                  {
-                    "set": {
-                      "header": {
-                        "action": "set",
-                        "header": "X-optimize",
-                        "value": "yes"
-                      }
-                    }
-                  }
-                ]
-              ]
-            ]
-          }
-        }
-      ],
-      "response_policies": [
-        {
-          "match": {
-            "field": "response_code",
-            "cases": [
-              [
-                "307",
-                [
-                  {
-                    "match": {
-                      "field": "response_header",
-                      "field_detail": "Location",
-                      "cases": [
-                        [
-                          "origin1.example.com/(.*)",
-                          [
-                            {
-                              "set": {
-                                "header": {
-                                  "action": "set",
-                                  "header": "Location",
-                                  "value": [
-                                    {
-                                      "field": "text",
-                                      "field_detail": "origin2.example.com/"
-                                    },
-                                    {
-                                      "field": "group",
-                                      "field_detail": "1"
-                                    }
-                                  ]
-                                }
-                              }
-                            }
-                          ]
-                        ]
-                      ]
-                    }
-                  }
-                ]
-              ]
-            ]
-          }
-        }
-      ]
-    })
-    const newHost = this.props.activeHost.setIn(['services',0,'configurations',0],activeConfig)
-
     this.props.hostActions.updateHost(
       this.props.params.brand,
       this.props.params.account,
       this.props.params.group,
-      newHost.toJS()
+      this.props.activeHost.toJS()
     )
   }
   activateTab(tabName) {
@@ -213,47 +72,11 @@ export class Configuration extends React.Component {
     if(this.props.fetching || !this.props.activeHost || !this.props.activeHost.size) {
       return <div className="container">Loading...</div>
     }
-    // const activeConfig = this.props.activeHost.get('services').get(0).get('configurations').find(
-    //   config => config.get('version') === this.props.params.version
-    // )
-    const activeConfig = Immutable.fromJS({
-      edge_configuration: {
-        published_name: "aaa",
-        origin_host_name: "bbb",
-        origin_host_port: "111",
-        host_header: "origin_host_name",
-        origin_path_append: "ddd"
-      },
-      response_policies: [
-        {
-          defaults: {
-            match: "*",
-            policies: [
-              {
-                type: "cache",
-                action: "set",
-                honor_origin_cache_policies: true
-              },
-              {
-                type: "cache",
-                action: "set",
-                ignore_case: false
-              },
-              {
-                type: "cache",
-                action: "set",
-                honor_etags: true
-              },
-              {
-                type: "cache",
-                action: "set",
-                cache_errors: "10s"
-              }
-            ]
-          }
-        }
-      ]
-    })
+    const activeConfig = this.props.activeHost.get('services').get(0).get('configurations').find(
+      config => {
+        return config.get('config_id') === this.props.params.version
+      }
+    )
 
     return (
       <div className="container">
