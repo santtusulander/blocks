a50abad5912c529f16e42ce182398683086bfe26
Jenny Steele <jenny.steele@idean.com>
2016-01-27 16:45:08 -0800

    Move cache rules list table into its own component

--- src/components/configuration/cache.jsx
+++ src/components/configuration/cache.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import {Button, Input, Row, Col, Table} from 'react-bootstrap'
+import {Input, Row, Col} from 'react-bootstrap'
 import Immutable from 'immutable'
 
 import ConfigurationDefaultPolicies from './default-policies'
+import ConfigurationCacheRules from './cache-rules'
 
 class ConfigurationCache extends React.Component {
   constructor(props) {
@@ -89,47 +90,7 @@ class ConfigurationCache extends React.Component {
         {/* CDN Cache Rules */}
 
         <h3>CDN Cache Rules</h3>
-
-        <Table striped={true} bordered={true} hover={true}>
-          <thead>
-            <tr>
-              <th>Rule Priority</th>
-              <th>Rule Type</th>
-              <th>Rule</th>
-              <th>TTL Value</th>
-              <th>Match Condition</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>extension</td>
-              <td>gif</td>
-              <td>1 day</td>
-              <td>positive</td>
-              <td><a href="#">edit</a> <a href="#">delete</a></td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>directory</td>
-              <td>/wp-content</td>
-              <td>no-store</td>
-              <td>positive</td>
-              <td><a href="#">edit</a> <a href="#">delete</a></td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>MIME-type</td>
-              <td>text/html</td>
-              <td>15 min</td>
-              <td>positive</td>
-              <td><a href="#">edit</a> <a href="#">delete</a></td>
-            </tr>
-          </tbody>
-        </Table>
-
-        <Button bsStyle="primary">Add Cache Rule</Button>
+        <ConfigurationCacheRules/>
       </form>
     )
   }
