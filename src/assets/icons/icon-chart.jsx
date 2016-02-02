cf5e4f3231937210f28b042e931351b51dce2dbf
Mika Paulasaari <mika.paulasaari@idean.com>
2016-02-02 10:31:26 -0800

    Add new assets

---
+++ src/assets/icons/icon-chart.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Icon from './icon.jsx'
+
+class IconChart extends React.Component {
+  render() {
+    return (
+      <Icon width="22" height="20">
+        <g>
+          <path d="M3.9,10C4,10,4,10,3.9,10L4,20c0,0,0,0-0.1,0H0.1C0,20,0,20,0,20l0-10c0,0,0,0,0.1,0H3.9z"/>
+          <path d="M12.9,0C13,0,13,0,13,0V20c0,0,0,0-0.1,0H9.1C9,20,9,20,9,20V0c0,0,0,0,0.1,0L12.9,0z"/>
+          <path d="M21.9,5C22,5,22,5,22,5V20c0,0,0,0-0.1,0h-3.8C18,20,18,20,18,20V5c0,0,0,0,0.1,0H21.9z"/>
+        </g>
+      </Icon>
+    );
+  }
+}
+
+IconChart.displayName = 'IconChart'
+IconChart.propTypes = {
+  className: React.PropTypes.string
+}
+
+module.exports = IconChart
---
+++ src/assets/icons/icon-h1-caret.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Icon from './icon.jsx'
+
+class IconH1Caret extends React.Component {
+  render() {
+    return (
+      <Icon width="23" height="14" className="no-fill">
+        <g>
+          <polyline points="1.1,1.1 11.5,11.5 21.9,1.1" strokeWidth="3"/>
+        </g>
+      </Icon>
+    );
+  }
+}
+
+IconH1Caret.displayName = 'IconH1Caret'
+IconH1Caret.propTypes = {
+  className: React.PropTypes.string
+}
+
+module.exports = IconH1Caret
