638e189c0f4c80d684bbe53af581f866ecb0002d
Bryan Downing <bryan.downing@idean.com>
2016-02-04 20:07:57 -0800

    UDNP-64 — Implements access log with morgan and custom logger
    - Logs an error for all requests with a status code >= 400
    - Logs an info for all requests with a status code < 400

--- analytics-api/app/server.js
+++ analytics-api/app/server.js
@@ -1,9 +1,26 @@
 'use strict';
 
 let express = require("express");
+let morgan  = require('morgan');
 let router  = require('./router');
+let log     = require('./logger');
 let app     = express();
 
+// Start access log
+// Morgan config options: https://github.com/expressjs/morgan
+// Log errors (status code >= 400)
+app.use(morgan('combined', {
+  stream: log.errorStream,
+  skip: (req, res) => { return res.statusCode < 400; }
+}));
+
+// Log everything else (status code < 400)
+app.use(morgan('combined', {
+  stream: log.infoStream,
+  skip: (req, res) => { return res.statusCode > 399; }
+}));
+
+// Attach the router
 app.use('/', router);
 
 module.exports = app;
