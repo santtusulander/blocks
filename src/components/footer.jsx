b3639cdf6f16c0d2d7da7df6638e6c4c7c45cef1
Mika Paulasaari <mika.paulasaari@idean.com>
2016-02-11 11:00:52 -0800

    Add extra logic to footer
    Added check for left side sidebars, which will affect footer's layout

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 
 class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      layoutHasSidebar: false
+    }
+  }
+  componentDidMount() {
+    if(document.getElementsByClassName('sidebar-layout').length !== 0) {
+      this.setState({
+        layoutHasSidebar: true
+      })
+    }
+  }
   render() {
-    let className = '';
+    let className = 'footer'
+    if(this.state.layoutHasSidebar) {
+      className += ' has-layout-sidebar'
+    }
     if(this.props.className) {
-      className = className + ' ' + this.props.className;
+      className += ' ' + this.props.className
     }
     return (
       <footer className={className}>
-        <!-- @echo PACKAGE_VERSION -->
+        <div className="footer-content">
+          <p>v <!-- @echo PACKAGE_VERSION --></p>
+        </div>
       </footer>
     );
   }
