007cf897e6da82a579f411631c02cd385afbd370
Bryan Downing <bryan.downing@idean.com>
2016-02-23 11:46:00 -0800

    UDNP-63 — Adds MySQL connection pool

---
+++ analytics-api/app/db.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let mysql   = require('mysql');
+let configs = require('./configs');
+let log     = require('./logger');
+let db      = {
+  pool: mysql.createPool({
+    connectionLimit : configs.dbConnectionLimit,
+    host            : configs.dbHost,
+    user            : configs.dbUser,
+    password        : configs.dbPassword,
+    database        : configs.dbName
+  })
+};
+
+
+module.exports = db;
