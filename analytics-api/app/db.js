9eacf24004b3aa464febd8dfd59934b87cff032b
Bryan Downing <bryan.downing@idean.com>
2016-02-24 16:02:32 -0800

    UDNP-100 — Adds helper function to execute a query

--- analytics-api/app/db.js
+++ analytics-api/app/db.js
@@ -16,3 +16,22 @@ let db      = {
 
 
 module.exports = db;
+
+/**
+ * Inject data into a parameterized query and execute it.
+ *
+ * @param  {string}  query A parameterized query
+ * @param  {array}   data  The data to inject into the query
+ * @return {Promise}       Returns a promise that is fulfilled with the query results
+ */
+function executeQuery(query, data) {
+  // NOTE: We do this instead of passing an array of data to db.pool.query so we
+  // can log the actual query before executing it.
+  // NOTE: mysql.format automatically escapes the data to avoid SQL injection attacks.
+  let queryFinal = mysql.format(query, data);
+
+  log.debug('query:', queryFinal);
+
+  // Grab a connection from the pool and run the query
+  return db.pool.query(queryFinal);
+}
