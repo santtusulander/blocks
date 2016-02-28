e7c72ff77257234fa0ca07b9a94f06fe6d569c04
Bryan Downing <bryan.downing@idean.com>
2016-02-28 00:51:42 -0800

    UDNP-118 — Adds parameter validation class and tests

---
+++ analytics-api/app/validate.js
@@ -0,0 +1,82 @@
+'use strict';
+let _   = require('lodash');
+let log = require('./logger');
+
+
+/**
+ * A collection of validation functions.
+ * NOTE: This file exports an instance of this class, effectively making it a singleton.
+ * @class
+ */
+class Validate {
+
+  /**
+   * Ensure the value is a 10 digit integer (seconds elapsed since UNIX epoch).
+   *
+   * @private
+   * @param  {object}      data The data to validate in the format:
+   *                            {key: <string>, value: <mixed>, required: <boolean>}
+   * @return {string|null}      If valid, return null. If invalid, return error string.
+   */
+  _validateTimestamp(data) {
+    let message = `Error with ${data.key} parameter: You must provide a valid date (seconds elapsed since UNIX epoch). Value received <${typeof data.value}>: ${data.value}`;
+                  // If the value is defined...
+    let isValid = !_.isUndefined(data.value)
+                  // ...then it must be an integer with 10 digits...
+                  ? _.isInteger(data.value) && /^\d{10}$/.test(data.value)
+                  // ...otherwise it's valid, unless it's required
+                  : !data.required;
+    return isValid ? null : message;
+  }
+
+  /**
+   * Ensure the value is an integer.
+   *
+   * @private
+   * @param  {object}      data The data to validate in the format:
+   *                            {key: <string>, value: <mixed>, required: <boolean>}
+   * @return {string|null}      If valid, return null. If invalid, return error string.
+   */
+  _validateID(data) {
+    let message = `Error with ${data.key} parameter: You must provide a valid ID (integer). Value received <${typeof data.value}>: ${data.value}`;
+                  // If the value is defined...
+    let isValid = !_.isUndefined(data.value)
+                  // ...then it must be an integer...
+                  ? _.isInteger(data.value)
+                  // ...otherwise it's valid, unless it's required
+                  : !data.required;
+    return isValid ? null : message;
+  }
+
+  /**
+   * Validate the values of query string parameters.
+   *
+   * @param {object} params Parameter data to validate (e.g. {start: 1451606400})
+   * @param {object} spec   Defines the validation rules for each parameter
+   *                        (e.g. {start: {required: true, type: 'Timestamp'}, ...})
+   *
+   * @return {string|null}  If valid, return null. If invalid, return error array.
+   */
+  params(params, spec) {
+    let errors = [];
+
+    log.debug('validating params:', params);
+
+    // Loop the properties in the spec and call the validate method that
+    // corresponds to the type of the property.
+    _.forOwn(spec, (value, key) => {
+      let errorMessage = this[`_validate${value.type}`]({
+        key: key,
+        value: params[key],
+        required: value.required
+      });
+      errorMessage && errors.push(errorMessage);
+    });
+
+    // Return an array of errors if any of the parameters failed validation
+    return errors.length ? errors : null;
+  }
+
+}
+
+module.exports = new Validate();
