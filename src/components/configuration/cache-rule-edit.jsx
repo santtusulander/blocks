d3ae3f2c64cce3058e94dbbaf464d7ffbfc59c1a
Mika Paulasaari <mika.paulasaari@idean.com>
2016-02-29 10:23:14 -0800

    Add modal elements to cache rule edit form
    Added Modal Header and Body to Cache rule edit component, so the header part is also easily editable. This component is then rendered inside a Modal component to create a complete Sidebar Modal.

--- src/components/configuration/cache-rule-edit.jsx
+++ src/components/configuration/cache-rule-edit.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button, Input, Row, Col, ButtonToolbar} from 'react-bootstrap'
+import {Button, Input, Modal, Row, Col, ButtonToolbar} from 'react-bootstrap'
 import Immutable from 'immutable'
 
 import Select from '../select'
@@ -21,90 +21,96 @@ class ConfigurationCacheRuleEdit extends React.Component {
   render() {
     return (
       <form className="configuration-cache-rules" onSubmit={this.handleSave}>
+        <Modal.Header>
+          <h1>Add Cache Rule</h1>
+          <p>Lorem ipsum dolor</p>
+        </Modal.Header>
+        <Modal.Body>
 
-        {/* Rule Type */}
-        <Input label="Rule Type">
-          <Select className="input-select"
-            value="request_method"
-            addonAfter=' '
-            options={[
-              ['request_method', 'Request Method'],
-              ['request_scheme', 'Request Scheme'],
-              ['request_url', 'Request URL'],
-              ['request_host', 'Request Host'],
-              ['request_path', 'Request Path'],
-              ['request_query', 'Request Query'],
-              ['request_query_arg', 'Request Query Argument'],
-              ['request_header', 'Request Header'],
-              ['request_cookie', 'Request Cookie'],
-              ['response_code', 'Response Code'],
-              ['response_header', 'Response Header']
-            ]}/>
-        </Input>
-
-
-        {/* Rule Value */}
-
-        <Input label="Rule Value">
-          <Row>
-            <Col xs={6}>
-              <Input type="text" id="configure__edge__add-cache-rule__rule-value"
-                placeholder="text/html"
-                onChange={this.handleChange(['path'])}/>
-            </Col>
-            <Col xs={6}>
-              <Input type="select"
-                id="configure__edge__add-cache-rule__rule-match"
-                onChange={this.handleChange(['path'])}>
-                <option value="1">Matches</option>
-                <option value="2">Doesn't Match</option>
-              </Input>
-            </Col>
-          </Row>
-        </Input>
+          {/* Rule Type */}
+          <Input label="Rule Type">
+            <Select className="input-select"
+              value="request_method"
+              addonAfter=' '
+              options={[
+                ['request_method', 'Request Method'],
+                ['request_scheme', 'Request Scheme'],
+                ['request_url', 'Request URL'],
+                ['request_host', 'Request Host'],
+                ['request_path', 'Request Path'],
+                ['request_query', 'Request Query'],
+                ['request_query_arg', 'Request Query Argument'],
+                ['request_header', 'Request Header'],
+                ['request_cookie', 'Request Cookie'],
+                ['response_code', 'Response Code'],
+                ['response_header', 'Response Header']
+              ]}/>
+          </Input>
 
 
-        {/* No-Store */}
-        <Input type="checkbox"
-          id="configure__edge__add-cache-rule__no-store"
-          label="No-Store"
-          onChange={this.handleChange(['path'])}/>
+          {/* Rule Value */}
+
+          <Input label="Rule Value">
+            <Row>
+              <Col xs={6}>
+                <Input type="text" id="configure__edge__add-cache-rule__rule-value"
+                  placeholder="text/html"
+                  onChange={this.handleChange(['path'])}/>
+              </Col>
+              <Col xs={6}>
+                <Input type="select"
+                  id="configure__edge__add-cache-rule__rule-match"
+                  onChange={this.handleChange(['path'])}>
+                  <option value="1">Matches</option>
+                  <option value="2">Doesn't Match</option>
+                </Input>
+              </Col>
+            </Row>
+          </Input>
 
 
-        {/* TTL Value */}
-
-        <Input label="TTL Value">
-          <Row>
-            <Col xs={6}>
-              <Input type="number"
-                id="configure__edge__add-cache-rule__ttl-value"
-                placeholder="number"
-                onChange={this.handleChange(['path'])}/>
-            </Col>
-            <Col xs={6}>
-              <Input type="select"
-                id="configure__edge__add-cache-rule__ttl-value-extension"
-                onChange={this.handleChange(['path'])}>
-                <option value="1">seconds</option>
-                <option value="2">minutes</option>
-                <option value="3">hours</option>
-                <option value="4">days</option>
-              </Input>
-            </Col>
-          </Row>
-        </Input>
+          {/* No-Store */}
+          <Input type="checkbox"
+            id="configure__edge__add-cache-rule__no-store"
+            label="No-Store"
+            onChange={this.handleChange(['path'])}/>
 
 
-        {/* Action buttons */}
+          {/* TTL Value */}
 
-        <ButtonToolbar className="text-right">
-          <Button bsStyle="primary" onClick={this.props.hideAction}>
-            Cancel
-          </Button>
-          <Button type="submit" bsStyle="primary">
-            Add
-          </Button>
-        </ButtonToolbar>
+          <Input label="TTL Value">
+            <Row>
+              <Col xs={6}>
+                <Input type="number"
+                  id="configure__edge__add-cache-rule__ttl-value"
+                  placeholder="number"
+                  onChange={this.handleChange(['path'])}/>
+              </Col>
+              <Col xs={6}>
+                <Input type="select"
+                  id="configure__edge__add-cache-rule__ttl-value-extension"
+                  onChange={this.handleChange(['path'])}>
+                  <option value="1">seconds</option>
+                  <option value="2">minutes</option>
+                  <option value="3">hours</option>
+                  <option value="4">days</option>
+                </Input>
+              </Col>
+            </Row>
+          </Input>
+
+
+          {/* Action buttons */}
+
+          <ButtonToolbar className="text-right">
+            <Button bsStyle="primary" onClick={this.props.hideAction}>
+              Cancel
+            </Button>
+            <Button type="submit" bsStyle="primary">
+              Add
+            </Button>
+          </ButtonToolbar>
+        </Modal.Body>
       </form>
     )
   }
