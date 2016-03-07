af2a437faeb68348c64306556be95ca3e9b277b8
Mika Paulasaari <mika.paulasaari@idean.com>
2016-03-07 10:40:32 -0800

    Add more tests and fix warnings
    Added more test coverage and fixed some dummy data types to fix the 'invalid prop type' warnings

--- src/components/__tests__/header.js
+++ src/components/__tests__/header.js
@@ -7,6 +7,10 @@ jest.autoMockOff() // Uses react-bootstrap extensively, so don't auto mock
 jest.dontMock('../header.jsx')
 const Header = require('../header.jsx')
 
+const handleThemeChange = jest.genMockFunction()
+const logOut = jest.genMockFunction()
+const activatePurge = jest.genMockFunction()
+
 describe('Header', () => {
   it('should exist', () => {
     let header = TestUtils.renderIntoDocument(
@@ -21,5 +25,34 @@ describe('Header', () => {
     );
     let container = TestUtils.findRenderedDOMComponentWithTag(header, 'nav');
     expect(ReactDOM.findDOMNode(container).className).toContain('foo');
+  });
+
+  it('should call purge function when link is clicked', () => {
+    let header = TestUtils.renderIntoDocument(
+      <Header theme="dark" activatePurge={activatePurge} />
+    );
+    let mainNavLinks = TestUtils.scryRenderedDOMComponentsWithClass(header, 'main-nav-link');
+    TestUtils.Simulate.click(mainNavLinks[4]);
+    expect(activatePurge.mock.calls.length).toEqual(1);
+  });
+
+  it('should call theme handling function when link is clicked', () => {
+    let header = TestUtils.renderIntoDocument(
+      <Header theme="dark" handleThemeChange={handleThemeChange} />
+    );
+    let themeMenu = TestUtils.findRenderedDOMComponentWithClass(header, 'menu-item-theme');
+    let links = themeMenu.getElementsByTagName('a');
+    TestUtils.Simulate.click(links[1]);
+    expect(handleThemeChange.mock.calls.length).toEqual(1);
+  });
+
+  it('should call log out function when link is clicked', () => {
+    let header = TestUtils.renderIntoDocument(
+      <Header theme="dark" logOut={logOut} />
+    );
+    let themeMenu = TestUtils.findRenderedDOMComponentWithClass(header, 'bottom-item');
+    let links = themeMenu.getElementsByTagName('a');
+    TestUtils.Simulate.click(links[0]);
+    expect(logOut.mock.calls.length).toEqual(1);
   });
 })
--- src/components/configuration/__tests__/version.js
+++ src/components/configuration/__tests__/version.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import TestUtils from 'react-addons-test-utils'
 
 jest.dontMock('../version.jsx')
@@ -11,6 +12,15 @@ describe('ConfigurationVersion', () => {
     );
     expect(TestUtils.isCompositeComponent(version)).toBeTruthy();
   });
+
+  it('has active class if active', () => {
+    let version = TestUtils.renderIntoDocument(
+      <ConfigurationVersion active={true}/>
+    )
+    let link = TestUtils.findRenderedDOMComponentWithClass(version,
+      'version-link')
+    expect(ReactDOM.findDOMNode(link).className).toContain('active');
+  })
 
   it('should activate a verson when clicked', () => {
     let activate = jest.genMockFunction()
