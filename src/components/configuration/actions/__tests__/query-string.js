9fc2d753eabfc4d6edbaa1ece1c63edf14841bfa
Mika Paulasaari <mika.paulasaari@idean.com>
2016-03-07 16:02:01 -0800

    Add more test coverage
    Although these will most likely change in the future

--- src/components/configuration/actions/__tests__/query-string.js
+++ src/components/configuration/actions/__tests__/query-string.js
@@ -23,4 +23,19 @@ describe('QueryString', () => {
     expect(changeValue.mock.calls[0][0]).toEqual(['edge_configuration', 'cache_rule', 'actions', 'query_string_name'])
     expect(changeValue.mock.calls[0][1]).toEqual('new')
   })
+
+  it('should handle select changes', () => {
+    let changeValue = jest.genMockFunction()
+    let queryString = TestUtils.renderIntoDocument(
+      <QueryString changeValue={changeValue}/>
+    )
+    expect(queryString.state.activeActivity).toBe('add')
+    queryString.handleSelectChange('activeActivity')('foo')
+    expect(queryString.state.activeActivity).toBe('foo')
+    expect(changeValue.mock.calls[0][1]).toBe('foo')
+    expect(queryString.state.activeDirection).toBe('to_origin')
+    queryString.handleSelectChange('activeDirection')('bar')
+    expect(queryString.state.activeDirection).toBe('bar')
+    expect(changeValue.mock.calls[1][1]).toBe('bar')
+  })
 })
--- src/components/configuration/actions/__tests__/redirection.js
+++ src/components/configuration/actions/__tests__/redirection.js
@@ -23,4 +23,19 @@ describe('Redirection', () => {
     expect(changeValue.mock.calls[0][0]).toEqual(['edge_configuration', 'cache_rule', 'actions', 'redirection_domain'])
     expect(changeValue.mock.calls[0][1]).toEqual('new')
   })
+
+  it('should handle select changes', () => {
+    let changeValue = jest.genMockFunction()
+    let redirection = TestUtils.renderIntoDocument(
+      <Redirection changeValue={changeValue}/>
+    )
+    expect(redirection.state.activeProtocol).toBe('http')
+    redirection.handleSelectChange('activeProtocol')('foo')
+    expect(redirection.state.activeProtocol).toBe('foo')
+    expect(changeValue.mock.calls[0][1]).toBe('foo')
+    expect(redirection.state.activeRedirectionType).toBe('301')
+    redirection.handleSelectChange('activeRedirectionType')('bar')
+    expect(redirection.state.activeRedirectionType).toBe('bar')
+    expect(changeValue.mock.calls[1][1]).toBe('bar')
+  })
 })
