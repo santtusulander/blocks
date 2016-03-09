38d0aff02f9d43d7988a58e95f25cd65238754a9
Bryan Downing <bryan.downing@idean.com>
2016-03-08 16:23:50 -0800

    UDNP-147, UDNP-148 — Setup parameters and manipulate query data for traffic/time endpoint

--- analytics-api/app/routes/traffic/time.js
+++ analytics-api/app/routes/traffic/time.js
@@ -1,9 +1,44 @@
 'use strict';
 
+require('express-jsend');
+let log      = require('../../logger');
+let db       = require('../../db');
+let validate = require('../../validate');
 let testData = require('./time-data');
 
 function routeTrafficTime(req, res) {
-  res.json(testData);
+  log.info('Getting hourly traffic');
+  log.debug('query params:', req.query);
+
+  let params = req.query;
+  let errors = validate.params(params, {
+    start    : {required: true, type: 'Timestamp'},
+    end      : {required: false, type: 'Timestamp'},
+    account  : {required: false, type: 'ID'},
+    group    : {required: false, type: 'ID'},
+    property : {required: false, type: 'Property'}
+  });
+
+  if (errors) {
+    return res.status(400).jerror('Bad Request Parameters', errors);
+  }
+
+  db.getEgressHourly({
+    start    : params.start,
+    end      : params.end,
+    account  : params.account,
+    group    : params.group,
+    property : params.property
+  }).then((trafficData) => {
+    if (trafficData) {
+      // res.jsend(trafficData);
+    }
+
+    res.jsend(testData);
+
+  }).catch(() => {
+    res.status(500).jerror('Database', 'There was a problem with the analytics database. Check the analytics-api logs for more information.');
+  });
 }
 
 module.exports = routeTrafficTime;
