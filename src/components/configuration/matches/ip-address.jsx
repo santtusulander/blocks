4e851cb7a42104647c452b07e0615ca91e031695
Mika Paulasaari <mika.paulasaari@idean.com>
2016-03-14 15:02:16 -0700

    Change matches forms toggles to select dropdowns

--- src/components/configuration/matches/ip-address.jsx
+++ src/components/configuration/matches/ip-address.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
-import { Col, Input, Modal, Row } from 'react-bootstrap'
+import { Input, Modal } from 'react-bootstrap'
 import Immutable from 'immutable'
 
-import Toggle from '../../toggle'
+import Select from '../../select'
 
 class IpAddress extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      activeFilter: 'matches'
+    }
+
     this.handleChange = this.handleChange.bind(this)
-    this.handleToggleChange = this.handleToggleChange.bind(this)
+    this.handleSelectChange = this.handleSelectChange.bind(this)
   }
   handleChange(path) {
     return e => {
       this.props.changeValue(path, e.target.value)
     }
   }
-  handleToggleChange(path) {
+  handleSelectChange(path) {
     return value => {
+      this.setState({
+        activeFilter: value
+      })
       this.props.changeValue(path, value)
     }
   }
@@ -45,19 +52,14 @@ class IpAddress extends React.Component {
               ['edge_configuration', 'cache_rule', 'matches', 'ip_address_include_x_forwarded_for']
             )}/>
 
-          <hr />
-
-          <Row className="no-gutters">
-            <Col xs={8} className="toggle-label">
-              <label>Matches Media type</label>
-            </Col>
-            <Col xs={4}>
-              <Toggle className="pull-right" value={true}
-                changeValue={this.handleToggleChange(
-                  ['edge_configuration', 'cache_rule', 'matches', 'ip_address']
-                )}/>
-            </Col>
-          </Row>
+          <Select className="input-select"
+            onSelect={this.handleSelectChange(
+              ['edge_configuration', 'cache_rule', 'matches', 'ip_address']
+            )}
+            value={this.state.activeFilter}
+            options={[
+              ['matches', 'Matches'],
+              ['does_not_match', 'Does not match']]}/>
 
         </Modal.Body>
       </div>
