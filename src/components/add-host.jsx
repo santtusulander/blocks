683eb8a9d57f12790ad820236577d0fea06ec020
Jenny Steele <jenny.steele@idean.com>
2016-03-18 13:57:46 -0700

    Add radio buttons for trial or production deployment mode during create property

--- src/components/add-host.jsx
+++ src/components/add-host.jsx
@@ -5,22 +5,43 @@ class AddHost extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      deploymentMode: 'trial'
+    }
+
     this.cancelChanges = this.cancelChanges.bind(this)
     this.createHost = this.createHost.bind(this)
+    this.setDeploymentMode = this.setDeploymentMode.bind(this)
   }
   createHost(e) {
     e.preventDefault()
-    this.props.createHost(this.refs.new_host_name.getValue())
+    this.props.createHost(
+      this.refs.new_host_name.getValue(),
+      this.state.deploymentMode
+    )
   }
   cancelChanges(e) {
     e.preventDefault()
     this.props.cancelChanges()
+  }
+  setDeploymentMode(mode) {
+    return () => this.setState({deploymentMode: mode})
   }
   render() {
     return (
       <form onSubmit={this.createHost}>
         <Input type="text" label="New Host Name" id="new_host_name"
           ref="new_host_name"/>
+        <Input label="Deployment Mode">
+          <Input type="radio"
+            label="Trial"
+            onChange={this.setDeploymentMode('trial')}
+            checked={this.state.deploymentMode === 'trial'}/>
+          <Input type="radio"
+            label="Production"
+            onChange={this.setDeploymentMode('production')}
+            checked={this.state.deploymentMode === 'production'}/>
+        </Input>
         <ButtonToolbar>
           <Button bsStyle="primary" onClick={this.cancelChanges}>Cancel</Button>
           <Button type="submit" bsStyle="primary">Save</Button>
--- src/redux/modules/host.js
+++ src/redux/modules/host.js
@@ -131,13 +131,13 @@ export default handleActions({
 
 // ACTIONS
 
-export const createHost = createAction(HOST_CREATED, (brand, account, group, id) => {
+export const createHost = createAction(HOST_CREATED, (brand, account, group, id, deploymentMode) => {
   return axios.post(`${urlBase}/VCDN/v2/${brand}/accounts/${account}/groups/${group}/published_hosts/${id}`,
     {
       services:[
         {
           service_type: "large",
-          deployment_mode: "trial",
+          deployment_mode: deploymentMode,
           configurations: [
             {
               edge_configuration: {
