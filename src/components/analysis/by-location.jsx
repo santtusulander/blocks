78e1b2def1b305bf39d6cd2e078063c427ea3039
Mika Paulasaari <mika.paulasaari@idean.com>
2016-03-23 11:40:09 -0700

    Tweak Location chart tooltip positioning
    Added more calculations to get the coordinates of the mouse inside the map container instead of relative to the content container

--- src/components/analysis/by-location.jsx
+++ src/components/analysis/by-location.jsx
@@ -109,12 +109,13 @@ export class AnalysisByLocation extends React.Component {
   // }
   moveMouse(country, percent) {
     return e => {
+      const bounds = this.refs.chart.getBoundingClientRect()
       e.stopPropagation()
       this.setState({
         tooltipCountry: country,
         tooltipPercent: percent,
-        tooltipX: e.pageX,
-        tooltipY: e.pageY
+        tooltipX: e.pageX - bounds.left,
+        tooltipY: e.pageY - (bounds.top + window.pageYOffset)
       })
     }
   }
@@ -171,12 +172,13 @@ export class AnalysisByLocation extends React.Component {
     }
 
     return (
-      <div className='analysis-by-location'>
-        <div className="chart">
+      <div className="analysis-by-location">
+        <div className="chart" ref="chart">
           <svg
             width={this.props.width}
             height={this.props.height}
-            onMouseMove={this.moveMouse(null, null)}>
+            onMouseMove={this.moveMouse(null, null)}
+            onMouseLeave={this.moveMouse(null, null)}>
             {countries.map((country, i) => {
               // let hideCountry = false
               const id = country.id.toLowerCase()
