cbc64b55a80932ae3e75c7c80e5455d8975e3e81
Mika Paulasaari <mika.paulasaari@idean.com>
2016-04-12 13:58:59 -0700

    Add more test coverage

--- src/components/__tests__/content-item-list.js
+++ src/components/__tests__/content-item-list.js
@@ -2,21 +2,25 @@ import React from 'react'
 import Immutable from 'immutable'
 import TestUtils from 'react-addons-test-utils'
 
-jest.autoMockOff()
 jest.dontMock('../content-item-list.jsx')
 const ContentItemList = require('../content-item-list.jsx')
 
 describe('ContentItemList', () => {
   it('should exist', () => {
     let contentItem = TestUtils.renderIntoDocument(
-      <ContentItemList account={Immutable.Map()} linkTo={'/'} />
+      <ContentItemList
+        account={Immutable.Map()}
+        linkTo={'/'}
+        analyticsLink={'/'}/>
     );
     expect(TestUtils.isCompositeComponent(contentItem)).toBeTruthy();
   });
 
   it('does not show an config button when it is not supposed to', () => {
     let contentItem = TestUtils.renderIntoDocument(
-      <ContentItemList linkTo={'/'} />
+      <ContentItemList
+        linkTo={'/'}
+        analyticsLink={'/'}/>
     );
     let editBtn = TestUtils.scryRenderedDOMComponentsWithClass(contentItem, 'edit-content-item');
     expect(editBtn.length).toBe(0)
@@ -24,7 +28,10 @@ describe('ContentItemList', () => {
 
   it('shows an config button when it is supposed to', () => {
     let contentItem = TestUtils.renderIntoDocument(
-      <ContentItemList configurationLink={'foo'} linkTo={'/'} />
+      <ContentItemList
+        configurationLink={'foo'}
+        linkTo={'/'}
+        analyticsLink={'/'} />
     );
     let editBtn = TestUtils.scryRenderedDOMComponentsWithClass(contentItem, 'edit-content-item');
     expect(editBtn.length).toBe(1)
