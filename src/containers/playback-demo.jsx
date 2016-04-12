5e03cc82ca53d94d122481ae6d2384599c82d690
Jenny Steele <jenny.steele@idean.com>
2016-04-12 15:40:07 -0700

    Start tracking buffer events for video playback demo

--- src/containers/playback-demo.jsx
+++ src/containers/playback-demo.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Hls from 'hls.js'
+import Immutable from 'immutable'
 
 import PageContainer from '../components/layout/page-container'
 import Content from '../components/layout/content'
@@ -10,7 +11,10 @@ export class PlaybackDemo extends React.Component {
     super(props)
 
     this.state = {
-      activeVideo: '/elephant/169ar/elephant_master.m3u8'
+      activeVideo: '/elephant/169ar/elephant_master.m3u8',
+      events: Immutable.List(),
+      fragMaxKbps: 0,
+      ttfp: 0
     }
 
     this.handleVideoChange = this.handleVideoChange.bind(this)
@@ -29,7 +33,34 @@ export class PlaybackDemo extends React.Component {
       const hls = new Hls()
       hls.loadSource('http://video.demo.cdx-stag.unifieddeliverynetwork.net'+this.state.activeVideo)
       hls.attachMedia(this.refs.player)
-      hls.on(Hls.Events.MANIFEST_PARSED, () => this.refs.player.play())
+      this.refs.player.addEventListener('loadedmetadata', () => {
+        this.setState({ttfp: ((new Date()) - this.state.videoStartPlayTime)})
+      });
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        this.refs.player.play()
+        this.setState({videoStartPlayTime: new Date()})
+      });
+      this.refs.player.play()
+      hls.on(Hls.Events.FRAG_BUFFERED, (event, data) => {
+        const bitrate = Math.round(8 * data.stats.length / (data.stats.tbuffered - data.stats.tfirst));
+        event = Immutable.fromJS({
+          type: "fragment",
+          id: data.frag.level,
+          id2: data.frag.sn,
+          timestamp: new Date(),
+          latency: data.stats.tfirst - data.stats.trequest,
+          load: data.stats.tload - data.stats.tfirst,
+          parsing: data.stats.tparsed - data.stats.tload,
+          buffer: data.stats.tbuffered - data.stats.tparsed,
+          duration: data.stats.tbuffered - data.stats.tfirst,
+          bw: bitrate,
+          size: data.stats.length
+        });
+        this.setState({
+          events: this.state.events.push(event),
+          fragMaxKbps:Math.max(data.stats.fragMaxKbps, bitrate)
+        })
+      })
     }
   }
   handleVideoChange(newVideo) {
@@ -41,6 +72,12 @@ export class PlaybackDemo extends React.Component {
         <Content>
           <div className="container-fluid">
             <h1>Video Playback Demo</h1>
+            <p>
+              Client Buffer Events: {this.state.events.size}
+            </p>
+            <p>
+              Time to First Play: {this.state.ttfp}ms
+            </p>
             <Select
               onSelect={this.handleVideoChange}
               value={this.state.activeVideo}
@@ -51,7 +88,8 @@ export class PlaybackDemo extends React.Component {
                 ['/sintel/43ar/sintel_master.m3u8', '/sintel/43ar/sintel_master.m3u8'],
                 ['/bbb/169ar/bbb_master.m3u8', '/bbb/169ar/bbb_master.m3u8'],
                 ['/bbb/43ar/bbb_master.m3u8', '/bbb/43ar/bbb_master.m3u8']]}/>
-              <video ref="player" controls={true}></video>
+            <br/>
+            <video ref="player" controls={true}></video>
           </div>
         </Content>
       </PageContainer>
