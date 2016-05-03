82100d1acc42e393a92ead706a45776888ded6b2
Andrew Longstaff <andrew.longstaff@idean.com>
2016-05-03 15:05:58 +0300

    Fixing linting errors

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,41 +1,12 @@
-var path = require('path');
-//
+const config = require('../webpack/config')
+
+//Fitler out the .jsx loaders, or storybook breaks
+const loaders = config.module.loaders.filter((item) => {
+  return !item.test instanceof RegExp || (item.test.test && !item.test.test('.jsx'));
+});
+
 module.exports = {
   module: {
-    loaders: [
-      {
-        test: /\.scss$/,
-        loaders: [
-          'style',
-          'css?sourceMap',
-          "autoprefixer?{browsers:['last 2 version', 'IE 9']}",
-          'sass?sourceMap'
-        ]
-      },
-      {
-        test: /\.(jpg|jpeg|gif|png)$/,
-        exclude: /node_modules/,
-        loader: 'file?name=src/assets/img/[name].[ext]'
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|svg)$/,
-        exclude: /(node_modules|img|icons)/,
-        loader: 'file?name=src/assets/fonts/[name].[ext]'
-      },
-      {
-        test: /\.(svg|ico)$/,
-        exclude: /(node_modules|fonts|img)/,
-        loader: 'file?name=src/assets/icons/[name].[ext]'
-      },
-      {
-        test: /\.svg$/,
-        exclude: /(node_modules|fonts|icons)/,
-        loader: 'file?name=src/assets/img/[name].[ext]'
-      },
-      {
-        test: require.resolve("react"),
-        loader: "expose?React"
-      }
-    ]
+    loaders: loaders
   }
 };
