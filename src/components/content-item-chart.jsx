78779faa20c7de4d8050dc2fb588fd9ce8952559
santtu-idean <santtu.sulander@idean.com>
2016-05-12 09:29:29 +0300

    Fix bug where it shows 'Loading...' instead of amoebas

--- src/components/content-item-chart.jsx
+++ src/components/content-item-chart.jsx
@@ -24,7 +24,7 @@ class ContentItemChart extends React.Component {
     }
   }
   render() {
-    if (!this.props.primaryData.size) {
+    if (!this.props.primaryData) {
       return <div>Loading...</div>
     }
     const primaryData = this.props.primaryData.toJS().reduce((points, data, i) => {
