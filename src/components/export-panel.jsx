e238023fb96f0ad8f7e533f312afd5ae640b17a5
Wille Ristimäki <ville.ristimaki@idean.com>
2016-05-13 15:59:20 +0300

    Add things to export-panel

--- src/components/export-panel.jsx
+++ src/components/export-panel.jsx
@@ -1,19 +1,52 @@
 import React from 'react'
-import { Modal } from 'react-bootstrap'
+import {
+  Modal,
+  Tabs,
+  Tab,
+  Input,
+  ButtonToolbar,
+  Button
+} from 'react-bootstrap'
+
+const fileTypes = [
+  {label: 'PDF', value: 'pdf'},
+  {label: 'ZIP', value: 'zip'},
+  {label: 'RAR', value: 'rar'}
+]
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+}
 
 const ExportPanel = (props) => {
   return (
     <Modal show={true}
            onHide={props.onHide}
-           dialogClassName="configuration-sidebar">
+           dialogClassName="export-sidebar">
       <Modal.Header>
         <h1>Export</h1>
-        <p>
-          Traffic overview report
-        </p>
+        <p>Traffic overview report</p>
       </Modal.Header>
       <Modal.Body>
-        <p>Export body</p>
+        <Tabs defaultActiveKey={1} className="export-panel-tabs">
+          <Tab eventKey={1} title="Download file">
+            <h4>Select file type</h4>
+            <form onSubmit={handleSubmit}>
+              <div className="file-types">
+                {fileTypes.map((fileType) => (
+                  <Input key={fileType.value} type="radio" name="exportFile" label={fileType.label}
+                         className="export-input-list-item"/>
+                ))}
+              </div>
+              <ButtonToolbar>
+                <Button bsStyle="primary" className="btn-outline" onClick={props.onHide}>Cancel</Button>
+                <Button type="submit" bsStyle="primary">Download</Button>
+              </ButtonToolbar>
+            </form>
+          </Tab>
+          <Tab eventKey={2} title="Send email" disabled={true}>
+          </Tab>
+        </Tabs>
       </Modal.Body>
     </Modal>
   )
