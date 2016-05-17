9ece685d82d2b520254b783271f6d7b1b71f28d0
mikkoalander <mikko.alander@nordsoftware.com>
2016-05-17 15:14:23 +0300

    override publicPath in webpack conf to fix static assets & sourcemaps

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,26 +1,24 @@
-const path = require('path')
+const config = require('../webpack/config')
 
-module.exports = {
+//Fitler out the .jsx loaders, or storybook breaks
+/*const loaders = config.module.loaders.filter((item) => {
+  return !item.test instanceof RegExp || (item.test.test && !item.test.test('.jsx'));
+});*/
+
+/*module.exports = {
   module: {
-    loaders: [
-      {
-        test: /\.(jpg|jpeg|gif|png)$/,
-        exclude: /node_modules/,
-        loader: 'file?name=assets/img/[name].[ext]'
-      },
-      {
-        test   : /\.scss$/,
-        loaders: ["style", "css", "sass"],
-        include: path.resolve(__dirname, '../src')
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader"
-      }
-    ]
+    loaders: loaders
   }
+};*/
+
+module.exports = (storybookBaseConfig, configType) => {
+
+  storybookBaseConfig.module = {
+      loaders: config.module.loaders
+  };
+
+  storybookBaseConfig.output.publicPath = 'http://localhost:9001/static/';
+
+
+return storybookBaseConfig;
 };
