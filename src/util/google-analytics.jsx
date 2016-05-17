2a722a7ca8670013f3ab4993d282bb59496e5866
Wille Ristimäki <ville.ristimaki@idean.com>
2016-05-17 13:24:08 +0300

    Add google-analytics to util-folder

--- src/util/google-analytics.jsx
+++ src/util/google-analytics.jsx
@@ -1,2 +1,15 @@
-import React from 'react'
+import ga from 'react-ga'
 
+/**
+ * Initialize React-GA
+ */
+ga.initialize(GA_TRACKING_ID, {
+  debug: (process.env.NODE_ENV === 'development') ? true : false
+});
+
+/**
+ * Logs simple page view
+ */
+export const LogPageView = () => {
+  ga.pageview(window.location.pathname)
+}
