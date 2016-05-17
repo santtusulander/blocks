e3d63869b6e4175fd5ec21d0b169d9e90f7f0451
mikkoalander <mikko.alander@nordsoftware.com>
2016-05-17 13:19:15 +0300

    change storybook web pack loaders

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,26 @@
-const config = require('../webpack/config')
-
-//Fitler out the .jsx loaders, or storybook breaks
-const loaders = config.module.loaders.filter((item) => {
-  return !item.test instanceof RegExp || (item.test.test && !item.test.test('.jsx'));
-});
+const path = require('path')
 
 module.exports = {
   module: {
-    loaders: loaders
+    loaders: [
+      {
+        test: /\.(jpg|jpeg|gif|png)$/,
+        exclude: /node_modules/,
+        loader: 'file?name=assets/img/[name].[ext]'
+      },
+      {
+        test   : /\.scss$/,
+        loaders: ["style", "css", "sass"],
+        include: path.resolve(__dirname, '../src')
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      }
+    ]
   }
 };
