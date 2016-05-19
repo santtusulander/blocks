8e000c1a8960f3e4f7f60129a8428d8cc8b2f591
mikkoalander <mikko.alander@nordsoftware.com>
2016-05-19 18:18:08 +0300

    add test for reducers

---
+++ src/redux/modules/__tests__/exports.js
@@ -0,0 +1,23 @@
+const Immutable = require('immutable');
+
+jest.dontMock('../exports.js')
+
+const {
+  showDialog,
+  hideDialog,
+  downloadFile,
+  sendEmail,
+} = require('../exports.js');
+
+describe('Exports Module', () => {
+  it('should show dialog', () => {
+    const newState = showDialog(Immutable.Map(), {payload: {} });
+    expect(newState.get('dialogVisible')).toBe(true);
+  });
+
+  it('should hide dialog', () => {
+    const newState = hideDialog(Immutable.Map(), {payload: {} });
+    expect(newState.get('dialogVisible')).toBe(false);
+  });
+
+});
