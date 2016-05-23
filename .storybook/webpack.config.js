bc2144faa2727b37dd19b612316355f37f89de00
mikkoalander <mikko.alander@nordsoftware.com>
2016-05-23 12:00:32 +0300

    remove commented config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,16 +1,5 @@
 const config = require('../webpack/config')
 
-//Fitler out the .jsx loaders, or storybook breaks
-/*const loaders = config.module.loaders.filter((item) => {
-  return !item.test instanceof RegExp || (item.test.test && !item.test.test('.jsx'));
-});*/
-
-/*module.exports = {
-  module: {
-    loaders: loaders
-  }
-};*/
-
 module.exports = (storybookBaseConfig, configType) => {
 
   storybookBaseConfig.module = {
