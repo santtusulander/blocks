8c3dae3810c2ed1cab32b1f3ee1551385512c2b4
mikkoalander <mikko.alander@nordsoftware.com>
2016-05-26 22:56:00 +0300

    change header font size to h2

--- src/components/account-management/account-management-header.jsx
+++ src/components/account-management/account-management-header.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { ButtonWrapper } from '../button.js'
 import IconAdd from '../icons/icon-add.jsx'
 
+import './account-management-header.scss'
+
 const AccountManagementHeader = (props) => {
   return (
-    <h3 className='accountManagementHeader'>
+    <h2 className='accountManagementHeader'>
       <span>{props.title}</span>
       <ButtonWrapper bsStyle='btn-primary' icon='true' addNew='true' onClick={props.onAdd}>
         <IconAdd />
       </ButtonWrapper>
-    </h3>
+    </h2>
   )
 }
 
