ac312ea5a4b97d42d3909cccad028dbcdce2cf85
mikkoalander <mikko.alander@nordsoftware.com>
2016-05-26 23:12:30 +0300

    Revert "add brand list to brands page"
    This reverts commit 614e95b1289569d20057e1fa8d88780dac107d5e.

--- src/components/account-management/system/brands.jsx
+++ src/components/account-management/system/brands.jsx
@@ -1,23 +1,10 @@
 import React from 'react'
-import {
-  BrandList,
-  AVAILABILITY_PRIVATE,
-  AVAILABILITY_SHARED,
-  }  from '../brand-list.jsx'
-
-const fakeBrands = [
-  {id: 1, logo: null, brand: 'Lorem Ipsum', availability: AVAILABILITY_PRIVATE, lastEdited: new Date().toString(), usedBy: 'Account Name #1'},
-  {id: 2, logo: null, brand: 'Lorem Ipsum', availability: AVAILABILITY_PRIVATE, lastEdited: new Date().toString(), usedBy: [{accountName: 'Account Name #2'},{accountName: 'Account Name #3'}] },
-  {id: 3, logo: null, brand: 'Lorem Ipsum', availability: AVAILABILITY_PRIVATE, lastEdited: new Date().toString(), usedBy: 'Account Name #1'},
-  {id: 4, logo: null, brand: 'Lorem Ipsum', availability: AVAILABILITY_PRIVATE, lastEdited: new Date().toString(), usedBy: 'Account Name #1'}
-]
-
 
 class AccountManagementSystemBrands extends React.Component {
   render() {
     return (
       <div className="account-management-system-brands">
-        <BrandList brands={fakeBrands} />
+        <h2>Brands</h2>
       </div>
     )
   }
---
+++ src/components/account-management/system/roles.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class AccountManagementSystemRoles extends React.Component {
+  render() {
+    return (
+      <div className="account-management-system-roles">
+        <h2>Roles</h2>
+      </div>
+    )
+  }
+}
+
+AccountManagementSystemRoles.displayName = 'AccountManagementSystemRoles'
+AccountManagementSystemRoles.propTypes = {}
+
+module.exports = AccountManagementSystemRoles
