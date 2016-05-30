199e561756e89f38be5cbf1e399dff35630ff433
mikkoalander <mikko.alander@nordsoftware.com>
2016-05-30 12:49:18 +0300

    add redux form to dns edit form

--- src/components/__stories__/account-management/dns-edit-form.js
+++ src/components/__stories__/account-management/dns-edit-form.js
@@ -2,25 +2,38 @@ import React from 'react';
 import Immutable from 'immutable';
 import { storiesOf, action } from '@kadira/storybook';
 
-//const ThemeWrap = require('../theme-wrap.jsx');
+import { createStore, combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import { Provider } from 'react-redux'
 
 import ThemeWrap from '../theme-wrap.jsx'
 import DnsEditForm from '../../account-management/dns-edit-form.jsx'
 
+const reducers = {
+  form: formReducer
+}
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+
+
 storiesOf('AccountManagement', module)
   .addDecorator((story) => (
     <ThemeWrap >
-      <div style={ {width: '70%', padding: '3%', border: '1px solid #ccc'} }>
-        {story()}
-      </div>
+      <Provider store={store}>
+        <div style={ {width: '70%', padding: '3%', border: '1px solid #ccc'} }>
+          {story()}
+        </div>
+      </Provider>
     </ThemeWrap>
   ))
   .add('DnsEditForm', () => (
     <DnsEditForm
       show={true}
       edit={false}
+      domain='foobar.com'
       onSave={ action('onSave') }
       onCancel={ action('onCancel')}
-      changeValue={ action('changeValue') }
+
     />
   ))
--- src/components/account-management/dns-edit-form.jsx
+++ src/components/account-management/dns-edit-form.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import { Modal, Input, ButtonToolbar, Button } from 'react-bootstrap'
 
+import Select from '../select.jsx'
+import {reduxForm} from 'redux-form'
+
+import './dns-edit-form.scss'
+
+let errors = {}
+
+const validate = values => {
+  errors = {}
+
+  const { recordName, targetValue, ttl } = values
+
+  if (!ttl || ttl.length === 0) errors.ttl = 'TTL is required'
+  if (!recordName || recordName.length === 0 ) errors.recordName = 'RecordName is required'
+  if (!targetValue ||targetValue.length === 0) errors.targetValue = 'TargetValue is required'
+
+  return errors;
+}
+
 const DnsEditForm = (props) => {
 
   const title = props.edit ? 'Edit DNS Record' : 'New DNS Record'
   const actionButtonTitle = props.edit ? 'Save' : 'Add'
+
+  const { fields: { recordName, targetValue, ttl} } = props
 
   return (
     <Modal
@@ -19,25 +40,43 @@ const DnsEditForm = (props) => {
       </Modal.Header>
 
       <Modal.Body>
+        <form>
+          <Input
+            { ...recordName }
+            type="text"
+            label="Record Name"
+            placeholder="Enter Record Name"
+            addonAfter={ `.${props.domain}` }
+            className='input-narrow'
+          />
 
-        <Select
-          value={recordType}
-          className='dns-dropdowns'
-          onSelect={type => changeRecordType(type)}
-          options={recordTypes.map(type => [type, type])}
-        />
+          {recordName.touched && recordName.error && <div>{recordName.error}</div>}
 
-        <Input type='text' label='Record name' onChange={ props.changeValue } /> <span>{ props.domain }</span>
+          <Input
+            { ...targetValue }
+          type="text"
+          label="Target Value"
+          placeholder="Enter Target Value"
+          />
 
-        <Input type='text' label='Target Value' onChange={ props.changeValue } />
+          {targetValue.touched && targetValue.error && <div>{targetValue.error}</div>}
 
-        <Input type='text' label='TTL Value' /> <span>seconds</span>
+          <Input
+            { ...ttl }
+          type="text"
+          label="TTL Value"
+          placeholder="Enter TTL Value"
+          className='input-narrow'
+          addonAfter='seconds'
+          />
 
-        <ButtonToolbar className="text-right extra-margin-top">
-          <Button bsStyle="primary" className="btn-outline" onClick={props.onCancel}>Cancel</Button>
-          <Button bsStyle="primary" onClick={props.onSave}>{ actionButtonTitle }</Button>
-        </ButtonToolbar>
+          {ttl.touched && ttl.error && <div>{ttl.error}</div>}
 
+          <ButtonToolbar className="text-right extra-margin-top">
+            <Button bsStyle="primary" className="btn-outline" onClick={props.onCancel}>Cancel</Button>
+            <Button disabled={ Object.keys(errors).length } bsStyle="primary" onClick={props.onSave} >{ actionButtonTitle }</Button>
+          </ButtonToolbar>
+        </form>
       </Modal.Body>
 
     </Modal>
@@ -45,12 +84,15 @@ const DnsEditForm = (props) => {
 }
 
 DnsEditForm.propTypes = {
-  changevalue: React.PropTypes.func,
-  onCancel: React.PropTypes.func,
+  edit: React.PropTypes.bool,
+  fields: React.PropTypes.object,
   onCancel: React.PropTypes.func,
   onSave: React.PropTypes.func,
-  edit: React.PropTypes.bool,
   show: React.PropTypes.bool
 }
 
-export default DnsEditForm
+export default reduxForm({
+  fields: ['recordName', 'targetValue', 'ttl'],
+  form: 'dns-edit',
+  validate
+})(DnsEditForm)
