3ba7d5609879aab3b68f4e7936acf5484dc09578
Wille Ristimäki <ville.ristimaki@idean.com>
2016-06-06 13:33:52 +0300

    Add constant for analytics report tab-titles for sidebar

---
+++ src/constants/report-tab-titles.js
@@ -0,0 +1,12 @@
+export default {
+  'traffic': 'traffic overview',
+  'visitors': 'visitors overview',
+  'sp-report': 'service provider report',
+  'file-error': 'file error',
+  'url-report': 'URL report',
+  'playback-demo': 'playback demo'
+}
+
+const getTitle = () => {
+  
+}
