9e769952535085ab40491f52b52deba5ccad6d5e
mikkoalander <mikko.alander@nordsoftware.com>
2016-06-10 23:41:19 +0300

    WIP: empty tests

---
+++ src/redux/modules/__tests__/traffic.js
@@ -0,0 +1,75 @@
+jest.unmock('../traffic.js')
+
+import Immutable from 'immutable'
+import {
+  trafficByTimeSuccess,
+  trafficByTimeFailure,
+  trafficByCountrySuccess,
+  trafficByCountryFailure,
+  trafficTotalEgressSuccess,
+  trafficTotalEgressFailure,
+  trafficOnOffNetSuccess,
+  trafficOnOffNetFailure,
+  trafficOnOffNetTodaySuccess,
+  trafficOnOffNetTodayFailure,
+  trafficServiceProvidersSuccess,
+  trafficServiceProvidersFailure,
+  trafficStorageSuccess,
+  trafficStorageFailure,
+  trafficStartFetch,
+  trafficFinishFetch
+  } from '../traffic.js'
+
+describe('Traffic Module', () => {
+
+    it('should handle trafficByTimeSuccess', () => {
+
+    })
+
+    it('should handle trafficByTimeFailure', () => {
+
+    })
+
+    it('should handle trafficByCountrySuccess', () => {
+
+    })
+    it('should handle trafficByCountryFailure', () => {
+
+    })
+    it('should handle trafficTotalEgressSuccess', () => {
+
+    })
+    it('should handle trafficTotalEgressFailure', () => {
+
+    })
+    it('should handle trafficOnOffNetSuccess', () => {
+
+    })
+    it('should handle trafficOnOffNetFailure', () => {
+
+    })
+    it('should handle trafficOnOffNetTodaySuccess', () => {
+
+    })
+    it('should handle trafficOnOffNetTodayFailure', () => {
+
+    })
+    it('should handle trafficServiceProvidersSuccess', () => {
+
+    })
+    it('should handle trafficServiceProvidersFailure', () => {
+
+    })
+    it('should handle trafficStorageSuccess', () => {
+
+    })
+    it('should handle trafficStorageFailure', () => {
+
+    })
+    it('should handle trafficStartFetch', () => {
+
+    })
+    it('should handle trafficFinishFetch', () => {
+
+    })
+})
