64e1702ae5543b96fbc60608490f9212a1cdfc3a
mikkoalander <mikko.alander@nordsoftware.com>
2016-06-13 12:52:26 +0300

    fix stop-color warning

--- src/components/loading-spinner/loading-spinner.jsx
+++ src/components/loading-spinner/loading-spinner.jsx
@@ -5,10 +5,10 @@ import './loading-spinner.scss'
 const circleSvg =
   <svg id='circleSvg' viewBox="0 0 250 250">
       <linearGradient id="linear-gradient" x1="6828.3" y1="-673.22" x2="7078.32" y2="-673.22" gradientTransform="matrix(1, 0, 0, -1, -6828.34, -548.22)" gradientUnits="userSpaceOnUse">
-        <stop offset="0" stop-color="#89ba17"/>
-        <stop offset="0.33" stop-color="#00a9d4"/>
-        <stop offset="0.66" stop-color="#2e4275"/>
-        <stop offset="1" stop-color="#7b0663"/>
+        <stop offset="0" stopColor="#89ba17"/>
+        <stop offset="0.33" stopColor="#00a9d4"/>
+        <stop offset="0.66" stopColor="#2e4275"/>
+        <stop offset="1" stopColor="#7b0663"/>
       </linearGradient>
 
     <path id="circle" d="M125,250A125,125,0,1,1,209,32.47a5.87,5.87,0,0,1-7.53,9l-0.36-.32a113.12,113.12,0,1,0,17.77,20.52,5.87,5.87,0,0,1,9.48-6.92l0.24,0.35A125,125,0,0,1,125,250Z" />
