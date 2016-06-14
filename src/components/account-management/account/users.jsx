ec34f9892ac19efcff3196b855871f9d193a1011
Craig Lubin <lubin.craig@gmail.com>
2016-06-14 15:34:37 -0700

    Adding users table view to add users page

--- src/components/account-management/account/users.jsx
+++ src/components/account-management/account/users.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Immutable from 'immutable'
-import {Button, Row, Col} from 'react-bootstrap'
+import {Table, Button, Row, Col} from 'react-bootstrap'
 
 import IconAdd from '../../icons/icon-add.jsx'
+import IconTrash from '../../icons/icon-trash.jsx'
+import TableSorter from '../../table-sorter'
 
 class AccountManagementAccountUsers extends React.Component {
   render() {
+    const sorterProps = {
+      activateSort: this.changeSort,
+      activeColumn: this.state.sortBy,
+      activeDirection: this.state.sortDir
+    }
+    const sortedUsers = this.sortedData(
+      this.props.users,
+      this.state.sortBy,
+      this.state.sortDir
+    )
     return (
       <div className="account-management-account-users">
         <Row className="header-btn-row">
@@ -21,6 +33,44 @@ class AccountManagementAccountUsers extends React.Component {
             </Button>
           </Col>
         </Row>
+        <Table striped={true}>
+          <thead>
+            <tr>
+              <TableSorter {...sorterProps} column="name">
+                Name
+              </TableSorter>
+              <th>Created On</th>
+              <th>Properties</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {sortedUsers.map((user, i) => {
+              return (
+                <tr key={i}>
+                  <td>
+                    {user.get('name')}
+                  </td>
+                  <td>
+                    NEEDS_API
+                  </td>
+                  <td>
+                    NEEDS_API
+                  </td>
+                  <td>
+                    <a href="#" onClick={this.editUser(user.get('id'))}>
+                      EDIT
+                    </a>
+                    <Button onClick={this.deleteUser(user.get('id'))}
+                      className="btn-link btn-icon">
+                      <IconTrash/>
+                    </Button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
       </div>
     )
   }
