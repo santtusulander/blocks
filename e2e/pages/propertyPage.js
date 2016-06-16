23da02d7ddcd62b0f588b016e4063645c30c75ec
Niclas Liimatainen <niclas.liimatainen@nordsoftware.com>
2016-06-16 14:28:46 +0300

    Create e2e tests for Group Summary page

---
+++ e2e/pages/propertyPage.js
@@ -0,0 +1,11 @@
+module.exports = {
+  url: 'http://localhost:3000/content/property/udn/2/5/property?name=www%2erenandstimpy%2ecom',
+  elements: {
+    pageHeader: {
+      selector: '.page-header-layout p'
+    },
+    selectedProperty: {
+      selector: '.page-header-layout .dropdown > button > h1'
+    }
+  }
+};
