91b373e6b360996b189e13a25dfec63c111ba993
Wille Ristimäki <ville.ristimaki@idean.com>
2016-06-28 13:01:03 +0300

    WIP for analytics-header

--- src/components/analytics/analytics-view-control.jsx
+++ src/components/analytics/analytics-view-control.jsx
@@ -1,73 +1,75 @@
 import React, { PropTypes } from 'react'
 import Immutable from 'immutable'
-import {Link} from 'react-router'
+import { Link } from 'react-router'
 import { Nav, ButtonToolbar, Button } from 'react-bootstrap'
 
-import Select from '../select.jsx'
+import HeadingDropdown from '../heading-dropdown/heading-dropdown.jsx'
+import Select from '../select'
 
 import { getTabLink, getAnalyticsUrl } from '../../util/helpers.js'
 
 import './analytics-view-control.scss'
 
-function createOptions( opts ){
-  return opts.map( opt => {
-    return [ opt.get('id').toString(), opt.get('name')]
+function createOptions(opts) {
+  return opts.map(opt => {
+    return [opt.get('id').toString(), opt.get('name')]
   })
 }
 
-function createPropertyOptions( opts ){
-  return opts.map( opt => {
-    return [ opt, opt]
+function createPropertyOptions(opts) {
+  return opts.map(opt => {
+    return [opt, opt]
   })
 }
 
 /* Not USED atm - will be used when filter dropdown is implemented
-function createDropdownOptions ( opts ) {
-  return opts.map(opt => {
-    return {
-      link: opt.get('id').toString(),
-      label: opt.get('name')
-    }
-  })
-}
+ function createDropdownOptions ( opts ) {
+ return opts.map(opt => {
+ return {
+ link: opt.get('id').toString(),
+ label: opt.get('name')
+ }
+ })
+ }
 
-function createPropertyDropdownOptions ( opts ){
-  return opts.map( opt => {
-    return {
-      link: opt,
-      label: opt
-    }
-  })
-}
-*/
+ function createPropertyDropdownOptions ( opts ){
+ return opts.map( opt => {
+ return {
+ link: opt,
+ label: opt
+ }
+ })
+ }
+ */
 const AnalyticsViewControl = (props) => {
   /*
-  const brandOptions = createOptions( props.brands )
+   const brandOptions = createOptions( props.brands )
    */
-  const accountOptions = createOptions( props.accounts )
-  const groupOptions = createOptions( props.groups )
-  const propertyOptions = createPropertyOptions( props.properties )
+  const accountOptions  = createOptions(props.accounts)
+  const groupOptions    = createOptions(props.groups)
+  const propertyOptions = createPropertyOptions(props.properties)
 
   /*const groupDropdownOptions = createDropdownOptions( props.groups )
-  const propertyDropdownOptions = createPropertyDropdownOptions( props.properties )
-  */
+   const propertyDropdownOptions = createPropertyDropdownOptions( props.properties )
+   */
 
   return (
     <div className='analytics-view-control'>
 
-    { /* If account is not selected (Needs to be: UDN ADMIN) */
-      !props.params.account &&
-      <Select
-        options={accountOptions}
-        onSelect={val => {
-          props.history.pushState(null, getAnalyticsUrl('account', val, props.params))
-        }}
-        value={props.params.group}
-      />
+      { /* If account is not selected (Needs to be: UDN ADMIN) */
+        !props.params.account &&
+        <HeadingDropdown
+          className="heading-dropdown"
+          options={accountOptions}
+          onSelect={val => {
+            props.history.pushState(null, getAnalyticsUrl('account', val, props.params))
+          }}
+          value={props.params.group}
+        />
       }
 
-    {groupOptions.count() > 0 &&
-      <Select
+      {groupOptions.count() > 0 &&
+      <HeadingDropdown
         options={groupOptions}
         onSelect={val => {
           props.history.pushState(null, getAnalyticsUrl('group', val, props.params))
@@ -75,14 +77,15 @@ const AnalyticsViewControl = (props) => {
         value={props.params.group}
       />
       }
+
       {propertyOptions.count() > 0 &&
-        <Select
-          options={propertyOptions}
-          onSelect={val => {
-            props.history.pushState(null, getAnalyticsUrl('property', val, props.params))
-          }}
-          value={props.location.query.property}
-        />
+      <HeadingDropdown
+        options={propertyOptions}
+        onSelect={val => {
+          props.history.pushState(null, getAnalyticsUrl('property', val, props.params))
+        }}
+        value={props.location.query.property}
+      />
       }
 
       <ButtonToolbar className="pull-right">
@@ -90,50 +93,50 @@ const AnalyticsViewControl = (props) => {
       </ButtonToolbar>
 
       { /* TODO: Implement filtered dropdown, when possible (component fixed)
-      <FilterDropdown
-        options={ groupDropdownOptions }
-        handleSelect={ (val) => {
-          props.history.pushState(null, getAnalyticsUrl('group', val, props.params) )
-        } }
-      />
+       <FilterDropdown
+       options={ groupDropdownOptions }
+       handleSelect={ (val) => {
+       props.history.pushState(null, getAnalyticsUrl('group', val, props.params) )
+       } }
+       />
 
-      <FilterDropdown
-        options={ propertyDropdownOptions }
-        handleSelect={ (val) => {
-          props.history.pushState(null, getAnalyticsUrl('property', val, props.params) )
-        } }
-      />
-      */ }
+       <FilterDropdown
+       options={ propertyDropdownOptions }
+       handleSelect={ (val) => {
+       props.history.pushState(null, getAnalyticsUrl('property', val, props.params) )
+       } }
+       />
+       */ }
 
 
-      <Nav bsStyle="tabs" >
+      <Nav bsStyle="tabs">
         <li>
           <Link to={getTabLink(props.location, 'traffic')}
-            activeClassName='active'>Traffic</Link>
+                activeClassName='active'>Traffic</Link>
         </li>
         <li>
           <Link to={getTabLink(props.location, 'visitors')}
-            activeClassName='active'>Visitors</Link>
+                activeClassName='active'>Visitors</Link>
         </li>
         <li>
           <Link to={getTabLink(props.location, 'on-off-net')}
-            activeClassName='active'>On/Off net</Link>
+                activeClassName='active'>On/Off net</Link>
         </li>
         <li>
           <Link to={getTabLink(props.location, 'service-providers')}
-            activeClassName='active'>Service Providers</Link>
+                activeClassName='active'>Service Providers</Link>
         </li>
         {props.location.query.property && <li>
           <Link to={getTabLink(props.location, 'file-error')}
-            activeClassName='active'>File Error</Link>
+                activeClassName='active'>File Error</Link>
         </li>}
         {props.location.query.property && <li>
           <Link to={getTabLink(props.location, 'url-report')}
-            activeClassName='active'>Url Report</Link>
+                activeClassName='active'>Url Report</Link>
         </li>}
         <li>
           <Link to={getTabLink(props.location, 'playback-demo')}
-            activeClassName='active'>Playback demo</Link>
+                activeClassName='active'>Playback demo</Link>
         </li>
       </Nav>
     </div>
