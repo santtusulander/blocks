e335d123d0b45cf2a3a1cdf81df5f8c3653582c4
Jenny Steele <jenny.steele@idean.com>
2016-06-30 16:12:45 -0700

    Make config header option save to api

--- src/components/configuration/actions/header.jsx
+++ src/components/configuration/actions/header.jsx
@@ -9,20 +9,21 @@ class Header extends React.Component {
   constructor(props) {
     super(props);
 
+    const value = props.set.get('value')
+
     this.state = {
-      activeActivity: 'add',
-      activeDirection: 'to_origin'
+      activeActivity: props.set.get('action') || 'set',
+      activeDirection: 'to_origin',
+      from_header: '',
+      from_value: '',
+      to_header: props.set.get('header'),
+      to_value: value && value.size ? value.get(0).get('field_detail') : ''
     }
 
-    this.handleChange = this.handleChange.bind(this)
     this.handleSelectChange = this.handleSelectChange.bind(this)
+    this.saveChanges = this.saveChanges.bind(this)
   }
-  handleChange(path) {
-    return e => {
-      this.props.changeValue(path, e.target.value)
-    }
-  }
-  handleSelectChange(key, path) {
+  handleSelectChange(key) {
     return key, value => {
       if (key === 'activeActivity') {
         this.setState({
@@ -33,8 +34,35 @@ class Header extends React.Component {
           activeDirection: value
         })
       }
-      this.props.changeValue(path, value)
     }
+  }
+  saveChanges() {
+    let newSet
+    if(this.state.activeActivity === 'unset') {
+      newSet = this.props.set.merge({
+        action: "unset",
+        header: this.state.to_header
+      })
+      // If there was perviously a value, be sure to delete it
+      if(newSet.has('value')) {
+        newSet = newSet.delete('value')
+      }
+    }
+    else if(this.state.activeActivity === 'set') {
+      newSet = this.props.set.merge({
+        action: "set",
+        header: this.state.to_header,
+        value: Immutable.fromJS([{
+          field: "text",
+          field_detail: this.state.to_value
+        }])
+      })
+    }
+    this.props.changeValue(
+      this.props.path,
+      newSet
+    )
+    this.props.close()
   }
   render() {
     return (
@@ -47,37 +75,37 @@ class Header extends React.Component {
           <div className="form-groups">
 
             <InputConnector show={true}
-              hasTwoEnds={this.state.activeActivity !== 'remove'}/>
+              hasTwoEnds={this.state.activeActivity !== 'unset'}/>
 
             <div className="form-group">
               <label className="control-label">Activity</label>
               <Select className="input-select"
-                onSelect={this.handleSelectChange('activeActivity',
-                  ['edge_configuration', 'cache_rule', 'actions', 'header']
-                )}
+                onSelect={this.handleSelectChange('activeActivity')}
                 value={this.state.activeActivity}
                 options={[
-                  ['add', 'Add'],
-                  ['modify', 'Modify'],
-                  ['remove', 'Remove']]}/>
+                  ['set', 'Add'],
+                  /*['modify', 'Modify'],*/
+                  ['unset', 'Remove']]}/>
             </div>
 
             <Panel className="form-panel" collapsible={true}
               expanded={this.state.activeActivity !== 'modify'}>
               <Input type="text" label="Name"
                 placeholder="Enter Header Name"
-                onChange={this.handleChange(
-                  ['edge_configuration', 'cache_rule', 'actions', 'header_name']
-                )}/>
+                value={this.state.to_header}
+                onChange={(e) => {this.setState({
+                  to_header: e.target.value
+                })}}/>
             </Panel>
 
             <Panel className="form-panel" collapsible={true}
-              expanded={this.state.activeActivity === 'add'}>
+              expanded={this.state.activeActivity === 'set'}>
               <Input type="text" label="Value"
                 placeholder="Enter Header Value"
-                onChange={this.handleChange(
-                  ['edge_configuration', 'cache_rule', 'actions', 'header_value']
-                )}/>
+                value={this.state.to_value}
+                onChange={(e) => {this.setState({
+                  to_value: e.target.value
+                })}}/>
             </Panel>
 
             <Panel className="form-panel" collapsible={true}
@@ -86,30 +114,34 @@ class Header extends React.Component {
                 <Col xs={6}>
                   <Input type="text" label="FROM Name"
                     placeholder="Enter Header Name"
-                    onChange={this.handleChange(
-                      ['edge_configuration', 'cache_rule', 'actions', 'header_name_from']
-                    )}/>
+                    value={this.state.from_header}
+                    onChange={(e) => {this.setState({
+                      from_header: e.target.value
+                    })}}/>
                 </Col>
                 <Col xs={6}>
                   <Input type="text" label="TO Name"
                     placeholder="Enter Header Name"
-                    onChange={this.handleChange(
-                      ['edge_configuration', 'cache_rule', 'actions', 'header_name_to']
-                    )}/>
+                    value={this.state.to_header}
+                    onChange={(e) => {this.setState({
+                      to_header: e.target.value
+                    })}}/>
                 </Col>
                 <Col xs={6}>
                   <Input type="text" label="FROM Value"
                     placeholder="Enter Header Value"
-                    onChange={this.handleChange(
-                      ['edge_configuration', 'cache_rule', 'actions', 'header_value_from']
-                    )}/>
+                    value={this.state.from_value}
+                    onChange={(e) => {this.setState({
+                      from_value: e.target.value
+                    })}}/>
                 </Col>
                 <Col xs={6}>
                   <Input type="text" label="TO Value"
                     placeholder="Enter Header Value"
-                    onChange={this.handleChange(
-                      ['edge_configuration', 'cache_rule', 'actions', 'header_value_to']
-                    )}/>
+                    value={this.state.to_value}
+                    onChange={(e) => {this.setState({
+                      to_value: e.target.value
+                    })}}/>
                 </Col>
               </Row>
             </Panel>
@@ -121,9 +153,7 @@ class Header extends React.Component {
           <div className="form-group">
             <label className="control-label">Direction</label>
             <Select className="input-select"
-              onSelect={this.handleSelectChange('activeDirection',
-                ['edge_configuration', 'cache_rule', 'actions', 'header_direction']
-              )}
+              onSelect={this.handleSelectChange('activeDirection')}
               value={this.state.activeDirection}
               options={[
                 ['to_origin', 'To Origin'],
@@ -135,8 +165,8 @@ class Header extends React.Component {
             <Button bsStyle="default" onClick={this.props.close}>
               Cancel
             </Button>
-            <Button bsStyle="primary" onClick={this.props.close}>
-              Save Match
+            <Button bsStyle="primary" onClick={this.saveChanges}>
+              Save Action
             </Button>
           </ButtonToolbar>
 
