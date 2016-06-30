ed77f74dd2589f19b638407b1fff0fd37d9f5d55
Mikko Alander <mikko.alander@idean.com>
2016-06-30 21:17:05 +0300

    fix to only send values

--- src/components/filter-checklist-dropdown/filter-checklist-dropdown.jsx
+++ src/components/filter-checklist-dropdown/filter-checklist-dropdown.jsx
@@ -27,11 +27,17 @@ export class FilterChecklistDropdown extends React.Component {
   }
 
   handleCheck(option) {
+    debugger
     let checked = this.state.checkedResults
+
     let newlist = checked.indexOf(option) === -1 ? checked.push(option) : checked.delete(checked.indexOf(option))
 
     this.setState({ checkedResults: newlist })
-    this.props.handleCheck(this.state.checkedResults)
+    const vals = newlist.map( option => {
+      return option.get('value')
+    })
+
+    this.props.handleCheck(vals)
   }
 
   handleFilter() {
