fd718f05ca9865495980291c76c3430dfff47332
Jenny Steele <jenny.steele@idean.com>
2016-07-05 15:38:08 -0700

    Revert "Show day slices only on the property starbursts"
    This reverts commit bf046f97c489420fb9e5bdff41238cd560220aea.

--- src/components/content/content-item-chart.jsx
+++ src/components/content/content-item-chart.jsx
@@ -69,12 +69,11 @@ class ContentItemChart extends React.Component {
     const primaryData = groupData(this.props.primaryData.toJS(), rayHours, 'bits_per_second');
     const secondaryData = groupData(this.props.secondaryData.toJS(), rayHours, 'bits_per_second');
     const differenceData = groupData(this.props.differenceData.toJS(), dayHours);
-    const daySlices = this.props.dailyTraffic ?
-      this.props.dailyTraffic.toJS().reduce(slices => {
-        slices.push((dayHours/rayHours)*2)
-        slices.push(1)
-        return slices
-      }, []) : []
+    const daySlices = this.props.dailyTraffic.toJS().reduce(slices => {
+      slices.push((dayHours/rayHours)*2)
+      slices.push(1)
+      return slices
+    }, [])
 
     const primaryMax = d3.max(primaryData, d => {
       return d || 0
@@ -229,7 +228,7 @@ class ContentItemChart extends React.Component {
                 <g className="hover-info">
                   {pie(daySlices).reduce((slices, arc, i) => {
                     if(!(i % 2)) {
-                      const data = this.props.dailyTraffic && this.props.dailyTraffic.get(Math.floor(i / 2))
+                      const data = this.props.dailyTraffic.get(Math.floor(i / 2))
                       if(data && data.get('transfer_rates') && data.get('transfer_rates').get('total')) {
                         slices.push(
                           <path key={i} className="day-arc" d={dayArc(arc)}
