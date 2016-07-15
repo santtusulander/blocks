9485ee647c6cabde550024cd221b72c0680ade19
Mika Paulasaari <mika.paulasaari@idean.com>
2016-07-15 12:03:04 -0700

    Add fakeHistory and fakeParams to Header

--- src/components/__tests__/header.js
+++ src/components/__tests__/header.js
@@ -21,23 +21,34 @@ jest.autoMockOff() // Uses react-bootstrap extensively, so don't auto mock
 jest.dontMock('../header.jsx')
 const Header = require('../header.jsx')
 
+function fakeHistoryMaker() {
+  return {
+    createHref: jest.genMockFunction(),
+    isActive: jest.genMockFunction()
+  }
+}
+
 const handleThemeChange = jest.genMockFunction()
 const logOut = jest.genMockFunction()
 const activatePurge = jest.genMockFunction()
 
 const fakeLocation = {query: {name: 'www.abc.com'}}
 
+const fakeParams = {brand: 'aaa', account: 'bbb', group: 'ccc', property: 'ddd'}
+
 describe('Header', () => {
   it('should exist', () => {
     let header = TestUtils.renderIntoDocument(
-      <Header theme="dark" location={fakeLocation} />
+      <Header theme="dark" location={fakeLocation}
+        params={fakeParams} history={fakeHistoryMaker()} />
     );
     expect(TestUtils.isCompositeComponent(header)).toBeTruthy();
   });
 
   it('can be passed a custom css class', () => {
     let header = TestUtils.renderIntoDocument(
-      <Header className="foo" theme="dark" location={fakeLocation} />
+      <Header className="foo"  theme="dark" location={fakeLocation}
+        params={fakeParams} history={fakeHistoryMaker()} />
     );
     let container = TestUtils.findRenderedDOMComponentWithTag(header, 'nav');
     expect(ReactDOM.findDOMNode(container).className).toContain('foo');
@@ -49,17 +60,21 @@ describe('Header', () => {
     // componentWillReceiveProps event, which is what we are testing here
     let node = document.createElement('div');
     let header = ReactDOM.render(
-      <Header theme="dark" fetching={false} location={fakeLocation} />
+      <Header className="foo"  theme="dark" fetching={false} location={fakeLocation}
+        params={fakeParams} history={fakeHistoryMaker()} />
     , node)
+    expect(header.state.animatingGradient).toBe(false);
     ReactDOM.render(
-      <Header theme="dark" fetching={true} location={fakeLocation} />
+      <Header className="foo"  theme="dark" fetching={true} location={fakeLocation}
+        params={fakeParams} history={fakeHistoryMaker()} />
     , node)
     expect(header.state.animatingGradient).toBe(true);
   });
 
   it('should show gradient animation when fetching', () => {
     let header = TestUtils.renderIntoDocument(
-      <Header theme="dark" fetching={true} location={fakeLocation} />
+      <Header theme="dark" fetching={true} location={fakeLocation}
+        params={fakeParams} history={fakeHistoryMaker()} />
     );
     let gradient = TestUtils.findRenderedDOMComponentWithClass(header, 'header-gradient');
     header.resetGradientAnimation()
@@ -68,7 +83,8 @@ describe('Header', () => {
 
   it('should not show gradient animation when not fetching', () => {
     let header = TestUtils.renderIntoDocument(
-      <Header theme="dark" fetching={false} location={fakeLocation} />
+      <Header theme="dark" fetching={false} location={fakeLocation}
+        params={fakeParams} history={fakeHistoryMaker()} />
     );
     let gradient = TestUtils.findRenderedDOMComponentWithClass(header, 'header-gradient');
     header.resetGradientAnimation()
@@ -77,7 +93,8 @@ describe('Header', () => {
 
   it('should call theme handling function when link is clicked', () => {
     let header = TestUtils.renderIntoDocument(
-      <Header theme="dark" handleThemeChange={handleThemeChange} location={fakeLocation} />
+      <Header theme="dark" handleThemeChange={handleThemeChange} location={fakeLocation}
+        params={fakeParams} history={fakeHistoryMaker()} />
     );
     let themeMenu = TestUtils.findRenderedDOMComponentWithClass(header, 'menu-item-theme');
     let links = themeMenu.getElementsByTagName('a');
@@ -87,7 +104,8 @@ describe('Header', () => {
 
   it('should call log out function when link is clicked', () => {
     let header = TestUtils.renderIntoDocument(
-      <Header theme="dark" logOut={logOut} location={fakeLocation} />
+      <Header theme="dark" logOut={logOut} location={fakeLocation}
+        params={fakeParams} history={fakeHistoryMaker()} />
     );
     let themeMenu = TestUtils.findRenderedDOMComponentWithClass(header, 'bottom-item');
     let links = themeMenu.getElementsByTagName('a');
