ffb05db690ba84faaba542df4f51b2fa26e6e22b
Craig Lubin <clubin@vidscale.com>
2016-07-15 15:59:16 -0400

    First cut at customer container

--- src/containers/support.jsx
+++ src/containers/support.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import PageContainer from '../components/layout/page-container'
 import Content from '../components/layout/content'
+import SupportPageHeader from '../components/support/support-page-header'
 
 export class Support extends React.Component {
   render() {
     return (
       <PageContainer>
+        
         <Content>
           <div className="container-fluid">
             <h1>Support</h1>
