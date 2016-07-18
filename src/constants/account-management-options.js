cd3dc8beb1208a4b18994acbfb103edd00e3664d
Ville Vaarala <ville.vaarala@idean.com>
2016-07-18 18:06:24 +0300

    Hide "Cloud Provider" and use same service type value for UDN Network Partner & Media Delivery

--- src/constants/account-management-options.js
+++ src/constants/account-management-options.js
@@ -1,11 +1,11 @@
 export const ACCOUNT_TYPES = [
   { value: 1, label: 'Content Provider' },
-  { value: 2, label: 'Service Provider' },
-  { value: 3, label: 'Cloud Provider' }
+  { value: 2, label: 'Service Provider' }
+  //{ value: 3, label: 'Cloud Provider' }
 ]
 
 export const SERVICE_TYPES = [
-  { value: 0, label: 'UDN Network Partner - On-Net', accountType: 2 },
+  { value: 1, label: 'UDN Network Partner - On-Net', accountType: 2 }, // TODO: Update the value when the service is implemented
   { value: 1, label: 'Media Delivery', accountType: 1 }
   // Not in 0.7 { value: 'storage', label: 'Storage', accountType: 3 }
 ]
