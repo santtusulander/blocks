0d479c03eaa0f922841582c99d9074b92354840c
Ville Vaarala <ville.vaarala@idean.com>
2016-07-18 14:37:53 +0300

    Update account and service types https://vidscale.atlassian.net/wiki/display/CDX/CloudScale+North+0.7

--- src/constants/account-management-options.js
+++ src/constants/account-management-options.js
@@ -1,10 +1,11 @@
 export const ACCOUNT_TYPES = [
   { value: 1, label: 'Content Provider' },
-  { value: 3, label: 'Service Provider' }
+  { value: 2, label: 'Service Provider' },
+  { value: 3, label: 'Cloud Provider' }
 ]
 
 export const SERVICE_TYPES = [
-  { value: 0, label: 'UDN Network Partner - On-Net', accountType: 3 },
+  { value: 0, label: 'UDN Network Partner - On-Net', accountType: 2 },
   { value: 1, label: 'Media Delivery', accountType: 1 }
   // Not in 0.7 { value: 'storage', label: 'Storage', accountType: 3 }
 ]
