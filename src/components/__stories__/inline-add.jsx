c50261bd2cd388b4cc0a7d7b32f70b0c60a549ce
santtu-idean <santtu.sulander@idean.com>
2016-07-20 10:48:33 +0300

    validate proptypes, pass inputs in an array

--- src/components/__stories__/inline-add.jsx
+++ src/components/__stories__/inline-add.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Input } from 'react-bootstrap'
 import {storiesOf, action} from '@kadira/storybook';
 import {reducer as form} from 'redux-form'
 import {createStore, combineReducers} from 'redux'
 import {Provider} from 'react-redux'
 
+import ActionLinks from '../account-management/action-links.jsx'
 import InlineAdd from '../inline-add.jsx'
 
 const stateReducer = combineReducers({ form })
 const store = createStore(stateReducer)
 const ThemeWrap = require('./theme-wrap.jsx')
-
+const users = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 storiesOf('Inline Add', module)
   .addDecorator((story) => (
     <ThemeWrap>
@@ -19,8 +21,16 @@ storiesOf('Inline Add', module)
     </ThemeWrap>
   ))
   .add('Default', () => (
-    <table>
-      <tbody>
+    <table className="table table-striped cell-text-left">
+        <thead >
+          <tr>
+            <th width="30%">NAME</th>
+            <th width="30%">ROLE</th>
+            <th width="30%">EMAIL</th>
+            <th width="8%"></th>
+          </tr>
+        </thead>
+        <tbody>
         <InlineAdd
             validate={({ a }) => {
               let errors = {}
@@ -29,12 +39,24 @@ storiesOf('Inline Add', module)
               }
               return errors
             }}
-            fields={['a', 'b']}
+            fields={['a', 'b', 'c']}
+            inputs={[[ <Input id='a' className="half" type="text"/> ], [ <Input id='b' type="text"/>, <Input id='c' type="text"/> ], []]}
             cancel={action('cancel pressed')}
-            save={(vals) => console.log(vals)}>
-            <input id='a' type="text"/>
-            <input id='b' type="text"/>
-        </InlineAdd>
-      </tbody>
-    </table>
+            save={vals => console.log(vals)}/>
+          {users.map((user, index) => {
+            return (
+              <tr key={index}>
+                <td>aaa</td>
+                <td>bbb</td>
+                <td>ccc</td>
+                <td>
+                  <ActionLinks
+                    onEdit={() => {}}
+                    onDelete={() => {}}/>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
   ))
--- src/components/inline-add.jsx
+++ src/components/inline-add.jsx
@@ -1,21 +1,24 @@
-import React, { PropTypes, Children, cloneElement } from 'react'
+import React, { PropTypes, cloneElement } from 'react'
 import { reduxForm, getValues } from 'redux-form'
 
 import UDNButton from './button'
 import IconClose from './icons/icon-close'
 
-const EditableRow = ({ save, cancel, children, fields, invalid, values }) => {
-  const onSave = () => save(values)
+const InlineAdd = ({ save, cancel, inputs, fields, invalid, values }) => {
   return (
     <tr className="edit-row">
-      {Children.map(children, child =>
-        <td>
-          {cloneElement(child, { ...fields[child.props.id] })}
-          {fields[child.props.id].error && <div className="error-msg">{fields[child.props.id].error}</div>}
+      {inputs.map((cell, index) =>
+        <td key={index}>
+          {cell.map((input, index) =>
+            <div key={index}>
+              {cloneElement(input, { ...fields[input.props.id] })}
+              {fields[input.props.id].error && <div className="error-msg">{fields[input.props.id].error}</div>}
+            </div>
+          )}
         </td>
       )}
       <td>
-        <UDNButton bsStyle="primary" disabled={invalid} onClick={onSave}>
+        <UDNButton bsStyle="primary" disabled={invalid} onClick={() => save(values)}>
           SAVE
         </UDNButton>
         <UDNButton bsStyle="primary" onClick={cancel} icon={true}>
@@ -26,4 +29,13 @@ const EditableRow = ({ save, cancel, children, fields, invalid, values }) => {
   )
 }
 
-export default reduxForm({ form: 'inlineAdd' }, state => { values: getValues(state.form.inlineAdd) })(EditableRow)
+InlineAdd.propTypes = {
+  cancel: PropTypes.func,
+  fields: PropTypes.object,
+  inputs: PropTypes.array,
+  invalid: PropTypes.bool,
+  save: PropTypes.func,
+  values: PropTypes.object
+}
+
+export default reduxForm({ form: 'inlineAdd' }, state => { values: getValues(state.form.inlineAdd) })(InlineAdd)
