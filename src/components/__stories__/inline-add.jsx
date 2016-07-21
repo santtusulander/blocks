30891a7b29c644beeabe1f0781ca0eafa8e3b0f4
santtu-idean <santtu.sulander@idean.com>
2016-07-21 15:32:55 +0300

    update inline add component story and some styles

--- src/components/__stories__/inline-add.jsx
+++ src/components/__stories__/inline-add.jsx
@@ -5,6 +5,7 @@ import {reducer as form} from 'redux-form'
 import {createStore, combineReducers} from 'redux'
 import {Provider} from 'react-redux'
 
+import SelectWrapper from '../select-wrapper.jsx'
 import ActionLinks from '../account-management/action-links.jsx'
 import InlineAdd from '../inline-add.jsx'
 
@@ -13,21 +14,43 @@ const store = createStore(stateReducer)
 const ThemeWrap = require('./theme-wrap.jsx')
 const users = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 /**
- * Each sub-array contains elements per <td>.
- * The style-field is meant for positional styling of the element. To get values from input fields,
- * the input elements' IDs must match the field prop's array items.
+ * Each sub-array contains elements per <td>. If no elements are needed for a <td>, insert empty array [].
+ * The positionClass-field is meant for positioning the div that wraps the input element and it's tooltip.
+ * To get values from input fields, the input elements' IDs must match the field prop's array items.
  */
-const addRowInputs = [
+const inlineAddInputs = [
   [
-    { input: <Input id='a' type="text"/>, style: { float: 'left' } },
-    { input: <Button>Do something</Button>, style: { float: 'right' } }
+    { input: <Input id='a' placeholder=" Name" type="text"/>, positionClass: 'half-width-item left' },
+    { input: <Button>Do something</Button>, positionClass: 'trailing-item'}
   ],
   [
-    { input: <Input id='b' type="text"/>, style: { float: 'left' } },
-    { input: <Input id='c' type="text"/>, style: { float: 'right' } }
+    { input: <Input id='b' placeholder=" Some" type="text"/>, positionClass: 'half-width-item left' },
+    { input: <Input id='c' placeholder=" Things" type="text"/>, positionClass: 'half-width-item right' }
   ],
+  [ {
+    input: <SelectWrapper
+        id='d'
+        numericValues={true}
+        className=" inline-add-dropdown"
+        options={[1, 2, 3 ,4, 5].map(item => [item, item])}/>
+    , positionClass: 'left'
+  } ],
   []
 ]
+
+function validateInlineAdd({ a, b, c }) {
+  let errors = {}
+  if( a && a.length > 0) {
+    errors.a = 'insert validation'
+  }
+  if( b && b.length > 0) {
+    errors.b = 'insert validation'
+  }
+  if( c && c.length > 0) {
+    errors.c = 'insert validation'
+  }
+  return errors
+}
 
 storiesOf('Inline Add', module)
   .addDecorator((story) => (
@@ -43,33 +66,21 @@ storiesOf('Inline Add', module)
         <Table striped={true}>
           <thead >
             <tr>
-              <th width="30%">NAME</th>
-              <th width="30%">ROLE</th>
-              <th width="30%">EMAIL</th>
-              <th width="8%"></th>
+              <th>Email</th>
+              <th>Password</th>
+              <th>Role</th>
+              <th>Groups</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {
-             <InlineAdd
-              validate={({ a, b, c }) => {
-                let errors = {}
-                if( a && a.length > 1) {
-                  errors.a = 'insert validation'
-                }
-                if( b && b.length > 1) {
-                  errors.b = 'insert validation'
-                }
-                if( c && c.length > 3) {
-                  errors.c = 'insert validation'
-                }
-                return errors
-              }}
-              fields={['a', 'b', 'c']}
-              inputs={addRowInputs}
+            <InlineAdd
+              validate={validateInlineAdd}
+              fields={['a', 'b', 'c', 'd']}
+              inputs={inlineAddInputs}
               cancel={() => {}}
+              unmount={() => console.log('will unmount!')}
               save={vals => console.log(vals)}/>
-            }
             {users.map((user, index) => {
               return (
                 <tr key={index}>
