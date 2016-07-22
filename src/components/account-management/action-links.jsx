9f82a554db6358e1c76ae97df67dcc11981d64df
Ville Vaarala <ville.vaarala@idean.com>
2016-07-22 18:59:11 +0300

    Use the same delete button in System and Account Management

--- src/components/account-management/action-links.jsx
+++ src/components/account-management/action-links.jsx
@@ -1,14 +1,14 @@
 import React, {PropTypes} from 'react'
+import {Button} from 'react-bootstrap'
 
-import UDNButton from '../button.js'
 import IconTrash from '../icons/icon-trash.jsx'
 
 const ActionLinks = props =>
     <div className='action-links cell-text-center'>
       <a id='edit-link' className='action-link-edit' onClick={props.onEdit}>EDIT</a>
-      <UDNButton id='delete-button' bsStyle='primary' icon={true} onClick={props.onDelete}>
+      <Button onClick={props.onDelete} className="btn-link btn-icon">
         <IconTrash/>
-      </UDNButton>
+      </Button>
     </div>
 
 ActionLinks.propTypes = {
