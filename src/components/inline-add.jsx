4d38f6a107c530c37333c8e3ede741479fea065e
santtu-idean <santtu.sulander@idean.com>
2016-07-26 15:20:18 +0300

    Move error tooltip rendering into a separate function for clarity

--- src/components/inline-add.jsx
+++ src/components/inline-add.jsx
@@ -5,32 +5,37 @@ import { reduxForm, getValues } from 'redux-form'
 import UDNButton from './button'
 import IconClose from './icons/icon-close'
 
-const InlineAdd = ({ save, inputs, fields, invalid, values, unmount }) =>
-      <tr className="inline-add-row">
-        {inputs.map((cell, index) =>
-          <td key={index} colSpan={index === inputs.length - 1 ? 2 : 1}>
-            {cell.map(({ input, positionClass }, index) =>
-              <div className={positionClass} key={index}>
-                {cloneElement(input, { ...fields[input.props.id] })}
-                {fields[input.props.id] && fields[input.props.id].error && fields[input.props.id].touched &&
-                  <Tooltip placement="bottom" className="in" id="tooltip-bottom">
-                    {fields[input.props.id].error}
-                  </Tooltip>}
-              </div>
-            )}
-            {index === inputs.length - 1 &&
-              <ButtonToolbar className="pull-right">
-                <UDNButton disabled={invalid} onClick={() => save(values)}>
-                  SAVE
-                </UDNButton>
-                <UDNButton bsStyle="primary" onClick={unmount} icon={true}>
-                  <IconClose/>
-                </UDNButton>
-              </ButtonToolbar>}
-          </td>
-        )}
-      </tr>
+const ErrorToolTip = attributes =>
+  attributes && attributes.error && attributes.touched &&
+    <Tooltip placement="bottom" className="in" id="tooltip-bottom">
+      {attributes.error}
+    </Tooltip>
 
+const InlineAdd = ({ save, inputs, fields, invalid, values, unmount }) => {
+  return (
+    <tr className="inline-add-row">
+      {inputs.map((cell, index) =>
+        <td key={index} colSpan={index === inputs.length - 1 ? 2 : 1}>
+          {cell.map(({ input, positionClass }, index) =>
+            <div className={positionClass} key={index}>
+              {cloneElement(input, { ...fields[input.props.id] })}
+              {ErrorToolTip(fields[input.props.id])}
+            </div>
+          )}
+          {index === inputs.length - 1 &&
+            <ButtonToolbar className="pull-right">
+              <UDNButton disabled={invalid} onClick={() => save(values)}>
+                SAVE
+              </UDNButton>
+              <UDNButton bsStyle="primary" onClick={unmount} icon={true}>
+                <IconClose/>
+              </UDNButton>
+            </ButtonToolbar>}
+        </td>
+      )}
+    </tr>
+  )
+}
 InlineAdd.propTypes = {
   fields: PropTypes.object,
   inputs: PropTypes.array,
