e06ecd36e4afe7699695e3504c87ea03a7c79dc2
Bryan Downing <bryan.downing@idean.com>
2016-07-26 16:09:08 -0700

    UDNP-478 — Add styling for support ticket lists

--- src/containers/support/tabs/tickets.jsx
+++ src/containers/support/tabs/tickets.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { Range } from 'immutable'
 import SupportTicketPanel from '../../../components/support/support-ticket-panel'
+
+import './tickets.scss'
 
 class SupportTabTickets extends React.Component {
   constructor(props) {
@@ -11,25 +13,26 @@ class SupportTabTickets extends React.Component {
     return (
       <div className="account-support-tickets">
 
-        <SupportTicketPanel
-          type="task"
-          assignee="Pending"
-          body="My end user in Tokyo is complaining about a slow streaming start. Lorem ipsum dolor sit amet, consectur rom..."
-          comments="3"
-          number="#1-5424"
-          status="open"
-          title="Poor Performance"
-          priority="high" />
+        <h2>5 Open Tickets</h2>
+        <div className="support-tickets-list">
+          {Range(0, 10).map((item, index) => {
+            return (
+              <SupportTicketPanel
+                type="task"
+                assignee="Pending"
+                body="My end user in Tokyo is complaining about a slow streaming start. Lorem ipsum dolor sit amet, consectur rom..."
+                comments="3"
+                number={index}
+                status="open"
+                title="Poor Performance"
+                priority="high" />
+            )
+          })}
 
-        <SupportTicketPanel
-          type="task"
-          assignee="Pending"
-          body="My end user in Tokyo is complaining about a slow streaming start. Lorem ipsum dolor sit amet, consectur rom..."
-          comments="3"
-          number="#1-5424"
-          status="closed"
-          title="Poor Performance"
-          priority="high" />
+        </div>
+
+        <h2>5 Closed Tickets</h2>
+
       </div>
     )
   }
