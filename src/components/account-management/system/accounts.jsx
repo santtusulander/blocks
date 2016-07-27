6ad035d74842e9dcbc2d19168ecc9fd4e8e40a71
santtu-idean <santtu.sulander@idean.com>
2016-07-27 15:34:53 +0300

    add sort support to account listing

--- src/components/account-management/system/accounts.jsx
+++ src/components/account-management/system/accounts.jsx
@@ -1,27 +1,95 @@
 import React, { PropTypes, Component } from 'react'
+import { Row, Col, Input } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { List, fromJS } from 'immutable'
 
 import ActionLinks from '../action-links'
 import { AccountManagementHeader } from '../account-management-header'
 import ArrayCell from '../../array-td/array-td'
+import TableSorter from '../../table-sorter'
 
 import { fetchAccounts } from '../../../redux/modules/account'
 
 import { SERVICE_TYPES, ACCOUNT_TYPES } from '../../../constants/account-management-options'
 
 class AccountList extends Component {
+  constructor(props) {
+    super(props);
+    this.changeSort = this.changeSort.bind(this)
+    this.state = {
+      search: '',
+      sortBy: 'name',
+      sortDir: 1
+    }
+  }
+
+
+  changeSort(column, direction) {
+    this.setState({
+      sortBy: column,
+      sortDir: direction
+    })
+  }
+
+  sortedData(data, sortBy, sortDir) {
+    return data.sort((a, b) => {
+      let aVal = a.get(sortBy)
+      let bVal = b.get(sortBy)
+      if(typeof a.get(sortBy) === 'string') {
+        aVal = aVal.toLowerCase()
+        bVal = bVal.toLowerCase()
+      }
+      if(aVal < bVal) {
+        return -1 * sortDir
+      }
+      else if(aVal > bVal) {
+        return 1 * sortDir
+      }
+      return 0
+    })
+  }
+
   render() {
     const { accounts, editAccount, deleteAccount, params: { brand } } = this.props
+    const filteredAccounts = accounts.filter(account =>
+      account.get('name').toLowerCase().includes(this.state.search.toLowerCase())
+    )
+    const sorterProps  = {
+      activateSort: this.changeSort,
+      activeColumn: this.state.sortBy,
+      activeDirection: this.state.sortDir
+    }
+    const sortedAccounts = this.sortedData(
+      filteredAccounts,
+      this.state.sortBy,
+      this.state.sortDir
+    )
+    const hiddenAccs = accounts.size - sortedAccounts.size
     const services = values =>
       values.map(value => SERVICE_TYPES.find(type => type.value === value).label)
     return (
       <div>
+        <Row className="header-btn-row">
+          <Col sm={6}>
+            <h3>
+              {sortedAccounts.size} Account{sortedAccounts.size === 1 ? '' : 's'} {!!hiddenAccs && `(${hiddenAccs} hidden)`}
+            </h3>
+          </Col>
+          <Col sm={6} className="text-right">
+            <Input
+              type="text"
+              className="search-input"
+              groupClassName="search-input-group"
+              placeholder="Search"
+              value={this.state.search}
+              onChange={this.changeSearch} />
+          </Col>
+        </Row>
         <AccountManagementHeader title={`${accounts.size} Accounts`} onAdd={() => {}}/>
         <table className="table table-striped cell-text-left">
           <thead >
           <tr>
-            <th width="30%">ACCOUNT NAME</th>
+            <TableSorter {...sorterProps} column="name" width="30%">ACCOUNT NAME</TableSorter>
             <th width="10%">TYPE</th>
             <th width="10%">ID</th>
             <th width="10%">BRAND</th>
