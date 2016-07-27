807aa28f1c32adbc1dbfe1e1609b4b6b4799a643
santtu-idean <santtu.sulander@idean.com>
2016-07-27 12:43:23 +0300

    WIP: create account list

--- src/components/account-management/system/accounts.jsx
+++ src/components/account-management/system/accounts.jsx
@@ -1,16 +1,77 @@
-import React from 'react'
+import React, { PropTypes, Component } from 'react'
+import { List } from 'immutable'
 
-class AccountManagementSystemAccounts extends React.Component {
+import ActionLinks from '../action-links.jsx'
+import { AccountManagementHeader } from '../account-management-header.jsx'
+
+import { SERVICE_TYPES, ACCOUNT_TYPES } from '../../../constants/account-management-options.js'
+
+function getServicesString(services) {
+  let string = ''
+  let servicesLength = services.length
+  if (servicesLength >= 0) {
+    string = string.concat(SERVICE_TYPES.find(item => item.value === services[0]).label)
+    if (servicesLength >= 1) {
+      string = string.concat(', ' + SERVICE_TYPES.find(item => item.value === services[1]).label)
+      if (servicesLength > 1) {
+        string = string.concat(', +' + servicesLength)
+      }
+    }
+  } else return 'No service types'
+  return string
+}
+
+class AccountList extends Component {
   render() {
+    const { accounts, editAccount, deleteAccount } = this.props
     return (
-      <div className="account-management-system-accounts">
-        Placeholder for account list
+      <div>
+        <AccountManagementHeader title={`${accounts.size} Accounts`} onAdd={() => {}}/>
+        <table className="table table-striped cell-text-left">
+          <thead >
+          <tr>
+            <th width="30%">ACCOUNT NAME</th>
+            <th width="10%">TYPE</th>
+            <th width="10%">ID</th>
+            <th width="10%">BRAND</th>
+            <th width="30%">SERVICES</th>
+            <th width="8%"/>
+          </tr>
+          </thead>
+          <tbody>
+          {!accounts.isEmpty() ? accounts.map((account, index) => {
+            const id = account('id')
+            return (
+              <tr key={index}>
+                <td>{account.get('name')}</td>
+                <td>{ACCOUNT_TYPES.find(type => account.get('provider_type') === type.value).label}</td>
+                <td>{id}</td>
+                <td>{account.get('brand_id')}</td>
+                <td>{getServicesString(account.get('services'))}</td>
+                <td>
+                  <ActionLinks
+                    onEdit={() => editAccount(id)}
+                    onDelete={() => deleteAccount(id)}/>
+                </td>
+              </tr>
+            )
+          }) : <tr id="empty-msg"><td colSpan="4">No accounts</td></tr>}
+          </tbody>
+        </table>
       </div>
     )
   }
 }
 
-AccountManagementSystemAccounts.displayName = 'AccountManagementSystemAccounts'
-AccountManagementSystemAccounts.propTypes = {}
+AccountList.propTypes = {
+  accounts: PropTypes.instanceOf(List),
+  addAccount: PropTypes.func,
+  deleteAccount: PropTypes.func,
+  editAccount: PropTypes.func
+}
 
-module.exports = AccountManagementSystemAccounts
+AccountList.defaultProps = {
+  accounts: List()
+}
+
+export default AccountList
