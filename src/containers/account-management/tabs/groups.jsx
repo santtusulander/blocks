ec551f9d253c9ce24d644e0ab602cbc14fcd0493
Taaha Chaudhry <taaha.chaudhry@idean.com>
2016-07-27 11:03:09 -0700

    UDNP-952: Fixed Add Group inline styles - updated the name input field to be the full width of column and added padding to the name input field; Fixed scss-lint errors in _inline-add.scss

--- src/containers/account-management/tabs/groups.jsx
+++ src/containers/account-management/tabs/groups.jsx
@@ -171,8 +171,7 @@ class AccountManagementAccountGroups extends React.Component {
         {
           input: <Input id='name' placeholder=" Name" type="text"
             onChange={this.changeNewName}
-            value={this.state.newName}/>,
-          positionClass: 'left'
+            value={this.state.newName}/>
         }
       ],
       [
