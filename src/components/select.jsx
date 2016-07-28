a0e4247c8b44b9a86c353f14d18c3fcc389db617
Simo Jokela <simo.jokela@nordsoftware.com>
2016-07-28 13:20:25 +0300

    Change Select -component to support options as array or object

--- src/components/select.jsx
+++ src/components/select.jsx
@@ -1,44 +1,69 @@
 import React, { PropTypes, Component } from 'react';
 
 import { Dropdown, MenuItem } from 'react-bootstrap'
-import IconSelectCaret from '../components/icons/icon-select-caret.jsx'
+import IconSelectCaret from '../components/icons/icon-select-caret'
 
 class Select extends Component {
   constructor(props) {
     super(props);
 
     this.selectOption = this.selectOption.bind(this)
+    this.getMenuItem = this.getMenuItem.bind(this)
+    this.getOptionLabel = this.getOptionLabel.bind(this)
+    this.getOptionValue = this.getOptionValue.bind(this)
+    this.getSelected = this.getSelected.bind(this)
   }
+
   selectOption(e) {
     const value = e.target.getAttribute('data-value')
     this.props.onSelect(this.props.numericValues ? Number(value) : value)
   }
+
+  getMenuItem(option, i) {
+    const value = this.getOptionValue(option)
+    const label = this.getOptionLabel(option)
+    return (
+      <MenuItem
+        key={i}
+        data-value={value}
+        className={this.props.value === value && 'hidden'}>
+        {label}
+      </MenuItem>
+    )
+  }
+
+  getOptionLabel(option) {
+    return Array.isArray(option) ? option[1] : option.label
+  }
+
+  getOptionValue(option) {
+    return Array.isArray(option) ? option[0] : option.value
+  }
+
+  getSelected() {
+    return this.props.options.find((option) => {
+      return this.getOptionValue(option) === this.props.value
+    })
+  }
+
   render() {
     let className = 'dropdown-select'
-    if(this.props.className) {
+    if (this.props.className) {
       className = className + ' ' + this.props.className
     }
-    let label = "Please Select"
-    const currentSelection = this.props.options.find(
-      option => option[0] === this.props.value
-    )
-    if(currentSelection) {
-      label = currentSelection[1]
-    }
+
+    const selected = this.getSelected()
+    const label = selected ? this.getOptionLabel(selected) : this.props.emptyLabel
+
     return (
-      <Dropdown id="" disabled={this.props.disabled || false} className={className}
-        onSelect={this.selectOption}>
+      <Dropdown id="" disabled={this.props.disabled} className={className}
+                onSelect={this.selectOption}>
         <Dropdown.Toggle noCaret={true}>
           <IconSelectCaret/>
           {label}
         </Dropdown.Toggle>
         <Dropdown.Menu>
-          {this.props.options.map((options, i) =>
-            <MenuItem key={i} data-value={options[0]}
-              className={this.props.value === options[0] && 'hidden'}>
-              {options[1]}
-            </MenuItem>
-          )}
+          {this.props.options.map(this.getMenuItem)}
         </Dropdown.Menu>
       </Dropdown>
     );
@@ -51,11 +76,14 @@ Select.propTypes = {
   numericValues: PropTypes.bool,
   onSelect: PropTypes.func,
   options: PropTypes.array,
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-};
-
-Select.defaultProps = {
-  options: []
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  emptyLabel: PropTypes.string
 }
 
-module.exports = Select
+Select.defaultProps = {
+  options: [],
+  disabled: false,
+  emptyLabel: 'Please Select'
+}
+
+export default Select
