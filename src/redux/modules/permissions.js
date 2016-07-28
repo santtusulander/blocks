78b6254d6d1253215a77132b6ed99c196baad1b9
Bryan Downing <bryan.downing@idean.com>
2016-07-28 14:51:44 -0700

    UDNP-883 — Populate permissions redux store with display friendly names

--- src/redux/modules/permissions.js
+++ src/redux/modules/permissions.js
@@ -1,22 +1,21 @@
 import { createAction, handleActions } from 'redux-actions'
 import { fromJS } from 'immutable'
+import axios from 'axios'
 
-import { mapReducers } from '../util'
+import { mapReducers, parseResponseData, urlBase } from '../util'
 
 const PERMISSIONS_FETCHED = 'PERMISSIONS_FETCHED'
 
-export const initialState = fromJS({
-  permissions: []
-})
+export const initialState = fromJS({})
 
 // REDUCERS
 
 export function permissionsFetchSuccess(state, action) {
-  return state.set('permissions', fromJS(action.payload.data))
+  return state.merge(fromJS(action.payload))
 }
 
 export function permissionsFetchFailure(state) {
-  return state.set('permissions', fromJS([]))
+  return state.merge(fromJS({}))
 }
 
 export default handleActions({
@@ -25,19 +24,16 @@ export default handleActions({
 
 // ACTIONS
 export const fetchPermissions = createAction(PERMISSIONS_FETCHED, () => {
-  return Promise.resolve({data: [
-    {id: 'content', name: 'Content'},
-    {id: 'analytics', name: 'Analytics'},
-    {id: 'analytics_cp_traffic', name: 'Analytics: CP: Traffic'},
-    {id: 'analytics_cp_unique_visitors', name: 'Analytics: CP: Unique Visitors'},
-    {id: 'analytics_cp_sp_contribution', name: 'Analytics: CP: SP Contribution'},
-    {id: 'analytics_cp_file_error', name: 'Analytics: CP: File Error'},
-    {id: 'analytics_cp_url', name: 'Analytics: CP: URL'},
-    {id: 'analytics_sp_on_off_net', name: 'Analytics: SP: On/Off Net'},
-    {id: 'security', name: 'Security'},
-    {id: 'services', name: 'Services'},
-    {id: 'account', name: 'Account'},
-    {id: 'config', name: 'Config'},
-    {id: 'support', name: 'Support'}
-  ]})
+  return Promise.all([
+    axios.get(`${urlBase}/v2/services/AAA`).then(parseResponseData),
+    axios.get(`${urlBase}/v2/services/North`).then(parseResponseData),
+    axios.get(`${urlBase}/v2/services/UI`).then(parseResponseData)
+  ])
+  .then(axios.spread((permissionsAAA, permissionsNorth, permissionsUI) => {
+    return {
+      aaa: permissionsAAA.resources,
+      north: permissionsNorth.resources,
+      ui: permissionsUI.resources
+    }
+  }))
 })
