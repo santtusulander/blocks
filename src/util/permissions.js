533aa31545b765834853c847736d00147297c887
Jenny Steele <jenny.steele@idean.com>
2016-08-02 13:07:45 -0700

    Add the rest of the ui permissions as constants

--- src/util/permissions.js
+++ src/util/permissions.js
@@ -3,8 +3,37 @@ import * as PERMISSIONS from '../constants/permissions'
 let permissionMapping = {};
 
 // This mapping connects permission constants to the permissions attached to roles.
+
+// Sections
+permissionMapping[PERMISSIONS.VIEW_ANALYTICS_SECTION] =
+  (role) => role.getIn(['permissions', 'ui', 'analytics'])
 permissionMapping[PERMISSIONS.VIEW_CONTENT_SECTION] =
   (role) => role.getIn(['permissions', 'ui', 'content'])
+permissionMapping[PERMISSIONS.VIEW_SECURITY_SECTION] =
+  (role) => role.getIn(['permissions', 'ui', 'security'])
+permissionMapping[PERMISSIONS.VIEW_SERVICES_SECTION] =
+  (role) => role.getIn(['permissions', 'ui', 'services'])
+permissionMapping[PERMISSIONS.VIEW_SUPPORT_SECTION] =
+  (role) => role.getIn(['permissions', 'ui', 'support'])
+
+
+// Analytics Reports
+permissionMapping[PERMISSIONS.VIEW_ANALYTICS_FILE_ERROR] =
+  (role) => role.getIn(['permissions', 'ui', 'analytics_file_error'])
+permissionMapping[PERMISSIONS.VIEW_ANALYTICS_SP_CONTRIBUTION] =
+  (role) => role.getIn(['permissions', 'ui', 'analytics_sp_contribution'])
+permissionMapping[PERMISSIONS.VIEW_ANALYTICS_SP_ON_OFF_NET] =
+  (role) => role.getIn(['permissions', 'ui', 'analytics_sp_on_off_net'])
+permissionMapping[PERMISSIONS.VIEW_ANALYTICS_TRAFFIC_OVERVIEW] =
+  (role) => role.getIn(['permissions', 'ui', 'analytics_traffic_overview'])
+permissionMapping[PERMISSIONS.VIEW_ANALYTICS_UNIQUE_VISITORS] =
+  (role) => role.getIn(['permissions', 'ui', 'analytics_unique_visitors'])
+permissionMapping[PERMISSIONS.VIEW_ANALYTICS_URL] =
+  (role) => role.getIn(['permissions', 'ui', 'analytics_url'])
+
+// Misc Functionality
+permissionMapping[PERMISSIONS.VIEW_PROPERTY_CONFIG] =
+  (role) => role.getIn(['permissions', 'ui', 'config'])
 
 /**
  * Determine if a user has a permission.
