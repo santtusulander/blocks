a9ae8442b731d601fed3f9a7ff3cce829b212c91
Mika Paulasaari <mika.paulasaari@idean.com>
2016-08-08 16:34:26 -0700

    Add exit confirm modal to Users inline add

--- src/containers/account-management/tabs/users.jsx
+++ src/containers/account-management/tabs/users.jsx
@@ -23,6 +23,7 @@ import TableSorter from '../../../components/table-sorter'
 import UserEditModal from '../../../components/account-management/user-edit/modal'
 import ArrayCell from '../../../components/array-td/array-td'
 import ActionModal from '../../../components/action-modal'
+import UDNButton from '../../../components/button'
 
 import { ROLES_MAPPING } from '../../../constants/account-management-options'
 
@@ -55,6 +56,8 @@ export class AccountManagementAccountUsers extends React.Component {
     this.toggleInlineAdd = this.toggleInlineAdd.bind(this)
     this.togglePasswordVisibility = this.togglePasswordVisibility.bind(this)
     this.togglePermissionModal = this.togglePermissionModal.bind(this)
+    this.shouldLeave = this.shouldLeave.bind(this)
+    this.isLeaving = false;
   }
 
   componentWillMount() {
@@ -65,6 +68,8 @@ export class AccountManagementAccountUsers extends React.Component {
       this.props.groupActions.fetchGroups(brand, account);
     }
     this.props.rolesActions.fetchRoles()
+    const { router, route } = this.props
+    router.setRouteLeaveHook(route, this.shouldLeave)
   }
 
   componentWillReceiveProps(nextProps) {
@@ -250,6 +255,27 @@ export class AccountManagementAccountUsers extends React.Component {
   getEmailForUser(user) {
     return user.get('email') || user.get('username')
   }
+
+  shouldLeave({ pathname }) {
+    if (!this.isLeaving && this.state.addingNew) {
+      this.props.uiActions.showInfoDialog({
+        title: 'Warning',
+        content: 'You have made changes to the User(s), are you sure you want to exit without saving?',
+        buttons:  [
+          <UDNButton key="button-1" onClick={() => {
+            //this.leavePage()
+            this.isLeaving = true
+            this.props.router.push(pathname)
+            this.props.uiActions.hideInfoDialog()
+          }} bsStyle="primary">Continue</UDNButton>,
+          <UDNButton key="button-2" onClick={this.props.uiActions.hideInfoDialog} bsStyle="primary">Stay</UDNButton>
+        ]
+      })
+      return false;
+    }
+    return true
+  }
+
   editUser(user) {
     this.setState({
       userToEdit: user,
@@ -405,6 +431,8 @@ AccountManagementAccountUsers.propTypes = {
   resetRoles: React.PropTypes.func,
   roles: React.PropTypes.instanceOf(List),
   rolesActions: React.PropTypes.object,
+  route: React.PropTypes.object,
+  router: React.PropTypes.object,
   uiActions: React.PropTypes.object,
   userActions: React.PropTypes.object,
   users: React.PropTypes.instanceOf(List)
