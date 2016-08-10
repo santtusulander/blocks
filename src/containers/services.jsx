c6dce16844897c9faf5107733f49e657708e9140
Ville Ristimäki <ville.ristimaki@idean.com>
2016-08-10 12:04:08 +0300

    Add dummy-content to services.jsx

--- src/containers/services.jsx
+++ src/containers/services.jsx
@@ -1,24 +1,63 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import { Map, List } from 'immutable'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import * as accountActionCreators from '../redux/modules/account'
 
 import PageContainer from '../components/layout/page-container'
 import Content from '../components/layout/content'
+import ServicesPageHeader from '../components/services/services-page-header'
 
 export class Services extends React.Component {
+  constructor(props) {
+    super (props);
+  }
+
   render() {
+
+    const { accounts, fetchAccount, activeAccount } = this.props;
+
     return (
-      <PageContainer>
-        <Content>
-          <div className="container-fluid">
-            <h1>Services</h1>
-          </div>
-        </Content>
+      <PageContainer className="account-management">
+        <div className="account-management-system-users">
+          <ServicesPageHeader
+            params={this.props.params}
+            accounts={accounts}
+            activeAccount={activeAccount.get ('name')}
+            fetchAccount={fetchAccount}/>
+          {/*Not in 0.8.1*/}
+          {/*{this.renderContent(certificateFormProps, sslListProps)}*/}
+          <Content className="tab-bodies">Coming soon!</Content>
+        </div>
       </PageContainer>
     )
   }
 }
 
 Services.displayName = 'Services'
+
 Services.propTypes = {
+  accounts: PropTypes.instanceOf (List),
+  activeAccount: PropTypes.instanceOf (Map),
+  fetchAccount: PropTypes.func,
+  params: PropTypes.object
 }
 
-module.exports = Services
+function mapStateToProps(state, ownProps) {
+  return {
+    accounts: state.account.get ('allAccounts'),
+    activeAccount: state.account.get ('activeAccount') || Map ({}),
+    groups: state.group.get ('allGroups')
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  const fetchAccount = bindActionCreators (accountActionCreators, dispatch).fetchAccount
+
+  return {
+    fetchAccount
+  };
+}
+
+export default connect (mapStateToProps, mapDispatchToProps) (Services)
