f7d17353cae8dc96962aa1ba77123488e1fc0c60
santtu-idean <santtu.sulander@idean.com>
2016-08-12 15:51:02 +0300

    add esc/enter support for delete modals

--- src/components/account-management/delete-user-modal.jsx
+++ src/components/account-management/delete-user-modal.jsx
@@ -1,31 +1,62 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
+import { once } from 'underscore'
 import { Button, ButtonToolbar, Modal } from 'react-bootstrap'
 
-const DeleteUserModal = ({ itemToDelete, onDelete, onCancel }) =>
-    <Modal show={true} className="delete-modal">
-      <Modal.Header  className="delete-modal-header">
-        <h1>Delete User?</h1>
-      </Modal.Header>
-      <Modal.Body className="delete-modal-body">
-        <h3>
-          {itemToDelete}<br/>
-        </h3>
-        <p>
-          Will lose access to UDN immediately and will not be able to log in anymore.
-        </p>
-      </Modal.Body>
+export default class DeleteUserModal extends Component {
+  constructor(props) {
+    super(props)
+    this.deleteOnce = once(props.onDelete)
+    this.cancelOnce = once(props.onCancel)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+  }
 
-      <Modal.Footer className="delete-modal-footer">
-        <ButtonToolbar className="pull-right">
-          <Button onClick={onCancel} className="btn-outline">Cancel</Button>
-          <Button onClick={onDelete}
-                  bsStyle="secondary"
-                  className="delete-modal-submit delete-user-submit">
-            Delete
-          </Button>
-        </ButtonToolbar>
-      </Modal.Footer>
-    </Modal>
+  componentWillMount() {
+    document.addEventListener('keydown', this.handleKeyDown)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown)
+  }
+
+  handleKeyDown({ keyCode }) {
+    switch(keyCode) {
+      case 13: this.deleteOnce()
+        break
+      case 27: this.cancelOnce()
+        break
+    }
+  }
+
+  render() {
+    const { itemToDelete, onDelete, onCancel } = this.props
+    return (
+      <Modal show={true} className="delete-modal">
+        <Modal.Header  className="delete-modal-header">
+          <h1>Delete User?</h1>
+        </Modal.Header>
+        <Modal.Body className="delete-modal-body">
+          <h3>
+            {itemToDelete}<br/>
+          </h3>
+          <p>
+            Will lose access to UDN immediately and will not be able to log in anymore.
+          </p>
+        </Modal.Body>
+        <Modal.Footer className="delete-modal-footer">
+          <ButtonToolbar className="pull-right">
+            <Button onClick={onCancel} className="btn-outline">Cancel</Button>
+            <Button onClick={onDelete}
+                    type="submit"
+                    bsStyle="secondary"
+                    className="delete-modal-submit delete-user-submit">
+              Delete
+            </Button>
+          </ButtonToolbar>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+}
 
 DeleteUserModal.propTypes = {
   fields: PropTypes.object,
@@ -33,6 +64,3 @@ DeleteUserModal.propTypes = {
   onCancel: PropTypes.func,
   onDelete: PropTypes.func
 }
-
-export default DeleteUserModal
-
--- src/components/delete-modal.jsx
+++ src/components/delete-modal.jsx
@@ -1,43 +1,66 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
+import { once } from 'underscore'
 import { Button, ButtonToolbar, Modal, Input } from 'react-bootstrap'
 import { reduxForm } from 'redux-form'
 
 let submitDisabled
-const DeleteModal = ({ itemToDelete, description, onDelete, onCancel, fields: { delField } }) => {
-  if (!description) {
-    description = `Please confirm by writing "delete" below, and pressing the delete button.
-          This ${itemToDelete} will be removed immediately. This action can't be undone`;
+export default class DeleteModal extends Component {
+  constructor(props) {
+    super(props)
+    this.deleteOnce = once(props.onDelete)
+    this.cancelOnce = once(props.onCancel)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
   }
 
-  return (
-    <Modal show={true} className="delete-modal">
-      <Modal.Header  className="delete-modal-header">
-        <h1>{`Delete ${ itemToDelete }`}</h1>
-        <hr/>
-      </Modal.Header>
-      <Modal.Body className="delete-modal-body">
-        <p>
-          {description}
-        </p>
-        <Input type="text" label="Type 'delete'" placeholder="delete" {...delField}/>
-      </Modal.Body>
+  componentWillMount() {
+    document.addEventListener('keydown', this.handleKeyDown)
+  }
 
-      <Modal.Footer className="delete-modal-footer">
-        <ButtonToolbar className="pull-right">
-          <Button onClick={onCancel} className="btn-outline">Cancel</Button>
-          <Button onClick={onDelete}
-            bsStyle="secondary"
-            className="delete-modal-submit"
-            disabled={submitDisabled}>
-            Delete
-          </Button>
-        </ButtonToolbar>
-      </Modal.Footer>
-    </Modal>
-  );
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown)
+  }
+
+  handleKeyDown({ keyCode }) {
+    switch(keyCode) {
+      case 13: !submitDisabled && this.deleteOnce()
+        break
+      case 27: this.cancelOnce()
+        break
+    }
+  }
+  render() {
+    const { itemToDelete, description, onDelete, onCancel, fields: { delField } } = this.props
+    const defaultDescription = `Please confirm by writing "delete" below, and pressing the delete button.
+          This ${itemToDelete} will be removed immediately. This action can't be undone`;
+    return (
+      <Modal show={true} className="delete-modal">
+        <Modal.Header  className="delete-modal-header">
+          <h1>{`Delete ${ itemToDelete }`}</h1>
+          <hr/>
+        </Modal.Header>
+        <Modal.Body className="delete-modal-body">
+          <p>
+            {description || defaultDescription}
+          </p>
+          <Input type="text" label="Type 'delete'" placeholder="delete" {...delField}/>
+        </Modal.Body>
+
+        <Modal.Footer className="delete-modal-footer">
+          <ButtonToolbar className="pull-right">
+            <Button onClick={onCancel} className="btn-outline">Cancel</Button>
+            <Button onClick={onDelete}
+              bsStyle="secondary"
+              className="delete-modal-submit"
+              disabled={submitDisabled}>
+              Delete
+            </Button>
+          </ButtonToolbar>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
 }
 
-DeleteModal.displayName = 'ErrorModal'
 DeleteModal.propTypes = {
   description: PropTypes.string,
   fields: PropTypes.object,
