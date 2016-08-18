360c972a2ef982d3e6eef164546d7a6d106343c1
Vitaliy Bondar-Tvorovskiy <vbondar@vidscale.com>
2016-08-18 15:07:53 -0700

    UDNP-1171 Internationalization - Phase 1: Move Strings to Resource File, WIP

--- src/components/account-management/account/account.jsx
+++ src/components/account-management/account/account.jsx
@@ -16,6 +16,8 @@ import { MODIFY_ACCOUNTS } from '../../../constants/permissions'
 
 import './account.scss'
 
+import {FormattedMessage, formatMessage, injectIntl} from 'react-intl';
+
 // const brandOptions = BRANDS.map( (e) => {
 //   return [ e.id, e.brandName ]
 // });
@@ -85,16 +87,16 @@ class AccountManagementAccountDetails extends React.Component {
   shouldLeave({ pathname }) {
     if (!this.isLeaving && this.isDirty() && this.props.account.size) {
       this.props.uiActions.showInfoDialog({
-        title: 'Warning',
-        content: 'You have made changes to the Account and/or Group(s), are you sure you want to exit without saving?',
+        title: <FormattedMessage id='portal.account.manage.unsavedChanges.warning.title'/>,
+        content: <FormattedMessage id='portal.account.manage.unsavedChanges.warning.content'/>,
         buttons: [
           <UDNButton key="button-1" onClick={() => {
             //this.leavePage()
             this.isLeaving = true
             this.props.router.push(pathname)
             this.props.uiActions.hideInfoDialog()
-          }} bsStyle="primary">Continue</UDNButton>,
-          <UDNButton key="button-2" onClick={this.props.uiActions.hideInfoDialog} bsStyle="primary">Stay</UDNButton>
+          }} bsStyle="primary"><FormattedMessage id="portal.button.continue"/></UDNButton>,
+          <UDNButton key="button-2" onClick={this.props.uiActions.hideInfoDialog} bsStyle="primary"><FormattedMessage id='portal.button.stay'/></UDNButton>
         ]
       })
 
@@ -164,7 +166,7 @@ class AccountManagementAccountDetails extends React.Component {
                 <input
                   {...accountName}
                   type="text"
-                  placeholder="Enter Account Name"
+                  placeholder={this.props.intl.formatMessage({id: 'portal.account.manage.enterAccount.placeholder.text'})}
                   className="form-control"/>
               </IsAllowed>
               <IsAllowed not={true} to={MODIFY_ACCOUNTS}>
@@ -195,7 +197,7 @@ class AccountManagementAccountDetails extends React.Component {
           </div>
 
           <div className="form-group">
-            <label className="col-xs-3 control-label">Account Type</label>
+            <label className="col-xs-3 control-label"><FormattedMessage id="portal.account.manage.accountType.text"/></label>
             <Col xs={3}>
               {/* Not editable in 0.8
               <div className="input-group">
@@ -230,7 +232,7 @@ class AccountManagementAccountDetails extends React.Component {
           </div>
 
           <div className="form-group">
-            <label className="col-xs-3 control-label">Services</label>
+            <label className="col-xs-3 control-label"><FormattedMessage id="portal.account.manage.services.text"/></label>
             <Col xs={3}>
               <CheckboxArray iterable={checkBoxes} field={services}/>
             </Col>
@@ -269,4 +271,4 @@ AccountManagementAccountDetails.defaultProps = {
 export default reduxForm({
   fields: ['accountName', 'brand', 'accountType', 'services'],
   form: 'account-details'
-})(withRouter(AccountManagementAccountDetails))
+})(withRouter(injectIntl(AccountManagementAccountDetails)))
--- src/locales/en.js
+++ src/locales/en.js
@@ -1,6 +1,25 @@
 const TRANSLATED_MESSAGES = {
 	'portal.brand.summary.message' : 'BRAND ACCOUNT SUMMARY',
-	'portal.brand.allAccounts.message' : 'All Accounts'
+	'portal.brand.allAccounts.message' : 'All Accounts',
+
+	'portal.account.manage.unsavedChanges.warning.title' : 'Warning',
+	'portal.account.manage.unsavedChanges.warning.content' : 'You have made changes to the Account and/or Group(s), are you sure you want to exit without saving?',
+	'portal.account.manage.enterAccount.placeholder.text' : 'Enter Account Name',
+	'portal.account.manage.accountName.placeholder.text' : 'Account Name',
+	'portal.account.manage.accountType.text' : 'Account Type',
+	'portal.account.manage.services.text' : 'Account Type',
+	'portal.account.manage.invalidAccountName.text' : 'Account name is invalid.',
+
+	'portal.account.list.accountName.title' : 'ACCOUNT NAME',
+	'portal.account.list.type.title' : 'TYPE',
+	'portal.account.list.id.title' : 'ID',
+	'portal.account.list.brand.title' : 'BRAND',
+	'portal.account.list.services.title' : 'SERVICES',
+	'portal.account.list.searchWithParams.empty.text' : 'No accounts found with the search term {searchTerm}',
+	'portal.account.list.search.empty.text' : 'No accounts found',
+
+	'portal.button.continue' : 'Continue',
+	'portal.button.stay' : 'Stay'
 }
 
 export default TRANSLATED_MESSAGES
