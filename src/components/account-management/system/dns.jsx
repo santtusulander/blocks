e0dfdcab034c03181fedb925f404cbc7507607a9
santtu-idean <santtu.sulander@idean.com>
2016-08-18 15:53:23 +0300

    move domain toolbar into it's own file

--- src/components/account-management/system/dns.jsx
+++ src/components/account-management/system/dns.jsx
@@ -1,16 +1,9 @@
 import React, { Component, PropTypes } from 'react'
-import { ButtonToolbar } from 'react-bootstrap'
 import { connect } from 'react-redux'
-
-import { CREATE_ZONE, MODIFY_ZONE } from '../../../constants/permissions'
 
 import { fetchDomains, changeActiveDomain } from '../../../redux/modules/dns'
 
-import IsAllowed from '../../is-allowed'
-import UDNButton from '../../button'
-import DomainSelector from '../../global-account-selector/selector-component'
-import IconAdd from '../../icons/icon-add'
-import IconEdit from '../../icons/icon-edit'
+import DomainToolbar from './domain-toolbar'
 import DNSList from '../dns-list'
 // import SoaEditForm from '../soa-edit-form'
 // import DnsEditForm from '../dns-edit-form'
@@ -31,7 +24,6 @@ class AccountManagementSystemDNS extends Component {
 
   render() {
     const { changeActiveDomain, onAddDomain, onEditDomain, domains, activeDomain } = this.props
-    const dnsListProps = {}
     const domainHeaderProps = {
       searchValue: this.state.domainSearchValue,
       searchFunc: ({ target: { value } }) => this.setState({ domainSearchValue: value }),
@@ -46,9 +38,7 @@ class AccountManagementSystemDNS extends Component {
         <DomainToolbar {...domainHeaderProps}/>
         <DNSList
           onAddEntry={() => {/*noop*/}}
-          onDeleteEntry={() => {/*noop*/}}
-          {...dnsListProps}
-        />
+          onDeleteEntry={() => {/*noop*/}}/>
         {/*activeModal === EDIT_DNS &&
           <DnsEditForm
             id="dns-form"
@@ -81,59 +71,6 @@ AccountManagementSystemDNS.propTypes = {
   onAddDomain: PropTypes.func,
   onEditDomain: PropTypes.func,
   params: PropTypes.object
-}
-
-const DomainToolbar = ({ activeDomain, changeActiveDomain, domains, onAddDomain, onEditDomain, searchFunc, searchValue }) => {
-  const sortedDomains = domains.sort((a,b) => {
-    if (a.toLowerCase() < b.toLowerCase()) return -1
-    if (a.toLowerCase() > b.toLowerCase()) return 1
-    return 0
-  })
-  return (
-    <div className="domain-toolbar">
-      {domains.length > 0 || searchValue !== '' ?
-        <DomainSelector
-          items={sortedDomains.map(domain => [domain, domain])}
-          onItemClick={changeActiveDomain}
-          searchValue={searchValue}
-          onSearch={searchFunc}>
-           <h3>{activeDomain}<span className="caret"></span></h3>
-        </DomainSelector> :
-        <h3 className="selector-component">NO DOMAINS</h3>}
-      <ButtonToolbar className='pull-right'>
-        <IsAllowed to={CREATE_ZONE}>
-          <UDNButton
-            id="add-domain"
-            bsStyle="primary"
-            icon={true}
-            addNew={true}
-            onClick={onAddDomain}>
-            <IconAdd/>
-          </UDNButton>
-        </IsAllowed>
-        {activeDomain &&
-          <IsAllowed to={MODIFY_ZONE}>
-            <UDNButton
-            id="edit-domain"
-            bsStyle="primary"
-            icon={true}
-            onClick={onEditDomain}>
-            <IconEdit/>
-          </UDNButton>
-        </IsAllowed>}
-      </ButtonToolbar>
-    </div>
-  )
-}
-DomainToolbar.propTypes = {
-  activeDomain: PropTypes.string,
-  changeActiveDomain: PropTypes.func,
-  domains: PropTypes.array,
-  fetchDomains: PropTypes.func,
-  onAddDomain: PropTypes.func,
-  onEditDomain: PropTypes.func,
-  searchFunc: PropTypes.func,
-  searchValue: PropTypes.string
 }
 
 function mapStateToProps(state) {
