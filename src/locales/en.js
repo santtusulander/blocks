98d23011c7ec8defae0f90cb9f84f3b7cf5b468f
Vitaliy Bondar-Tvorovskiy <vbondar@vidscale.com>
2016-08-18 13:33:06 -0700

    UDNP-1171 Internationalization - Phase 1: Move Strings to Resource File, WIP

---
+++ src/locales/en.js
@@ -0,0 +1,6 @@
+const TRANSLATED_MESSAGES = {
+	'portal.brand.summary.message' : 'BRAND ACCOUNT SUMMARY',
+	'portal.brand.allAccounts.message' : 'All Accounts'
+}
+
+export default TRANSLATED_MESSAGES
