74b665416ef9c830fc869d0b76dfaea50b46e955
Mika Paulasaari <mika.paulasaari@idean.com>
2016-08-19 15:21:37 -0700

    Update Icon component
    - Remove xmlns since React would strip it anyways
    - Add viewbox prop
    - Change component format to functional

--- src/components/icon.jsx
+++ src/components/icon.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
 
-class Icon extends React.Component {
-  render() {
-    let className = 'icon';
-    if(this.props.className) {
-      className = className + ' ' + this.props.className;
-    }
-    return (
-      <svg className={className}
-        xmlns="http://www.w3.org/svg/2000"
-        viewBox={'0 0 ' +  this.props.width + ' ' + this.props.height}
-        width={this.props.width}
-        height={this.props.height}>
-        {this.props.children}
-      </svg>
-    );
+const Icon = (props) => {
+  const {children, className, height, viewbox, width} = props
+  let classNames = 'icon';
+  if(className) {
+    classNames = classNames + ' ' + className;
   }
+  return (
+    <svg className={classNames}
+      viewBox={viewbox || '0 0 36 36'}
+      width={width || 36}
+      height={height || 36}>
+      {children}
+    </svg>
+  )
 }
 
-Icon.displayName = 'Icon'
 Icon.propTypes = {
   children: React.PropTypes.node,
   className: React.PropTypes.string,
-  height: React.PropTypes.string,
-  width: React.PropTypes.string
+  height: React.PropTypes.number,
+  viewbox: React.PropTypes.string,
+  width: React.PropTypes.number
 }
 
-module.exports = Icon
+export default Icon
