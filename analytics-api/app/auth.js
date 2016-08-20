29d337ae8b543d6cfab5bd37ccab96dd6a7db133
Bryan Downing <bryan.downing@idean.com>
2016-08-19 17:06:12 -0700

    UDNP-874 — Add authentication to Analytics API

---
+++ analytics-api/app/auth.js
@@ -0,0 +1,65 @@
+'use strict';
+
+require('express-jsend');
+let _       = require('lodash');
+let axios   = require('axios');
+let configs = require('./configs');
+let log     = require('./logger');
+
+// Build an HTTP client instance to interface with the AAA auth API
+const axiosAAA = axios.create({
+  baseURL: configs.aaaBaseURL,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+});
+
+function unauthorized(res, status, message) {
+  status = status || 401;
+  message = message || 'Unauthorized';
+  log.error(`Authentication Error (${status}): ${message}`);
+  res.status(status).jerror(message);
+}
+
+function auth(req, res, next) {
+  const token = req.header('X-Auth-Token');
+
+  // Send the token in the X-Auth-Token header
+  if (token) {
+    axiosAAA.defaults.headers.common['X-Auth-Token'] = token;
+
+  // If the token doesn't exist, throw a 401
+  } else {
+    return unauthorized(res, 401, 'No token was provided');
+  }
+
+  // Reference the AAA API to check the validity of the token
+  log.debug('Checking validity of token');
+  axiosAAA.get(`/tokens/${token}`).then(resp => {
+    const data                             = resp.data;
+    const username                         = data.username;
+    // NOTE: userAccount is the account ID the user is associated with
+    const userAccount                      = data.account_id;
+    const requestedAccount                 = req.query.account;
+    const userCanListAccounts              = _.get(data, 'services.AAA.permissions.resources.accounts.list.allowed', false);
+    const userIsTryingToAccessWrongAccount = !userCanListAccounts && (userAccount != requestedAccount);
+
+    // If the user is not allowed to list accounts, and they are trying to request
+    // data for an account (or all accounts) they do not belong to, respond with 403.
+    if (userIsTryingToAccessWrongAccount) {
+      return unauthorized(res, 403, `The user (${username}) may only access data for account_id: ${userAccount}`);
+    }
+
+    // Allow the request to continue on to the router
+    log.debug(`Token authorized for user ${username}`);
+    next();
+
+  // We couldn't validate the user token with the AAA API
+  }).catch((err) => {
+    const data = err.response.data;
+    return unauthorized(res, data.status, data.message);
+  });
+}
+
+module.exports = auth;
