5a8785c5f272ae198bd8661e5a33f9f85d910f66
Taaha Chaudhry <taaha.chaudhry@idean.com>
2016-08-23 15:19:02 -0700

    UDNP-1161: Fixed content item chart and list buttons - fixed header buttons

--- src/components/content/content-item-chart.jsx
+++ src/components/content/content-item-chart.jsx
@@ -293,24 +293,24 @@ class ContentItemChart extends React.Component {
             <ButtonToolbar>
               {this.props.analyticsLink &&
                 <Link to={this.props.analyticsLink}
-                  className="btn btn-sm btn-primary btn-icon btn-round invisible">
+                  className="btn btn-primary btn-icon btn-round invisible">
                   <IconChart/>
                 </Link>
               }
               {this.props.configurationLink &&
                 <Link to={this.props.configurationLink}
-                  className="btn btn-sm edit-content-item btn-primary btn-icon btn-round invisible">
+                  className="btn edit-content-item btn-primary btn-icon btn-round invisible">
                   <IconConfiguration/>
                 </Link>
               }
               {this.props.onConfiguration &&
                 <a onClick={this.props.onConfiguration}
-                  className="btn btn-sm edit-content-item btn-primary btn-icon btn-round invisible">
+                  className="btn edit-content-item btn-primary btn-icon btn-round invisible">
                   <IconConfiguration/>
                 </a>
               }
               <Link to="/starburst-help"
-                className="btn btn-sm show-help btn-primary btn-icon btn-round invisible">
+                className="btn show-help btn-primary btn-icon btn-round invisible">
                 <IconQuestionMark/>
               </Link>
             </ButtonToolbar>
