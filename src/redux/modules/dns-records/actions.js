eb93e06f60d006f06c8fb14d7bf9c19623ce21c4
Mikko Alander <mikko.alander@idean.com>
2016-08-24 19:57:16 +0300

    refactor variable & function names

--- src/redux/modules/dns-records/actions.js
+++ src/redux/modules/dns-records/actions.js
@@ -1,7 +1,6 @@
-import * as DnsRecordsApi from './api'
-import * as reducer from './reducers'
-
+import {fromJS} from 'immutable'
 import { createAction, handleActions } from 'redux-actions'
+
 import { mapReducers } from '../../util'
 
 export const DNS_RECORDS_CREATED = 'DNS_RECORDS_CREATED'
@@ -10,25 +9,28 @@ export const DNS_RECORDS_RECEIVE_RESOURCE = 'DNS_RECORDS_RECEIVE_RESOURCE'
 export const DNS_RECORDS_UPDATED = 'DNS_RECORDS_UPDATED'
 export const DNS_RECORDS_DELETED = 'DNS_RECORDS_DELETED'
 
+import * as dnsRecordsApi from './api'
+import * as dnsRecordsReducers from './reducers'
+
 //ACTIONS
 export const fetchResourcesList = createAction(DNS_RECORDS_RECEIVE_RESOURCES, (zone)  => {
-  return DnsRecordsApi.fetchAll( zone )
+  return dnsRecordsApi.fetchAll( zone )
 })
 
 export const fetchResourceDetails = createAction(DNS_RECORDS_RECEIVE_RESOURCE, (zone, resource)  => {
-  return DnsRecordsApi.fetchDetailsByName( zone, resource )
+  return dnsRecordsApi.fetchDetailsByName( zone, resource )
 })
 
 export const createResource = createAction(DNS_RECORDS_CREATED, (zone, resource, data) => {
-  return DnsRecordsApi.create( zone, resource, data)
+  return dnsRecordsApi.create( zone, resource, data)
 })
 
 export const updateResource = createAction(DNS_RECORDS_UPDATED, (zone, resource, data) => {
-  return DnsRecordsApi.update( zone, resource, data)
+  return dnsRecordsApi.update( zone, resource, data)
 })
 
 export const removeResource = createAction(DNS_RECORDS_DELETED, (zone, resource) => {
-  return DnsRecordsApi.remove( zone, resource)
+  return dnsRecordsApi.remove( zone, resource)
 })
 
 //RESOURCE MODEL
@@ -54,9 +56,9 @@ const InitialState = fromJS({
 })
 
 export default handleActions({
-  DNS_RECORDS_CREATED: mapReducers(reducer.createSucess, reducer.createFailed),
-  DNS_RECORDS_RECEIVE_RESOURCES: mapReducers(reducer.receiveResourcesList, reducer.failedResourcesList),
-  DNS_RECORDS_RECEIVE_RESOURCE: mapReducers(reducer.receiveResourceDetails, reducer.failedResourceDetails),
-  DNS_RECORDS_UPDATED: mapReducers(reducer.updateSucess, reducer.updateFailed),
-  DNS_RECORDS_DELETED: mapReducers(reducer.deleteSucess, reducer.deleteFailed)
+  DNS_RECORDS_CREATED: mapReducers(dnsRecordsReducers.createSuccess, dnsRecordsReducers.createFailed),
+  DNS_RECORDS_RECEIVE_RESOURCES: mapReducers(dnsRecordsReducers.receiveResourcesList, dnsRecordsReducers.resourcesListFailed),
+  DNS_RECORDS_RECEIVE_RESOURCE: mapReducers(dnsRecordsReducers.receiveResourceDetails, dnsRecordsReducers.resourceDetailsFailed),
+  DNS_RECORDS_UPDATED: mapReducers(dnsRecordsReducers.updateSuccess, dnsRecordsReducers.updateFailed),
+  DNS_RECORDS_DELETED: mapReducers(dnsRecordsReducers.deleteSuccess, dnsRecordsReducers.deleteFailed)
 }, InitialState)
