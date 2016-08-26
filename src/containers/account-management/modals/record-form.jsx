11ab36f401d6dc65a1b68aad26d0c55060de6aa2
santtu-idean <santtu.sulander@idean.com>
2016-08-26 13:18:47 +0300

    have record form container render the modal component wrapping the form, pass initialValues conditionally

--- src/containers/account-management/modals/record-form.jsx
+++ src/containers/account-management/modals/record-form.jsx
@@ -1,24 +1,86 @@
-class AccountManagementSystemDNS extends Component {
+import React, { Component, PropTypes } from 'react'
+import { reduxForm } from 'redux-form'
+import { Modal } from 'react-bootstrap'
+
+import { checkForErrors } from '../../util/helpers'
+import { recordFields } from '../../../constants/dns-record-types'
+
+import RecordForm from '../../../components/account-management/record-form'
+
+/**
+ *
+ * Filter fields to validate according to the fields that get rendered for the active record type.
+ * The 'recordFields' -constant dictates which fields get rendered per record type.
+ */
+
+const isShown = recordType => field => recordFields[field].includes(recordType)
+const filterFields = fields => {
+  let filteredFields = {}
+  for(const field in fields) {
+    if (isShown(fields.recordType)(field)) {
+      filteredFields[field] = fields[field]
+    }
+  }
+  return filteredFields
+}
+
+const validate = fields => {
+  let filteredFields = filterFields(fields)
+  delete filteredFields.hostName
+  const conditions = {
+    priority: {
+      condition: !new RegExp('^[0-9]*$').test(filteredFields.priority),
+      errorText: 'Priority must be a number.'
+    },
+    ttl: {
+      condition: !new RegExp('^[0-9]*$').test(filteredFields.ttl),
+      errorText: 'TTL value must be a number.'
+    },
+    targetValue: {
+      condition: !new RegExp('^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$').test(filteredFields.targetValue),
+      errorText: 'Address must be an IP address.'
+    }
+  }
+  return checkForErrors(filteredFields, conditions)
+}
+
+class RecordFormContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      domainSearch: '',
-      recordSearch: ''
+      search: ''
     }
   }
+
   render() {
+    const { domain, edit, saveRecord, addRecord,  } = this.props
     const recordFormProps = {
       domain,
       edit,
+      values: filterFields(this.props.values),
+      searchValue: this.state.search,
+      shouldShowField: isShown(this.props.fields.recordType.value),
+      searchFunc: ({ target: { value } }) => this.setState({ search: value }),
       onSave: values => this.editingRecord ? saveRecord(values) : addRecord(values),
-      onCancel: () => toggleModal(null)
+      onCancel: () => toggleModal(null),
+      initialValues: edit ? initialValues : {}
     }
-    <RecordForm
-      id="record-form"
-      edit={edit}
-      domain='foobar.com'
-      onSave={values => this.editingRecord ? saveRecord(values) : addRecord(values)}
-      onCancel={() => toggleModal(null)}
-      {...dnsFormInitialValues}/>}
+    return (
+      <Modal show={true} dialogClassName="dns-edit-form-sidebar">
+        <Modal.Header>
+          <h1>{edit ? 'Edit DNS Record' : 'New DNS Record'}</h1>
+          {edit && <p>{hostName.value}</p>}
+        </Modal.Header>
+        <Modal.Body>
+          <RecordForm {...recordFormProps}/>
+        </Modal.Body>
+      </Modal>
+    )
   }
 }
+
+export default reduxForm({
+  form: 'dns-edit',
+  fields: ['recordType', 'hostName', 'targetValue', 'ttl', 'priority'],
+  validate
+})(RecordFormContainer)
