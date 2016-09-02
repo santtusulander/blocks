88d375fafa47e3eef5eaa871d3f262b958b83602
Mika Paulasaari <mika.paulasaari@idean.com>
2016-09-02 15:24:41 -0700

    Add JS colors

---
+++ src/constants/colors.js
@@ -0,0 +1,13 @@
+export default {
+  purple: '#7b0663',
+  red: '#e32119',
+  orange: '#f08a00',
+  yellow: '#fabb00',
+  green: '#89ba17',
+  darkgreen: '#00625f',
+  paleblue: '#00a9d4',
+  black: '#000',
+  grey: '#333',
+  silver: '#b1b3b4',
+  darkblue: '#0c546e'
+}
