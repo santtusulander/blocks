5ca4bfa0a9bb7e8eff0e30cc5d6a12f8634672eb
Mikko Alander <mikko.alander@idean.com>
2016-09-05 14:02:49 +0300

    add localisation support

--- src/constants/date-ranges.js
+++ src/constants/date-ranges.js
@@ -1,9 +1,11 @@
+import { FormattedMessage } from 'react-intl'
+
 export default {
-  MONTH_TO_DATE: 'Month to date',
-  LAST_28: 'Last 28 Days',
-  LAST_MONTH: 'Last Month',
-  THIS_WEEK: 'This Week',
-  TODAY: 'Today',
-  YESTERDAY: 'Yesterday',
-  CUSTOM_TIMERANGE: 'Custom Date Range'
+  MONTH_TO_DATE: <FormattedMessage id='portal.constants.date-ranges.month_to_date' />,
+  LAST_28: <FormattedMessage id='portal.constants.date-ranges.last_28' />,
+  LAST_MONTH: <FormattedMessage id='portal.constants.date-ranges.last_month' />,
+  THIS_WEEK: <FormattedMessage id='portal.constants.date-ranges.this_week' />,
+  TODAY: <FormattedMessage id='portal.constants.date-ranges.today' />,
+  YESTERDAY: <FormattedMessage id='portal.constants.date-ranges.yesterday' />,
+  CUSTOM_TIMERANGE: <FormattedMessage id='portal.constants.date-ranges.custom_timerange' />
 }
