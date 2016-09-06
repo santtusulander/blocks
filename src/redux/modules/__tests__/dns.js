8501d92380e5fd660482253f48837dd77ceb34d2
santtu-idean <santtu.sulander@idean.com>
2016-09-06 16:01:15 +0300

    WIP: create test logic for dns module reducers

--- src/redux/modules/__tests__/dns.js
+++ src/redux/modules/__tests__/dns.js
@@ -13,163 +13,139 @@ import {
   fetchedAllDomainsFailure,
   fetchedDomainSuccess,
   fetchedDomainFailure,
-  activeDomainChange,
+  activeDomainChange
 } from '../dns'
 
-import Immutable from 'immutable'
+import { is, fromJS } from 'immutable'
 
 describe('User Module', () => {
-    const state = Immutable.Map();
-
-    it('should handle userLoggedInSuccess', () => {
-      const newState = userLoggedInSuccess(state, { payload: {username: 'Username' } })
-
-      const expectedState = Immutable.fromJS({
-        loggedIn: true
-      })
-
-      expect( Immutable.is(newState, expectedState)).toBeTruthy()
-
+  let state = null
+  beforeEach(() => {
+    state = fromJS({
+      domains: [{ id: 'aa' }, { id: 'bb' }, { id: 'cc' }]
     })
+  })
 
-    it('should handle userLoggedInFailure', () => {
-      const newState = userLoggedInFailure(state, { payload: {username: 'Username' } })
+  it('should handle startedFetching', () => {
+    const newState = startedFetching(state)
+    expect(is(newState, state.set('loading', true)).toBeTruthy()
+  )
 
-      expect( newState.get('loggedIn')).toBe(false)
-      expect( newState.get('currentUser').size).toBe(0)
+  it('should handle stoppedFetching', () => {
+    const newState = stoppedFetching(state)
+    expect(is(newState, state.set('loading', false)).toBeTruthy()
+  )
 
+  it('should handle createDomainSuccess', () => {
+    const payload = { data: 'data', domain: 'domain' }
+    const newState = createDomainSuccess(state, { payload })
+    const expectedState = state.merge({
+      domains: state.get('domains').push(fromJS({ id: payload.domain, details: payload.data }))
     })
+    expect(is(newState, expectedState)).toBeTruthy()
+  })
 
-    it('should handle userLoggedOutSuccess', () => {
-      const newState = userLoggedOutSuccess(state, { payload: {username: 'Username' } })
+  it('should handle createDomainFailure', () => {
+    const newState = createDomainFailure(state)
+    expect(is(newState, state)).toBe(true)
+  })
 
-      expect( newState.get('loggedIn')).toBe(false)
-
+  it('should handle deleteDomainSuccess', () => {
+    const newState = deleteDomainSuccess(state, { payload: 'aa' })
+    const expectedState = fromJS({
+      domains: fromJS([{ id: 'bb' }, { id: 'cc' }]), activeDomain: 'bb'
     })
+    expect(is(newState, expectedState)).toBe(true)
+  })
 
-    it('should handle userStartFetch', () => {
-      const newState = userStartFetch(state )
-      expect( newState.get('fetching')).toBe(true)
+  it('should handle deleteDomainFailure', () => {
+    const newState = createDomainFailure(state)
+    expect(is(newState, state)).toBe(true)
+  })
+
+    expect( Immutable.is(newState, expectedState) ).toBeTruthy()
+
+  })
+
+  it('should handle editDomainSuccess', () => {
+    const payload = { data: 'data', domain: 'aa' }
+    const newState = deleteDomainSuccess(state, { payload })
+    const expectedState = fromJS({
+      activeDomain: 'bb',
+      domains: fromJS([
+        { id: payload.domain, details: payload.data },
+        { id: 'bb' },
+        { id: 'cc' },
+      ])
     })
+    expect(is(newState, expectedState)).toBe(true)
+  })
 
-    it('should handle userFinishFetch', () => {
-      const newState = userFinishFetch(state )
-      expect( newState.get('fetching')).toBe(false)
+  it('should handle editDomainFailure', () => {
+    const newState = editDomainFailure(state)
+    expect(is(newState, state)).toBe(true)
+  })
+
+  it('should handle fetchedAllDomainsSuccess', () => {
+    const newState = fetchedAllDomainsSuccess(state)
+    const expectedState = Immutable.fromJS({
+      currentUser: {},
+      fetching: false
     })
+    expect( Immutable.is(newState, expectedState)).toBeTruthy()
+  })
 
-    it('should handle userTokenChecked -- with payload', () => {
-      const newState = userTokenChecked(state, {payload: {token: 'foorbar'} } )
-      const expectedState = Immutable.fromJS({
-        loggedIn: true
-
-      })
-
-      expect( Immutable.is(newState, expectedState) ).toBeTruthy()
-
+  it('should handle fetchedAllDomainsFailure', () => {
+    const newState = fetchedAllDomainsFailure(state, { payload: [1] })
+    const expectedState = Immutable.fromJS({
+      allUsers: [1],
+      fetching: false
     })
+    expect( Immutable.is(newState, expectedState)).toBeTruthy()
+  })
 
-    it('should handle userTokenChecked -- without payload', () => {
-      const newState = userTokenChecked(state, {payload: null } )
-
-      expect( newState.get('loggedIn')).toBe(false)
+  it('should handle fetchedDomainSuccess', () => {
+    const newState = fetchedDomainSuccess(state)
+    const expectedState = Immutable.fromJS({
+      allUsers: [],
+      fetching: false
     })
+    expect( Immutable.is(newState, expectedState)).toBeTruthy()
+  })
 
-    it('should handle fetchSuccess', () => {
-      const newState = fetchSuccess(state, { payload: {username: 'Username' } })
-      const expectedState = Immutable.fromJS({
-        currentUser: {username: 'Username'}
-      })
-      expect( Immutable.is(newState, expectedState)).toBeTruthy()
+  it('should handle activeDomainChange', () => {
+    const initialState = Immutable.fromJS({
+      allUsers: [
+        {email: 'abc'},
+        {email: 'def'}
+      ],
+      fetching: false
     })
+    const newState = activeDomainChange(initialState, { payload: 'abc' })
+    const expectedState = Immutable.fromJS({
+      allUsers: [
+        {email: 'def'}
+      ],
+      fetching: false
+    })
+    expect( Immutable.is(newState, expectedState)).toBeTruthy()
+  })
 
-    it('should handle fetchFailure', () => {
-      const newState = fetchFailure(state)
-      const expectedState = Immutable.fromJS({
-        currentUser: {},
-        fetching: false
-      })
-      expect( Immutable.is(newState, expectedState)).toBeTruthy()
+  it('should handle fetchedDomainFailure', () => {
+    const initialState = Immutable.fromJS({
+      allUsers: [
+        {email: 'abc'}
+      ],
+      fetching: false
     })
-
-    it('should handle fetchAllSuccess', () => {
-      const newState = fetchAllSuccess(state, { payload: [1] })
-      const expectedState = Immutable.fromJS({
-        allUsers: [1],
-        fetching: false
-      })
-      expect( Immutable.is(newState, expectedState)).toBeTruthy()
+    const newState = fetchedDomainFailure(initialState, { payload: {email: 'def'} })
+    const expectedState = Immutable.fromJS({
+      allUsers: [
+        {email: 'abc'},
+        {email: 'def'}
+      ],
+      fetching: false
     })
-
-    it('should handle fetchAllFailure', () => {
-      const newState = fetchAllFailure(state)
-      const expectedState = Immutable.fromJS({
-        allUsers: [],
-        fetching: false
-      })
-      expect( Immutable.is(newState, expectedState)).toBeTruthy()
-    })
-
-    it('should handle deleteUserSuccess', () => {
-      const initialState = Immutable.fromJS({
-        allUsers: [
-          {email: 'abc'},
-          {email: 'def'}
-        ],
-        fetching: false
-      })
-      const newState = deleteUserSuccess(initialState, { payload: 'abc' })
-      const expectedState = Immutable.fromJS({
-        allUsers: [
-          {email: 'def'}
-        ],
-        fetching: false
-      })
-      expect( Immutable.is(newState, expectedState)).toBeTruthy()
-    })
-
-    it('should handle deleteUserFailure', () => {
-      const initialState = Immutable.fromJS({
-        allUsers: [
-          {email: 'abc'},
-          {email: 'def'}
-        ],
-        fetching: false
-      })
-      const newState = deleteUserFailure(initialState)
-      expect( Immutable.is(newState, initialState)).toBeTruthy()
-    })
-
-    it('should handle createUserSuccess', () => {
-      const initialState = Immutable.fromJS({
-        allUsers: [
-          {email: 'abc'}
-        ],
-        fetching: false
-      })
-      const newState = createUserSuccess(initialState, { payload: {email: 'def'} })
-      const expectedState = Immutable.fromJS({
-        allUsers: [
-          {email: 'abc'},
-          {email: 'def'}
-        ],
-        fetching: false
-      })
-      expect( Immutable.is(newState, expectedState)).toBeTruthy()
-    })
-
-    it('should handle createUserFailure', () => {
-      const initialState = Immutable.fromJS({
-        allUsers: [
-          {email: 'abc'}
-        ],
-        fetching: false
-      })
-      const newState = createUserFailure(initialState)
-      expect( Immutable.is(newState, initialState)).toBeTruthy()
-    })
-
-    it('should handle userNameSave', () => {
-      const newState = userNameSave(state, {payload: 'aaa'})
-      expect( newState.get('username')).toBe('aaa')
-    })
+    expect( Immutable.is(newState, expectedState)).toBeTruthy()
+  })
 })
