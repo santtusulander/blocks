e9faa97aef0af3c7fb99a9327e2a770262538af2
Mika Paulasaari <mika.paulasaari@idean.com>
2016-09-06 16:36:29 -0700

    Add navigation links to network and dashboard
    And hide content link if activeAccount is SP account

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -7,6 +7,8 @@ import {
   getAccountManagementUrlFromParams,
   getAnalyticsUrlFromParams,
   getContentUrlFromParams,
+  getDashboardUrlFromParams,
+  getNetworkUrlFromParams,
   getServicesUrlFromParams,
   getSupportUrlFromParams,
   getSecurityUrlFromParams
@@ -21,9 +23,12 @@ import {VIEW_ACCOUNT_SECTION,
   VIEW_SERVICES_SECTION,
   VIEW_SUPPORT_SECTION} from '../../constants/permissions'
 
+import { ACCOUNT_TYPES } from '../../constants/account-management-options'
+
 import IconAccount from '../icons/icon-account.jsx'
 import IconAnalytics from '../icons/icon-analytics.jsx'
 import IconContent from '../icons/icon-content.jsx'
+import IconDashboard from '../icons/icon-dashboard.jsx'
 import IconServices from '../icons/icon-services.jsx'
 import IconSecurity from '../icons/icon-security.jsx'
 import IconSupport from '../icons/icon-support.jsx'
@@ -39,12 +44,15 @@ const Navigation = (props) => {
   const contentActive = router.isActive(getRoute('content')) ? ' active' : '',
     analyticsActive = router.isActive(getRoute('analytics')) ? ' active' : ''
 
+  const accountType = ACCOUNT_TYPES.find(type => props.activeAccount.get('provider_type') === type.value)
+  const isSP = accountType ? accountType.value === 2 : true
+
   return (
     <nav className='navigation-sidebar text-sm'>
       <ul>
         {/* TODO: “Content" should link to the Account or Group that they looked at last when they navigated in content in this session.
         List view or starburst view, depending which one they used. */}
-        <IsAllowed to={VIEW_CONTENT_SECTION}>
+        <IsAllowed to={VIEW_CONTENT_SECTION} not={isSP}>
           <li>
             <Link to={getContentUrlFromParams(params)} activeClassName="active" className={contentActive}>
               <IconContent />
@@ -52,6 +60,24 @@ const Navigation = (props) => {
             </Link>
           </li>
         </IsAllowed>
+
+        {isSP &&
+          <li>
+            <Link to={getDashboardUrlFromParams(params)} activeClassName="active">
+              <IconDashboard />
+              <span>Dashboard</span>
+            </Link>
+          </li>
+        }
+
+        {isSP &&
+          <li>
+            <Link to={getNetworkUrlFromParams(params)} activeClassName="active">
+              <IconContent />
+              <span>Network</span>
+            </Link>
+          </li>
+        }
 
         {/* Analytics should always default to account level analytics, and not depend on the content leaf. */}
         <IsAllowed to={VIEW_ANALYTICS_SECTION}>
@@ -105,6 +131,7 @@ const Navigation = (props) => {
 
 Navigation.displayName = 'Navigation'
 Navigation.propTypes = {
+  activeAccount: React.PropTypes.instanceOf(Immutable.Map),
   currentUser: React.PropTypes.instanceOf(Immutable.Map),
   params: React.PropTypes.object,
   roles: React.PropTypes.instanceOf(Immutable.List),
