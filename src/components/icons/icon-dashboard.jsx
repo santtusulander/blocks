99d6e90ee9e6645d47acefc8bdb012597ff17f3f
Mika Paulasaari <mika.paulasaari@idean.com>
2016-09-06 14:20:18 -0700

    Add assets for IBC demo pages

---
+++ src/components/icons/icon-dashboard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Icon from '../icon.jsx'
+
+const IconDashboard = (props) => {
+  const {className, height, width} = props
+  return (
+    <Icon className={className} width={width} height={height} viewbox="0 0 36 36">
+      <g>
+        <path d="M8.4,19.1h8.5V8.4H8.4V19.1z M8.4,27.6h8.5v-6.4H8.4V27.6z M19.1,27.6h8.5V16.9h-8.5V27.6z M19.1,8.4v6.4h8.5 V8.4H19.1z"/>
+      </g>
+    </Icon>
+  )
+}
+
+IconDashboard.propTypes = {
+  className: React.PropTypes.string,
+  height: React.PropTypes.number,
+  width: React.PropTypes.number
+}
+
+export default IconDashboard
