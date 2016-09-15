8de003296514b52f35dc31e24c4a252ccd757255
Mikko Alander <mikko.alander@idean.com>
2016-09-15 15:47:06 +0300

    fixed test

--- src/containers/account-management/tabs/__tests__/dns.js
+++ src/containers/account-management/tabs/__tests__/dns.js
@@ -4,11 +4,26 @@ import {shallow} from 'enzyme'
 jest.unmock('../dns.jsx');
 import DNS from '../dns.jsx'
 
+const fetchDomainsMock = () => {
+  return new Promise((resolve, reject) => {
+    process.nextTick(
+      () => resolve(
+        []
+      )
+    )
+  })
+}
+
+const fakeDomains = [];
+const fakeRecords = [];
+
 describe('AccountManagementSystemDNS', () => {
-  it('should exist', () => {
+  it('should exist jee', () => {
     const dns = shallow(
-      <DNS/>
+      <DNS params={{}} fetchDomains={ fetchDomainsMock } domains={ fakeDomains } records={ fakeRecords }/>
     )
     expect(dns.length).toBe(1)
   })
 })
+
+
