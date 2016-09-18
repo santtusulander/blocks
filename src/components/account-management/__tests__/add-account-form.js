f3cd18f48e0fdf6d1fafd29be1b1dfc7d35cbd37
Ville Ristimäki <ville.ristimaki@idean.com>
2016-09-18 23:57:20 +0300

    Refactoring

--- src/components/account-management/__tests__/add-account-form.js
+++ src/components/account-management/__tests__/add-account-form.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { fromJS } from 'immutable'
 import { shallow, mount } from 'enzyme'
 import { reducer as form } from 'redux-form'
-import { createStore } from 'redux'
-import { combineReducers } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import jsdom from 'jsdom'
 
 jest.unmock('../../account-management/account-form.jsx')
@@ -13,17 +11,17 @@ import AccountForm from '../../account-management/account-form.jsx'
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
 global.window = document.defaultView
 
+const intlMaker = () => {
+  return {
+    formatMessage: jest.fn()
+  }
+}
+
 describe('AccountForm', () => {
   const onCancel = jest.fn()
   const onSave = jest.fn()
   let subject, error, props = null
   let touched = false
-
-  const intlMaker = () => {
-    return {
-      formatMessage: jest.fn()
-    }
-  }
 
   beforeEach(() => {
     subject = () => {
@@ -71,111 +69,57 @@ describe('AccountForm', () => {
 })
 
 describe('Connected AccountForm', () => {
-  const onCancel = jest.fn()
-  const onSave = jest.fn()
-  const store = createStore(combineReducers({ form }))
-  let subject, error, props = null
+  let store, subject, error, props = null
   let touched = false
 
-  const intlMaker = () => {
-    return {
-      formatMessage: jest.fn()
-    }
-  }
+  const onCancel = jest.fn()
+  const onSave = jest.fn()
 
   beforeEach(() => {
-    subject = () => {
-      props = {
-        onCancel,
-        onSave,
-        store,
-        intl: intlMaker(),
-        fields: {
-          accountName: { touched, error, value: '' },
-          accountBrand: { touched, error, value: '' },
-          accountType: { touched, error, value: '' },
-          services: { touched, error, value: '' },
-        }
+    store = createStore(combineReducers({ form }), () => form)
+    props = {
+      show: true,
+      store,
+      intl: intlMaker(),
+      fields: {
+        accountName: { touched, error, value: '' },
+        accountBrand: { touched, error, value: '' },
+        accountType: { touched, error, value: '' },
+        services: { touched, error, value: '' },
+      },
+      initialValues: {
+        accountName: '',
+        accountBrand: '',
+        accountType: '',
+        services: ''
       }
-      return shallow(<AccountForm {...props}/>)
     }
   })
-})
-// describe('Connected AccountForm', () => {
-//   const onCancel = jest.fn()
-//   const onSave = jest.fn()
-//   let subject, error, props = null
-//   let store = null
-//   let touched = false
-//
-//   const intlMaker = () => {
-//     return {
-//       formatMessage: jest.fn()
-//     }
-//   }
-//
-//   beforeEach(() => {
-//     store = createStore(combineReducers({ form }))
-//     props = {
-//       onCancel,
-//       onSave,
-//       intl: intlMaker(),
-//       fields: {
-//         accountName: { touched, error, value: '' },
-//         accountBrand: { touched, error, value: '' },
-//         accountType: { touched, error, value: '' },
-//         services: { touched, error, value: '' },
-//       }
-//     }
-//   })
-//
-//   describe('Blank input errors', () => {
-//     beforeEach(() => {
-//       subject = mount(<AccountForm {...props}/>)
-//     })
-//     it('shows error message when domain name is blank', () => {
-//       const input = subject.find('#domain_name .soa-form-input')
-//       input.simulate('blur')
-//       expect(subject.find('#domain_name .error-msg').text()).toBe(REQUIRED)
-//     })
-//   })
-// })
 
-//   describe("Validation errors", () => {
-//     let store = null
-//     let props = null
-//     let subject = null
-//     beforeEach(() => {
-//       store = createStore(combineReducers({form}))
-//       props = {
-//         activeDomain: fromJS({}),
-//         store,
-//         initialValues: {
-//           domainName: 'aaa',
-//           nameServer: 'bbb',
-//           personResponsible: 'ccc',
-//           refresh: 'ddd',
-//           zoneSerialNumber: 'eee'
-//         }
-//       }
-//       subject = mount(<FormContainer {...props}/>)
-//     })
-//     it("shows error message when responsible person is invalid", () => {
-//       const input = subject.find('#responsible_person_mailbox .soa-form-input')
-//       input.simulate('blur')
-//       expect(subject.find('#responsible_person_mailbox .error-msg').text()).toBe(INVALID_INPUT)
-//     })
-//
-//     it("shows error message when zone serial # is invalid", () => {
-//       const input = subject.find('#zone_serial_number .soa-form-input')
-//       input.simulate('blur')
-//       expect(subject.find('#zone_serial_number .error-msg').text()).toBe(INVALID_INPUT)
-//     })
-//
-//     it("shows error message when refresh is invalid", () => {
-//       const input = subject.find('#refresh .soa-form-input').first()
-//       input.simulate('blur')
-//       expect(subject.find('#refresh .error-msg').text()).toBe(INVALID_INPUT)
-//     })
-//   })
-// })
+  describe('Blank input errors', () => {
+
+    beforeEach(() => {
+      subject = mount(<AccountForm {...props}/>)
+    })
+
+    it('shows error message when account name is blank', () => {
+      const input = subject.find('input').at(0)
+      input.simulate('blur')
+      // expect(subject.find('.error-msg', input).text()).toBe('')
+    })
+  })
+
+  // describe('Validation errors', () => {
+  //
+  //   beforeEach(() => {
+  //     subject = mount(<AccountForm {...props}/>)
+  //   })
+  //
+  //   it('Shows error message when account name is not valid', () => {
+  //     const input = subject.find('input').at(0)
+  //     input.simulate('blur')
+  //     // expect(subject.find('.error-msg', input).text()).toBe('')
+  //   })
+  // })
+
+})
