2d9fd7b23683539140a6e0875e91c52562896a3f
santtu-idean <santtu.sulander@idean.com>
2016-09-21 10:54:44 +0300

    Add id-prop to active account heading. correct beforeEach-function

--- src/components/security/security-page-header.jsx
+++ src/components/security/security-page-header.jsx
@@ -6,7 +6,9 @@ import PageHeader from '../layout/page-header'
 const SecurityPageHeader = ({ activeAccount, intl }) => {
   return (
     <PageHeader pageSubTitle={<FormattedMessage id="portal.security.header.text"/>}>
-      <h1>{activeAccount || intl.formatMessage({id: 'portal.account.manage.selectAccount.text'})}</h1>
+      <h1 id="active-account">
+        {activeAccount || intl.formatMessage({id: 'portal.account.manage.selectAccount.text'})}
+      </h1>
     </PageHeader>
   )
 }
