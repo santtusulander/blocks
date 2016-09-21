6217e3eeed0a7b3fa9f14d5e8680d00b257a28e0
santtu-idean <santtu.sulander@idean.com>
2016-09-21 12:28:24 +0300

    hide tools-tab

--- src/containers/support/support.jsx
+++ src/containers/support/support.jsx
@@ -48,11 +48,11 @@ class Support extends React.Component {
               <FormattedMessage id="portal.support.tabs.TICKETS.text"/>
             </Link>
           </li>
-          <li className="navbar">
+          {/*<li className="navbar">
             <Link to={baseUrl + '/tools'} activeClassName="active">
               <FormattedMessage id="portal.support.tabs.TOOLS.text"/>
             </Link>
-          </li>
+          </li>*/}
           <li className="navbar">
             <Link to={baseUrl + '/documentation'} activeClassName="active">
               <FormattedMessage id="portal.support.tabs.DOCUMENTATION.text"/>
