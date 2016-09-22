82c0e32c30253123b98497305c7f2855516cf156
Taaha Chaudhry <taaha.chaudhry@idean.com>
2016-09-22 14:58:41 -0700

    Deleted commented line

--- src/components/account-management/system/accounts.jsx
+++ src/components/account-management/system/accounts.jsx
@@ -148,7 +148,6 @@ class AccountList extends Component {
         content: <FormattedMessage id='portal.account.manage.unsavedChanges.warning.content'/>,
         stayButton: this.props.uiActions.hideInfoDialog,
         continueButton: () => {
-          //this.leavePage()
           this.isLeaving = true
           this.props.router.push(pathname)
           this.props.uiActions.hideInfoDialog()
