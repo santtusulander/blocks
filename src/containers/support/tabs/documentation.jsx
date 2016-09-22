a75229c0fa9b3e20987fac06f78e34696d15cd4d
Jeremy White <jeremy.white@idean.com>
2016-09-21 23:18:09 -0700

    UDNP-1385 — Add documentation PDFs

--- src/containers/support/tabs/documentation.jsx
+++ src/containers/support/tabs/documentation.jsx
@@ -1,25 +1,56 @@
-import React from 'react'
-
+import React, { PropTypes } from 'react'
+import { FormattedMessage } from 'react-intl'
+import { fromJS } from 'immutable'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
+import {
+  ROLES_MAPPING
+} from '../../../constants/account-management-options'
 
 class SupportTabDocumentation extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
+    const { currentUserRole } = this.props
+    const role = fromJS(ROLES_MAPPING).find(obj => obj.get('id') === currentUserRole)
+    const accountType = role.get('accountTypes').get(0)
+
+    let documentPath = ""
+
+    // TODO: replace magic numbers with enums once they're merged
+    switch (accountType) {
+      case 1:
+        documentPath = "/assets/pdf/CP_User_Guide.pdf"
+        break
+      case 2:
+        documentPath = "/assets/pdf/SP_User_Guide.pdf"
+        break
+      case 3:
+        documentPath = "/assets/pdf/UDN_Admin_Guide.pdf"
+        break
+    }
+
     return (
       <div className="account-support-documentation">
-
+        <p><FormattedMessage id="portal.support.documentation.body.text" /></p>
+        <p>
+          <a href={documentPath} target="_blank">
+            <FormattedMessage id="portal.support.documentation.body.link" />
+          </a>
+        </p>
       </div>
     )
   }
 }
 
-SupportTabDocumentation.propTypes = {
-
+function mapStateToProps(state) {
+  return {
+    currentUserRole: state.user.get('currentUser').get('roles').get(0)
+  };
 }
 
-SupportTabDocumentation.defaultProps = {}
+SupportTabDocumentation.displayName = 'SupportTabDocumentation'
+SupportTabDocumentation.propTypes = {
+  currentUserRole: PropTypes.number,
+  documentPath: PropTypes.string
+}
 
-
-export default SupportTabDocumentation;
+export default connect(mapStateToProps)(SupportTabDocumentation)
