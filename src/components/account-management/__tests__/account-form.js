f364f0ff8ed3b4f8ea0b55d3d76c50b98a3f13f1
Mika Paulasaari <mika.paulasaari@idean.com>
2016-09-27 15:21:13 -0700

    Fix account, account-form and users tests
    Added polyfill for Array.includes function

--- src/components/account-management/__tests__/account-form.js
+++ src/components/account-management/__tests__/account-form.js
@@ -4,12 +4,39 @@ import { reducer as form } from 'redux-form'
 import { createStore, combineReducers } from 'redux'
 import jsdom from 'jsdom'
 
-jest.unmock('../../select-wrapper.jsx')
-jest.unmock('../../checkboxes.jsx')
-jest.unmock('../../account-management/account-form.jsx')
+if (![].includes) {
+  Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+    'use strict';
+    var O = Object(this);
+    var len = parseInt(O.length) || 0;
+    if (len === 0) {
+      return false;
+    }
+    var n = parseInt(arguments[1]) || 0;
+    var k;
+    if (n >= 0) {
+      k = n;
+    } else {
+      k = len + n;
+      if (k < 0) {k = 0;}
+    }
+    var currentElement;
+    while (k < len) {
+      currentElement = O[k];
+      if (searchElement === currentElement ||
+         (searchElement !== searchElement && currentElement !== currentElement)) {
+        return true;
+      }
+      k++;
+    }
+    return false;
+  };
+}
+
+jest.unmock('../account-form.jsx')
 jest.genMockFromModule('react-bootstrap')
 
-import AccountForm from '../../account-management/account-form.jsx'
+import AccountForm from '../account-form.jsx'
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
 global.window = document.defaultView
