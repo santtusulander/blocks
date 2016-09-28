ef6f178a7ad42304d85a7f9aa6b65b0c5488811c
Simo Jokela <simo.jokela@idean.com>
2016-09-28 12:44:02 +0300

    Changed tests to use enzyme instead of react test utils

--- src/components/analysis/__tests__/stacked-by-time.js
+++ src/components/analysis/__tests__/stacked-by-time.js
@@ -1,16 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import TestUtils from 'react-addons-test-utils'
+import { shallow } from 'enzyme'
 
-jest.autoMockOff()
 jest.unmock('../stacked-by-time.jsx')
-const AnalysisStackedByTime = require('../stacked-by-time.jsx')
+jest.unmock('moment')
+jest.unmock('numeral')
 
-// Set up mocks to make sure formatting libs are used correctly
-jest.mock('numeral')
-const numeral = require('numeral')
-const numeralFormatMock = jest.fn(() => 'number')
-numeral.mockReturnValue({ format: numeralFormatMock })
+import AnalysisStackedByTime from '../stacked-by-time'
 
 const fakeData = [
   [
@@ -37,57 +32,40 @@ const fakeData = [
 
 describe('AnalysisStackedByTime', () => {
   it('should exist', () => {
-    let stacks = TestUtils.renderIntoDocument(
+    const wrapper = shallow(
       <AnalysisStackedByTime />
-    );
-    expect(TestUtils.isCompositeComponent(stacks)).toBeTruthy();
+    )
+    expect(wrapper.length).toBe(1);
   });
 
   it('can be passed a custom css class', () => {
-    let stacks = TestUtils.renderIntoDocument(
+    const wrapper = shallow(
       <AnalysisStackedByTime className="foo" width={400} height={200} padding={10}
         dataSets={fakeData}/>
-    );
-    let div = TestUtils.findRenderedDOMComponentWithTag(stacks, 'div');
-    expect(ReactDOM.findDOMNode(div).className).toContain('foo');
+    )
+    expect(wrapper.hasClass('foo')).toBeTruthy();
   });
 
   it('should show loading message if there is no width or data', () => {
-    let stacks = TestUtils.renderIntoDocument(
+    const wrapper = shallow(
       <AnalysisStackedByTime/>
-    );
-    let div = TestUtils.findRenderedDOMComponentWithTag(stacks, 'div')
-    expect(div.textContent).toContain('Loading');
+    )
+    expect(wrapper.text()).toContain('Loading');
   });
 
   it('should have data lines', () => {
-    let stacks = TestUtils.renderIntoDocument(
+    const wrapper = shallow(
       <AnalysisStackedByTime width={400} height={200} padding={10} dataSets={fakeData}/>
-    );
-    let lines = TestUtils.scryRenderedDOMComponentsWithTag(stacks, 'line')
+    )
+    const lines = wrapper.find('line')
     expect(lines.length).toBe(4);
   });
 
   it('should have an x axis', () => {
-    let stacks = TestUtils.renderIntoDocument(
+    const wrapper = shallow(
       <AnalysisStackedByTime width={400} height={200} padding={10} dataSets={fakeData}/>
-    );
-    let texts = TestUtils.scryRenderedDOMComponentsWithTag(stacks, 'text')
-    expect(texts[0].getAttribute('x')).toBe('20')
-    expect(texts[0].textContent).toBeGreaterThan('01:00')
-  });
-
-  it('should have a y axis', () => {
-     numeral.mockClear()
-     numeralFormatMock.mockClear()
-    let stacks = TestUtils.renderIntoDocument(
-      <AnalysisStackedByTime width={400} height={200} padding={10} dataSets={fakeData}/>
-    );
-    let texts = TestUtils.scryRenderedDOMComponentsWithTag(stacks, 'text')
-
-    expect(texts[13].textContent).toBe('number')
-    expect(numeral.mock.calls.length).toBe(2)
-    expect(numeral.mock.calls[0]).toEqual([500])
-    expect(numeralFormatMock.mock.calls[0][0]).toBe('0 a')
+    )
+    const texts = wrapper.find('text')
+    expect(texts.first().text()).toBeGreaterThan('01:00')
   });
 });
