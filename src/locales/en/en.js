ffff25dd0155e665a95d2bd04f0edf67f17f8a7d
santtu-idean <santtu.sulander@idean.com>
2016-09-28 12:42:49 +0300

    Add localized strings, don't extend styles from modal form panel to have space between inputs

--- src/locales/en/en.js
+++ src/locales/en/en.js
@@ -89,6 +89,9 @@ const TRANSLATED_MESSAGES = {
   'portal.group.edit.name.required.text': 'Group name is required',
   'portal.group.edit.name.label': 'Group Name',
   'portal.group.edit.name.enter.text': 'Enter Group Name',
+  'portal.group.edit.charge_id.label': 'Billing ID',
+  'portal.group.edit.charge_id.enter.text': 'Enter Billing ID',
+  'portal.group.edit.charge_model.label': 'Billing Mode',
   'portal.group.edit.editGroup.title': 'Edit Group',
   'portal.group.edit.newGroup.title': 'Add new group',
 
