51eca6e9b13204d5cbb8b947026faa63223febcf
Mikko Alander <mikko.alander@idean.com>
2016-09-30 16:38:06 +0300

    remove - "array-includes" -plugin is taking care of this

--- src/components/account-management/__tests__/account-form.js
+++ src/components/account-management/__tests__/account-form.js
@@ -4,35 +4,6 @@ import { reducer as form } from 'redux-form'
 import { createStore, combineReducers } from 'redux'
 import jsdom from 'jsdom'
 
-if (![].includes) {
-  Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
-    'use strict';
-    var O = Object(this);
-    var len = parseInt(O.length) || 0;
-    if (len === 0) {
-      return false;
-    }
-    var n = parseInt(arguments[1]) || 0;
-    var k;
-    if (n >= 0) {
-      k = n;
-    } else {
-      k = len + n;
-      if (k < 0) {k = 0;}
-    }
-    var currentElement;
-    while (k < len) {
-      currentElement = O[k];
-      if (searchElement === currentElement ||
-         (searchElement !== searchElement && currentElement !== currentElement)) {
-        return true;
-      }
-      k++;
-    }
-    return false;
-  };
-}
-
 jest.unmock('../account-form.jsx')
 jest.genMockFromModule('react-bootstrap')
 
