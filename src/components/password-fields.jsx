ed99d0a944cce23a1f59611d4c642c4d43b401cc
Taaha Chaudhry <taaha.chaudhry@idean.com>
2016-10-03 14:15:46 -0700

    UDNP-1441: Updated password-validation component to password-fields, which includes password validation from set-password – Refactored set-password component – Added localization to password-fields

---
+++ src/components/password-fields.jsx
@@ -0,0 +1,194 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl'
+import { Input, Tooltip } from 'react-bootstrap'
+
+import IconPassword from '../components/icons/icon-password.jsx'
+import IconEye from '../components/icons/icon-eye.jsx'
+
+import { injectIntl } from 'react-intl';
+
+class PasswordFields extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      password: '',
+      passwordActive: false,
+      passwordError: null,
+      passwordFocus: false,
+      passwordValid: false,
+      confirm: '',
+      confirmActive: false,
+      confirmFocus: false,
+      confirmValid: false,
+      confirmVisible: false,
+      passwordLengthValid: false,
+      passwordUppercaseValid: false,
+      passwordNumberValid: false,
+      passwordSpecialCharValid: false
+    }
+
+    this.togglePasswordVisibility = this.togglePasswordVisibility.bind(this)
+    this.passwordFocus = this.passwordFocus.bind(this)
+    this.changePassword = this.changePassword.bind(this)
+    this.toggleConfirmVisibility = this.toggleConfirmVisibility.bind(this)
+    this.confirmFocus = this.confirmFocus.bind(this)
+    this.changeConfirm = this.changeConfirm.bind(this)
+    this.changeField = this.changeField.bind(this)
+    this.validatePassword = this.validatePassword.bind(this)
+    this.validatePasswordLengthValid = this.validatePasswordLengthValid.bind(this)
+    this.validatePasswordUppercaseValid = this.validatePasswordUppercaseValid.bind(this)
+    this.validatePasswordNumberValid = this.validatePasswordNumberValid.bind(this)
+    this.validatePasswordSpecialCharValid = this.validatePasswordSpecialCharValid.bind(this)
+  }
+
+  togglePasswordVisibility() {
+    this.setState({
+      passwordVisible: !this.state.passwordVisible
+    })
+  }
+
+  passwordFocus(hasFocus) {
+    return () => {
+      this.setState({
+        passwordActive: hasFocus || !this.state.password,
+        passwordFocus: hasFocus
+      })
+    }
+  }
+
+  changePassword(e) {
+    this.changeField('password')(e)
+    this.setState({
+      passwordValid: this.validatePassword(e.target.value),
+      passwordLengthValid: this.validatePasswordLengthValid(e.target.value),
+      passwordUppercaseValid: this.validatePasswordUppercaseValid(e.target.value),
+      passwordNumberValid: this.validatePasswordNumberValid(e.target.value),
+      passwordSpecialCharValid: this.validatePasswordSpecialCharValid(e.target.value)
+    })
+  }
+
+  toggleConfirmVisibility() {
+    this.setState({
+      confirmVisible: !this.state.confirmVisible
+    })
+  }
+
+  confirmFocus(hasFocus) {
+    return () => {
+      this.setState({
+        confirmActive: hasFocus || !this.state.confirm,
+        confirmFocus: hasFocus
+      })
+    }
+  }
+
+  changeConfirm(e) {
+    this.changeField('confirm')(e)
+    if(this.state.passwordValid) {
+      this.setState({
+        confirmValid: this.state.password === e.target.value
+      })
+    }
+  }
+
+  changeField(key) {
+    return e => {
+      const newState = {}
+      newState[key] = e.target.value
+      this.setState(newState)
+    }
+  }
+
+  validatePassword(password) {
+    return password.match(/^(?=.*\d)(?=.*[A-Z])(?=.*[ !"#$%&'()*+,-.\/:;<=>?@\[\\\]^_`{|}~])[0-9a-zA-Z !"#$%&'()*+,-.\/:;<=>?@\[\\\]^_`{|}~]{8,}$/) !== null
+  }
+
+  validatePasswordLengthValid(password) {
+    return password.match(/([0-9a-zA-Z !"#$%&'()*+,-.\/:;<=>?@\[\\\]^_`{|}~]{8,})/) !== null
+  }
+
+  validatePasswordUppercaseValid(password) {
+    return password.match(/([A-Z]+)/) !== null
+  }
+
+  validatePasswordNumberValid(password) {
+    return password.match(/([0-9]+)/) !== null
+  }
+
+  validatePasswordSpecialCharValid(password) {
+    return password.match(/([ !"#$%&'()*+,-.\/:;<=>?@\[\\\]^_`{|}~]+)/) !== null
+  }
+
+  render() {
+    const { loginPassword, intl } = this.props
+    const showPasswordRequirements = this.state.passwordFocus && !this.state.passwordValid
+    const showPasswordError = !this.state.passwordValid && !this.state.passwordFocus && this.state.password !== ''
+    const showConfirmError = this.state.passwordValid && !this.state.confirmValid && !this.state.confirmFocus && this.state.confirm !== ''
+
+    return (
+      <div>
+        {showPasswordRequirements ?
+          <Tooltip id="password-requirements" placement="top" className="input-tooltip interactive-password-tooltip in">
+            <span>Requirements:</span><br/>
+            <span className={this.state.passwordLengthValid ? 'valid' : ''}>- <FormattedMessage id="portal.password.passwordLengthValid.text"/></span><br/>
+            <span className={this.state.passwordUppercaseValid ? 'valid' : ''}>- <FormattedMessage id="portal.password.passwordUppercaseValid.text"/></span><br/>
+            <span className={this.state.passwordNumberValid ? 'valid' : ''}>- <FormattedMessage id="portal.password.passwordNumberValid.text"/></span><br/>
+            <span className={this.state.passwordSpecialCharValid ? 'valid' : ''}>- <FormattedMessage id="portal.password.passwordSpecialCharValid.text"/></span>
+          </Tooltip>
+        : null}
+
+        <Input id="password"
+          type={this.state.passwordVisible ? 'text' : 'password'}
+          placeholder={!loginPassword ? intl.formatMessage({id: 'portal.user.edit.newPassword.text'}) : ''}
+          wrapperClassName={loginPassword && ('input-addon-before input-addon-after-outside '
+            + 'has-login-label login-label-password'
+            + (showPasswordError ? ' invalid' : '')
+            + (this.state.passwordValid ? ' valid' : '')
+            + (this.state.passwordFocus || this.state.password ? ' active' : ''))}
+          addonBefore={loginPassword && <IconPassword/>}
+          addonAfter={loginPassword && <a className={'input-addon-link' +
+              (this.state.passwordVisible ? ' active' : '')}
+              onClick={this.togglePasswordVisibility}>
+                <IconEye/>
+            </a>}
+          onFocus={loginPassword && this.passwordFocus(true)}
+          onBlur={loginPassword && this.passwordFocus(false)}
+          value={this.state.password}
+          onChange={this.changePassword} />
+
+        <Input id="confirm"
+          type={this.state.confirmVisible ? 'text' : 'password'}
+          wrapperClassName={loginPassword && ('input-addon-before input-addon-after-outside '
+            + 'has-login-label login-label-confirm'
+            + (showConfirmError ? ' invalid' : '')
+            + (this.state.confirmValid && this.state.confirm !== '' ? ' valid' : '')
+            + (this.state.confirmFocus || this.state.confirm ? ' active' : ''))}
+          addonBefore={loginPassword && <IconPassword/>}
+          addonAfter={loginPassword && <a className={'input-addon-link' +
+              (this.state.confirmVisible ? ' active' : '')}
+              onClick={this.toggleConfirmVisibility}>
+                <IconEye/>
+            </a>}
+          onFocus={loginPassword && this.confirmFocus(true)}
+          onBlur={loginPassword && this.confirmFocus(false)}
+          value={this.state.confirm}
+          onChange={this.changeConfirm} />
+
+        {showConfirmError ?
+          <Tooltip id="confirm-error" placement="bottom" className="input-tooltip in">
+            <FormattedMessage id="portal.password.passwordDoNotMatch.text"/>
+          </Tooltip>
+        : null}
+      </div>
+    )
+  }
+}
+
+PasswordFields.displayName = 'PasswordFields'
+PasswordFields.propTypes = {
+  intl: React.PropTypes.object,
+  loginPassword: React.PropTypes.bool
+};
+
+export default injectIntl(PasswordFields)
--- src/containers/set-password.jsx
+++ src/containers/set-password.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Button, Col, Input, Modal, Row, Tooltip } from 'react-bootstrap'
+import { FormattedMessage } from 'react-intl'
+import { Button, Col, Modal, Row } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import { bindActionCreators } from 'redux'
@@ -8,48 +9,19 @@ import { getContentUrl } from '../util/routes'
 
 import * as userActionCreators from '../redux/modules/user'
 
-import PasswordValidation from '../components/password-validation'
-import IconPassword from '../components/icons/icon-password.jsx'
-import IconEye from '../components/icons/icon-eye.jsx'
+import PasswordFields from '../components/password-fields'
 
-import { FormattedMessage } from 'react-intl'
 
 export class SetPassword extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      confirm: '',
-      confirmActive: false,
-      confirmFocus: false,
-      confirmValid: false,
-      confirmVisible: false,
-      password: '',
-      passwordActive: false,
-      passwordError: null,
-      passwordFocus: false,
-      passwordValid: false,
-      passwordLengthValid: false,
-      passwordUppercaseValid: false,
-      passwordNumberValid: false,
-      passwordSpecialCharValid: false,
-      passwordVisible: false
+      validPassword: false
     }
 
-    this.toggleConfirmVisibility = this.toggleConfirmVisibility.bind(this)
-    this.togglePasswordVisibility = this.togglePasswordVisibility.bind(this)
-    this.confirmFocus = this.confirmFocus.bind(this)
-    this.passwordFocus = this.passwordFocus.bind(this)
-    this.changeField = this.changeField.bind(this)
-    this.changeConfirm = this.changeConfirm.bind(this)
-    this.changePassword = this.changePassword.bind(this)
     this.goToLoginPage = this.goToLoginPage.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
-    this.validatePassword = this.validatePassword.bind(this)
-    this.validatePasswordLengthValid = this.validatePasswordLengthValid.bind(this)
-    this.validatePasswordUppercaseValid = this.validatePasswordUppercaseValid.bind(this)
-    this.validatePasswordNumberValid = this.validatePasswordNumberValid.bind(this)
-    this.validatePasswordSpecialCharValid = this.validatePasswordSpecialCharValid.bind(this)
   }
   goToLoginPage() {
     this.props.router.push(getContentUrl('/login', {}))
@@ -73,85 +45,8 @@ export class SetPassword extends React.Component {
     //   }
     // })
   }
-  toggleConfirmVisibility() {
-    this.setState({
-      confirmVisible: !this.state.confirmVisible
-    })
-  }
-  togglePasswordVisibility() {
-    this.setState({
-      passwordVisible: !this.state.passwordVisible
-    })
-  }
-  confirmFocus(hasFocus) {
-    return () => {
-      this.setState({
-        confirmActive: hasFocus || !this.state.confirm,
-        confirmFocus: hasFocus
-      })
-    }
-  }
-  passwordFocus(hasFocus) {
-    return () => {
-      this.setState({
-        passwordActive: hasFocus || !this.state.password,
-        passwordFocus: hasFocus
-      })
-    }
-  }
-  changeConfirm(e) {
-    this.changeField('confirm')(e)
-    if(this.state.passwordValid) {
-      this.setState({
-        confirmValid: this.state.password === e.target.value
-      })
-    }
-  }
-  changePassword(e) {
-    this.changeField('password')(e)
-    this.setState({
-      passwordValid: this.validatePassword(e.target.value),
-      passwordLengthValid: this.validatePasswordLengthValid(e.target.value),
-      passwordUppercaseValid: this.validatePasswordUppercaseValid(e.target.value),
-      passwordNumberValid: this.validatePasswordNumberValid(e.target.value),
-      passwordSpecialCharValid: this.validatePasswordSpecialCharValid(e.target.value)
-    })
-  }
-  changeField(key) {
-    return e => {
-      const newState = {}
-      newState[key] = e.target.value
-      this.setState(newState)
-    }
-  }
-
-  /* Regex explanation
-    /^([0-9a-zA-Z\ ~\!@\#\$\-_]{8,})$/     should contain at least 8 from the mentioned characters
-    /([A-Z]+)/                             should contain at least one uppercase
-    /([0-9]+)/                             should contain at least one digit
-    ([?!@#\$%\^\&*\)\(\/\\\<\>\ ~`+=._-]+) should contain at least one special character
-  */
-  validatePassword(password) {
-    return password.match(/^(?=.*\d)(?=.*[A-Z])(?=.*[\ ~\?!@\#\$\-_])[0-9a-zA-Z\ ~\?!@\#\$\-_]{8,}$/) !== null
-  }
-  validatePasswordLengthValid(password) {
-    return password.match(/([0-9a-zA-Z\ ~\?!@\#\$\-_]{8,})/) !== null
-  }
-  validatePasswordUppercaseValid(password) {
-    return password.match(/([A-Z]+)/) !== null
-  }
-  validatePasswordNumberValid(password) {
-    return password.match(/([0-9]+)/) !== null
-  }
-  validatePasswordSpecialCharValid(password) {
-    return password.match(/([\ ~\?!@\#\$\-_]+)/) !== null
-  }
 
   render() {
-    const showPasswordRequirements = this.state.passwordFocus && !this.state.passwordValid
-    const showConfirmError = this.state.passwordValid && !this.state.confirmValid && !this.state.confirmFocus
-    // const showConfirmError = this.state.passwordValid && this.state.confirm && this.state.confirmValid && !this.state.confirmFocus
-
     return (
       <Modal.Dialog className="login-modal">
 
@@ -166,64 +61,11 @@ export class SetPassword extends React.Component {
 
         <Modal.Body>
           <form onSubmit={this.onSubmit}>
-            <PasswordValidation loginPassword={true} />
-
-            {showPasswordRequirements ?
-              <Tooltip id="password-requirements" placement="top" className="input-tooltip interactive-password-tooltip in">
-                <span>Requirements:</span><br/>
-                <span className={this.state.passwordLengthValid ? 'valid' : ''}>- at least 8 characters</span><br/>
-                <span className={this.state.passwordUppercaseValid ? 'valid' : ''}>- at least one uppercase character</span><br/>
-                <span className={this.state.passwordNumberValid ? 'valid' : ''}>- at least one number</span><br/>
-                <span className={this.state.passwordSpecialCharValid ? 'valid' : ''}>- at least one special character (!,?,#,$,...)</span>
-              </Tooltip>
-            : null}
-
-            <Input id="password"
-              type={this.state.passwordVisible ? 'text' : 'password'}
-              wrapperClassName={'input-addon-before input-addon-after-outside '
-                + 'has-login-label login-label-password'
-                + (!this.state.passwordValid && !this.state.passwordFocus && this.state.password !== '' ? ' invalid' : '')
-                + (this.state.passwordValid ? ' valid' : '')
-                + (this.state.passwordFocus || this.state.password ? ' active' : '')}
-              addonBefore={<IconPassword/>}
-              addonAfter={<a className={'input-addon-link' +
-                  (this.state.passwordVisible ? ' active' : '')}
-                  onClick={this.togglePasswordVisibility}>
-                    <IconEye/>
-                </a>}
-              onFocus={this.passwordFocus(true)}
-              onBlur={this.passwordFocus(false)}
-              value={this.state.password}
-              onChange={this.changePassword}/>
-
-            <Input id="confirm"
-              type={this.state.confirmVisible ? 'text' : 'password'}
-              wrapperClassName={'input-addon-before input-addon-after-outside '
-                + 'has-login-label login-label-confirm'
-                + (showConfirmError && this.state.confirm !== '' ? ' invalid' : '')
-                + (this.state.confirmValid && this.state.confirm !== '' ? ' valid' : '')
-                + (this.state.confirmFocus || this.state.confirm ? ' active' : '')}
-              addonBefore={<IconPassword/>}
-              addonAfter={<a className={'input-addon-link' +
-                  (this.state.confirmVisible ? ' active' : '')}
-                  onClick={this.toggleConfirmVisibility}>
-                    <IconEye/>
-                </a>}
-              onFocus={this.confirmFocus(true)}
-              onBlur={this.confirmFocus(false)}
-              value={this.state.confirm}
-              onChange={this.changeConfirm}/>
-
-            {showConfirmError && this.state.confirm !== '' ?
-              <Tooltip id="confirm-error" placement="bottom" className="input-tooltip in">
-                <FormattedMessage id="portal.passsword.passwordDoNotMatch.text"/>
-              </Tooltip>
-            : null}
-
+            <PasswordFields loginPassword={true} />
             <Row>
               <Col xs={12}>
                 <Button type="submit" bsStyle="primary" className="pull-right"
-                  disabled={this.props.fetching || !this.state.confirmValid}>
+                  disabled={this.props.fetching || !this.state.validPassword}>
                   {this.props.reset ?
                     this.props.fetching ? <FormattedMessage id="portal.button.resetting"/> : <FormattedMessage id="portal.button.reset"/>
                   : this.props.fetching ? <FormattedMessage id="portal.button.setting"/> : <FormattedMessage id="portal.button.set"/>}
