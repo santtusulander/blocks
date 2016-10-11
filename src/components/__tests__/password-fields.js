4b697615c6010b28fa77b264591c3867a5a9338a
Taaha Chaudhry <taaha.chaudhry@idean.com>
2016-10-11 14:00:03 -0700

    UDNP-1441: Added test to check if the password and confirmation states update

--- src/components/__tests__/password-fields.js
+++ src/components/__tests__/password-fields.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { shallow } from 'enzyme'
 
-jest.autoMockOff()
+// jest.autoMockOff()
 jest.dontMock('../password-fields.jsx')
 const PasswordFields = require('../password-fields.jsx').PasswordFields
 
@@ -15,6 +15,48 @@ describe('PasswordFields', () => {
   it('should exist', () => {
     const passwordFields = shallow(<PasswordFields intl={intlMaker()}/>)
     expect(passwordFields).toBeDefined()
-    // expect(passwordFields.find('PasswordFields').length).toBe(1)
+  })
+
+  it('updates password and confirmation state', () => {
+    const passwordFields = shallow(<PasswordFields intl={intlMaker()}/>)
+    let inputs = passwordFields.find('input')
+    inputs.at(0).simulate('change', {target: {value: 'aaa'}})
+    inputs.at(1).simulate('change', {target: {value: 'bbb'}})
+    expect(passwordFields.state('password')).toBe('aaa')
+    expect(passwordFields.state('confirm')).toBe('bbb')
+  })
+
+  xit('can be passed a stackedPassword prop for a stacked input layout', () => {
+    const passwordFields = shallow(<PasswordFields intl={intlMaker()} stackedPassword={true}/>)
+
+  })
+
+  xit('can be passed a inlinePassword prop for an inline input layout', () => {
+    const passwordFields = shallow(<PasswordFields intl={intlMaker()} inlinePassword={true}/>)
+
+  })
+
+  xit('shows an interactive password requirement tooltip', () => {
+
+  })
+
+  xit('validates the password length requirement', () => {
+
+  })
+
+  xit('validates the password uppercase requirement', () => {
+
+  })
+
+  xit('validates the password number requirement', () => {
+
+  })
+
+  xit('validates the password special character requirement', () => {
+
+  })
+
+  xit('validates the password with all the requirements', () => {
+
   })
 })
