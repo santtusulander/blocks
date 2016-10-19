6dd4d559dbf8d1801d1cff6f84d76d06c513cdf6
Taaha Chaudhry <taaha.chaudhry@idean.com>
2016-10-19 14:08:37 -0700

    UDNP-1104: Added buttons to side panel footer with button props – added base styling to side panel from modal windows

--- src/components/side-panel.jsx
+++ src/components/side-panel.jsx
@@ -1,7 +1,10 @@
 import React, { PropTypes } from 'react'
-import { Modal } from 'react-bootstrap'
+import { Button, ButtonToolbar, Modal } from 'react-bootstrap'
+import { FormattedMessage } from 'react-intl'
 
-const SidePanel = ({ children, show, subTitle, title }) => {
+// import keyStrokeSupport from '../decorators/key-stroke-decorator'
+
+const SidePanel = ({ account, cancel, cancelButton, children, invalid, show, submit, submitButton, subTitle, title }) => {
   return (
     <Modal show={show} dialogClassName="side-panel">
       <Modal.Header>
@@ -14,7 +17,25 @@ const SidePanel = ({ children, show, subTitle, title }) => {
       </Modal.Body>
 
       <Modal.Footer>
+        <ButtonToolbar className="pull-right">
+          {cancelButton &&
+          <Button
+            id="cancel-btn"
+            className="btn-secondary"
+            onClick={cancel}>
+            <FormattedMessage id="portal.button.cancel"/>
+          </Button>}
 
+          {submitButton &&
+          <Button
+            bsStyle="primary"
+            onClick={submit}
+            disabled={invalid}>
+            {account ?
+            <FormattedMessage id="portal.button.save" /> :
+            <FormattedMessage id="portal.button.add" />}
+          </Button>}
+        </ButtonToolbar>
       </Modal.Footer>
     </Modal>
   )
@@ -22,13 +43,18 @@ const SidePanel = ({ children, show, subTitle, title }) => {
 
 SidePanel.displayName = 'SidePanel'
 SidePanel.propTypes = {
+  account: React.PropTypes.instanceOf(Map),
+  cancel: PropTypes.func,
+  cancelButton: PropTypes.bool,
   children: PropTypes.node,
-  intl: React.PropTypes.object,
+  invalid: PropTypes.bool,
   show: PropTypes.bool,
   subTitle: PropTypes.oneOfType([
     React.PropTypes.string,
     React.PropTypes.node
   ]),
+  submit: PropTypes.func,
+  submitButton: PropTypes.bool,
   title: PropTypes.oneOfType([
     React.PropTypes.string,
     React.PropTypes.node
