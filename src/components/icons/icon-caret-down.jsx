201310985f4a2108e09c5884f573663c3839900e
Taaha Chaudhry <taaha.chaudhry@idean.com>
2016-10-19 11:54:50 -0700

    UDNP-1630: Updated IconCaretDown viewbox

--- src/components/icons/icon-caret-down.jsx
+++ src/components/icons/icon-caret-down.jsx
@@ -8,7 +8,7 @@ const IconCaretDown = (props) => {
     classNames = classNames + ' ' + className;
   }
   return (
-    <Icon className={classNames} width={width} height={height} viewbox="0 0 35 32">
+    <Icon className={classNames} width={width} height={height} viewbox="0 0 36 36">
       <g>
         <circle cx="18" cy="18" r="12" className="base"/>
         <polygon fill="#FFFFFF" points="12,15.9 18,21.9 24,15.9 " className="primary"/>
