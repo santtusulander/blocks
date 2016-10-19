5a90ad4eb1f14db78ae598f6bdc72110f2d87b8c
Jeremy White <jeremy.white@idean.com>
2016-10-19 08:04:36 -0700

    UDNP-1660, UDNP-1661 - verify presence of values
    for countries and redirect URL

--- src/components/configuration/actions/content-targeting.jsx
+++ src/components/configuration/actions/content-targeting.jsx
@@ -17,6 +17,7 @@ class ContentTargeting extends React.Component {
     this.handleTypeChange = this.handleTypeChange.bind(this)
     this.handleInclusionChange = this.handleInclusionChange.bind(this)
     this.handleRedirectURLChange = this.handleRedirectURLChange.bind(this)
+    this.disableSaveButton = this.disableSaveButton.bind(this)
     this.saveChanges = this.saveChanges.bind(this)
   }
   componentWillReceiveProps(nextProps) {
@@ -83,10 +84,15 @@ class ContentTargeting extends React.Component {
   }
   handleRedirectURLChange() {
     return e => {
+      const redirectURL = e.target.value === "" ? null : e.target.value
       this.setState({
-        redirectURL: e.target.value
+        redirectURL
       })
     }
+  }
+  disableSaveButton() {
+    return this.state.countries.count() === 0
+            || (this.state.type === 'redirect' && !this.state.redirectURL)
   }
   saveChanges() {
     const countries = this.state.countries.map(country => country.id)
@@ -182,7 +188,11 @@ class ContentTargeting extends React.Component {
             <Button bsStyle="default" id="close-button" onClick={this.props.close}>
               <FormattedMessage id="portal.button.cancel"/>
             </Button>
-            <Button bsStyle="primary" id="save-button" onClick={this.saveChanges}>
+            <Button
+              bsStyle="primary"
+              id="save-button"
+              disabled={this.disableSaveButton()}
+              onClick={this.saveChanges}>
               <FormattedMessage id="portal.button.saveAction"/>
             </Button>
           </ButtonToolbar>
