55caef1abaae06c4765f45fa4b28fadeb1bed767
santtu-idean <santtu.sulander@idean.com>
2016-10-21 16:15:38 +0300

    fix starburst help & styleguide tests

--- src/containers/__tests__/styleguide.js
+++ src/containers/__tests__/styleguide.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import TestUtils from 'react-addons-test-utils'
+import { shallow } from 'enzyme'
 
-jest.autoMockOff()
 jest.unmock('../styleguide.jsx')
 import Styleguide from '../styleguide.jsx'
 
 describe('Styleguide', () => {
   it('should exist', () => {
-    let styleguide = TestUtils.renderIntoDocument(
-      <Styleguide />
-    );
-    expect(TestUtils.isCompositeComponent(styleguide)).toBeTruthy();
+    expect(shallow(<Styleguide/>).length).toBe(1)
   });
 })
