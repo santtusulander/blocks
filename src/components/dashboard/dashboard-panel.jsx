d2b3d35cb8aa29888e47b1ded619174f5a37ae57
Mika Paulasaari <mika.paulasaari@idean.com>
2016-10-27 11:34:58 -0700

    Create dashboard panel component
    And add it to styleguide

---
+++ src/components/dashboard/dashboard-panel.jsx
@@ -0,0 +1,35 @@
+import React, { PropTypes } from 'react'
+import classNames from 'classnames'
+
+import './dashboard-panel.scss'
+
+const DashboardPanel = (props) => {
+  const { children, className, noPadding, title } = props
+  return (
+    <div className={classNames(
+      'dashboard-panel',
+      {
+        className,
+        'no-padding': noPadding
+      }
+    )}>
+      {title ?
+        <div className="dashboard-panel-header">
+          <h5>{title}</h5>
+        </div>
+      : null}
+      <div className="dashboard-panel-content">
+        {children}
+      </div>
+    </div>
+  )
+}
+
+DashboardPanel.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  noPadding: PropTypes.bool,
+  title: PropTypes.string
+}
+
+export default DashboardPanel
