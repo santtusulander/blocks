f221af058d356a3c02aa4de84c46d07fc14e0d71
Bryan Downing <bryan.downing@idean.com>
2016-10-27 18:42:01 -0700

    UDNP-1706 — Calculate detail arrays and totals
    …for traffic (total, on, and off net), bandwidth, latency, connections, and cache hit ratio.

--- analytics-api/app/routes/sp-dashboard.js
+++ analytics-api/app/routes/sp-dashboard.js
@@ -1,8 +1,9 @@
 'use strict';
 
 require('express-jsend');
-// let _         = require('lodash');
+let _         = require('lodash');
 let log       = require('../logger');
+let dataUtils = require('../data-utils')
 let db        = require('../db');
 let validator = require('../validator');
 
@@ -32,6 +33,8 @@ function routeSpDashboard(req, res) {
   };
 
   db.getSpDashboardMetrics(options).spread((globalData, countryData, providerData) => {
+    const duration = options.end - options.start + 1;
+    const secondsPerGranularity = dataUtils.secondsPerGranularity[options.granularity];
     let finalData = {
       traffic: {
         bytes: 0,
@@ -48,6 +51,7 @@ function routeSpDashboard(req, res) {
         detail: []
       },
       connections: {
+        connections: 0,
         connections_per_second: 0,
         detail: []
       },
@@ -63,7 +67,103 @@ function routeSpDashboard(req, res) {
       }
     }
 
-    res.jsend({globalData, countryData, providerData, finalData});
+    let bytesTotal        = 0;
+    let bytesOnNetTotal   = 0;
+    let bytesOffNetTotal  = 0;
+    let connectionsTotal  = 0;
+    let chitRatioTotal    = 0;
+    let avgFBLTotal       = 0;
+    let detail            = [];
+    let fullDetail        = [];
+    let globalDataGrouped = _.groupBy(globalData, 'epoch_start');
+
+    // Iterate over each timestamp
+    _.forEach(globalDataGrouped, (records, timestamp) => {
+      let bytes           = 0;
+      let bytesOnNet      = 0;
+      let bytesOffNet     = 0;
+      let connections     = 0;
+      let chitRatio       = 0;
+      let avgFBL          = 0;
+      let chitRatioOnNet  = 0;
+      let chitRatioOffNet = 0;
+      let avgFBLOnNet     = 0;
+      let avgFBLOffNet    = 0;
+
+      records.forEach(record => {
+        bytes       += record.bytes;
+        connections += record.connections;
+
+        if (record.net_type === 'on') {
+          bytesOnNet = record.bytes;
+          chitRatioOnNet = record.chit_ratio * record.connections;
+          avgFBLOnNet = record.avg_fbl * record.connections;
+        }
+
+        if (record.net_type === 'off') {
+          bytesOffNet = record.bytes;
+          chitRatioOffNet = record.chit_ratio * record.connections;
+          avgFBLOffNet = record.avg_fbl * record.connections;
+        }
+      });
+
+      // Calculate the weighted metrics
+      chitRatio = Math.round((chitRatioOnNet + chitRatioOffNet) / connections);
+      avgFBL = `${Math.round((avgFBLOnNet + avgFBLOffNet) / connections)} ms`;
+
+      // Build a big detail array with all the metrics
+      detail.push({
+        timestamp: parseInt(timestamp),
+        bytes: bytes,
+        bytes_net_on: bytesOnNet,
+        bytes_net_off: bytesOffNet,
+        bits_per_second: dataUtils.getBPSFromBytes(bytes, options.granularity),
+        connections: connections,
+        connections_per_second: parseFloat((connections / secondsPerGranularity).toFixed(4)),
+        chit_ratio: chitRatio,
+        avg_fbl: avgFBL
+      })
+
+      // Calculate Totals
+      bytesTotal       += bytes;
+      bytesOnNetTotal  += bytesOnNet;
+      bytesOffNetTotal += bytesOffNet;
+      connectionsTotal += connections;
+      chitRatioTotal   += chitRatio * connections;
+      avgFBLTotal      += parseInt(avgFBL) * connections;
+
+    });
+
+    fullDetail = dataUtils.buildContiguousTimeline(
+      detail,
+      options.start,
+      options.end,
+      options.granularity,
+      ['bytes', 'bytes_net_on', 'bytes_net_off', 'bits_per_second', 'connections', 'connections_per_second', 'chit_ratio', 'avg_fbl']
+    );
+
+    // Populate Final Detail Data
+    fullDetail.forEach(record => {
+      let { timestamp, bytes, bytes_net_on, bytes_net_off, bits_per_second, connections, connections_per_second, chit_ratio, avg_fbl } = record;
+      finalData.traffic.detail.push({timestamp, bytes, bytes_net_on, bytes_net_off});
+      finalData.bandwidth.detail.push({timestamp, bits_per_second});
+      finalData.latency.detail.push({timestamp, avg_fbl});
+      finalData.connections.detail.push({timestamp, connections, connections_per_second});
+      finalData.cache_hit.detail.push({timestamp, chit_ratio});
+    });
+
+    // Populate Final Totals Data
+    finalData.traffic.bytes                      = bytesTotal;
+    finalData.traffic.bytes_net_on               = bytesOnNetTotal;
+    finalData.traffic.bytes_net_off              = bytesOffNetTotal;
+    finalData.bandwidth.bits_per_second          = dataUtils.getBPSFromBytes(bytesTotal, duration);
+    finalData.latency.avg_fbl                    = `${Math.round(avgFBLTotal / connectionsTotal)} ms`;
+    finalData.connections.connections            = connectionsTotal;
+    finalData.connections.connections_per_second = parseFloat((connectionsTotal / duration).toFixed(4));
+    finalData.cache_hit.chit_ratio               = Math.round(chitRatioTotal / connectionsTotal);
+
+
+    res.jsend({finalData, countryData, providerData});
 
   }).catch((err) => {
     log.error(err);
