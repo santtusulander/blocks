33a88709fc4f428e703b14e01ef773e05fd1cfb2
Taaha Chaudhry <taaha.chaudhry@idean.com>
2016-11-01 12:52:23 -0700

    UDNP-1703: Updated tests to use correct props

--- src/components/dashboard/__tests__/stacked-by-time-summary.js
+++ src/components/dashboard/__tests__/stacked-by-time-summary.js
@@ -10,62 +10,9 @@ jest.mock('../../../util/helpers', () => { return {
 
 import StackedByTimeSummary from '../stacked-by-time-summary.jsx'
 
-
-const fakeData = {
-  "traffic": {
-    "bytes": 446265804980374,
-    "bytes_net_on": 352569123057670,
-    "bytes_net_off": 93696681922704,
-    "detail": [
-      {
-        "timestamp": new Date('Thu May 26 2016 12:17:01 GMT-0700 (PDT)'),
-        "bytes": 92020173697866,
-        "bytes_net_on": 71856580682504,
-        "bytes_net_off": 20163593015362
-      },
-      {
-        "timestamp": new Date('Thu May 26 2016 13:17:01 GMT-0700 (PDT)'),
-        "bytes": 99672709053865,
-        "bytes_net_on": 76848354018252,
-        "bytes_net_off": 22824355035613
-      },
-      {
-        "timestamp": new Date('Thu May 26 2016 14:17:01 GMT-0700 (PDT)'),
-        "bytes": 94821186769899,
-        "bytes_net_on": 72941835769369,
-        "bytes_net_off": 21879351000530
-      },
-      {
-        "timestamp": new Date('Thu May 26 2016 15:17:01 GMT-0700 (PDT)'),
-        "bytes": 117441291619312,
-        "bytes_net_on": 90477417340581,
-        "bytes_net_off": 26963874278731
-      },
-      {
-        "timestamp": new Date('Thu May 26 2016 16:17:01 GMT-0700 (PDT)'),
-        "bytes": 81546375702611,
-        "bytes_net_on": 62160286504951,
-        "bytes_net_off": 19386089197660
-      },
-      {
-        "timestamp": new Date('Thu May 26 2016 17:17:01 GMT-0700 (PDT)'),
-        "bytes": 117341539984300,
-        "bytes_net_on": 90364165873239,
-        "bytes_net_off": 26977374111061
-      },
-      {
-        "timestamp": new Date('Thu May 26 2016 18:17:01 GMT-0700 (PDT)'),
-        "bytes": 94064934029131,
-        "bytes_net_on": 72989086766237,
-        "bytes_net_off": 21075847262894
-      },
-      {
-        "timestamp": new Date('Thu May 26 2016 19:17:01 GMT-0700 (PDT)'),
-        "bytes": 93196929110225,
-        "bytes_net_on": 72133332220394,
-        "bytes_net_off": 21063596889831
-      }
-    ]
+function intlMaker() {
+  return {
+    formatMessage: jest.fn()
   }
 }
 
@@ -76,9 +23,47 @@ describe('StackedByTimeSummary', () => {
     subject = () => {
       props = {
         dataKey: "bytes",
-        data: fakeData
+        datasetAArray: [
+          {
+            "bytes": 71856580682504,
+            "timestamp": "2016-05-26T19:17:01.000Z"
+          },
+          {
+            "bytes": 76848354018252,
+            "timestamp": "2016-05-26T20:17:01.000Z"
+          },
+          {
+            "bytes": 72941835769369,
+            "timestamp": "2016-05-26T21:17:01.000Z"
+          },
+          {
+            "bytes": 90477417340581,
+            "timestamp": "2016-05-26T22:17:01.000Z"
+          },
+          {
+            "bytes": 62160286504951,
+            "timestamp": "2016-05-26T23:17:01.000Z"
+          },
+          {
+            "bytes": 90364165873239,
+            "timestamp": "2016-05-27T00:17:01.000Z"
+          },
+          {
+            "bytes": 72989086766237,
+            "timestamp": "2016-05-27T01:17:01.000Z"
+          },
+          {
+            "bytes": 72133332220394,
+            "timestamp": "2016-05-27T02:17:01.000Z"
+          }
+        ],
+        datasetALabel: "On-Net",
+        datasetAUnit: "%",
+        datasetAValue: "71",
+        totalDatasetUnit: "TB",
+        totalDatasetValue: "446"
       }
-      return shallow(<StackedByTimeSummary {...props}/>)
+      return shallow(<StackedByTimeSummary {...props} intl={intlMaker()}/>)
     }
   })
 
