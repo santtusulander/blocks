8e675b09b057457045b52fd7c6de90638838d09a
santtu-idean <santtu.sulander@idean.com>
2016-11-04 11:57:48 +0200

    Include examples of new bar chart into styleguide

--- src/containers/styleguide.jsx
+++ src/containers/styleguide.jsx
@@ -26,7 +26,9 @@ import {
   Tabs
 } from 'react-bootstrap';
 
+import SectionContainer from '../components/layout/section-container'
 import SelectWrapper from '../components/select-wrapper'
+import BarChart from '../components/charts/bar-chart'
 import FilterChecklistDropdown from '../components/filter-checklist-dropdown/filter-checklist-dropdown.jsx'
 import AccountSelector from '../components/global-account-selector/selector-component'
 import StackedByTimeSummary from '../components/stacked-by-time-summary'
@@ -208,6 +210,67 @@ export default class Styleguide extends React.Component {
           </Breadcrumb>
 
 
+          <h1 className="page-header">Charts</h1>
+            <Row>
+                <label>Stacked barchart, tooltip only on bar hover</label>
+                <SectionContainer className="analysis-contribution">
+                  {<BarChart
+                    chartLabel="Month to Date"
+                    tooltipAlwaysActive={false}
+                    chartData={[
+                      {
+                        "name": "Datafone Inc.",
+                        "onNetHttp": 9149792187422,
+                        "onNetHttps": 4324269843760,
+                        "offNetHttp": 2297510618946,
+                        "offNetHttps": 1090755001954
+                      },
+                      {
+                        "name": "AsiaNet",
+                        "onNetHttp": 58034767339905,
+                        "onNetHttps": 27260875504858,
+                        "offNetHttp": 16598076780724,
+                        "offNetHttps": 6941781887919
+                      },
+                      {
+                        "name": "QXT",
+                        "onNetHttp": 17640581263893,
+                        "onNetHttps": 8905041306312,
+                        "offNetHttp": 4413020296483,
+                        "offNetHttps": 2063509423994
+                      }
+                    ]}
+                  barModels={[
+                    { dataKey: 'onNetHttp', name: 'On-Net HTTP', className: 'line-0' },
+                    { dataKey: 'onNetHttps', name: 'On-Net HTTPS', className: 'line-1' },
+                    { dataKey: 'offNetHttp', name: 'Off-Net HTTP', className: 'line-2' },
+                    { dataKey: 'offNetHttps', name: 'Off-Net HTTPS', className: 'line-3' }
+                  ]}/>}
+              </SectionContainer>
+              </Row>
+              <Row>
+                <label>Normal barchart, tooltip always active</label>
+                <SectionContainer className="analysis-contribution">
+                  <BarChart
+                    chartLabel="This Week"
+                    chartData={[
+                      {
+                        "name": "Datafone Inc.",
+                        "onNetHttp": 9149792187422
+                      },
+                      {
+                        "name": "AsiaNet",
+                        "onNetHttp": 58034767339905
+                      },
+                      {
+                        "name": "QXT",
+                        "onNetHttp": 17640581263893
+                      }
+                    ]}
+                  barModels={[ { dataKey: 'onNetHttp', name: 'On-Net HTTP', className: 'line-0' } ]}/>
+              </SectionContainer>
+            </Row>
+
           <h1 className="page-header">Tabs</h1>
 
           <Tabs defaultActiveKey={1} className="styleguide-row">
@@ -335,7 +398,6 @@ export default class Styleguide extends React.Component {
               </tr>
             </tbody>
           </Table>
-
 
           <h1 className="page-header">Forms</h1>
 
