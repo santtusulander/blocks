9e7d0b9df5cf182b9a31211e5ea6b1e7c7c86bfb
santtu-idean <santtu.sulander@idean.com>
2016-11-04 11:57:22 +0200

    create custom legend component for new bar chart

---
+++ src/components/charts/custom-tooltip.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { formatBytes } from '../../util/helpers'
+
+const CustomTooltip = ({ payload = [], iconClass }) => {
+  const dataForTooltip = payload.map(({ name, value, dataKey }) => ({
+    name,
+    value: formatBytes(value),
+    className: iconClass(dataKey)
+  }))
+  const total = formatBytes(payload.reduce((sum, { value }) => sum += value, 0))
+  return (
+    <div className="rechart-tooltip">
+      {dataForTooltip.map(({ name, value, className }, i) =>
+        <div key={i} className="tooltip-item">
+          <span className="legend-label">
+            <span className={`legend-icon ${className}`}>&mdash; </span>
+            {name}
+          </span>
+          <span id='legend-value' className='legend-value'>{value}</span>
+        </div>
+      )}
+      <hr style={{ margin: '7px 0' }}
+      />
+      <div className="tooltip-item">
+        <span className="legend-label">
+          Total
+        </span>
+        <span className='legend-value'>{total}</span>
+      </div>
+    </div>
+  )
+}
+
+export default CustomTooltip
