b689ed80b765a9c746601690a34d173d378f99f8
Taaha Chaudhry <taaha.chaudhry@idean.com>
2016-11-09 14:08:50 -0800

    UDNP-1630: Removed fill from icon caret down and icon caret right

--- src/components/icons/icon-caret-down.jsx
+++ src/components/icons/icon-caret-down.jsx
@@ -11,7 +11,7 @@ const IconCaretDown = (props) => {
     <Icon className={classNames} width={width} height={height} viewbox="0 0 36 36">
       <g>
         <circle cx="18" cy="18" r="12" className="base"/>
-        <polygon fill="#FFFFFF" points="12,15.9 18,21.9 24,15.9" className="primary"/>
+        <polygon points="12,15.9 18,21.9 24,15.9" className="primary"/>
       </g>
     </Icon>
   )
