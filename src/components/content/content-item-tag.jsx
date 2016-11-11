445cb33de5d13597c0107949651e052b549f2764
santtu-idean <santtu.sulander@idean.com>
2016-11-11 08:59:49 +0200

    rearrange

--- src/components/content/content-item-tag.jsx
+++ src/components/content/content-item-tag.jsx
@@ -1,10 +1,13 @@
 import React, { PropTypes } from 'react'
 import classnames from 'classnames'
 
-const ContentItemTag = ({ customClass, ...rest }) =>
-  <span className={classnames('content-item-tag', customClass)} {...rest}/>
+const ContentItemTag = ({ customClass, children }) =>
+  <span className={classnames('content-item-tag', customClass)}>
+   {children}
+  </span>
 
 ContentItemTag.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   customClass: PropTypes.string
 }
 
