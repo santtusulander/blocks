c7ed273315dca90290517cd7cfc730269319d8f5
Ville Ristimäki <ville.ristimaki@idean.com>
2016-11-14 17:00:59 +0200

    Move sorting & filtering out of render, use getSortData from utils, remove pagination

--- src/components/content/property/purge-status.jsx
+++ src/components/content/property/purge-status.jsx
@@ -1,50 +1,44 @@
 import React from 'react'
 import { List } from 'immutable'
 import { injectIntl, FormattedMessage } from 'react-intl'
-// import { Pagination } from 'react-bootstrap'
 
 import SectionHeader from '../../layout/section-header'
 import SectionContainer from '../../layout/section-container'
 import TableSorter from '../../table-sorter'
 import SelectWrapper from '../../../components/select-wrapper'
 
-import { formatUnixTimestamp } from '../../../util/helpers'
+import { formatUnixTimestamp, getSortData } from '../../../util/helpers'
 
 const filterOptions = [
   { value: 'all', label: <FormattedMessage id="portal.content.property.purgeStatus.all.label"/> },
   { value: 'created', label: <FormattedMessage id="portal.content.property.purgeStatus.created.label"/> },
   { value: 'completed', label: <FormattedMessage id="portal.content.property.purgeStatus.completed.label"/> }
 ]
-// Comment this out until the API supports pagination
-// const PAGINATION = {
-//   items: 10,
-//   maxButtons: 5
-// }
 
 class PurgeHistoryReport extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      activePage: 1,
       sortBy: 'status',
       sortDir: -1,
       sortFunc: '',
-      filteredStatus: filterOptions[0].value
+      filteredStatus: filterOptions[0].value,
+      sortedStats: List()
     }
 
     this.changeSort = this.changeSort.bind(this)
     this.filterData = this.filterData.bind(this)
-    this.sortedData = this.sortedData.bind(this)
-    this.handlePaginate = this.handlePaginate.bind(this)
   }
 
-  changeSort(column, direction, sortFunc) {
-    this.setState({
-      sortBy: column,
-      sortDir: direction,
-      sortFunc: sortFunc
-    })
+  componentWillReceiveProps(nextProps) {
+    const { historyData } = nextProps
+    this.setData(historyData)
+  }
+
+  setData(historyData){
+    const sortedStats = getSortData(this.filterData(historyData), this.state.sortBy, this.state.sortDir, this.state.sortFunc)
+    return this.setState({ sortedStats })
   }
 
   filterData(data) {
@@ -54,67 +48,24 @@ class PurgeHistoryReport extends React.Component {
     })
   }
 
-  sortedData(data, sortBy, sortDir) {
-    let sortFunc = ''
-    if (this.state.sortFunc === 'specific' && sortBy.indexOf(',') > -1) {
-      sortFunc = data.sort((a, b) => {
-        sortBy = sortBy.toString().split(',')
-
-        const lhs = a.get(sortBy[0])
-        const rhs = b.get(sortBy[0])
-
-        // the following conditionals handle cases where a & b contain null data
-        if (!lhs && rhs) {
-          return -1 * sortDir
-        }
-        if (lhs && !rhs) {
-          return 1 * sortDir
-        }
-        if (lhs && rhs) {
-          if (lhs.get(sortBy[1]) < rhs.get(sortBy[1])) {
-            return -1 * sortDir
-          } else if (lhs.get(sortBy[1]) > rhs.get(sortBy[1])) {
-            return 1 * sortDir
-          }
-        }
-
-        return 0
-      })
-    } else {
-      sortFunc = data.sort((a, b) => {
-        if (a.get(sortBy) < b.get(sortBy)) {
-          return -1 * sortDir
-        }
-        else if (a.get(sortBy) > b.get(sortBy)) {
-          return 1 * sortDir
-        }
-        return 0
-      })
-    }
-    return sortFunc
-  }
-
-  handlePaginate(e, paginationEvent) {
+  changeSort(column, direction, sortFunc) {
     this.setState({
-      activePage: paginationEvent.eventKey
-    })
+      sortBy: column,
+      sortDir: direction,
+      sortFunc: sortFunc
+    }, () => this.setData(this.props.historyData))
   }
 
   render() {
 
-    const {
-      historyData,
-      intl
-    } = this.props
+    const { intl } = this.props
+    const { sortedStats } = this.state
 
     const sorterProps = {
       activateSort: this.changeSort,
       activeColumn: this.state.sortBy,
       activeDirection: this.state.sortDir
     }
-
-    const filteredData = this.filterData(historyData)
-    const sortedStats = this.sortedData(filteredData, this.state.sortBy, this.state.sortDir)
 
     return (
       <div>
@@ -125,7 +76,7 @@ class PurgeHistoryReport extends React.Component {
               id='filtered-status'
               value={this.state.filteredStatus}
               onChange={value => {
-                this.setState({ filteredStatus: value })
+                this.setState({ filteredStatus: value }, () => this.setData(this.props.historyData))
               }}
               options={filterOptions}/>
           </div>
@@ -170,19 +121,6 @@ class PurgeHistoryReport extends React.Component {
             <p><FormattedMessage id="portal.content.property.purgeStatus.notFound.label"/></p>
           }
         </SectionContainer>
-
-        {/* Comment this out until the API supports pagination */}
-        {/*        <Pagination className="pull-right"
-         items={PAGINATION.items}
-         maxButtons={PAGINATION.maxButtons}
-         prev={true}
-         next={true}
-         first={true}
-         last={true}
-         ellipsis={true}
-         activePage={this.state.activePage}
-         onSelect={this.handlePaginate}
-         />*/}
       </div>
     )
   }
