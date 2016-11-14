f5ec858fc60d7579c9f181caa50c8e6c70aa0056
Ville Ristimäki <ville.ristimaki@idean.com>
2016-11-14 12:30:01 +0200

    Fix typos in locales, remove redundand root-div, remove redundand params from proptypes & defaultProps

--- src/components/content/property/property-tab-control.jsx
+++ src/components/content/property/property-tab-control.jsx
@@ -12,45 +12,38 @@ const PropertyTabControl = (props) => {
   const tabs = [
     {
       key: 'summary',
-      label: props.intl.formatMessage({id: 'portal.content.property.tabs.summary.label'}),
+      label: props.intl.formatMessage({ id: 'portal.content.property.tabs.summary.label' }),
       permission: PERMISSIONS.VIEW_ANALYTICS_TRAFFIC_OVERVIEW
     },
     {
       key: 'purge-status',
-      label: props.intl.formatMessage({id: 'portal.content.property.tabs.purgeStatus.label'}),
+      label: props.intl.formatMessage({ id: 'portal.content.property.tabs.purgeStatus.label' }),
       permission: PERMISSIONS.VIEW_ANALYTICS_TRAFFIC_OVERVIEW
     }
   ]
 
   return (
-    <div>
-      <Nav bsStyle="tabs">
-        {tabs.reduce((lis, tab) => {
-          const tabContent = (
-            <IsAllowed key={tab.key} to={tab.permission}>
-              <li role="tab">
-                <Link to={getTabLink(props.location, tab.key)}
-                      activeClassName='active'>{tab.label}</Link>
-              </li>
-            </IsAllowed>
-          )
-          lis.push(tabContent)
-          return lis
-        }, [])}
-      </Nav>
-    </div>
+    <Nav bsStyle="tabs">
+      {tabs.reduce((lis, tab) => {
+        const tabContent = (
+          <IsAllowed key={tab.key} to={tab.permission}>
+            <li role="tab">
+              <Link to={getTabLink(props.location, tab.key)}
+                    activeClassName='active'>{tab.label}</Link>
+            </li>
+          </IsAllowed>
+        )
+        lis.push(tabContent)
+        return lis
+      }, [])}
+    </Nav>
   )
 }
 
 
 PropertyTabControl.propTypes = {
   intl: PropTypes.object,
-  location: PropTypes.object,
-  params: PropTypes.object
-}
-
-PropertyTabControl.defaultProps = {
-  params: {}
+  location: PropTypes.object
 }
 
 export default withRouter(injectIntl(PropertyTabControl))
