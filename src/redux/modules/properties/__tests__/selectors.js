c1131b1424a27ad919db9b33d6a958bee431169d
Mikko Alander <mikko.alander@idean.com>
2016-11-15 16:08:46 +0200

    add units tests for reducers and selectors

---
+++ src/redux/modules/properties/__tests__/selectors.js
@@ -0,0 +1,59 @@
+import {fromJS} from 'immutable'
+jest.unmock('../selectors')
+import * as selectors from '../selectors'
+
+const properties = {
+  'aa': {
+    published_host_id: 'aa',
+    accountId: 1,
+    groupId: 2,
+  },
+  'bb': {
+    published_host_id: 'bb',
+    accountId: 3,
+    groupId: 4,
+  }
+}
+
+const initialState = {
+  properties: {
+    properties: fromJS(properties)
+  }
+}
+
+describe('Properties Module', () => {
+  it('should getAllHosts', () => {
+      const p = selectors.getAllHosts( initialState )
+      expect(p).toEqual(['aa','bb'])
+  });
+
+  it('should getProperties', () => {
+    const p = selectors.getProperties( initialState )
+    expect(p).toEqual(properties)
+  });
+
+  it('should getByAccount', () => {
+    const p = selectors.getProperties( initialState, null, 3 )
+    expect(p).toEqual( {bb: {
+      published_host_id: 'bb',
+      accountId: 3,
+      groupId: 4,
+    }})
+  });
+
+  it('should getByGroup', () => {
+    const p = selectors.getProperties( initialState, null, 1,2 )
+    expect(p).toEqual( {  'aa': {
+        published_host_id: 'aa',
+        accountId: 1,
+        groupId: 2,
+      },})
+  });
+
+  it('should getById', () => {
+    const p = selectors.getById( initialState, 'aa' )
+    expect(p).toEqual({
+        published_host_id: 'aa'
+    })
+  });
+})
