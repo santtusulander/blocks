cd3efe449efe3151cdbdba9c50ceef3498b2957b
Jeremy White <jeremy.white@idean.com>
2016-11-15 16:37:21 -0800

    UDNP-775 — use POLICY_TYPES constant

--- src/constants/property-config.js
+++ src/constants/property-config.js
@@ -13,55 +13,55 @@ export const availableMatches = [
   {
     key: 'request_host',
     name: 'portal.policy.edit.matchesSelection.hostname.text',
-    compatibleWith: ['request_policy']
+    compatibleWith: [POLICY_TYPES.REQUEST]
   },
   {
     key: 'request_url',
     name: 'portal.policy.edit.matchesSelection.url.text',
-    compatibleWith: ['request_policy']
+    compatibleWith: [POLICY_TYPES.REQUEST]
   },
   {
     key: 'request_path',
     name: 'portal.policy.edit.matchesSelection.directoryPath.text',
-    compatibleWith: ['request_policy']
+    compatibleWith: [POLICY_TYPES.REQUEST]
   },
   {
     key: 'request_query_arg',
     name: 'portal.policy.edit.matchesSelection.queryString.text',
-    compatibleWith: ['request_policy']
+    compatibleWith: [POLICY_TYPES.REQUEST]
   },
   {
     key: 'request_header',
     name: 'portal.policy.edit.matchesSelection.header.text',
-    compatibleWith: ['request_policy', 'response_policy']
+    compatibleWith: [POLICY_TYPES.REQUEST, POLICY_TYPES.RESPONSE]
   },
   {
     key: 'request_cookie',
     name: 'portal.policy.edit.matchesSelection.cookie.text',
-    compatibleWith: ['request_policy', 'response_policy']
+    compatibleWith: [POLICY_TYPES.REQUEST, POLICY_TYPES.RESPONSE]
   },
   {
     key: 'content_targeting',
     name: 'portal.policy.edit.matchesSelection.contentTargeting.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     requiresAdmin: true
   },
   { // File Extension (not yet implemented on backend)
     key: null,
     name: 'portal.policy.edit.matchesSelection.fileExtension.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   },
   { // File Name (not yet implemented on backend)
     key: 'File Name',
     name: 'portal.policy.edit.matchesSelection.fileName.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   },
   { // File Type (not yet implemented on backend)
     key: 'File Type',
     name: 'portal.policy.edit.matchesSelection.fileType.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   }
 ]
@@ -70,76 +70,76 @@ export const availableActions = [
   {
     key: 'cache_control',
     name: 'portal.policy.edit.actionSelection.cache.text',
-    compatibleWith: ['request_policy']
+    compatibleWith: [POLICY_TYPES.REQUEST]
   },
   {
     key: 'cache_name',
     name: 'portal.policy.edit.actionSelection.cacheKeyQueryString.text',
-    compatibleWith: ['request_policy']
+    compatibleWith: [POLICY_TYPES.REQUEST]
   },
   {
     key: 'header',
     name: 'portal.policy.edit.actionSelection.header.text',
-    compatibleWith: ['request_policy', 'response_policy']
+    compatibleWith: [POLICY_TYPES.REQUEST, POLICY_TYPES.RESPONSE]
   },
   {
     key: 'tokenauth',
     name: 'portal.policy.edit.actionSelection.tokenauth.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     requiresAdmin: true
   },
   {
     key: null,
     name: 'portal.policy.edit.actionSelection.redirection.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   },
   {
     key: null,
     name: 'portal.policy.edit.actionSelection.originHostname.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   },
   {
     key: null,
     name: 'portal.policy.edit.actionSelection.compression.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   },
   {
     key: null,
     name: 'portal.policy.edit.actionSelection.path.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   },
   {
     key: null,
     name: 'portal.policy.edit.actionSelection.queryString.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   },
   {
     key: null,
     name: 'portal.policy.edit.actionSelection.removeVary.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   },
   {
     key: null,
     name: 'portal.policy.edit.actionSelection.allowBlock.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   },
   {
     key: null,
     name: 'portal.policy.edit.actionSelection.postSupport.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   },
   {
     key: null,
     name: 'portal.policy.edit.actionSelection.cors.text',
-    compatibleWith: ['request_policy'],
+    compatibleWith: [POLICY_TYPES.REQUEST],
     notYetImplemented: true
   }
 ]
