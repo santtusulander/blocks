c437a805ab53df2f2baf444805904dcab7540185
Mikko Alander <mikko.alander@idean.com>
2016-11-21 13:30:46 +0200

    make sure payload container properties

--- src/redux/modules/properties/reducers.js
+++ src/redux/modules/properties/reducers.js
@@ -2,7 +2,10 @@ import {fromJS} from 'immutable'
 
 /*Reducers*/
 export const receive = (state, action) => {
-  return state.merge( state, fromJS(action.payload.entities.properties))
+  if (action.payload.entities && action.payload.entities.properties )
+    return state.merge( state, fromJS(action.payload.entities.properties))
+
+  return state
 }
 
 export const fail = (state/*, action*/) => {
