52f3898198745abcf7316fe442bcd37e278949f4
Mikko Alander <mikko.alander@idean.com>
2016-11-22 15:40:10 +0200

    move SSL logic to own tab

--- src/containers/security/security.jsx
+++ src/containers/security/security.jsx
@@ -1,64 +1,39 @@
 import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { withRouter } from 'react-router'
+
 import { FormattedMessage } from 'react-intl'
 import { Map, List } from 'immutable'
 import Tabs from '../../components/tabs'
-import { Link, withRouter } from 'react-router'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import { Link } from 'react-router'
 
-import { getTabName } from '../../util/helpers'
+//import { getTabName } from '../../util/helpers'
 import { getSecurityUrlFromParams } from '../../util/routes'
-
 import * as accountActionCreators from '../../redux/modules/account'
-import * as securityActionCreators from '../../redux/modules/security'
-import * as uiActionCreators from '../../redux/modules/ui'
 
-import ModalWindow from '../../components/modal'
 import PageContainer from '../../components/layout/page-container'
 import SecurityPageHeader from '../../components/security/security-page-header'
-import CertificateForm from '../../components/security/certificate-form-container'
 import Content from '../../components/layout/content'
 
 import { getUrl } from '../../util/routes.js'
 
-import {
-  UPLOAD_CERTIFICATE,
-  EDIT_CERTIFICATE,
-  DELETE_CERTIFICATE
-} from '../../constants/account-management-modals.js'
-
 export class Security extends React.Component {
-  constructor(props) {
-    super(props)
-    this.fetchData = this.fetchData.bind(this)
-  }
-  componentWillMount() {
-    this.fetchData(this.props)
-  }
+  render() {
+    const {
+      accounts,
+      activeAccount,
+      fetchAccount,
+      groups,
+      params
+    } = this.props
 
-  componentWillReceiveProps(nextProps) {
-    if(JSON.stringify(nextProps.params) !== JSON.stringify(this.props.params)) {
-      this.fetchData(nextProps)
+    const activeGroup = groups.find(obj => obj.get('id') === Number(params.group))
+
+    const itemSelectorFunc = (linkType, val) => {
+      this.props.router.push(getUrl('/security', linkType, val, this.props.params))
     }
-  }
 
-  fetchData(props) {
-    const { location: { pathname }, params: { brand, account, group } } = props
-    switch(getTabName(pathname)) {
-      case 'ssl-certificate':
-        props.securityActions.fetchSSLCertificates(brand, Number(account), Number(group))
-        break
-      // case 'token-authentication':  securityActions.fetchTokenAuthentication(account)
-      // case 'content-targeting': securityActions.fetchContentTrageting(account)
-      default: break
-    }
-  }
-
-  renderContent(certificateFormProps, sslListProps) {
-    const params = this.props.params
-    //const subPage = getTabName(this.props.location.pathname)
-    //
-    // for token auth & content targeting post-1.0
     const securityBaseUrl = getSecurityUrlFromParams(params);
 
     if (!params.account) {
@@ -70,100 +45,35 @@ export class Security extends React.Component {
       )
     }
 
-    // for token auth & content targeting post-1.0
-    return (
-      <div>
-        <Tabs activeKey={this.props.children.props.route.path}>
-          <li className="navbar">
-            <Link to={securityBaseUrl + '/ssl-certificate'} activeClassName="active"><FormattedMessage id="portal.security.sslCertificate.text"/></Link>
-          </li>
-          <li className="navbar">
-            <Link to={securityBaseUrl + '/token-authentication'} activeClassName="active"><FormattedMessage id="portal.security.tokenAuth.text"/></Link>
-          </li>
-          <li className="navbar">
-            <Link to={securityBaseUrl + '/content-targeting'} activeClassName="active"><FormattedMessage id="portal.security.contentTargeting.text"/></Link>
-          </li>
-        </Tabs>
-
-        <PageContainer className="tab-bodies">
-        {this.props.children && React.cloneElement(this.props.children, {
-          params: params,
-          sslListProps
-        })
-        }
-        </PageContainer>
-      </div>
-    )
-  }
-
-  render() {
-    const {
-      accounts,
-      groups,
-      activeAccount,
-      activeCertificates,
-      activeModal,
-      fetchAccount,
-      params,
-      onDelete,
-      sslCertificates,
-      securityActions: { toggleActiveCertificates, fetchSSLCertificate },
-      toDelete,
-      toggleModal
-    } = this.props
-
-    const certificateFormProps = {
-      title: activeModal === EDIT_CERTIFICATE ? <FormattedMessage id="portal.security.editCertificate.text"/> : <FormattedMessage id="portal.security.uploadCertificate.text"/>,
-      activeAccount,
-      fetchAccount,
-      toggleModal
-    }
-
-    const sslListProps = {
-      groups,
-      activeModal,
-      activeCertificates,
-      certificates: sslCertificates,
-      onCheck: commonName => toggleActiveCertificates(commonName),
-      uploadCertificate: () => toggleModal(UPLOAD_CERTIFICATE),
-      editCertificate: (...args) => fetchSSLCertificate(...args).then(() => toggleModal(EDIT_CERTIFICATE)),
-      deleteCertificate: (...args) => fetchSSLCertificate(...args).then(() => toggleModal(DELETE_CERTIFICATE))
-    }
-
-    const activeGroup = groups.find(obj => obj.get('id') === Number(params.group))
-
-    const itemSelectorFunc = (...params) => {
-      this.props.router.push(getUrl('/security', ...params))
-    }
-
     return (
       <Content>
         <SecurityPageHeader
-          params={this.props.params}
+          params={params}
           accounts={accounts}
-          activeAccount={activeAccount.get('name')}
-          activeGroup={activeGroup ? activeGroup.get('name') : null}
+          activeAccount={activeAccount && activeAccount.get('name')}
+          activeGroup={activeGroup && activeGroup.get('name')}
           itemSelectorFunc={itemSelectorFunc}
           fetchAccount={fetchAccount}/>
 
-          {this.renderContent(certificateFormProps, sslListProps)}
+          <Tabs activeKey={this.props.children.props.route.path}>
+            <li className="navbar">
+              <Link to={`${securityBaseUrl}/ssl-certificate`} activeClassName="active"><FormattedMessage id="portal.security.sslCertificate.text"/></Link>
+            </li>
+            <li className="navbar">
+              <Link to={`${securityBaseUrl}/token-authentication`} activeClassName="active"><FormattedMessage id="portal.security.tokenAuth.text"/></Link>
+            </li>
+            <li className="navbar">
+              <Link to={`${securityBaseUrl}/content-content`} activeClassName="active"><FormattedMessage id="portal.security.contentTargeting.text"/></Link>
+            </li>
+          </Tabs>
 
-        {activeModal === EDIT_CERTIFICATE && <CertificateForm {...certificateFormProps}/>}
-        {activeModal === UPLOAD_CERTIFICATE && <CertificateForm {...certificateFormProps}/>}
-        {activeModal === DELETE_CERTIFICATE &&
-        <ModalWindow
-          title={<FormattedMessage id="portal.deleteModal.header.text" values={{itemToDelete: "Certificate"}}/>}
-          cancelButton={true}
-          deleteButton={true}
-          cancel={() => toggleModal(null)}
-          submit={() => onDelete(toDelete)}
-          invalid={true}
-          verifyDelete={true}>
-          <p>
-            <FormattedMessage id="portal.deleteModal.warning.text" values={{itemToDelete : "Certificate"}}/>
-          </p>
-        </ModalWindow>
-        }
+          <PageContainer className="tab-bodies">
+          {
+            this.props.children && React.cloneElement(this.props.children, {
+              ...this.props
+            })
+          }
+          </PageContainer>
       </Content>
     )
   }
@@ -172,54 +82,26 @@ export class Security extends React.Component {
 Security.propTypes = {
   accounts: PropTypes.instanceOf(List),
   activeAccount: PropTypes.instanceOf(Map),
-  activeCertificates: PropTypes.instanceOf(List),
-  activeModal: PropTypes.string,
   children: PropTypes.object,
   fetchAccount: PropTypes.func,
-  fetchListData: PropTypes.func,
   groups: PropTypes.instanceOf(List),
-  location: PropTypes.object,
-  onDelete: PropTypes.func,
   params: PropTypes.object,
-  router: React.PropTypes.object,
-  securityActions: PropTypes.object,
-  sslCertificates: PropTypes.instanceOf(List),
-  toDelete: PropTypes.instanceOf(Map),
-  toggleModal: PropTypes.func
+  router: PropTypes.object
 }
 
-function mapStateToProps(state, ownProps) {
-  return {
-    toDelete: state.security.get('certificateToEdit'),
-    activeCertificates: state.security.get('activeCertificates'),
-    activeModal: state.ui.get('accountManagementModal'),
-    accounts: state.account.get('allAccounts'),
-    activeAccount: state.account.get('activeAccount') || Map({}),
-    groups: state.group.get('allGroups'),
-    sslCertificates: state.security.get('sslCertificates').filter(cert =>
-      ownProps.params.group ? cert.get('group') === Number(ownProps.params.group) : false
-    )
-  };
-}
-
-function mapDispatchToProps(dispatch) {
+const mapDispatchToProps = (dispatch) => {
   const fetchAccount = bindActionCreators(accountActionCreators, dispatch).fetchAccount
-  const securityActions = bindActionCreators(securityActionCreators, dispatch)
-  const uiActions = bindActionCreators(uiActionCreators, dispatch)
-  const toggleModal = uiActions.toggleAccountManagementModal
-  function onDelete(toDelete) {
-    toggleModal(null)
-    securityActions.deleteSSLCertificate('udn', toDelete.get('account'), toDelete.get('group'), toDelete.get('cn'))
-      .then(() => {
-        securityActions.resetCertificateToEdit()
-      })
-  }
   return {
-    fetchAccount,
-    onDelete,
-    securityActions: securityActions,
-    toggleModal
-  };
+    fetchAccount
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    accounts: state.account.get('allAccounts'),
+    activeAccount: state.account.get('activeAccount'),
+    groups: state.group.get('allGroups')
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Security))
--- src/containers/security/tabs/ssl-certificate.jsx
+++ src/containers/security/tabs/ssl-certificate.jsx
@@ -1,13 +1,160 @@
-import React, {Component} from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { withRouter } from 'react-router'
+import { FormattedMessage } from 'react-intl'
+import { List } from 'immutable'
 
+import * as accountActionCreators from '../../../redux/modules/account'
+import * as securityActionCreators from '../../../redux/modules/security'
+import * as uiActionCreators from '../../../redux/modules/ui'
+
+import ModalWindow from '../../../components/modal'
+import CertificateForm from '../../../components/security/certificate-form-container'
 import SSLList from '../../../components/security/ssl-list'
+import LoadingSpinner from '../../../components/loading-spinner/loading-spinner'
+
+import {
+  UPLOAD_CERTIFICATE,
+  EDIT_CERTIFICATE,
+  DELETE_CERTIFICATE
+} from '../../../constants/account-management-modals.js'
 
 class TabSslCertificate extends Component {
+  componentWillMount() {
+    this.fetchData(this.props)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if ( !Object.is(nextProps.params, this.props.params) ) this.fetchData(nextProps)
+  }
+
+  fetchData(props) {
+    const { params: { brand, account, group } } = props
+
+    this.props.securityActions.fetchSSLCertificates(brand, Number(account), Number(group))
+  }
+
   render(){
+    const {
+      activeAccount,
+      activeModal,
+      activeCertificates,
+      fetchAccount,
+      groups,
+      toggleModal,
+      onDelete,
+      securityActions: { toggleActiveCertificates, fetchSSLCertificate },
+      sslCertificates,
+      toDelete
+      } = this.props
+
+    const modalTitle = EDIT_CERTIFICATE
+      ? <FormattedMessage id="portal.security.editCertificate.text"/>
+      : <FormattedMessage id="portal.security.uploadCertificate.text"/>
+
+    const certificateFormProps = {
+      title: modalTitle,
+      activeAccount,
+      fetchAccount,
+      toggleModal
+    }
+
+    const sslListProps = {
+      groups,
+      activeModal,
+      activeCertificates,
+      certificates: sslCertificates,
+      onCheck: commonName => toggleActiveCertificates(commonName),
+      uploadCertificate: () => toggleModal(UPLOAD_CERTIFICATE),
+      editCertificate: (...args) => fetchSSLCertificate(...args).then(() => toggleModal(EDIT_CERTIFICATE)),
+      deleteCertificate: (...args) => fetchSSLCertificate(...args).then(() => toggleModal(DELETE_CERTIFICATE))
+    }
+
+    if ( !this.props.params.group) return (
+      <p className='text-center'>Please select a group<br/>
+        from top left to see SSL Ceritificates</p>
+    )
+
+    if (this.props.isFetching) {
+      return <LoadingSpinner />
+    }
+
     return (
-      <SSLList {...this.props}/>
+      <div className='ssl-certificate-tab'>
+
+        <SSLList {...sslListProps}/>
+
+        {activeModal === EDIT_CERTIFICATE && <CertificateForm {...certificateFormProps}/>}
+        {activeModal === UPLOAD_CERTIFICATE && <CertificateForm {...certificateFormProps}/>}
+        {activeModal === DELETE_CERTIFICATE &&
+          <ModalWindow
+            title={<FormattedMessage id="portal.deleteModal.header.text" values={{itemToDelete: "Certificate"}}/>}
+            cancelButton={true}
+            deleteButton={true}
+            cancel={() => toggleModal(null)}
+            submit={() => onDelete(toDelete)}
+            invalid={true}
+            verifyDelete={true}>
+            <p>
+              <FormattedMessage id="portal.deleteModal.warning.text" values={{itemToDelete : "Certificate"}}/>
+            </p>
+          </ModalWindow>
+        }
+      </div>
     )
   }
 }
 
-export default TabSslCertificate
+TabSslCertificate.propTypes = {
+  activeAccount: PropTypes.instanceOf(Map),
+  activeCertificates: PropTypes.instanceOf(List),
+  activeModal: PropTypes.string,
+  fetchAccount: PropTypes.func,
+  groups: PropTypes.instanceOf(List),
+  isFetching: PropTypes.bool,
+  onDelete: PropTypes.func,
+  params: PropTypes.object,
+  securityActions: PropTypes.object,
+  sslCertificates: PropTypes.instanceOf(List),
+  toDelete: PropTypes.instanceOf(Map),
+  toggleModal: PropTypes.func
+}
+
+function mapStateToProps(state, ownProps) {
+  return {
+    toDelete: state.security.get('certificateToEdit'),
+    activeCertificates: state.security.get('activeCertificates'),
+    activeModal: state.ui.get('accountManagementModal'),
+    accounts: state.account.get('allAccounts'),
+    activeAccount: state.account.get('activeAccount'),
+    groups: state.group.get('allGroups'),
+    isFetching: state.security.get('fetching'),
+    sslCertificates: state.security.get('sslCertificates').filter(cert =>
+      ownProps.params.group && cert.get('group') === Number(ownProps.params.group)
+    )
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  const fetchAccount = bindActionCreators(accountActionCreators, dispatch).fetchAccount
+  const securityActions = bindActionCreators(securityActionCreators, dispatch)
+  const uiActions = bindActionCreators(uiActionCreators, dispatch)
+  const toggleModal = uiActions.toggleAccountManagementModal
+
+  function onDelete(toDelete) {
+    toggleModal(null)
+    securityActions.deleteSSLCertificate('udn', toDelete.get('account'), toDelete.get('group'), toDelete.get('cn'))
+      .then(() => {
+        securityActions.resetCertificateToEdit()
+      })
+  }
+  return {
+    fetchAccount,
+    onDelete,
+    securityActions: securityActions,
+    toggleModal
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(TabSslCertificate))
