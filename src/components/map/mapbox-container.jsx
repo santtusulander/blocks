88bbf8737c213f94d0c576f566dd75e0218deba5
Niklas Lepisto <niklas.lepisto@idean.com>
2016-12-07 14:13:35 -0800

    Original mapbox.jsx was renamed and was missing from the repo

---
+++ src/components/map/mapbox-container.jsx
@@ -0,0 +1,67 @@
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+
+import {getTheme} from '../../redux/modules/ui'
+import {fetchCountries, getCountryTopo, clearCountries} from '../../redux/modules/topo'
+
+import * as countriesGeoJSON from '../../assets/topo/countries.geo.json';
+
+import MapPoc from './poc'
+
+class MapBox extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  componentWillMount(){
+    this.fetchData()
+  }
+
+  shouldComponentUpdate(){
+    return true
+  }
+
+  componentWillUnmount(){
+    this.props.clearCountryTopo()
+  }
+
+  fetchData(){
+    this.props.getCountryTopo()
+  }
+
+  render(){
+    const {geoData, theme} = this.props
+
+    return (
+      <MapPoc
+        geoData={countriesGeoJSON}
+        theme={theme}
+      />
+    )
+  }
+
+
+}
+
+MapBox.propTypes = {
+  clearCountryTopo: PropTypes.func,
+  geoData: PropTypes.object,
+  getCountryTopo: PropTypes.func,
+  theme: PropTypes.string
+}
+
+const mapStateToProps = (state) => {
+  return {
+    geoData: getCountryTopo(state),
+    theme: getTheme(state)
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    clearCountryTopo: () => dispatch( clearCountries() ),
+    getCountryTopo: () => dispatch( fetchCountries() )
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MapBox)
