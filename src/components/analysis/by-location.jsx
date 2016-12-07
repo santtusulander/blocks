b51290a6178fcdb989b725e81ef81b17b12c7e3d
Niklas Lepisto <niklas.lepisto@idean.com>
2016-12-07 13:52:49 -0800

    Initial Mapbox implementation for analysis page

--- src/components/analysis/by-location.jsx
+++ src/components/analysis/by-location.jsx
@@ -1,32 +1,18 @@
 import React from 'react'
-import d3 from 'd3'
-import numeral from 'numeral'
-import * as topojson from 'topojson'
 import Immutable from 'immutable'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import classNames from 'classnames'
 
 import * as topoActionCreators from '../../redux/modules/topo'
-import Tooltip from '../tooltip'
+
+import Mapbox from '../map/mapbox';
+
+import * as countriesGeoJSON from '../../assets/topo/custom.geo.json';
 
 class AnalysisByLocation extends React.Component {
   constructor(props) {
     super(props)
-
-    this.state = {
-      zoom: null,
-      statesHidden: true,
-      citiesHidden: true,
-      tooltipCountry: null,
-      tooltipPercent: 0,
-      tooltipX: 0,
-      tooltipY: 0
-    }
-
-    // this.selectCountry = this.selectCountry.bind(this)
-    // this.selectState = this.selectState.bind(this)
-    // this.zoomOut = this.zoomOut.bind(this)
   }
   componentWillMount() {
     if(this.props.topoActions) {
@@ -34,225 +20,22 @@ class AnalysisByLocation extends React.Component {
       this.props.topoActions.fetchCountries()
     }
   }
-  // selectCountry(country, path) {
-  //   const id = country.id.toLowerCase()
-  //   return () => {
-  //     this.setState({
-  //       statesHidden: true,
-  //       citiesHidden: true
-  //     })
-  //     this.props.topoActions.changeActiveCountry(id)
-  //     this.props.topoActions.fetchStates(id).then((action) => {
-  //       this.setState({zoom: this.getZoomBounds(country, path)})
-  //       if(action.error) {
-  //         return false;
-  //       }
-  //       setTimeout(() => {
-  //         this.setState({statesHidden: false})
-  //       }, 500)
-  //     })
-  //   }
-  // }
-  // selectState(state, path) {
-  //   const name = state.properties.name
-  //   return () => {
-  //     this.setState({citiesHidden: true})
-  //     this.props.topoActions.changeActiveState(name)
-  //     this.props.topoActions.fetchCities(this.props.activeCountry).then((action) => {
-  //       this.setState({zoom: this.getZoomBounds(state, path)})
-  //       if(action.error) {
-  //         return false;
-  //       }
-  //       setTimeout(() => {
-  //         this.setState({citiesHidden: false})
-  //       }, 500)
-  //     })
-  //   }
-  // }
-  // getZoomBounds(d, path) {
-  //   const bounds = path.bounds(d)
-  //   const w_scale = (bounds[1][0] - bounds[0][0]) / this.props.width
-  //   const h_scale = (bounds[1][1] - bounds[0][1]) / this.props.height
-  //   const z = 0.96 / Math.max(w_scale, h_scale)
-  //   const x = (bounds[1][0] + bounds[0][0]) / 2
-  //   const y = (bounds[1][1] + bounds[0][1]) / 2 + (this.props.height / z / 6)
-  //   return [x, y, z]
-  // }
-  // zoomOut(path) {
-  //   return (e) => {
-  //     e.preventDefault()
-  //     if(this.props.activeState) {
-  //       this.props.topoActions.changeActiveState(null)
-  //       const countries = topojson.feature(
-  //         this.props.countries.toJS(),
-  //         this.props.countries.toJS().objects.countries
-  //       ).features
-  //       const country = countries.find(
-  //         data => data.id.toLowerCase() === this.props.activeCountry
-  //       )
-  //       this.setState({zoom: this.getZoomBounds(country, path)})
-  //     }
-  //     else if(this.props.activeCountry) {
-  //       this.props.topoActions.changeActiveCountry(null)
-  //       this.setState({zoom: null})
-  //     }
-  //   }
-  // }
-  moveMouse(country, percent) {
-    return e => {
-      const bounds = this.refs.chart.getBoundingClientRect()
-      e.stopPropagation()
-      this.setState({
-        tooltipCountry: country,
-        tooltipPercent: percent,
-        tooltipX: e.pageX - bounds.left,
-        tooltipY: e.pageY - (bounds.top + window.pageYOffset)
-      })
-    }
-  }
+
   render() {
     if(!this.props.width || !this.props.countries || !this.props.countries.size
       || this.props.fetching || !this.props.countryData) {
       return <div>Loading...</div>
     }
 
-    const projection = d3.geo.mercator().scale(this.props.width / 7)
-      .translate([this.props.width / 2, this.props.height / 1.45])
-    const path = d3.geo.path().projection(projection)
-    const countries = topojson.feature(
-      this.props.countries.toJS(),
-      this.props.countries.toJS().objects.countries
-    ).features
-
-    // let states = null
-    // if(this.props.states && this.props.states.size) {
-    //   states = topojson.feature(
-    //     this.props.states.toJS(),
-    //     this.props.states.toJS().objects.states
-    //   ).features
-    // }
-    //
-    // let cities = null
-    // if(this.props.cities && this.props.cities.size) {
-    //   cities = topojson.feature(
-    //     this.props.cities.toJS(),
-    //     this.props.cities.toJS().objects.cities
-    //   ).features.filter(d => {
-    //     return this.props.activeState == d.properties.state
-    //   })
-    // }
-
-    let transform = ''
-    let strokeWidth = 1
-    // if(this.state.zoom) {
-    //   let projTranslate = projection.translate()
-    //   transform = "translate(" +
-    //   projTranslate[0] + 'px,' +
-    //   projTranslate[1] +
-    //   "px) scale(" +
-    //   this.state.zoom[2] +
-    //   ") translate(-" +
-    //   this.state.zoom[0] +
-    //   "px,-" +
-    //   this.state.zoom[1] +
-    //   "px)"
-    //   strokeWidth = 1.0 / this.state.zoom[2] + "px"
-    // }
-    const pathStyle = {
-      transform: transform,
-      strokeWidth: strokeWidth
-    }
-
     return (
       <div className={classNames(
         'analysis-by-location',
         { 'no-bg': this.props.noBg })}>
-        <div className="chart" ref="chart">
-          <svg
-            viewBox={'0 0 ' + this.props.width + ' ' + this.props.height}
-            width={this.props.width}
-            height={this.props.height}
-            onMouseMove={this.moveMouse(null, null)}
-            onMouseLeave={this.moveMouse(null, null)}>
-            {countries.map((country, i) => {
-              // let hideCountry = false
-              const id = country.id.toLowerCase()
-              // if(!this.state.statesHidden &&
-              //   this.props.activeCountry === id) {
-              //   hideCountry = true
-              // }
-              const dataIndex = this.props.countryData.findIndex(
-                data => data.get('code').toLowerCase() === id
-              )
-              let classes = `country country-${dataIndex}`
-              // if(hideCountry) {
-              //   classes += ' hiddenpath'
-              // }
-              let label = ''
-              if(dataIndex >= 0 && this.props.countryData.get(dataIndex)) {
-                label = this.props.tooltipCustomFormat ?
-                  this.props.tooltipCustomFormat(
-                    this.props.countryData.get(dataIndex).get(this.props.dataKey) || 0
-                  ) : numeral(
-                    this.props.countryData.get(dataIndex).get(this.props.dataKey) || 0
-                  ).format('0,0')
-              }
-              return (
-                <path key={i} d={path(country)}
-                  onMouseMove={this.moveMouse(country.id, label)}
-                  className={classes}
-                  style={pathStyle}/>
-              )
-            })}
-            {/*states ? states.map((state, i) => {
-              const data = this.props.stateData.find(
-                data => data.get('id') === state.properties.name
-              )
-              let classes = 'country'
-              if(this.state.statesHidden) {
-                classes += ' hiddenpath'
-              }
-              if(data) {
-                classes += ' ' + getTrendClass(data)
-              }
-              return (
-                <path key={i} d={path(state)}
-                  className={classes}
-                  style={pathStyle}
-                  onClick={this.selectState(state, path)}/>
-              )
-            }) : null*/}
-            {/*cities ? cities.map((city, i) => {
-              const data = this.props.cityData.find(
-                data => data.get('name') === city.properties.name && data.get('state') === city.properties.state
-              )
-              let classes = 'country'
-              if(this.state.citiesHidden) {
-                classes += ' hiddenpath'
-              }
-              if(data) {
-                classes += ' ' + getTrendClass(data)
-              }
-              let zoomLevel = this.state.zoom ? this.state.zoom[2] : 1
-              return (
-                <path key={i} d={path.pointRadius(20 / zoomLevel)(city)}
-                  className={classes}
-                  style={pathStyle}/>
-              )
-            }) : null*/}
-          </svg>
-          {/*<div className="zoom-out">
-            <a href="#" onClick={this.zoomOut(path)}
-              title="Zoom Out"
-              className={this.props.activeCountry || this.props.activeState ? '' : 'hidden'}>
-              -
-            </a>
-          </div>*/}
-        </div>
-        <Tooltip x={this.state.tooltipX} y={this.state.tooltipY}
-          hidden={!this.state.tooltipCountry}>
-          {this.state.tooltipCountry} {this.state.tooltipPercent}
-        </Tooltip>
+        <Mapbox
+          geoData={countriesGeoJSON}
+          countryData={this.props.countryData.toJS()}
+          cityData={[]}
+          theme="dark" />
       </div>
     )
   }
--- src/components/map/mapbox.jsx
+++ src/components/map/mapbox.jsx
@@ -1,67 +1,322 @@
-import React, {Component, PropTypes} from 'react'
-import {connect} from 'react-redux'
+import React from 'react'
+import ReactMapboxGl, { Layer, Feature, Popup, ZoomControl } from 'react-mapbox-gl'
+import Typeahead from 'react-bootstrap-typeahead'
 
-import {getTheme} from '../../redux/modules/ui'
-import {fetchCountries, getCountryTopo, clearCountries} from '../../redux/modules/topo'
+import {MAPBOX_LIGHT_THEME, MAPBOX_DARK_THEME} from '../../constants/mapbox'
 
-import * as countriesGeoJSON from '../../assets/topo/countries.geo.json';
+import IconExpand from '../icons/icon-expand';
+import IconMinimap from '../icons/icon-minimap';
+import IconGlobe from '../icons/icon-globe';
 
-import MapPoc from './poc'
+const heatMapColors = [
+  '#e32119', //red dark
+  '#e32119', //red light
+  '#00a9d4', //cyan
+  '#009f80', //green
+  '#89ba17'  //lime
+]
 
-class MapBox extends Component {
-  constructor(props){
+const cities = [
+  { id: 'LDN', countryId: 'GBR', name: 'London', bytes: 500000, requests: 10000, position: [-0.1278, 51.5074] },
+  { id: 'MCR', countryId: 'GBR', name: 'Manchester', bytes: 300000, requests: 10000, position: [-2.2426, 53.4808] },
+  { id: 'NYC', countryId: 'USA', name: 'New York', bytes: 400000, requests: 20000, position: [-74.0059, 40.7128] },
+  { id: 'BTN', countryId: 'USA', name: 'Boston', bytes: 300000, requests: 20000, position: [-71.0589, 42.3601] },
+  { id: 'WCR', countryId: 'USA', name: 'Worchester', bytes: 200000, requests: 20000, position: [-71.8023, 42.2626] },
+  { id: 'HEL', countryId: 'FIN', name: 'Helsinki', bytes: 300000, requests: 30000, position: [24.9384, 60.1699] },
+  { id: 'TKU', countryId: 'FIN', name: 'Turku', bytes: 200000, requests: 40000, position: [22.2666, 60.4518] },
+  { id: 'BJN', countryId: 'CHN', name: 'Beijing', bytes: 100000, requests: 50000, position: [116.4074, 39.9042] }
+]
+
+/**
+ * Calculate Median -value
+ * @param values
+ * @returns {value}
+ */
+const calculateMedian = (values) => {
+
+  values.sort( function(a,b) {return a - b;} );
+
+  var half = Math.floor(values.length/2);
+
+  if(values.length % 2)
+    return values[half];
+  else
+    return (values[half-1] + values[half]) / 2.0;
+}
+/**
+ * Get Score for value compared to 'median' range (0 - steps)
+ * @param median
+ * @param value
+ * @param steps
+ * @returns {number}
+ */
+const getScore = (median, value, steps = 5) => {
+
+  const diff = (value / median)
+  const score = Math.ceil(diff * (steps / 2)) ;
+
+  return score;
+}
+
+class Mapbox extends React.Component {
+  constructor(props) {
     super(props)
+    this.state = {
+      zoom: 1,
+      countryGeoJson: null,
+      popupCoords: [0, 0],
+      popupContent: null,
+      layers: [],
+      hoveredLayer: null
+    }
+
+    this.countryGeoJson = {
+      type: 'FeatureCollection',
+      features: []
+    }
+
+    this.map = null;
+
+    this.setHoverStyle = this.setHoverStyle.bind(this);
+
+  }
+  componentDidMount() {
+    // TODO: Move this to be handled either a on parent level or in a reducer / API
+    // GeoJSON should just passed in as a prop.
+
   }
 
-  componentWillMount(){
-    this.fetchData()
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.countryData !== this.props.countryData) {
+      this.countryGeoJson.features = this.props.geoData.features.filter((data) => {
+        const countryExists = this.props.countryData.some(country => country.code === data.properties.iso_a3)
+
+        if (countryExists) {
+          if (!this.map.getSource('geo-' + data.properties.iso_a3)) {
+            this.map.addSource('geo-' + data.properties.iso_a3, {
+              type: 'geojson',
+              data: { type: 'FeatureCollection', features: [data] }
+            })
+
+            this.setState({ layers: this.state.layers.push('country-fill-' + data.properties.iso_a3) })
+          }
+          return data
+        }
+      })
+    }
   }
 
-  shouldComponentUpdate(){
-    return true
+  onStyleLoaded(map) {
+    this.map = map;
   }
 
-  componentWillUnmount(){
-    this.props.clearCountryTopo()
+  onZoomEnd(e){
+    this.setState({ scale: e.transform.scale, zoom: e.transform._zoom })
   }
 
-  fetchData(){
-    this.props.getCountryTopo()
+  openPopup(content, coords) {
+    this.setState({ popupContent: content, popupCoords: coords })
   }
 
-  render(){
-    const {geoData, theme} = this.props
+  closePopup() {
+    this.setState({ popupContent: null })
+  }
+
+  onMouseMove(map, feature) {
+    if (map.style._loaded) {
+      if (this.state.hoveredLayer) {
+        this.setHoverStyle(map)('opacity', 0.5)('default')
+        this.setState({ hoveredLayer: null })
+        this.closePopup()
+      }
+
+      const features = map.queryRenderedFeatures(feature.point, { layers: this.state.layers })
+
+      if (features.length) {
+        const hoveredLayer = { id: features[0].layer.id, type: features[0].layer.type }
+
+        this.setState({ hoveredLayer })
+        this.setHoverStyle(map)('opacity', 0.9)('pointer')
+        this.openPopup(features[0].properties.name, [feature.lngLat.lng, feature.lngLat.lat])
+      }
+    }
+  }
+
+  /**
+   * Set hover style for a hovered layer. Style param should match one of the
+   * paint property names defined in Mapbox Style Spec
+   * https://www.mapbox.com/mapbox-gl-style-spec/#layer-paint
+   * The type prefix (e.g. 'fill') is not needed as it's already being gotten
+   * from this.state.hoveredLayer.
+   * @param map
+   * @param style, value
+   * @param cursor
+   * @returns {function}
+   */
+  setHoverStyle(map) {
+    return (style, value) => (cursor) => {
+      map.setPaintProperty(this.state.hoveredLayer.id, this.state.hoveredLayer.type + '-' + style, value)
+      map.getCanvas().style.cursor = cursor
+    }
+  }
+
+  renderCountryHighlight() {
+    const countryMedian = calculateMedian(this.props.countryData.map((country => country.total)))
+
+    const highlights = this.countryGeoJson.features.map((country, i) => {
+      const trafficCountry = this.props.countryData.find(c => c.code === country.properties.iso_a3)
+      const trafficHeat = trafficCountry && getScore(countryMedian, trafficCountry.total)
+      const countryColor = trafficCountry && trafficHeat < heatMapColors.length ? heatMapColors[trafficHeat - 1] : '#00a9d4'
+
+      return (
+        <div key={i}>
+          <Layer
+            id={`country-fill-${country.properties.iso_a3}`}
+            type="fill"
+            sourceId={`geo-${country.properties.iso_a3}`}
+            paint={{
+              'fill-color': countryColor,
+              'fill-opacity': 0.5
+            }}>
+              <Feature
+                properties={{
+                  name: country.properties.name
+                }}
+                coordinates={country.geometry.coordinates}/>
+          </Layer>
+
+          <Layer
+            id={`country-stroke-${country.properties.iso_a3}`}
+            type="line"
+            sourceId={`geo-${country.properties.iso_a3}`}
+            paint={{
+              'line-color': countryColor,
+              'line-width': 2
+            }}/>
+        </div>
+      )
+    })
+
+    return highlights
+  }
+
+  renderCityCircles() {
+    const cityMedian = calculateMedian( cities.map( (city => city.bytes) ) )
+
+    return cities.map((city, i) => {
+      const cityHeat = getScore(cityMedian, city.bytes)
+      const cityColor = cityHeat ? heatMapColors[ cityHeat - 1 ] : '#000000'
+      const cityId = city.name.split(' ').join('-').toLowerCase()
+
+      return (
+        <Layer
+          id={cityId}
+          key={i}
+          type="circle"
+          paint={{
+            'circle-radius': cityHeat * (this.state.scale / 5),
+            'circle-color': cityColor,
+            'circle-opacity': 0.5
+          }}>
+          <Feature
+            coordinates={city.position}
+            properties={{
+              name: city.name
+            }} />
+        </Layer>
+      )
+    })
+  }
+
+  onZoomClick(map, value) {
+    return value < 0 ? map.zoomOut() : map.zoomIn()
+  }
+
+  render() {
+    const mapboxUrl = (this.props.theme === 'light') ? MAPBOX_LIGHT_THEME : MAPBOX_DARK_THEME
 
     return (
-      <MapPoc
-        geoData={countriesGeoJSON}
-        theme={theme}
-      />
+      <ReactMapboxGl
+        accessToken="pk.eyJ1IjoiZXJpY3Nzb251ZG4iLCJhIjoiY2lyNWJsZGVmMDAxYmcxbm5oNjRxY2VnZCJ9.r1KILF4ik_gkwZ4BCyy1CA"
+        style={mapboxUrl}
+        containerStyle={{
+          height: '600px'
+        }}
+        zoom={[this.state.zoom]}
+        minZoom={1}
+        maxZoom={13}
+        center={cities[0].position}
+        onZoom={this.onZoomEnd.bind(this)}
+        onStyleLoad={this.onStyleLoaded.bind(this)}
+        onMouseMove={this.onMouseMove.bind(this)}>
+
+        <div className="map-search">
+          <Typeahead
+            minLength={1}
+            onChange={() => null}
+            options={[
+              {id: 'BY', label: 'Belarus'},
+              {id: 'CA', label: 'Canada'},
+              {id: 'FI', label: 'Finland'},
+              {id: 'DE', label: 'Germany'},
+              {id: 'SE', label: 'Sweden'},
+              {id: 'UA', label: 'Ukraine'},
+              {id: 'US', label: 'United States'}
+            ]}/>
+        </div>
+
+        {this.renderCountryHighlight()}
+        {this.renderCityCircles()}
+
+        {!!this.state.popupContent &&
+          <Popup anchor="bottom-left" coordinates={this.state.popupCoords}>
+            <span>{this.state.popupContent}</span>
+          </Popup>
+        }
+
+        <div className="map-controls">
+          <div className="control map-fullscreen">
+            <IconExpand width={32} height={32} />
+          </div>
+          <div className="control map-zoom">
+            <ZoomControl
+              style={{
+                height: 'calc(100% - 32px)',
+                display: 'flex',
+                justifyContent: 'space-between',
+                boxShadow: 'none',
+                border: 0,
+                position: 'relative',
+                top: 'auto',
+                right: 'auto',
+                zIndex: 1
+              }}
+              onControlClick={this.onZoomClick.bind(this)} />
+            <div className="map-zoom-reset">
+              <IconGlobe width={32} height={32} />
+            </div>
+          </div>
+          <div className="control map-minimap">
+            <IconMinimap width={32} height={32} />
+          </div>
+        </div>
+
+        <div className="map-heat-legend">
+          <span>Low</span>
+          <div className="heat-gradient" />
+          <span>High</span>
+        </div>
+
+      </ReactMapboxGl>
     )
   }
-
-
 }
 
-MapBox.propTypes = {
-  clearCountryTopo: PropTypes.func,
-  geoData: PropTypes.object,
-  getCountryTopo: PropTypes.func,
-  theme: PropTypes.string
+Mapbox.propTypes = {
+  cityData: React.PropTypes.array,
+  countryData: React.PropTypes.array,
+  geoData: React.PropTypes.object,
+  theme: React.PropTypes.string
 }
 
-const mapStateToProps = (state) => {
-  return {
-    geoData: getCountryTopo(state),
-    theme: getTheme(state)
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    clearCountryTopo: () => dispatch( clearCountries() ),
-    getCountryTopo: () => dispatch( fetchCountries() )
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(MapBox)
+export default Mapbox;
