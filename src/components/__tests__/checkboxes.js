f43e6d06bf9f6d9b24c8274793bd8ad314805a11
santtu-idean <santtu.sulander@idean.com>
2016-12-20 13:13:10 +0200

    use classnames-module for checkboxes classnames, create exists test for checkboxes

---
+++ src/components/__tests__/checkboxes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+jest.unmock('../checkboxes')
+import Checkboxes from '../checkboxes'
+
+const subject = shallow(<Checkboxes iterable={[]} field={{ value: [] }}/>)
+
+describe('Confirmation', () => {
+  it('should exist', () => {
+    expect(subject.length).toBe(1)
+  })
+
+})
