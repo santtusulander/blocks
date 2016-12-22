08db1c5cd0dca15ddb028009fde41cc48706d935
Niklas Lepisto <niklas.lepisto@idean.com>
2016-12-21 16:29:27 -0800

    Zoom reset was hidden under other controls when having set height for controls

--- src/components/map/mapbox.jsx
+++ src/components/map/mapbox.jsx
@@ -376,7 +376,6 @@ class Mapbox extends React.Component {
           <div className="control map-zoom">
             <ZoomControl
               style={{
-                height: 'calc(100% - 32px)',
                 display: 'flex',
                 justifyContent: 'space-between',
                 boxShadow: 'none',
