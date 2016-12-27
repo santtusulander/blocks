527e2294f280a7679069b29f971f8495232052a5
Mykhailo Prochkaruk <mprochkaruk@vidscale.com>
2016-12-27 10:48:09 +0200

    Add small loading spinner implementation

---
+++ src/components/loading-spinner/loading-spinner-sm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './loading-spinner.scss'
+
+function LoadingSpinnerSmall() {
+  return (
+    <div className='loading-spinner-sm'>
+      <div className='spinner-content-sm' />
+    </div>
+  )
+}
+
+export default LoadingSpinnerSmall
