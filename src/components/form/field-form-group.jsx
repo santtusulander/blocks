e0e7083be96c91f525cdd244def2d1bf8d6ad2dc
Mikko Alander <mikko.alander@idean.com>
2016-12-27 12:57:08 +0200

    add FieldFormGroup component

---
+++ src/components/form/field-form-group.jsx
@@ -0,0 +1,28 @@
+import React, { PropTypes } from 'react';
+import {FormGroup, FormControl, ControlLabel, HelpBlock} from 'react-bootstrap';
+
+const FieldFormGroup  = ({ input, placeholder, type, meta: { dirty, touched, error }, children }) => {
+  return (
+    <FormGroup controlId={input.name} validationState={touched && error ? 'error' : null}>
+      <ControlLabel>{children}</ControlLabel>
+      <FormControl type={type} placeholder={placeholder} value={input.value} onChange={input.onChange} />
+      <FormControl.Feedback />
+
+      {error && dirty &&
+        <HelpBlock className='error-msg'>{error}</HelpBlock>
+      }
+    </FormGroup>
+  );
+}
+
+FieldFormGroup.propTypes = {
+  children: PropTypes.object,
+  input: PropTypes.object,
+  label: PropTypes.object,
+  meta: PropTypes.object,
+  placeholder: PropTypes.string,
+  type: PropTypes.string
+
+}
+
+export default FieldFormGroup
