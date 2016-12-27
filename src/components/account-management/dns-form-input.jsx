4371f860a424825731058dcf46472b67c45aaa13
santtu-idean <santtu.sulander@idean.com>
2016-12-27 17:10:40 +0200

    new redux form Field-component for record form inputs

--- src/components/account-management/dns-form-input.jsx
+++ src/components/account-management/dns-form-input.jsx
@@ -1,21 +1,21 @@
 import React, { PropTypes } from 'react'
-import { FormControl, FormGroup, InputGroup, ControlLabel } from 'react-bootstrap'
+import { FormControl, FormGroup, InputGroup, ControlLabel, HelpBlock } from 'react-bootstrap'
 import { FormattedMessage } from 'react-intl'
 
-const Input = ({ id, required = true, addonAfter, labelID, children, isVisible = true, ...inputProps }) =>
+const Input = ({ id, required = true, addonAfter, labelID, meta: { error, touched }, isVisible = true, input }) =>
   isVisible &&
     <FormGroup controlId={id}>
       <ControlLabel>
         <FormattedMessage id={labelID}/>{required && ' *'}
       </ControlLabel>
       <InputGroup>
-        <FormControl {...inputProps}/>
+        <FormControl {...input}/>
         {addonAfter &&
           <InputGroup.Addon>
             {addonAfter}
           </InputGroup.Addon>}
       </InputGroup>
-      {children}
+      {touched && error && <HelpBlock className='error-msg' id={id + '-err'}>{error}</HelpBlock>}
     </FormGroup>
 
 Input.propTypes = {
--- src/components/account-management/record-form.jsx
+++ src/components/account-management/record-form.jsx
@@ -1,93 +1,107 @@
 import React, { PropTypes } from 'react'
 import { ButtonToolbar, Button } from 'react-bootstrap'
 import { injectIntl, FormattedMessage } from 'react-intl'
+import { Field } from 'redux-form'
 
 import Input from './dns-form-input'
-import SelectWrapper from '../select-wrapper'
+import FormGroupSelect from '../form/field-form-group-select'
+
 import keyStrokeSupport from '../../decorators/key-stroke-decorator'
+
 import recordTypes from '../../constants/dns-record-types'
 
 import './record-form.scss'
 
-const RecordForm = ({ domain, loading, edit, submit, cancel, invalid, fields: { type, name, value, ttl, prio }, shouldShowField, intl }) =>
-  <form>
-    <SelectWrapper
-      {...type}
-      disabled={edit}
-      options={recordTypes.map(type => [type, type])}
-      label={intl.formatMessage({id: 'portal.account.recordForm.selectRecordType.label'})}
-    />
-    <Input
-      {...name}
-      id="name-field"
-      isVisible={shouldShowField('name')}
-      labelID="portal.account.recordForm.hostName.label"
-      disabled={edit}
-      placeholder={intl.formatMessage({ id: 'portal.account.recordForm.hostName.placeholder'})}
-      className="input-narrow host-name-input"
-      addonAfter={`.${domain}`}>
-      {name.touched && name.error && <div className='error-msg' id='name-err'>{name.error}</div>}
-    </Input>
-    <Input
-      {...value}
-      id="value-field"
-      isVisible={shouldShowField('value')}
-      labelID="portal.account.recordForm.address.label"
-      disabled={edit}
-      placeholder={intl.formatMessage({ id: 'portal.account.recordForm.address.placeholder'})}>
-      {value.touched && value.error && <div className='error-msg' id='value-err'>{value.error}</div>}
-    </Input>
-    <Input
-      {...prio}
-      id="prio-field"
-      isVisible={shouldShowField('prio')}
-      labelID="portal.account.recordForm.prio.label"
-      disabled={edit}
-      required={false}
-      className='input-narrow priority-input'
-      placeholder={intl.formatMessage({ id: 'portal.account.recordForm.prio.placeholder'})}>
-      {prio.touched && prio.error && <div className='error-msg' id='prio-err'>{prio.error}</div>}
-    </Input>
-    {shouldShowField('ttl') && [
-      <hr key={0} />,
-      <Input
-        key={1}
-        {...ttl}
-        id="ttl-field"
-        labelID="portal.account.recordForm.ttl.label"
-        className='input-narrow ttl-value-input'
-        placeholder={intl.formatMessage({ id: 'portal.account.recordForm.ttl.placeholder'})}
-        addonAfter="seconds">
-        {ttl.touched && ttl.error && <div className='error-msg' id='ttl-err'>{ttl.error}</div>}
-      </Input>
-    ]}
-    <ButtonToolbar className="text-right extra-margin-top">
-      <Button
-        id='cancel-button'
-        className="btn-outline"
-        onClick={cancel}>
-        <FormattedMessage id='portal.common.button.cancel' />
-      </Button>
-      <Button
-        id='submit-button'
-        disabled={invalid || loading}
-        bsStyle="primary"
-        onClick={submit}>{loading ? <FormattedMessage id='portal.common.button.saving' />  : edit ? <FormattedMessage id='portal.common.button.save' /> : <FormattedMessage id='portal.common.button.add' />}</Button>
-    </ButtonToolbar>
-  </form>
-
-RecordForm.displayName = 'RecordForm'
+const RecordForm = ({ domain, loading, edit, onSubmit, cancel, invalid, shouldShowField, intl }) => {
+  return (
+    <form>
+      <Field
+        name="type"
+        component={input =>
+          <FormGroupSelect
+            {...input}
+            disabled={edit}
+            options={recordTypes.map(type => [type, type])}
+            label={intl.formatMessage({id: 'portal.account.recordForm.selectRecordType.label'})}/>
+      }/>
+      <Field
+        name="name"
+        component={input =>
+          <Input
+            {...input}
+            id="name-field"
+            isVisible={shouldShowField('name')}
+            labelID="portal.account.recordForm.hostName.label"
+            disabled={edit}
+            placeholder={intl.formatMessage({ id: 'portal.account.recordForm.hostName.placeholder'})}
+            className="input-narrow host-name-input"
+            addonAfter={`.${domain}`}/>
+      }/>
+      <Field
+        name="value"
+        component={input =>
+          <Input
+            {...input}
+            id="value-field"
+            isVisible={shouldShowField('value')}
+            labelID="portal.account.recordForm.address.label"
+            disabled={edit}
+            placeholder={intl.formatMessage({ id: 'portal.account.recordForm.address.placeholder'})}/>
+        }/>
+      <Field
+        name="prio"
+        component={input =>
+          <Input
+            {...input}
+            id="prio-field"
+            isVisible={shouldShowField('prio')}
+            labelID="portal.account.recordForm.prio.label"
+            disabled={edit}
+            required={false}
+            className='input-narrow priority-input'
+            placeholder={intl.formatMessage({ id: 'portal.account.recordForm.prio.placeholder'})}/>
+      }/>
+      {shouldShowField('ttl') && [
+        <hr key={0} />,
+        <Field
+          name="ttl"
+          component={input =>
+            <Input
+              {...input}
+              key={1}
+              id="ttl-field"
+              labelID="portal.account.recordForm.ttl.label"
+              className='input-narrow ttl-value-input'
+              placeholder={intl.formatMessage({ id: 'portal.account.recordForm.ttl.placeholder'})}
+              addonAfter="seconds"/>
+        }/>
+      ]}
+      <ButtonToolbar className="text-right extra-margin-top">
+        <Button
+          id='cancel-button'
+          className="btn-outline"
+          onClick={cancel}>
+          <FormattedMessage id='portal.common.button.cancel' />
+        </Button>
+        <Button
+          id='submit-button'
+          disabled={invalid || loading}
+          bsStyle="primary"
+          onClick={onSubmit}>{loading ? <FormattedMessage id='portal.common.button.saving' />  : edit ? <FormattedMessage id='portal.common.button.save' /> : <FormattedMessage id='portal.common.button.add' />}</Button>
+      </ButtonToolbar>
+    </form>
+  )
+}
 
 RecordForm.propTypes = {
   cancel: PropTypes.func,
   domain: PropTypes.string,
   edit: PropTypes.bool,
-  fields: PropTypes.object,
   intl: PropTypes.object,
   invalid: PropTypes.bool,
   loading: PropTypes.bool,
-  shouldShowField: PropTypes.func,
-  submit: PropTypes.func
+  onSubmit: PropTypes.func,
+  shouldShowField: PropTypes.func
 }
 
 export default injectIntl(keyStrokeSupport(RecordForm))
