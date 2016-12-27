d7be7244df586eaa9bd624cb21abb1c9e7380b03
santtu-idean <santtu.sulander@idean.com>
2016-12-27 17:13:29 +0200

    adapt the container for new redux form

--- src/containers/account-management/modals/record-form.jsx
+++ src/containers/account-management/modals/record-form.jsx
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
-import { reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { reduxForm, getFormValues } from 'redux-form'
 import { Modal } from 'react-bootstrap'
 import { injectIntl, FormattedMessage } from 'react-intl'
 
@@ -72,13 +73,13 @@ const validate = (fields, props) => {
 }
 
 const RecordFormContainer = props => {
-  const { domain, edit, updateRecord, addRecord, closeModal, values, activeRecord, ...formProps } = props
-  const filteredValues = filterFields(values)
+  const { domain, edit, updateRecord, addRecord, closeModal, vals = {}, activeRecord, ...formProps } = props
+  const filteredValues = filterFields(vals)
   const recordFormProps = {
     domain,
     edit,
-    shouldShowField: isShown(props.fields.type.value),
-    submit: () => {
+    shouldShowField: isShown(vals.type),
+    onSubmit: () => {
       let { ttl, prio } = filteredValues
       if (ttl) {
         filteredValues.ttl = Number(ttl)
@@ -97,7 +98,7 @@ const RecordFormContainer = props => {
     <Modal show={true} dialogClassName="dns-edit-form-sidebar">
       <Modal.Header>
         <h1>{edit ? <FormattedMessage id='portal.account.recordForm.editRecord.title' /> : <FormattedMessage id='portal.account.recordForm.newRecord.title' />}</h1>
-        {edit && <p>{props.fields.name.value}</p>}
+        {edit && <p>{vals.name}</p>}
       </Modal.Header>
       <Modal.Body>
         <RecordForm {...recordFormProps}/>
@@ -114,50 +115,58 @@ RecordFormContainer.propTypes = {
   edit: PropTypes.bool,
   fields: PropTypes.object,
   updateRecord: PropTypes.func,
-  values: PropTypes.object
+  vals: PropTypes.object
 
 }
 
-function mapStateToProps({ dnsRecords, dns }, { edit }) {
+function mapStateToProps(state, { edit }) {
+  const { dnsRecords, dns } = state
   const getRecordById = recordActionCreators.getById
   let activeRecord = getRecordById(dnsRecords.get('resources'), dnsRecords.get('activeRecord'))
   let initialValues = undefined
-  initialValues = activeRecord && edit && getRecordFormInitialValues(activeRecord.toJS())
+  if (activeRecord && edit) {
+    initialValues = getRecordFormInitialValues(activeRecord.toJS())
+  }
   let props = {
     activeRecord,
+    vals: getFormValues('record-edit')(state),
     domain: dns.get('activeDomain'),
     loading: dnsRecords.get('fetching')
   }
   if (initialValues) {
     props.initialValues = initialValues
   }
+
   return props
 }
 
 function mapDispatchToProps(dispatch, { closeModal }) {
   const { startFetching, createResource, updateResource } = bindActionCreators(recordActionCreators, dispatch)
   return {
-    addRecord: (values, domain) => {
-      values = recordValues(values)
+    addRecord: (vals, domain) => {
+      vals = recordValues(vals)
       // Hardcode class-key as it is not set anywhere
-      values.class = 'IN'
+      vals.class = 'IN'
       startFetching()
-      createResource(domain, values.name, values)
+      createResource(domain, vals.name, vals)
         .then(() => closeModal())
     },
     updateRecord: (formValues, zone, activeRecord) => {
-      let values = recordValues(formValues)
-      values.id = activeRecord.get('id')
-      values.class = 'IN'
+      let vals = recordValues(formValues)
+      vals.id = activeRecord.get('id')
+      vals.class = 'IN'
       startFetching()
-      updateResource(zone, activeRecord.get('name'), values)
+      updateResource(zone, activeRecord.get('name'), vals)
         .then(() => closeModal())
     }
   }
 }
 
-export default injectIntl(reduxForm({
-  form: 'dns-edit',
-  fields: ['type', 'name', 'value', 'ttl', 'prio'],
-  validate
-}, mapStateToProps, mapDispatchToProps)(RecordFormContainer))
+export default injectIntl(
+  connect(mapStateToProps, mapDispatchToProps)(
+    reduxForm({
+      form: 'record-edit',
+      validate
+    })(RecordFormContainer)
+  )
+)
