9075cf68147b49c803e3de54b8cd510791910027
Mikko Alander <mikko.alander@idean.com>
2016-12-27 13:54:30 +0200

    add wrapper for selects

---
+++ src/components/form/field-form-group-select.jsx
@@ -0,0 +1,35 @@
+import React, { PropTypes } from 'react';
+import {FormGroup, ControlLabel, HelpBlock} from 'react-bootstrap';
+import Select from '../select.jsx'
+
+const FieldFormGroupSelect  = ({ input, options, numericValues, className, label, disabled, meta: { dirty, touched, error }, children }) => {
+  return (
+    <FormGroup controlId={input.name} validationState={touched && error ? 'error' : null}>
+      <ControlLabel>{children}</ControlLabel>
+
+      <Select
+        numericValues={numericValues}
+        disabled={disabled || false}
+        className={className}
+        onSelect={e => input.onChange(e)}
+        options={options}
+        value={input.value} />
+
+      {error && dirty &&
+        <HelpBlock className='error-msg'>{error}</HelpBlock>
+      }
+    </FormGroup>
+  );
+}
+
+FieldFormGroupSelect.propTypes = {
+  children: PropTypes.object,
+  input: PropTypes.object,
+  label: PropTypes.object,
+  meta: PropTypes.object,
+  placeholder: PropTypes.string,
+  type: PropTypes.string
+
+}
+
+export default FieldFormGroupSelect
