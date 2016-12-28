e4ae5cc7259dbcb4dc5c031db6cf6f2243044d7d
Mikko Alander <mikko.alander@idean.com>
2016-12-28 13:16:08 +0200

    text area / select support

--- src/components/form/field-form-group.jsx
+++ src/components/form/field-form-group.jsx
@@ -2,11 +2,13 @@ import React, { PropTypes } from 'react';
 import {FormGroup, FormControl, ControlLabel, HelpBlock} from 'react-bootstrap';
 
 const FieldFormGroup  = ({ input, placeholder, type, meta: { dirty, touched, error }, children }) => {
+  const componentClass = type === 'select' ? 'select' : type === 'textarea' ? 'textarea' : 'input'
+
   return (
     <FormGroup controlId={input.name} validationState={touched && error ? 'error' : null}>
       <ControlLabel>{children}</ControlLabel>
 
-      <FormControl type={type} placeholder={placeholder} value={input.value} onChange={input.onChange} />
+      <FormControl componentClass={componentClass} type={type} placeholder={placeholder} value={input.value} onChange={input.onChange} />
       <FormControl.Feedback />
 
       {error && dirty &&
