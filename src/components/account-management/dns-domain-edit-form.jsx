2dccc10e0a7094370544217b52b28a4e12a1cc93
Mikko Alander <mikko.alander@idean.com>
2016-12-28 17:55:29 +0200

    domain edit form refactor

--- src/components/account-management/dns-domain-edit-form.jsx
+++ src/components/account-management/dns-domain-edit-form.jsx
@@ -1,10 +1,86 @@
 import React from 'react'
-import { ButtonToolbar, Button } from 'react-bootstrap'
+import { reduxForm, Field, propTypes as reduxFormPropTypes } from 'redux-form'
+
+import { Button } from 'react-bootstrap'
+import FormFooterButtons from '../form/form-footer-buttons'
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl'
 
-import Input from './dns-form-input'
+import FieldFormGroup from '../form/field-form-group'
+
+//import Input from './dns-form-input'
 
 import './dns-domain-edit-form.scss'
+import { checkForErrors } from '../../util/helpers'
+import {
+  isValidFQDN,
+  isInt,
+  isValidIPv4Address,
+  isValidNameserver,
+  isValidSOARecord
+} from '../../util/validators'
+
+const validate = fields => {
+  // TODO: name_server validation
+  // From API-docs:
+  // Any name server that will respond authoritatively for the domain.
+  // Called the Primary Master in the context of Dynamic DNS (DDNS).
+  // If DDNS is not used this may be any suitable name server either
+  // within the zone file (in-zone) or in an external or foreign zone
+  // (also called out-of-zone or even out-of-bailiwick by those of a
+  // more literary bent or with a taste for the exotic). To mimimise
+  // confusion this is most commonly written as a Fully-qualified
+  // Domain Name (FQDN - ends with a dot). If the record points to
+  // an EXTERNAL server (not defined in this zone) it MUST be a FQDN
+  // and end with a '.' (dot), for example, ns1.example.net.
+  const { ttl, negative_ttl, email_addr, name, name_server, refresh } = fields
+  const maxTtl = 2147483647;
+
+  const customConditions = {
+    name_server: {
+      condition: !isValidIPv4Address(name_server) ? !isValidNameserver(name_server) : false,
+      errorText: <FormattedMessage id='portal.account.domainForm.validation.nameServer'/>
+    },
+    name: {
+      condition: !isValidFQDN(name),
+      errorText: <FormattedMessage id='portal.account.domainForm.validation.domainName'/>
+    },
+    email_addr: {
+      condition: !isValidSOARecord(email_addr),
+      errorText: <FormattedMessage id='portal.account.domainForm.validation.mailbox'/>
+    },
+    refresh: {
+      condition: !isInt(refresh),
+      errorText:<FormattedMessage id="portal.accountManagement.dns.form.validation.refresh.text"/>
+    },
+    ttl: [
+      {
+        condition: !isInt(ttl),
+        errorText:<FormattedMessage id="portal.accountManagement.dns.form.validation.ttl.text"/>
+      },
+      {
+        condition: parseInt(ttl) > maxTtl,
+        errorText: <FormattedMessage id='portal.accountManagement.dns.form.validation.maxTtl.text' values={{maxTtl}}/>
+      }
+    ],
+    negative_ttl: [
+      {
+        condition: isNaN(negative_ttl),
+        errorText:<FormattedMessage id="portal.account.domainForm.validation.negativeTtl.text"/>
+      },
+      {
+        condition: parseInt(negative_ttl) > maxTtl,
+        errorText: <FormattedMessage id='portal.accountManagement.dns.form.validation.maxTtl.text' values={{maxTtl}}/>
+      }
+    ]
+  }
+  const requiredTexts = {
+    name: <FormattedMessage id="portal.accountManagement.dns.form.validation.name.text"/>,
+    email_addr: <FormattedMessage id="portal.accountManagement.dns.form.validation.email.text"/>,
+    name_server: <FormattedMessage id="portal.accountManagement.dns.form.validation.nameServer.text"/>,
+    refresh: <FormattedMessage id="portal.accountManagement.dns.form.validation.refresh.text"/>
+  }
+  return checkForErrors(fields, customConditions, requiredTexts)
+}
 
 const DnsDomainEditForm = (props) => {
 
@@ -12,20 +88,106 @@ const DnsDomainEditForm = (props) => {
     edit,
     fetching,
     intl,
-    fields: {
+    onCancel,
+    invalid,
+    submitting
+    /*fields: {
       name,
       email_addr,
       name_server,
       refresh,
       ttl,
       negative_ttl
-    }
+    }*/
   } = props
 
   const actionButtonTitle = fetching ? <FormattedMessage id="portal.button.saving"/> : edit ? <FormattedMessage id="portal.button.save"/> : <FormattedMessage id="portal.button.add"/>
 
   return (
     <form autoComplete="off">
+
+      <Field
+        type="text"
+        name="name"
+        placeholder={intl.formatMessage({id: 'portal.accountManagement.dns.form.domainNamePlaceholder.text'})}
+        component={FieldFormGroup}
+        >
+        <FormattedMessage id="portal.accountManagement.dns.form.domainName.text" />
+      </Field>
+
+      <hr/>
+
+      <Field
+        type="text"
+        name="name_server"
+        placeholder={intl.formatMessage({id: 'portal.accountManagement.dns.form.nameServerPlaceholder.text'})}
+        component={FieldFormGroup}
+        >
+        <FormattedMessage id="portal.accountManagement.dns.form.nameServer.text" />
+      </Field>
+
+      <hr/>
+
+      <Field
+        type="text"
+        name="email_addr"
+        placeholder={intl.formatMessage({id: 'portal.accountManagement.dns.form.emailPlaceholder.text'})}
+        component={FieldFormGroup}
+        >
+        <FormattedMessage id="portal.accountManagement.dns.form.nameServerPlaceholder.text" />
+      </Field>
+
+      <hr/>
+
+      <Field
+        type="number"
+        name="refresh"
+        placeholder={intl.formatMessage({id: 'portal.accountManagement.dns.form.refresh.text'})}
+        component={FieldFormGroup}
+        >
+        <FormattedMessage id="portal.accountManagement.dns.form.refreshPlaceholder.text" />
+      </Field>
+
+      <hr/>
+
+      <Field
+        type="number"
+        name="ttl"
+        placeholder={intl.formatMessage({id: 'portal.accountManagement.dns.form.ttlPlaceholder.text'})}
+        component={FieldFormGroup}
+        >
+        <FormattedMessage id="portal.accountManagement.dns.form.ttl.text" />
+      </Field>
+
+      <hr/>
+
+      <Field
+        type="number"
+        name="negative_ttl"
+        placeholder={intl.formatMessage({id: 'portal.accountManagement.dns.form.negativeTtlPlaceholder.text'})}
+        component={FieldFormGroup}
+        >
+        <FormattedMessage id="portal.accountManagement.dns.form.negativeTtl.text" />
+      </Field>
+
+      <hr/>
+
+      <FormFooterButtons>
+          <Button
+            id="cancel-btn"
+            className="btn-secondary"
+            onClick={onCancel}>
+            <FormattedMessage id="portal.button.cancel"/>
+          </Button>
+
+          <Button
+            type="submit"
+            bsStyle="primary"
+            disabled={invalid||submitting}>
+            <FormattedMessage id="portal.button.save"/>
+          </Button>
+        </FormFooterButtons>
+{/*
       <Input
         {...name}
         labelID="portal.accountManagement.dns.form.domainName.text"
@@ -92,6 +254,7 @@ const DnsDomainEditForm = (props) => {
         <Button disabled={Object.keys(props.errors).length > 0 || fetching} bsStyle="primary"
         onClick={() => props.onSave(props.fields)}>{actionButtonTitle}</Button>
       </ButtonToolbar>
+      */}
     </form>
   )
 }
@@ -100,12 +263,14 @@ DnsDomainEditForm.displayName = 'DnsDomainEditForm'
 
 DnsDomainEditForm.propTypes = {
   edit: React.PropTypes.bool,
-  errors: React.PropTypes.object,
   fetching: React.PropTypes.bool,
-  fields: React.PropTypes.object.isRequired,
   intl: React.PropTypes.shape(intlShape),
   onCancel: React.PropTypes.func,
-  onSave: React.PropTypes.func
+  onSave: React.PropTypes.func,
+  ...reduxFormPropTypes
 }
 
-export default injectIntl(DnsDomainEditForm)
+export default reduxForm({
+  form: 'dnsDomainEditForm',
+  validate
+})(injectIntl(DnsDomainEditForm))
--- src/containers/account-management/modals/domain-form.jsx
+++ src/containers/account-management/modals/domain-form.jsx
@@ -1,8 +1,9 @@
 import React, { PropTypes, Component } from 'react'
+import { connect } from 'react-redux'
+
 import { bindActionCreators } from 'redux'
 
-import { reduxForm } from 'redux-form'
-import { Modal } from 'react-bootstrap'
+import SidePanel from '../../../components/side-panel'
 import { FormattedMessage } from 'react-intl'
 
 import * as dnsActionCreators from '../../../redux/modules/dns'
@@ -11,88 +12,15 @@ import { showInfoDialog, hideInfoDialog } from '../../../redux/modules/ui'
 
 import DnsDomainEditForm from '../../../components/account-management/dns-domain-edit-form'
 
-import { checkForErrors } from '../../../util/helpers'
-import {
-  isValidFQDN,
-  isInt,
-  isValidIPv4Address,
-  isValidNameserver,
-  isValidSOARecord
-} from '../../../util/validators'
-
-
-const validate = fields => {
-  // TODO: name_server validation
-  // From API-docs:
-  // Any name server that will respond authoritatively for the domain.
-  // Called the Primary Master in the context of Dynamic DNS (DDNS).
-  // If DDNS is not used this may be any suitable name server either
-  // within the zone file (in-zone) or in an external or foreign zone
-  // (also called out-of-zone or even out-of-bailiwick by those of a
-  // more literary bent or with a taste for the exotic). To mimimise
-  // confusion this is most commonly written as a Fully-qualified
-  // Domain Name (FQDN - ends with a dot). If the record points to
-  // an EXTERNAL server (not defined in this zone) it MUST be a FQDN
-  // and end with a '.' (dot), for example, ns1.example.net.
-  const { ttl, negative_ttl, email_addr, name, name_server, refresh } = fields
-  const maxTtl = 2147483647;
-
-  const customConditions = {
-    name_server: {
-      condition: !isValidIPv4Address(name_server) ? !isValidNameserver(name_server) : false,
-      errorText: <FormattedMessage id='portal.account.domainForm.validation.nameServer'/>
-    },
-    name: {
-      condition: !isValidFQDN(name),
-      errorText: <FormattedMessage id='portal.account.domainForm.validation.domainName'/>
-    },
-    email_addr: {
-      condition: !isValidSOARecord(email_addr),
-      errorText: <FormattedMessage id='portal.account.domainForm.validation.mailbox'/>
-    },
-    refresh: {
-      condition: !isInt(refresh),
-      errorText:<FormattedMessage id="portal.accountManagement.dns.form.validation.refresh.text"/>
-    },
-    ttl: [
-      {
-        condition: !isInt(ttl),
-        errorText:<FormattedMessage id="portal.accountManagement.dns.form.validation.ttl.text"/>
-      },
-      {
-        condition: parseInt(ttl) > maxTtl,
-        errorText: <FormattedMessage id='portal.accountManagement.dns.form.validation.maxTtl.text' values={{maxTtl}}/>
-      }
-    ],
-    negative_ttl: [
-      {
-        condition: isNaN(negative_ttl),
-        errorText:<FormattedMessage id="portal.account.domainForm.validation.negativeTtl.text"/>
-      },
-      {
-        condition: parseInt(negative_ttl) > maxTtl,
-        errorText: <FormattedMessage id='portal.accountManagement.dns.form.validation.maxTtl.text' values={{maxTtl}}/>
-      }
-    ]
-  }
-  const requiredTexts = {
-    name: <FormattedMessage id="portal.accountManagement.dns.form.validation.name.text"/>,
-    email_addr: <FormattedMessage id="portal.accountManagement.dns.form.validation.email.text"/>,
-    name_server: <FormattedMessage id="portal.accountManagement.dns.form.validation.nameServer.text"/>,
-    refresh: <FormattedMessage id="portal.accountManagement.dns.form.validation.refresh.text"/>
-  }
-  return checkForErrors(fields, customConditions, requiredTexts)
-}
-
 class DnsDomainEditFormContainer  extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
   }
 
-  componentDidMount(){
-    //show errors on edit even without touching fields
-    if (this.props.edit) this.props.touchAll()
-  }
+  // componentDidMount(){
+  //   //show errors on edit even without touching fields
+  //   if (this.props.edit) this.props.touchAll()
+  // }
 
   render() {
     const { edit, saveDomain, closeModal, ...formProps } = this.props
@@ -106,18 +34,24 @@ class DnsDomainEditFormContainer  extends Component {
       },
       ...formProps
     }
+
+    const title = edit
+      ? <FormattedMessage id='portal.account.domainForm.editDomain.title' />
+      : <FormattedMessage id='portal.account.domainForm.newDomain.title' />
+
+    const subTitle = 'domain name'
+
     return (
-      <div className="dns-edit-container">
-        <Modal show={true} dialogClassName="dns-edit-form-sidebar">
-          <Modal.Header>
-            <h1>{edit ? <FormattedMessage id='portal.account.domainForm.editDomain.title' /> : <FormattedMessage id='portal.account.domainForm.newDomain.title' />}</h1>
-            {edit && <p>{this.props.fields.name.value}</p>}
-          </Modal.Header>
-          <Modal.Body>
-            <DnsDomainEditForm {...domainFormProps}/>
-          </Modal.Body>
-        </Modal>
-      </div>
+      <SidePanel
+        show={true}
+        title={title}
+        subTitle={subTitle}
+        cancel={domainFormProps.onCancel}
+      >
+
+        <DnsDomainEditForm {...domainFormProps/>
+
+      </SidePanel>
     )
   }
 }
@@ -198,8 +132,4 @@ function mapDispatchToProps(dispatch, { closeModal }) {
   }
 }
 
-export default reduxForm({
-  form: 'dnsDomainEditForm',
-  fields: ['name', 'email_addr', 'name_server', 'refresh', 'ttl', 'negative_ttl'],
-  validate
-}, mapStateToProps, mapDispatchToProps)(DnsDomainEditFormContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(DnsDomainEditFormContainer)
