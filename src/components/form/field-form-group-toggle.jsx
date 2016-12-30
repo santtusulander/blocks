25075ca91e9f7208d8c0065e12d92da8c628ce6b
Ville Ristimäki <ville.ristimaki@idean.com>
2016-12-30 10:06:45 +0200

    Remove redundand PropTypes, fix typos, add fallback method if onToggle is not defined

--- src/components/form/field-form-group-toggle.jsx
+++ src/components/form/field-form-group-toggle.jsx
@@ -3,6 +3,7 @@ import { FormGroup, ControlLabel } from 'react-bootstrap';
 import Toggle from '../toggle.jsx'
 
 const FieldFormGroupToggle  = ({ input, onToggle, offText, onText, className, readonly, children }) => {
+  onToggle = onToggle ? onToggle : e => input.onChange(e)
   return (
     <FormGroup controlId={input.name} className={className}>
       <ControlLabel>{children}</ControlLabel>
@@ -20,11 +21,10 @@ FieldFormGroupToggle.propTypes = {
   children: PropTypes.object,
   className: PropTypes.string,
   input: PropTypes.object,
-  meta: PropTypes.object,
   offText: PropTypes.string,
   onText: PropTypes.string,
   onToggle: PropTypes.func,
-  readOnly: PropTypes.bool
+  readonly: PropTypes.bool
 }
 
 export default FieldFormGroupToggle
