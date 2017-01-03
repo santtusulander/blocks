04bf369111e0bd42c0f69236a40d3b0b3d0f24f0
Jeremy White <jeremy.white@idean.com>
2017-01-03 13:58:20 -0800

    UDNP-2223 — improve accuracy of fetching status

--- src/containers/main.jsx
+++ src/containers/main.jsx
@@ -222,19 +222,17 @@ Main.defaultProps = {
 }
 
 function mapStateToProps(state) {
+  const stateMap = Immutable.Map(state)
+
   return {
     accounts: state.account.get('allAccounts'),
     activeAccount: state.account.get('activeAccount'),
     activeGroup: state.group.get('activeGroup'),
     activeHost: state.host.get('activeHost'),
     currentUser: state.user.get('currentUser'),
-    fetching: state.account.get('fetching') ||
-      state.content.get('fetching') ||
-      state.group.get('fetching') ||
-      state.host.get('fetching') ||
-      state.topo.get('fetching') ||
-      state.traffic.get('fetching') ||
-      state.visitors.get('fetching'),
+    fetching: stateMap.some(
+      store => store && (store.get ? store.get('fetching') : store.fetching)
+    ),
     notification: state.ui.get('notification'),
     roles: state.roles.get('roles'),
     showErrorDialog: state.ui.get('showErrorDialog'),
@@ -244,7 +242,7 @@ function mapStateToProps(state) {
     user: state.user,
     viewingChart: state.ui.get('viewingChart'),
     breadcrumbs: state.ui.get('breadcrumbs')
-  };
+  }
 }
 
 function mapDispatchToProps(dispatch) {
