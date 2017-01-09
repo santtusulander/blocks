703a57a2a3b8f5cef0ebfa43fba8794a839606fa
Jeremy White <jeremy.white@idean.com>
2017-01-09 15:19:42 -0800

    UDNP-2262 — throw error on logs, warns, & errors

--- jest-env-setup.js
+++ jest-env-setup.js
@@ -7,3 +7,30 @@ global.localStorage = {
 
 global.GOOGLE_SITE_KEY = '1234567890'
 global.VERSION = '11.22.33'
+
+/* eslint-disable no-console */
+if (console) {
+  if (console.log) {
+    const old = console.log
+    console.log = function() {
+      old.apply(this, arguments)
+      throw new Error("Console logs are not allowed in unit tests")
+    }
+  }
+
+  if (console.warn) {
+    const old = console.warn
+    console.warn = function() {
+      old.apply(this, arguments)
+      throw new Error("Console warnings are not allowed")
+    }
+  }
+
+  if (console.error) {
+    var old = console.error
+    console.error = function() {
+      old.apply(this, arguments)
+      throw new Error("Console errors are not allowed")
+    }
+  }
+}
