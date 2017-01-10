894a2e7a070f022dc3600b3bf2f2fedf4ab86f69
Mika Paulasaari <mika.paulasaari@idean.com>
2017-01-10 13:28:46 -0800

    Add missing components/analysis tests

---
+++ src/components/analysis/__tests__/by-key.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+jest.unmock('../by-key')
+import AnalysisByKey from '../by-key'
+
+describe('AnalysisByKey', () => {
+  let subject, error, props = null
+
+  beforeEach(() => {
+    subject = (noWidth) => {
+      props = {
+        area: true,
+        axes: true,
+        className: 'foobar',
+        height: 1,
+        padding: 1,
+        primaryData: [{foo: 'bar'}, {zyx: 'qwe'}],
+        primaryLabel: 'foo',
+        secondaryData: [{foo: 'bar'}, {zyx: 'qwe'}],
+        secondaryLabel: 'bar',
+        width: !noWidth && 1,
+        yKey: 'foo',
+        xKey: 'bar'
+      }
+      return shallow(<AnalysisByKey {...props}/>)
+    }
+  })
+
+  it('should exist', () => {
+    expect(subject().length).toBe(1)
+    expect(subject(true).length).toBe(1)
+  })
+})
