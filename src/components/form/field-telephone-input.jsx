4bf7ab046dc91e3955ba2eb702281621ffb703fc
Mikko Alander <mikko.alander@idean.com>
2017-01-10 14:59:16 +0200

    add label to telephone input field

--- src/components/form/field-telephone-input.jsx
+++ src/components/form/field-telephone-input.jsx
@@ -1,20 +1,26 @@
 import React, { PropTypes } from 'react'
 import ReactTelephoneInput from 'react-telephone-input'
+import { FormattedMessage } from 'react-intl'
 
-const FieldTelephoneInput = ({ input }) => {
+const FieldTelephoneInput = ({ input, label }) => {
   return (
-    <ReactTelephoneInput
-      value={input.value.val}
-      onChange={(val, {dialCode})=> {
-        input.onChange({val, dialCode})
-      }}
-      defaultCountry="us"
-    />
+    <div>
+      {label && <label className="control-label"><FormattedMessage id="portal.user.edit.phoneNumber.text"/></label>}
+
+      <ReactTelephoneInput
+        value={input.value.val}
+        onChange={(val, {dialCode})=> {
+          input.onChange({val, dialCode})
+        }}
+        defaultCountry="us"
+      />
+    </div>
   )
 }
 
 FieldTelephoneInput.displayName = 'FieldTelephoneInput'
 FieldTelephoneInput.propTypes = {
+  label: PropTypes.object,
   input: PropTypes.shape({
     onChange: PropTypes.func,
     value: PropTypes.object
