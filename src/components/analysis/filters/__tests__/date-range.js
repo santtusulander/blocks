98128999f86e01a9e744b359912d0c52a42f014d
Mika Paulasaari <mika.paulasaari@idean.com>
2017-01-11 12:09:18 -0800

    Add new tests for component/analysis

---
+++ src/components/analysis/filters/__tests__/date-range.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import moment from 'moment'
+
+jest.unmock('../date-range')
+import FilterDateRange from '../date-range'
+
+const onFilterChange = jest.fn()
+
+describe('FilterDateRange', () => {
+  let subject, error, props = null
+
+  beforeEach(() => {
+    subject = () => {
+      props = {
+        onFilterChange: jest.fn(),
+        showComparison: true
+      }
+      return shallow(<FilterDateRange {...props}/>)
+    }
+  })
+
+  it('should exist', () => {
+    expect(subject().length).toBe(1)
+  })
+
+  it('should limit over 4 month date ranges', () => {
+    const component = subject()
+    const onFilterChangeMock = jest.fn()
+    let props = {
+      includeComparison: true,
+      startDate: moment.utc(),
+      endDate: moment.utc().add(2, 'months'),
+      onFilterChange: onFilterChangeMock
+    }
+
+    component.setProps(props)
+    expect(onFilterChangeMock.mock.calls.length).toEqual(0)
+
+    props.endDate = moment.utc().add(5, 'months')
+    component.setProps(props)
+    expect(onFilterChangeMock.mock.calls.length).toEqual(1)
+  })
+})
