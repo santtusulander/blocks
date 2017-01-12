65c012c15b430682ba5212c7c9e744960b339894
Mikko Alander <mikko.alander@idean.com>
2017-01-12 15:51:48 +0200

    refactor isAllowed to use context instead of redux refactor to stateless

--- src/components/is-allowed.jsx
+++ src/components/is-allowed.jsx
@@ -1,43 +1,33 @@
-import React, { PropTypes, Component, Children } from 'react'
+import React, { PropTypes, Children } from 'react'
 import { List, Map } from 'immutable'
-import { connect } from 'react-redux'
 
 import * as PERMISSIONS from '../constants/permissions.js'
 import checkPermissions from '../util/permissions'
 
-class IsAllowed extends Component {
-  render() {
-    const { children, currentUser, roles, to, not } = this.props;
-    let isAllowed = checkPermissions(roles, currentUser, to)
-    if(not) {
-      isAllowed = !isAllowed
-    }
-    return (
-      isAllowed && Children.only(children)
-    )
+const IsAllowed = (props, context) => {
+  const { children,  to, not } = props;
+  const { currentUser, roles } = context;
+
+  let isAllowed = checkPermissions(roles, currentUser, to)
+  if(not) {
+    isAllowed = !isAllowed
   }
+  return (
+    isAllowed && Children.only(children)
+  )
 }
 
-
 IsAllowed.displayName = 'IsAllowed'
+
+IsAllowed.contextTypes = {
+  currentUser: PropTypes.instanceOf(Map),
+  roles: PropTypes.instanceOf(List)
+}
+
 IsAllowed.propTypes = {
   children: React.PropTypes.node,
-  currentUser: PropTypes.instanceOf(Map),
   not: React.PropTypes.bool,
-  roles: PropTypes.instanceOf(List),
   to: PropTypes.oneOf(Object.keys(PERMISSIONS))
 }
 
-IsAllowed.defaultProps = {
-  currentUser: Map(),
-  roles: List()
-}
-
-function mapStateToProps(state) {
-  return {
-    roles: state.roles.get('roles'),
-    currentUser: state.user.get('currentUser')
-  };
-}
-
-export default connect(mapStateToProps)(IsAllowed)
+export default IsAllowed
