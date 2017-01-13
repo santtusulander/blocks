1530a43ba16f58aca1dcd79716873ae45843a9a2
Mikko Alander <mikko.alander@idean.com>
2017-01-13 13:33:34 +0200

    helpers for localStorage

---
+++ src/util/local-storage.js
@@ -0,0 +1,84 @@
+const LOCALSTORAGE_USER_TOKEN = 'EricssonUDNUserToken'
+const LOCALSTORAGE_USER_TOKEN_META = 'EricssonUDNUserTokenMeta'
+const LOCALSTORAGE_USER_NAME = 'EricssonUDNUserName'
+const LOCALSTORAGE_UI_THEME = 'EricssonUDNUiTheme'
+
+/**
+ * Set User token to localStorage
+ * @type {String}
+ */
+export const setUserToken = (token) => {
+  localStorage.setItem( LOCALSTORAGE_USER_TOKEN , token)
+}
+
+/**
+ * Get User token from localStorage
+ * @return {String}
+ */
+export const getUserToken = () => {
+  localStorage.setItem( LOCALSTORAGE_USER_TOKEN )
+}
+
+/**
+ * Delete User Token from localStorage
+ * @return {[type]} [description]
+ */
+export const deleteUserToken = () => {
+  localStorage.removeItem( LOCALSTORAGE_USER_TOKEN )
+}
+
+/**
+ * setTokenMeta to localStorage
+ * @param {Object} tokenMeta
+ */
+export const setTokenMeta = ( tokenMeta ) => {
+  localStorage.setItem( LOCALSTORAGE_USER_TOKEN_META, JSON.stringify(tokenMeta) )
+}
+
+/**
+ * getTokenMeta from localStorage
+ * @return {Object}
+ */
+export const getTokenMeta = () => {
+  return JSON.parse(localStorage.getItem( LOCALSTORAGE_USER_TOKEN_META )||{})
+}
+
+/**
+ * Delete Token Meta from localStorage
+ * @return {[type]} [description]
+ */
+export const deleteTokenMeta = () => {
+  localStorage.removeItem( LOCALSTORAGE_USER_TOKEN_META )
+}
+
+/**
+ * Set theme in localStorage
+ * @param {String} theme
+ */
+export const setTheme = (theme) => {
+  localStorage.setItem( LOCALSTORAGE_UI_THEME, theme)
+}
+
+/**
+ * Get theme from localStorage
+ * @return {String} theme
+ */
+export const getTheme = () => {
+  return localStorage.getItem(LOCALSTORAGE_UI_THEME)
+}
+
+/**
+ * Set userName in localStorage
+ * @param {String} userName
+ */
+export const setUserName = (userName) => {
+  localStorage.setItem( LOCALSTORAGE_USER_NAME, userName)
+}
+
+/**
+ * get UserName from localStorage]
+ * @return {String} userName
+ */
+export const getUserName = () => {
+  return localStorage.setItem( LOCALSTORAGE_USER_NAME )
+}
