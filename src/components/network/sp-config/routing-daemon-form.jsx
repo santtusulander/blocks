5578ef4c39cc0a605c5f8fd7c04e1a7da65526d4
Ville Ristimäki <ville.ristimaki@idean.com>
2017-01-18 12:57:38 +0200

    Add routig daemon form logic

--- src/components/network/sp-config/routing-daemon-form.jsx
+++ src/components/network/sp-config/routing-daemon-form.jsx
@@ -1,17 +1,36 @@
 import React, { PropTypes } from 'react'
-import { reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { reduxForm, Field, change } from 'redux-form'
 import { FormattedMessage, injectIntl } from 'react-intl'
-
-import { checkForErrors } from '../../../util/helpers'
+import { Button } from 'react-bootstrap'
 
 import SidePanel from '../../side-panel'
+import FieldFormGroup from '../../form/field-form-group'
 import FormFooterButtons from '../../form/form-footer-buttons'
 
-const validate = () => {
-  const conditions = {}
+const errors = {}
 
-  return checkForErrors({}, conditions)
+const validate = (values) => {
 
+  const { bgp_as_number, bgp_as_name, bgp_router_ip, bgp_password } = values
+
+  if (!bgp_as_number) {
+    errors.bgp_as_number = <FormattedMessage values={{ field: <FormattedMessage id="portal.network.spConfig.routingDaemon.editForm.bgp_as_number.label"/> }} id="portal.network.spConfig.routingDaemon.editForm.required.text"/>
+  }
+
+  if (!bgp_as_name) {
+    errors.bgp_as_name = <FormattedMessage values={{ field: <FormattedMessage id="portal.network.spConfig.routingDaemon.editForm.bgp_as_name.label"/> }} id="portal.network.spConfig.routingDaemon.editForm.required.text"/>
+  }
+
+  if (!bgp_router_ip) {
+    errors.bgp_router_ip = <FormattedMessage values={{ field: <FormattedMessage id="portal.network.spConfig.routingDaemon.editForm.bgp_router_ip.label"/> }} id="portal.network.spConfig.routingDaemon.editForm.required.text"/>
+  }
+
+  if (!bgp_password) {
+    errors.bgp_password = <FormattedMessage values={{ field: <FormattedMessage id="portal.network.spConfig.routingDaemon.editForm.bgp_password.label"/> }} id="portal.network.spConfig.routingDaemon.editForm.required.text"/>
+  }
+
+  return errors
 }
 
 class RoutingDaemonForm extends React.Component {
@@ -19,11 +38,55 @@ class RoutingDaemonForm extends React.Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      isFetchingBGPName: false,
+      BGPNumber: null,
+      BGPName: null,
+      BGPNameNotFound: false
+    }
+
+    this.fetchBGPName = this.fetchBGPName.bind(this)
     this.onSubmit = this.onSubmit.bind(this)
   }
 
   onSubmit(values) {
     const { onSave } = this.props
+
+    onSave(values)
+  }
+
+  fetchBGPName(e) {
+    const { fetchBGPName, setBGPName } = this.props
+    const { BGPName } = this.state
+    const BGPNumber = e.target.value
+
+    if (!BGPNumber || this.state.BGPNumber === BGPNumber) {
+      return;
+    }
+
+    // TODO: Remove me when this is actually connected to Redux
+    this.setState({
+      isFetchingBGPName: true
+    })
+
+    fetchBGPName()
+      .then(res => {
+        this.setState({
+          BGPNumber,
+          BGPName: res.payload,
+          BGPNameNotFound: false,
+          isFetchingBGPName: false
+        }, () => setBGPName(res.payload))
+
+      })
+      .catch(() => {
+        this.setState({
+          BGPNumber,
+          BGPName: null,
+          BGPNameNotFound: true,
+          isFetchingBGPName: false
+        }, () => setBGPName(this.props.intl.formatMessage({ id: 'portal.network.spConfig.routingDaemon.editForm.asNameNotFound' })))
+      })
   }
 
   render() {
@@ -34,16 +97,62 @@ class RoutingDaemonForm extends React.Component {
       onCancel,
       show,
       submitting,
+      editing
     } = this.props
+
+    const { BGPName, BGPNameNotFound, isFetchingBGPName } = this.state
+
+    const formTitle = editing ? 'portal.network.spConfig.routingDaemon.editForm.title' : 'portal.network.spConfig.routingDaemon.addForm.title'
+
+    const BGB_AS_NUMBER_PROPS = {
+      input: {
+        onBlur: (e) => this.fetchBGPName(e)
+      }
+    }
 
     return (
       <SidePanel
         show={show}
-        title={intl.formatMessage({ id: 'portal.account.roleEdit.title' })}
-        subTitle={intl.formatMessage({ id: 'portal.account.roleEdit.disclaimer.text' })}
+        title={intl.formatMessage({ id: formTitle })}
         cancel={onCancel}
       >
         <form onSubmit={handleSubmit(this.onSubmit)}>
+
+          <Field
+            type="text"
+            name="bgp_as_number"
+            label={intl.formatMessage({ id: 'portal.network.spConfig.routingDaemon.editForm.bgp_as_number.label' })}
+            placeholder={intl.formatMessage({ id: 'portal.network.spConfig.routingDaemon.editForm.bgp_as_number.label' })}
+            component={FieldFormGroup}
+            props={BGB_AS_NUMBER_PROPS}
+          />
+
+          <Field
+            type="text"
+            name="bgp_as_name"
+            label={intl.formatMessage({ id: 'portal.network.spConfig.routingDaemon.editForm.bgp_as_name.label' })}
+            placeholder={isFetchingBGPName ? intl.formatMessage({ id: 'portal.network.spConfig.routingDaemon.editForm.asNameFetching.label' }) : intl.formatMessage({ id: 'portal.network.spConfig.routingDaemon.editForm.asNamePlaceholder.label' })}
+            disabled={!BGPName && !BGPNameNotFound}
+            addonAfter={isFetchingBGPName ? 'spin' : ''}
+            component={FieldFormGroup}
+          />
+
+          <Field
+            type="text"
+            name="bgp_router_ip"
+            label={intl.formatMessage({ id: 'portal.network.spConfig.routingDaemon.editForm.bgp_router_ip.label' })}
+            placeholder={intl.formatMessage({ id: 'portal.network.spConfig.routingDaemon.editForm.bgp_router_ip.label' })}
+            component={FieldFormGroup}
+          />
+
+          <Field
+            type="password"
+            name="bgp_password"
+            label={intl.formatMessage({ id: 'portal.network.spConfig.routingDaemon.editForm.bgp_password.label' })}
+            placeholder={intl.formatMessage({ id: 'portal.network.spConfig.routingDaemon.editForm.bgp_password.label' })}
+            component={FieldFormGroup}
+          />
+
           <FormFooterButtons>
             <Button
               id="cancel-btn"
@@ -55,7 +164,7 @@ class RoutingDaemonForm extends React.Component {
             <Button
               type="submit"
               bsStyle="primary"
-              disabled={invalid || submitting}>
+              disabled={invalid || submitting || isFetchingBGPName}>
               <FormattedMessage id="portal.button.save"/>
             </Button>
           </FormFooterButtons>
@@ -67,18 +176,31 @@ class RoutingDaemonForm extends React.Component {
 
 RoutingDaemonForm.displayName = 'RoutingDaemonForm'
 RoutingDaemonForm.propTypes = {
+  editing: PropTypes.bool,
+  fetchBGPName: PropTypes.func,
   handleSubmit: PropTypes.func,
   intl: PropTypes.object,
   invalid: PropTypes.bool,
   onCancel: PropTypes.func,
   onSave: PropTypes.func,
+  setBGPName: PropTypes.func,
   show: PropTypes.bool,
   submitting: PropTypes.bool
 }
 
+const mapStateToProps = (state) => {
+  return {}
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setBGPName: (name) => dispatch(change('routing-daemon-form', 'bgp_as_name', name))
+  }
+}
+
 const form = reduxForm({
-  form: 'routing-daemon',
+  form: 'routing-daemon-form',
   validate
 })(RoutingDaemonForm)
 
-export default injectIntl(form)
+export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(form))
