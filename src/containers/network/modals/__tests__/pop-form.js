a5b6d5c95fc4de162edc7cab7a8ad09763dc0a8a
Mykhailo Prochkaruk <mprochkaruk@vidscale.com>
2017-01-18 15:45:13 +0200

    Add exist test for POP form container

---
+++ src/containers/network/modals/__tests__/pop-form.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+jest.unmock('../pop-form.jsx')
+jest.genMockFromModule('react-bootstrap')
+import NetworkPopFormContainer from '../pop-form.jsx'
+
+const intlMaker = () => {
+  return {
+    formatMessage: jest.fn()
+  }
+}
+
+describe('NetworkPopFormContainer', () => {
+  let subject = null
+
+  beforeEach(() => {
+    subject = (edit = false) => {
+      let props = {
+        closeModal: jest.fn(),
+        onSave: jest.fn(),
+        handleSubmit: jest.fn(),
+        intl: intlMaker(),
+        initialValues: {
+          name: 'udn'
+        },
+        edit: edit
+      }
+      return shallow(<NetworkPopFormContainer {...props}/>)
+    }
+  })
+
+ it('should exist', () => {
+   expect(subject().length).toBe(1)
+ })
+})
