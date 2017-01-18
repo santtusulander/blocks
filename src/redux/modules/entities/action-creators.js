8eea2e8acc4379e0a0b7bffadd292472391f88cb
santtu-idean <santtu.sulander@idean.com>
2017-01-18 13:33:02 +0200

    create ActionCreator generator

---
+++ src/redux/modules/entities/action-creators.js
@@ -0,0 +1,54 @@
+import { actionTypes } from './'
+
+export default ({
+  api,
+  entityType,
+  receiveActionTypes = [actionTypes.REQUEST, actionTypes.RECEIVE, actionTypes.FAIL],
+  removeActionTypes = [actionTypes.REQUEST, actionTypes.REMOVE, actionTypes.FAIL]
+}) => {
+
+  const fetchOne = (forceReload, ...requestParameters) => ({
+    forceReload,
+    types: receiveActionTypes,
+    cacheKey: `fetch-one-${entityType}-${requestParameters.join('-')}`,
+    callApi: api.fetch(...requestParameters)
+  })
+
+  const fetchAllThunk = dispatch => (...requestParameters) => {
+    api.fetchAll(...requestParameters)
+      .then(data =>
+        data.map(id => dispatch(fetchOne(...requestParameters, id))
+      )
+    )
+  }
+
+  return {
+
+    fetchAllThunk,
+
+    fetchOne,
+
+    fetchAll: (forceReload, ...requestParameters) => ({
+      forceReload,
+      types: receiveActionTypes,
+      cacheKey: `fetch-all-${entityType}-${requestParameters.join('-')}`,
+      callApi: () => api.fetchAll(...requestParameters)
+    }),
+
+    create: (...requestParameters) => ({
+      types: receiveActionTypes,
+      callApi: () => api.create(...requestParameters)
+    }),
+
+    update: (...requestParameters) => ({
+      types: receiveActionTypes,
+      callApi: () => api.update(...requestParameters)
+    }),
+
+    remove: (...requestParameters) => ({
+      types: removeActionTypes,
+      callApi: () => api.remove(...requestParameters)
+    })
+
+  }
+}
