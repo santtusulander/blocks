d193d6c4b093ab29b02f6604c195eb968d005e6b
santtu-idean <santtu.sulander@idean.com>
2017-01-18 17:56:27 +0200

    WIP: requests to api caller file

--- src/redux/modules/entities/nodes/api.js
+++ src/redux/modules/entities/nodes/api.js
@@ -1,5 +1,7 @@
-import { normalize } from 'normalizr'
-import { Schemas } from '../schemas'
+import { normalize, schema } from 'normalizr'
+import axios from 'axios'
+
+import { BASE_URL_AAA } from '../../../util'
 
 const mockNodeList = [
   {
@@ -84,4 +86,65 @@ const mockNodeList = [
   }
 ]
 
-export const fetch = () => Promise.resolve(normalize(mockNodeList, Schemas.node))
+const mockNodeIdList = [
+  "test.xxx.neppari",
+  "sp-edge10.sfo.cdx-dev",
+  "testingen.automatium",
+  "sp-edge10.sfo.cdx-dev.unifieddeliverynetwork.net"
+]
+
+const mockNodeToCreate = {
+  "account_id": 1,
+  "brand_id": "udn",
+  "cloud_driver": 6,
+  "created": 1474291378,
+  "custom_grains": [],
+  "env": "cdx-dev",
+  "group_id": 10,
+  "id": "to.create",
+  "name": "to.create",
+  "network_id": "udn",
+  "pod_id": "pod10",
+  "pop_id": "par",
+  "roles": [
+    "cache"
+  ],
+  "status": 1,
+  "type": 1,
+  "updated": 1474291378
+}
+
+const nodeSchema = new schema.Entity('nodes')
+
+// export const fetchAll = () => Promise.resolve(mockNodeIdList)
+export const fetchAll = (brand, account, group) =>
+  axios.get(`${BASE_URL_AAA}/brands/${brand}/accounts/${account}/groups/${group}/networks/${brand}/pops/par/nodes`)
+    .then(data => {
+      console.log(data)
+      return data
+    })
+
+// export const fetch = (brand, account, group, nodeId) =>
+//   Promise.resolve(normalize(mockNodeList.find(({ id }) => id === nodeId), Schemas.node))
+export const fetch = (brand, account, group, id) =>
+  axios.get(`${BASE_URL_AAA}/brands/${brand}/accounts/${account}/groups/${group}/networks/${brand}/pops/par/nodes/${id}`)
+    .then(data => {
+      console.log(data)
+      return normalize(data, nodeSchema)
+    })
+
+
+// export const create = () => Promise.resolve(normalize(mockNodeToCreate, nodeSchema))
+export const create = (brand, account, group, id, payload) =>
+  axios.post(`${BASE_URL_AAA}/brands/${brand}/accounts/${account}/groups/${group}/networks/${brand}/pops/par/nodes`, payload)
+    .then(data => normalize(data, nodeSchema))
+
+// export const update = () => Promise.resolve(normalize(mockNodeList[0], nodeSchema))
+export const update = (brand, account, group, id, payload) =>
+  axios.put(`${BASE_URL_AAA}/brands/${brand}/accounts/${account}/groups/${group}/networks/${brand}/pops/par/nodes/${id}`, payload)
+    .then(data => normalize(data, nodeSchema))
+
+// export const remove = () => Promise.resolve('1')
+export const remove = (brand, account, group, id) =>
+  axios.delete(`${BASE_URL_AAA}/brands/${brand}/accounts/${account}/groups/${group}/networks/${brand}/pops/par/nodes/${id}`)
+    .then(() => { id })
