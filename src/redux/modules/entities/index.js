6cf99a04d5cbb772f1d27d3249543b8881707158
santtu-idean <santtu.sulander@idean.com>
2017-01-19 17:06:54 +0200

    create locations-module

--- src/redux/modules/entities/index.js
+++ src/redux/modules/entities/index.js
@@ -12,30 +12,20 @@ export const actionTypes = {
   RECEIVE: 'entities/RECEIVE',
   REMOVE: 'entities/REMOVE',
   FAIL: 'entities/FAIL'
-
-  // CREATE:'entities/CREATE',
-  // FETCH: 'entities/FETCH',
-  // FETCH_ALL: 'entities/FETCH_ALL',
-  // FETCH_ALL_WITH_DETAILS: 'entities/FETCH_ALL_WITH_DETAILS',
-  // REMOVE: 'entities/REMOVE',
-  // UPDATE: 'entities/UPDATE',
-  //
-  //START_FETCHING: 'entities/START_FETCHING'
 }
+
+const locations =
+  handleActions({
+    [actionTypes.RECEIVE] : receiveEntity('location'),
+    [actionTypes.REMOVE] : removeEntity,
+    [actionTypes.FAIL] : failEntity
+  }, Map())
 
 const accounts =
   handleActions({
     [actionTypes.RECEIVE] : receiveEntity('accounts'),
     [actionTypes.REMOVE] : removeEntity,
     [actionTypes.FAIL] : failEntity
-
-    //[actionTypes.REQUEST] : receiveEntity('accounts'),
-    // [actionTypes.CREATE]:  mapReducers(receiveEntity('accounts'), failEntity),
-    // [actionTypes.FETCH]:  mapReducers(receiveEntity('accounts'), failEntity),
-    // [actionTypes.FETCH_ALL]:  mapReducers(receiveEntity('accounts'), failEntity),
-    // [actionTypes.FETCH_ALL_WITH_DETAILS]:  mapReducers(receiveEntity('accounts'), failEntity),
-    // [actionTypes.REMOVE]:  mapReducers(removeEntity, failEntity),
-    // [actionTypes.UPDATE]:  mapReducers(receiveEntity('accounts'), failEntity)
   }, Map())
 
 const groups =
@@ -43,14 +33,6 @@ const groups =
     [actionTypes.RECEIVE] : receiveEntity('groups'),
     [actionTypes.REMOVE] : removeEntity,
     [actionTypes.FAIL] : failEntity
-
-
-    // [actionTypes.CREATE]:  mapReducers(receiveEntity('groups'), failEntity),
-    // [actionTypes.FETCH]:  mapReducers(receiveEntity('groups'), failEntity),
-    // [actionTypes.FETCH_ALL]:  mapReducers(receiveEntity('groups'), failEntity),
-    // [actionTypes.FETCH_ALL_WITH_DETAILS]:  mapReducers(receiveEntity('groups'), failEntity),
-    // [actionTypes.REMOVE]:  mapReducers(removeEntity, failEntity),
-    // [actionTypes.UPDATE]:  mapReducers(receiveEntity('groups'), failEntity)
   }, Map())
 
 const properties =
@@ -58,35 +40,11 @@ const properties =
     [actionTypes.RECEIVE] : receiveEntity('properties'),
     [actionTypes.REMOVE] : removeEntity,
     [actionTypes.FAIL] : failEntity
-
-    // [actionTypes.CREATE]:  mapReducers(receiveEntity('properties'), failEntity),
-    // [actionTypes.FETCH]:  mapReducers(receiveEntity('properties'), failEntity),
-    // [actionTypes.FETCH_ALL]:  mapReducers(receiveEntity('properties'), failEntity),
-    // [actionTypes.FETCH_ALL_WITH_DETAILS]:  mapReducers(receiveEntity('properties'), failEntity),
-    // [actionTypes.REMOVE]:  mapReducers(removeEntity, failEntity),
-    // [actionTypes.UPDATE]:  mapReducers(receiveEntity('properties'), failEntity)
   }, Map())
-
-// const groupProperties =
-//     handleActions({
-//       [actionTypes.RECEIVE]:  receiveEntity('groupProperties')
-//     }, Map())
-//
-// const accountGroups =
-//     handleActions({
-//       [actionTypes.RECEIVE]:  receiveEntity('accountGroups')
-//       // [actionTypes.FETCH_ALL]:  mapReducers(receiveEntity('accountGroups'), failEntity)
-//     }, Map())
-//
-// const brandAccounts =
-//     handleActions({
-//       [actionTypes.RECEIVE]:  receiveEntity('brandAccounts')
-//       // [actionTypes.FETCH_ALL]:  mapReducers(receiveEntity('accountGroups'), failEntity)
-//     }, Map())
-
 
 const entities = combineReducers({
   accounts,
+  locations,
   //accountGroups,
   groups,
   //groupProperties,
