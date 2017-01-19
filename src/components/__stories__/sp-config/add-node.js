63b8c2f0efedf275232d353fdbf9de1b066af354
Mikko Lakomaa <mikko.lakomaa@nordsoftware.com>
2017-01-19 16:32:55 +0200

    Add Add Node story to storybook

---
+++ src/components/__stories__/sp-config/add-node.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {createStore, combineReducers} from 'redux'
+import {reducer as formReducer} from 'redux-form'
+import {Provider} from 'react-redux'
+
+import {storiesOf, action} from '@kadira/storybook'
+
+import ThemeWrap from '../theme-wrap.jsx'
+import AddNode from '../../sp-config/add-node/add-node.jsx'
+
+const reducers = {
+  form: formReducer
+}
+
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+
+let showModal = true;
+
+storiesOf('SPConfig', module)
+  .addDecorator((story) => (
+    <ThemeWrap>
+      <Provider store={store}>
+        {story()}
+      </Provider>
+    </ThemeWrap>
+  ))
+  .add('AddNode', () => (
+    <AddNode show={showModal} onSave={action('onSave')} onCancel={action('onCancel')}/>
+  ));
