6d7dbb2c2fdaf49a9a03da89780f5f7c79ef7881
santtu-idean <santtu.sulander@idean.com>
2017-01-20 12:54:08 +0200

    correct entityType

--- src/redux/modules/entities/footprints/selectors.js
+++ src/redux/modules/entities/footprints/selectors.js
@@ -1,4 +1,4 @@
 import * as entitySelectors from '../../entity/selectors'
 
-export const getByAccount = state => accountId => entitySelectors.getEntitiesByParent(state, 'locations', accountId)
-export const getById = state => id => entitySelectors.getEntityById(state, 'locations', id)
+export const getByAccount = state => accountId => entitySelectors.getEntitiesByParent(state, 'footprints', accountId)
+export const getById = state => id => entitySelectors.getEntityById(state, 'footprints', id)
