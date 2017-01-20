f1f7eb3bed6ba514edeec78296f07d2e387e339e
santtu-idean <santtu.sulander@idean.com>
2017-01-20 12:43:46 +0200

    inject parent-id into normalized objects, add selectors

--- src/redux/modules/entities/footprints/api.js
+++ src/redux/modules/entities/footprints/api.js
@@ -3,10 +3,13 @@ import axios from 'axios'
 
 import { BASE_URL_NORTH } from '../../../util'
 
-const baseURL = ({ brand, account, group }) =>
+const baseURL = ({ brand, account }) =>
   `${BASE_URL_NORTH}/brands/${brand}/accounts/${account}/footprints`
 
-const locationSchema = new schema.Entity('footprints')
+const footprintSchema = new schema.Entity('footprints')
+
+const normalizeWithParentId = (footprint, parentId) =>
+  normalize({ ...footprint, parentId: parentId }, footprintSchema)
 
 export const fetchAll = (urlParams) =>
   axios.get(baseURL(urlParams))
@@ -19,16 +22,16 @@ export const fetch = ({ id, ...baseUrlParams }) =>
   axios.get(`${baseURL(baseUrlParams)}/${id}`)
     .then(({ data }) => {
       console.log(data)
-      return normalize(data, locationSchema)
+      return normalizeWithParentId(data, baseUrlParams.account)
     })
 
 export const create = ({ payload, ...urlParams }) =>
   axios.post(baseURL(urlParams), payload)
-    .then(({ data }) => normalize(data, locationSchema))
+    .then(({ data }) => normalizeWithParentId(data, urlParams.account))
 
 export const update = ({ id, payload, ...baseUrlParams }) =>
   axios.put(`${baseURL(baseUrlParams)}/${id}`, payload)
-    .then(({ data }) => normalize(data, locationSchema))
+    .then(({ data }) => normalizeWithParentId(data, baseUrlParams.account))
 
 export const remove = ({ id, ...baseUrlParams }) =>
   axios.delete(`${baseURL(baseUrlParams)}/${id}`)
