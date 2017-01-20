6b97429aef175062bf2a966ea818cdaa1d5dc7b2
santtu-idean <santtu.sulander@idean.com>
2017-01-20 12:52:38 +0200

    correct id-param names

--- src/redux/modules/entities/nodes/selectors.js
+++ src/redux/modules/entities/nodes/selectors.js
@@ -1,6 +1,6 @@
 import * as entitySelectors from '../../entity/selectors'
 
-export const getByBrand = state => accountId => entitySelectors.getEntitiesByParent(state, 'nodes', accountId, 'brand_id')
-export const getByGroup = state => accountId => entitySelectors.getEntitiesByParent(state, 'nodes', accountId, 'group_id')
+export const getByBrand = state => brandId => entitySelectors.getEntitiesByParent(state, 'nodes', brandId, 'brand_id')
 export const getByAccount = state => accountId => entitySelectors.getEntitiesByParent(state, 'nodes', accountId, 'account_id')
+export const getByGroup = state => groupId => entitySelectors.getEntitiesByParent(state, 'nodes', groupId, 'group_id')
 export const getById = state => id => entitySelectors.getEntityById(state, 'nodes', id)
