b9216e94633edc9f37185ac8edaef1f1059b763c
santtu-idean <santtu.sulander@idean.com>
2017-01-23 15:19:46 +0200

    adjust api module to converse with cloudscale correctly

--- src/redux/modules/entities/nodes/api.js
+++ src/redux/modules/entities/nodes/api.js
@@ -3,31 +3,32 @@ import axios from 'axios'
 
 import { BASE_URL_NORTH } from '../../../util'
 
-const baseURL = ({ brand, account, group, pop }) =>
-  `${BASE_URL_NORTH}/brands/${brand}/accounts/${account}/groups/${group}/networks/${brand}/pops/${pop}/nodes`
+const baseURL = ({ brand, account, group, network, pop }) =>
+  `${BASE_URL_NORTH}/brands/${brand}/accounts/${account}/groups/${group}/networks/${network}/pops/${pop}/nodes`
 
 const nodeSchema = new schema.Entity('nodes')
 
+/**
+ * This endpoint supports pagination. Extract only data objects for now.
+ */
 export const fetchAll = (urlParams) =>
   axios.get(baseURL(urlParams))
     .then(({ data }) => {
-      console.log(data)
-      return data
+      return normalize(data.data, [ nodeSchema ])
     })
 
 export const fetch = ({ id, ...baseUrlParams }) =>
   axios.get(`${baseURL(baseUrlParams)}/${id}`)
     .then(({ data }) => {
-      console.log(data)
       return normalize(data, nodeSchema)
     })
 
 export const create = ({ payload, ...urlParams }) =>
-  axios.post(baseURL(urlParams), payload)
+  axios.post(baseURL(urlParams), payload, { headers: { 'Content-Type': 'application/json' } })
     .then(({ data }) => normalize(data, nodeSchema))
 
 export const update = ({ id, payload, ...baseUrlParams }) =>
-  axios.put(`${baseURL(baseUrlParams)}/${id}`, payload)
+  axios.put(`${baseURL(baseUrlParams)}/${id}`, payload, { headers: { 'Content-Type': 'application/json' } })
     .then(({ data }) => normalize(data, nodeSchema))
 
 export const remove = ({ id, ...baseUrlParams }) =>
