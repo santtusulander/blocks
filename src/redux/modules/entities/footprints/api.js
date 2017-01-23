f0f6889479631fe4c57649b941361ade22b61da3
santtu-idean <santtu.sulander@idean.com>
2017-01-23 16:25:30 +0200

    adjust api module to converse with api properly

--- src/redux/modules/entities/footprints/api.js
+++ src/redux/modules/entities/footprints/api.js
@@ -14,23 +14,21 @@ const normalizeWithParentId = (footprint, parentId) =>
 export const fetchAll = (urlParams) =>
   axios.get(baseURL(urlParams))
     .then(({ data }) => {
-      console.log(data)
-      return data
+      return normalize(data.data, [ footprintSchema ])
     })
 
 export const fetch = ({ id, ...baseUrlParams }) =>
   axios.get(`${baseURL(baseUrlParams)}/${id}`)
     .then(({ data }) => {
-      console.log(data)
       return normalizeWithParentId(data, baseUrlParams.account)
     })
 
 export const create = ({ payload, ...urlParams }) =>
-  axios.post(baseURL(urlParams), payload)
+  axios.post(baseURL(urlParams), payload, { headers: { 'Content-Type': 'application/json' } })
     .then(({ data }) => normalizeWithParentId(data, urlParams.account))
 
 export const update = ({ id, payload, ...baseUrlParams }) =>
-  axios.put(`${baseURL(baseUrlParams)}/${id}`, payload)
+  axios.put(`${baseURL(baseUrlParams)}/${id}`, payload, { headers: { 'Content-Type': 'application/json' } })
     .then(({ data }) => normalizeWithParentId(data, baseUrlParams.account))
 
 export const remove = ({ id, ...baseUrlParams }) =>
