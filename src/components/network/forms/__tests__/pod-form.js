586a4a3d12f56126352fcf9378b190aeb98b48c1
Nizar Rahme <nizar.rahme@nordsoftware.com>
2017-01-23 14:39:14 +0200

    Unit test for POD form

---
+++ src/components/network/forms/__tests__/pod-form.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { List } from 'immutable';
+
+jest.unmock('../pod-form')
+import PodForm from '../pod-form'
+
+function intlMaker() {
+  return {
+    formatMessage: jest.fn()
+  }
+}
+
+describe('PodForm', () => {
+  let subject, error, props = null
+
+  beforeEach(() => {
+    subject = () => {
+      props = {
+        accountIsServiceProviderType: false,
+        handleSubmit: jest.genMockFunction(),
+        intl: intlMaker()
+      }
+      return shallow(<PodForm {...props}/>)
+    }
+  })
+
+  it('should exist', () => {
+    expect(subject().length).toBe(1)
+  })
+})
