04ea64fd382da65b39286f190f88b121ebc37201
Mika Paulasaari <mika.paulasaari@idean.com>
2017-01-25 12:25:58 -0800

    Update test to be more robust

--- src/redux/modules/__tests__/traffic.js
+++ src/redux/modules/__tests__/traffic.js
@@ -55,9 +55,9 @@ describe('Traffic Module', () => {
     })
 
     it('should handle trafficByTimeSuccess', () => {
-      const newState = trafficByTimeSuccess(state, {payload: {data: {details: [{value: 'test', timestamp: timestamp}] }} })
+      const newState = trafficByTimeSuccess(state, {payload: {data: {totals: {}, details: [{value: 'test', timestamp: timestamp}] }} })
 
-      expect(newState.getIn(['byTime', 'details']).count() ).toBe(1);
+      expect(newState.get('byTime').count() ).toBe(2);
     })
 
     it('should handle trafficByTimeFailure', () => {
