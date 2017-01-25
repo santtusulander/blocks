0068dc28a00a1ca1f6bfbad452bae338974a5fa3
Andrii Frankiv <andrii.frankiv@gmail.com>
2017-01-25 15:15:15 +0200

    improve formFooterButtons, TODO:fix UnitTests

--- src/components/form/form-footer-buttons.jsx
+++ src/components/form/form-footer-buttons.jsx
@@ -1,27 +1,36 @@
 import React, { PropTypes } from 'react'
 import { ButtonToolbar } from 'react-bootstrap'
 
-const FormFooterButtons = ({children, autoAlign}) => {
-  const footerButtonsElems = autoAlign ? (
-    <ButtonToolbar className="pull-right">
-      {children}
-    </ButtonToolbar>
-  ) : children
 
+const getAlignStyle = (val) => {
+  const key = 'textAlign'
+
+  return ['left', 'right', 'center'].includes(val)
+    ? {[key]: val}
+    : {[key]: FormFooterButtons.defaultProps.alignment}
+}
+
+const FormFooterButtons = ({children, alignment}) => {
   return (
-    <div className='modal-footer'>
-      {footerButtonsElems}
+    <div
+      className='modal-footer'
+      style={getAlignStyle(alignment)}
+    >
+      <ButtonToolbar>
+        {children}
+      </ButtonToolbar>
     </div>
   )
 }
 
 FormFooterButtons.displayName = 'FormFooterButtons'
 FormFooterButtons.propTypes = {
-  autoAlign: PropTypes.bool,
+  alignment: PropTypes.string,
   children: PropTypes.array
 }
+
 FormFooterButtons.defaultProps = {
-  autoAlign: true
+  alignment: 'right'
 }
 
 export default FormFooterButtons
