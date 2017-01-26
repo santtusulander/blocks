4958b7a770a40c8cbfba1cb1587e1b24117ad571
Olga Metlyakova <ometlyakova@vidscale.com>
2017-01-26 12:40:53 +0200

    Removed faked api

--- fakevcdn/server.js
+++ fakevcdn/server.js
@@ -6,26 +6,16 @@ var middlewares = jsonServer.defaults()
 
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-  "/v2/service_info": "/v2/brands/udn/service_info",
-  "/v2/brands/udn/accounts/:account_id/groups/:group_id": "/v2/brands/udn/groups/:group_id",
-
-  "/VCDN/v2/brands/udn/accounts/:account_id/groups/:group_id/published_hosts": "/v2/brands/udn/published_hosts",
-  "/VCDN/v2/brands/udn/accounts/:account_id/groups/:group_id/published_hosts/:siteId": "/v2/brands/udn/host_data/1"
-
-
+  "/VCDN/v2/udn/accounts/:accountId/groups/:id": "/VCDN/v2/udn/groups/:id",
+  "/VCDN/v2/udn/accounts/:accountId/groups/:id/published_hosts": "/VCDN/v2/udn/groups/:id/published_hosts",
+  "/VCDN/v2/udn/accounts/:accountId/groups/:id/published_hosts/:siteId": "/VCDN/v2/udn/host_data/1"
 }))
 
 server.use(middlewares)
-server.use("/v2/brands/udn/", router)
+server.use("/VCDN/v2/udn", router)
 
 router.render = function (req, res) {
 
-  if (req.url.match(/accounts\/\d+$/i)) {
-    res.jsonp(res.locals.data)
-  } else
-  if (req.url.match(/service_info$/i)) {
-    res.jsonp(res.locals.data)
-  } else
   if (req.url.match(/published_hosts$/i)) {
     res.jsonp(res.locals.data[0].hosts)
   } else {
