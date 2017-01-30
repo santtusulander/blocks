d1d059efd37d2b35abd7099f81238325c60441d1
Mikko Alander <mikko.alander@idean.com>
2017-01-30 16:22:24 +0200

    add selectors

---
+++ src/redux/modules/entities/pods/selectors.js
@@ -0,0 +1,21 @@
+import {getEntityById, getEntitiesByParent} from '../../entity/selectors'
+
+/**
+ * Get property by ID
+ * @param  {} state from redux
+ * @param  String id of the item
+ * @return {} property
+ */
+export const getById = (state, id) => {
+  return getEntityById(state, 'pods', id)
+}
+
+/**
+ * Get Accounts By Brand
+ * @param  {} state
+ * @param  {String} brand [description]
+ * @return List
+ */
+export const getByPop = (state, popId) => {
+  return getEntitiesByParent(state, 'pods', popId)
+}
