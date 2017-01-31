2a950b84df01cee86f17146d7742da3062d8c893
Niklas Lepisto <niklas.lepisto@idean.com>
2017-01-31 13:52:39 -0800

    No need for full reference since this.state was already destructed

--- src/containers/network.jsx
+++ src/containers/network.jsx
@@ -662,7 +662,7 @@ class Network extends React.Component {
         {networkModal === ADD_EDIT_POD &&
           <PodFormContainer
             id="pod-form"
-            podId={this.state.podId}
+            podId={podId}
             edit={(podId !== null) ? true : false}
             onSave={this.handlePodSave}
             onCancel={() => this.handleCancel(ADD_EDIT_POD)}
@@ -671,7 +671,7 @@ class Network extends React.Component {
             {...params}
           />
         }
-        
+
         {networkModal === ADD_NODE &&
           <AddNodeContainer
             id="node-form"
