36d280df85ffb7f5f710ce60bfa3ef825fd43ab1
Niklas Lepisto <niklas.lepisto@idean.com>
2017-01-31 17:07:26 -0800

    Updates test message to be more descriptive

--- src/containers/__tests__/network.js
+++ src/containers/__tests__/network.js
@@ -9,17 +9,9 @@ import Network from '../network.jsx'
 import routeConstants from '../../constants/routes'
 
 describe('Network', () => {
-  it('URLs should end in "groups"', () => {
+  it('URLs should end in "groups" so that navigating and horizontal scrolling work', () => {
     expect(routeConstants.networkGroups).toMatch(/groups$/)
     // TODO UDNP-2563: This V2 can be removed after all the changes for Network are done.
     expect(routeConstants.networkGroupsV2).toMatch(/groups$/)
-  })
-
-  it('URLs should not end in "groups"', () => {
-    routeConstants.networkGroups += '/nope'
-    expect(routeConstants.networkGroups).not.toMatch(/groups$/)
-    // TODO UDNP-2563: This V2 can be removed after all the changes for Network are done.
-    routeConstants.networkGroupsV2 += '/nope'
-    expect(routeConstants.networkGroupsV2).not.toMatch(/groups$/)
   })
 })
