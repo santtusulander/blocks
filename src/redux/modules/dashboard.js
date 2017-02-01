95a48907e385223b24b10a97f3a35cefe953b240
Mika Paulasaari <mika.paulasaari@idean.com>
2017-01-31 16:02:19 -0800

    Requested changes

--- src/redux/modules/dashboard.js
+++ src/redux/modules/dashboard.js
@@ -60,119 +60,114 @@ export const fetchDashboard = createAction(DASHBOARD_FETCHED, (opts) => {
     axios.get(`${analyticsBase()}/traffic/country${qsBuilder(opts)}`).then(parseResponseData),
     axios.get(`${analyticsBase()}/traffic/cp-contribution${qsBuilder(contributionOpts)}`).then(parseResponseData)
   ])
-  .then(axios.spread((traffic, trafficOnOffNet, countries, cpContribution) => {
-    const trafficMap = Immutable.fromJS(traffic)
-    const trafficOnOffNetMap = Immutable.fromJS(trafficOnOffNet)
-    const countriesMap = Immutable.fromJS(countries)
-    const cpContributionMap = Immutable.fromJS(cpContribution)
-
-    const timeSpanInSeconds = opts.endDate - opts.startDate
-
-    let bandwidth = []
-    let latency = []
-    let connections = []
-    let cacheHit = []
-
-    const trafficDetails = trafficMap.getIn(['data', 0, 'detail'], []).reduce((details, detail) => {
-      const timestamp = detail.getIn(['timestamp'], null)
-      const connectionsTotalDetail = detail.getIn(['connections', 'total'], null)
-      const avg_fbl = detail.getIn(['avg_fbl'], '') || ''
-
-      bandwidth.push({
-        timestamp: timestamp,
-        bits_per_second: detail.getIn(['transfer_rates', 'total'], null)
-      })
-      latency.push({
-        timestamp: timestamp,
-        avg_fbl: Number(avg_fbl.split(' ', 1))
-      })
-      connections.push({
-        timestamp: timestamp,
-        connections: connectionsTotalDetail,
-        connections_per_second: connectionsTotalDetail / timeSpanInSeconds
-      })
-      cacheHit.push({
-        timestamp: timestamp,
-        chit_ratio: detail.getIn(['chit_ratio'], null)
-      })
-
-      details.bandwidth = bandwidth
-      details.latency = latency
-      details.connections = connections
-      details.cacheHit = cacheHit
-
-      return details
-    }, {
-      bandwidth: [],
-      latency: [],
-      connections: [],
-      cacheHit: []
-    })
-
-    return {
-      data: {
-        traffic: {
-          bytes: trafficMap.getIn(['data', 0, 'totals', 'bytes', 'total'], null),
-          bytes_net_on: trafficOnOffNetMap.getIn(['data', 'net_on', 'bytes'], null),
-          bytes_net_off: trafficOnOffNetMap.getIn(['data', 'net_off', 'bytes'], null),
-          detail: trafficOnOffNetMap.getIn(['data', 'detail'], []).map(detail => {
-            return {
-              timestamp: Number(detail.getIn(['timestamp'], null)),
-              bytes: detail.getIn(['total'], null),
-              bytes_net_on: detail.getIn(['net_on', 'bytes'], null),
-              bytes_net_off: detail.getIn(['net_off', 'bytes'], null)
-            }
-          }, {
-            timestamp: null,
-            bytes: null,
-            bytes_net_on: null,
-            bytes_net_off: null
-          }).toJS()
-        },
-        bandwidth: {
-          bits_per_second: trafficMap.getIn(['data', 0, 'totals', 'transfer_rates', 'average'], null),
-          detail: trafficDetails.bandwidth
-        },
-        latency: {
-          avg_fbl: Number(trafficMap.getIn(['data', 0, 'totals', 'avg_fbl']).split(' ', 1), null),
-          detail: trafficDetails.latency
-        },
-        connections: {
-          connections: trafficMap.getIn(['data', 0, 'totals', 'connections', 'total'], null),
-          connections_per_second: trafficMap.getIn(['data', 0, 'totals', 'connections', 'per_second'], null),
-          detail: trafficDetails.connections
-        },
-        cache_hit: {
-          chit_ratio: trafficMap.getIn(['data', 0, 'totals', 'chit_ratio'], null),
-          detail: trafficDetails.cacheHit
-        },
-        countries: countriesMap.getIn(['data', 'countries']),
-        providers: cpContributionMap.getIn(['data', 'details'], Immutable.List()).map(provider => {
-          const bytes = (
-            provider.getIn(['http', 'net_off_bytes'], 0) +
-            provider.getIn(['http', 'net_on_bytes'], 0) +
-            provider.getIn(['https', 'net_off_bytes'], 0) +
-            provider.getIn(['https', 'net_on_bytes'], 0)
-          )
-          const bits_per_second = (
-            provider.getIn(['http', 'net_off_bps'], 0) +
-            provider.getIn(['http', 'net_on_bps'], 0) +
-            provider.getIn(['https', 'net_off_bps'], 0) +
-            provider.getIn(['https', 'net_on_bps'], 0)
-          )
-          return {
-            account: provider.getIn(['account'], null),
-            bytes: bytes,
-            bits_per_second: bits_per_second,
-            detail: provider.getIn(['detail'], []),
-            percent_total: provider.getIn(['percent_total'], null)
-          }
-        }).toJS()
-      }
-    }
-  }))
+  .then(axios.spread(processDashboardData))
 })
 
 export const startFetching = createAction(DASHBOARD_START_FETCH)
 
 export const finishFetching = createAction(DASHBOARD_FINISH_FETCH)
+
+
+// DATA PROCESSING
+
+// This function is used for combining the data coming from several endpoints into
+// a single object, which get passed to the dashboard container. This was done
+// so that there was no need to do a lot of changes in the container, which used
+// previously the sp-dashboard endpoint and received an ob
+export function processDashboardData(traffic, trafficOnOffNet, countries, cpContribution) {
+  // Creating Immutable objects for easier error handling
+  const trafficMap = Immutable.fromJS(traffic)
+  const trafficOnOffNetMap = Immutable.fromJS(trafficOnOffNet)
+  const countriesMap = Immutable.fromJS(countries)
+  const cpContributionMap = Immutable.fromJS(cpContribution)
+
+  // Creates detail arrays for bandwidth, latency, connections and cache hit
+  const trafficDetails = trafficMap.getIn(['data', 0, 'detail'], []).reduce((details, detail) => {
+    const timestamp = detail.getIn(['timestamp'], null)
+    const avg_fbl = detail.getIn(['avg_fbl'], '') || ''
+
+    details.bandwidth.push({
+      timestamp: timestamp,
+      bits_per_second: detail.getIn(['transfer_rates', 'total'], null)
+    })
+    details.latency.push({
+      timestamp: timestamp,
+      avg_fbl: Number(avg_fbl.split(' ', 1))
+    })
+    details.connections.push({
+      timestamp: timestamp,
+      connections: detail.getIn(['connections', 'total'], null),
+      connections_per_second: detail.getIn(['connections', 'per_second'], null)
+    })
+    details.cacheHit.push({
+      timestamp: timestamp,
+      chit_ratio: detail.getIn(['chit_ratio'], null)
+    })
+
+    return details
+  }, {
+    bandwidth: [],
+    latency: [],
+    connections: [],
+    cacheHit: []
+  })
+
+  // Final data object returned
+  return {
+    data: {
+      traffic: {
+        bytes: trafficMap.getIn(['data', 0, 'totals', 'bytes', 'total'], null),
+        bytes_net_on: trafficOnOffNetMap.getIn(['data', 'net_on', 'bytes'], null),
+        bytes_net_off: trafficOnOffNetMap.getIn(['data', 'net_off', 'bytes'], null),
+        detail: trafficOnOffNetMap.getIn(['data', 'detail'], Immutable.List()).map(detail => {
+          return {
+            timestamp: Number(detail.getIn(['timestamp'], null)),
+            bytes: detail.getIn(['total'], null),
+            bytes_net_on: detail.getIn(['net_on', 'bytes'], null),
+            bytes_net_off: detail.getIn(['net_off', 'bytes'], null)
+          }
+        }).toJS()
+      },
+      bandwidth: {
+        bits_per_second: trafficMap.getIn(['data', 0, 'totals', 'transfer_rates', 'average'], null),
+        detail: trafficDetails.bandwidth
+      },
+      latency: {
+        avg_fbl: Number(trafficMap.getIn(['data', 0, 'totals', 'avg_fbl'], '').split(' ', 1)),
+        detail: trafficDetails.latency
+      },
+      connections: {
+        connections: trafficMap.getIn(['data', 0, 'totals', 'connections', 'total'], null),
+        connections_per_second: trafficMap.getIn(['data', 0, 'totals', 'connections', 'per_second'], null),
+        detail: trafficDetails.connections
+      },
+      cache_hit: {
+        chit_ratio: trafficMap.getIn(['data', 0, 'totals', 'chit_ratio'], null),
+        detail: trafficDetails.cacheHit
+      },
+      countries: countriesMap.getIn(['data', 'countries'], []),
+      providers: cpContributionMap.getIn(['data', 'details'], Immutable.List()).map(provider => {
+        // Calculate bytes and bits_per_second since these are not returned as totals
+        const bytes = (
+          provider.getIn(['http', 'net_off_bytes'], 0) +
+          provider.getIn(['http', 'net_on_bytes'], 0) +
+          provider.getIn(['https', 'net_off_bytes'], 0) +
+          provider.getIn(['https', 'net_on_bytes'], 0)
+        )
+        const bits_per_second = (
+          provider.getIn(['http', 'net_off_bps'], 0) +
+          provider.getIn(['http', 'net_on_bps'], 0) +
+          provider.getIn(['https', 'net_off_bps'], 0) +
+          provider.getIn(['https', 'net_on_bps'], 0)
+        )
+        return {
+          account: provider.getIn(['account'], null),
+          bytes: bytes,
+          bits_per_second: bits_per_second,
+          detail: provider.getIn(['detail'], []),
+          percent_total: provider.getIn(['percent_total'], null)
+        }
+      }).toJS()
+    }
+  }
+}
