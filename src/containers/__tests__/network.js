e95c66c4b9e767a55e9cba0f6fb237c9c192fdeb
Niklas Lepisto <niklas.lepisto@idean.com>
2017-01-31 18:02:21 -0800

    Adds exist test for Network

--- src/containers/__tests__/network.js
+++ src/containers/__tests__/network.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {shallow} from 'enzyme'
+import { Map } from 'immutable'
+import { shallow } from 'enzyme'
 
 jest.unmock('../network.jsx')
 jest.unmock('../../constants/routes')
@@ -8,10 +9,33 @@ import Network from '../network.jsx'
 
 import routeConstants from '../../constants/routes'
 
+const mockFetchData = jest.fn()
+const mockAccount = Map()
+const mockParams = Map()
+const mockLocation = {
+  pathname: ''
+}
+
+const subject = () => {
+  return (
+    <Network
+      params={mockParams}
+      location={mockLocation}
+      account={mockAccount}
+      fetchData={mockFetchData}
+    />
+  )
+}
+
 describe('Network', () => {
   it('URLs should end in "groups" so that navigating and horizontal scrolling work', () => {
     expect(routeConstants.networkGroups).toMatch(/groups$/)
     // TODO UDNP-2563: This V2 can be removed after all the changes for Network are done.
     expect(routeConstants.networkGroupsV2).toMatch(/groups$/)
+  })
+
+  it('should exist', () => {
+    const network = shallow(subject())
+    expect(network.length).toBe(1)
   })
 })
