8c5309f2a24042f3457dfbc4d56a456be9fcb0b8
Mikko Alander <mikko.alander@idean.com>
2017-02-02 23:35:06 +0200

    flatten POD service - array values for UI

--- src/redux/modules/entities/pods/api.js
+++ src/redux/modules/entities/pods/api.js
@@ -10,12 +10,29 @@ const baseUrl = ({ brand, account, group, network, pop }) => {
 const pod = new schema.Entity('pods', {
   //footprints: [ footprint ]
 }, {
+  //Add POP-id tomake unique POD IDs
   idAttribute: (value, parent) => { return `${parent.id}-${value.pod_name}`},
   processStrategy: (value, parent) => {
+
+    const {footprints, services: [ { cloud_lookup_id, lb_method, local_as, request_fwd_type, provider_weight, sp_bgp_router_ip, sp_bgp_router_as, sp_bgp_router_password } ] } = value
+
+    /* UI - params are extracted from services to keep UI - object flat */
     return {
-      ...value,
       parentId: parent.id,
-      name: value.pod_name
+      UIName: value.pod_name,
+      UIId: value.pod_name,
+      UICloudLookUpId: cloud_lookup_id,
+      UILbMethod: lb_method,
+      UILocalAs: local_as,
+      UIRequestFwdType: request_fwd_type,
+      UIProviderWeight: provider_weight,
+      UIDiscoveryMethod: footprints && footprints.length > 0 ? 'footprints' : 'BGP',
+      UIBGP: {
+        sp_bgp_router_as,
+        sp_bgp_router_ip,
+        sp_bgp_router_password
+      },
+      ...value
     }
   }
 })
