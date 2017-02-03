612f037ed11d27330acef5fea61b60b2a09e1c0a
Ville Ristimäki <ville.ristimaki@idean.com>
2017-02-03 16:36:21 +0200

    Remove FormGroup -components

--- src/components/network/forms/footprint-form.jsx
+++ src/components/network/forms/footprint-form.jsx
@@ -130,77 +130,69 @@ class FootprintForm extends React.Component {
           {this.props.error}
           </span>
 
-        <FormGroup>
-          <Field
-            name="addFootprintMethod"
-            type="radio"
-            value="manual"
-            component={FieldRadio}
-            label={<FormattedMessage id="portal.network.footprintForm.checkbox.option.manual.text"/>}
-          />
+        <Field
+          name="addFootprintMethod"
+          type="radio"
+          value="manual"
+          component={FieldRadio}
+          label={<FormattedMessage id="portal.network.footprintForm.checkbox.option.manual.text"/>}
+        />
 
-          <Field
-            name="addFootprintMethod"
-            type="radio"
-            value="addfile"
-            component={FieldRadio}
-            label={<FormattedMessage id="portal.network.footprintForm.checkbox.option.useCSV.text"/>}
-          />
-        </FormGroup>
+        <Field
+          name="addFootprintMethod"
+          type="radio"
+          value="addfile"
+          component={FieldRadio}
+          label={<FormattedMessage id="portal.network.footprintForm.checkbox.option.useCSV.text"/>}
+        />
 
         { addManual === 'manual' &&
         <div>
-          <FormGroup>
-            <Field
-              type="text"
-              name="name"
-              placeholder={intl.formatMessage({ id: 'portal.network.footprintForm.name.placeholder.text' })}
-              component={FieldFormGroup}
-              label={<FormattedMessage id="portal.network.footprintForm.name.title.text"/>}
-            />
+          <Field
+            type="text"
+            name="name"
+            placeholder={intl.formatMessage({ id: 'portal.network.footprintForm.name.placeholder.text' })}
+            component={FieldFormGroup}
+            label={<FormattedMessage id="portal.network.footprintForm.name.title.text"/>}
+          />
 
-            <Field
-              name="description"
-              type="text"
-              placeholder={intl.formatMessage({ id: 'portal.network.footprintForm.description.placeholder.text' })}
-              component={FieldFormGroup}
-              label={<FormattedMessage id="portal.network.footprintForm.description.title.text"/>}
-            />
-          </FormGroup>
+          <Field
+            name="description"
+            type="text"
+            placeholder={intl.formatMessage({ id: 'portal.network.footprintForm.description.placeholder.text' })}
+            component={FieldFormGroup}
+            label={<FormattedMessage id="portal.network.footprintForm.description.title.text"/>}
+          />
 
-          <FormGroup>
+          <ControlLabel>
+            <FormattedMessage id="portal.network.footprintForm.dataType.title.text"/>*
+          </ControlLabel>
 
-            <ControlLabel>
-              <FormattedMessage id="portal.network.footprintForm.dataType.title.text"/>*
-            </ControlLabel>
+          <Field
+            name="data_type"
+            type="radio"
+            value="ipv4cidr"
+            component={FieldRadio}
+            label={<FormattedMessage id="portal.network.footprintForm.dataType.option.cidr.text"/>}
+          />
 
-            <Field
-              name="data_type"
-              type="radio"
-              value="ipv4cidr"
-              component={FieldRadio}
-              label={<FormattedMessage id="portal.network.footprintForm.dataType.option.cidr.text"/>}
-            />
+          <Field
+            type="radio"
+            name="data_type"
+            value="asnlist"
+            component={FieldRadio}
+            label={<FormattedMessage id="portal.network.footprintForm.dataType.option.asn.text"/>}
+          />
 
-            <Field
-              type="radio"
-              name="data_type"
-              value="asnlist"
-              component={FieldRadio}
-              label={<FormattedMessage id="portal.network.footprintForm.dataType.option.asn.text"/>}
-            />
-
-            <Field
-              required={true}
-              name="value"
-              allowNew={true}
-              component={FieldFormGroupTypeahead}
-              multiple={true}
-              options={[]}
-              validation={typeaheadValidationMethod}
-            />
-
-          </FormGroup>
+          <Field
+            required={true}
+            name="value"
+            allowNew={true}
+            component={FieldFormGroupTypeahead}
+            multiple={true}
+            options={[]}
+            validation={typeaheadValidationMethod}
+          />
 
           <Field
             name="udn_type"
