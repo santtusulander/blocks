e3f6f3723eb41fb9a08fe18b80341d3cbc07ffe2
Nizar Rahme <nizar.rahme@nordsoftware.com>
2017-02-04 14:50:10 +0200

    UDNP-2598 | Add delete confirmation to the action item - v2

--- src/components/action-buttons.jsx
+++ src/components/action-buttons.jsx
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react'
+import React, { Component, PropTypes } from 'react'
 import {Button} from 'react-bootstrap'
 import classNames from 'classnames'
 
@@ -9,70 +9,117 @@ import IconClose from './icons/icon-close.jsx'
 import IconArrowUp from './icons/icon-arrow-up.jsx'
 import IconArrowDown from './icons/icon-arrow-down.jsx'
 
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import Confirmation from './confirmation.jsx'
+import { FormattedMessage, injectIntl } from 'react-intl'
+
 import { ALLOW_ALWAYS } from '../constants/permissions'
 
-function ActionButtons ({ arrowDownDisabled, arrowUpDisabled, className, deleteDisabled, removeDisabled, onArrowDown, onArrowUp, onEdit, onDelete, onRemove, permissions }) {
-  let finalClassName = classNames(
-    'action-buttons',
-    {
-      'primary': !className
-    },
-    className
-  );
+class ActionButtons extends Component {
+  constructor() {
+    super()
+    this.state={
+      showConfirmation: false
+    }
+  }
 
-  return (
-    <div className={finalClassName}>
-      {onArrowUp &&
-      <Button
-        onClick={onArrowUp}
-        className="btn btn-icon arrow-down-button"
-        disabled={arrowUpDisabled}>
-        <IconArrowUp />
-      </Button>
-      }
+  render() {
+    const { arrowDownDisabled,
+            arrowUpDisabled,
+            className,
+            deleteDisabled,
+            removeDisabled,
+            onArrowDown,
+            onArrowUp,
+            onEdit,
+            onDelete,
+            onRemove,
+            permissions,
+            intl,
+            showConfirmation } = this.props
 
-      {onArrowDown &&
-      <Button
-        onClick={onArrowDown}
-        className="btn btn-icon arrow-up-button"
-        disabled={arrowDownDisabled}>
-        <IconArrowDown />
-      </Button>
-      }
+    const finalClassName = classNames(
+      'action-buttons',
+      {
+        'primary': !className
+      },
+      className
+    );
 
-      {onEdit &&
-      <IsAllowed to={permissions ? permissions.modify : ALLOW_ALWAYS}>
+    return (
+      <div className={finalClassName}>
+        {onArrowUp &&
         <Button
-          onClick={onEdit}
-          className="btn btn-icon edit-button">
-          <IconEdit />
+          onClick={onArrowUp}
+          className="btn btn-icon arrow-down-button"
+          disabled={arrowUpDisabled}>
+          <IconArrowUp />
         </Button>
-      </IsAllowed>
-      }
+        }
 
-      {onDelete &&
-      <IsAllowed to={permissions ? permissions.delete : ALLOW_ALWAYS}>
+        {onArrowDown &&
         <Button
-          onClick={onDelete}
-          className="btn btn-icon delete-button"
-          disabled={deleteDisabled}>
-          <IconTrash/>
+          onClick={onArrowDown}
+          className="btn btn-icon arrow-up-button"
+          disabled={arrowDownDisabled}>
+          <IconArrowDown />
         </Button>
-      </IsAllowed>
-      }
+        }
 
-      {onRemove &&
-      <IsAllowed to={permissions ? permissions.delete : ALLOW_ALWAYS}>
-        <Button
-          onClick={onRemove}
-          className="btn btn-icon remove-button"
-          disabled={removeDisabled}>
-          <IconClose/>
-        </Button>
-      </IsAllowed>
-      }
-    </div>
-  )
+        {onEdit &&
+        <IsAllowed to={permissions ? permissions.modify : ALLOW_ALWAYS}>
+          <Button
+            onClick={onEdit}
+            className="btn btn-icon edit-button">
+            <IconEdit />
+          </Button>
+        </IsAllowed>
+        }
+
+        {onDelete &&
+        <IsAllowed to={permissions ? permissions.delete : ALLOW_ALWAYS}>
+          <Button
+            onClick={onDelete}
+            className="btn btn-icon delete-button"
+            disabled={deleteDisabled}>
+            <IconTrash/>
+          </Button>
+        </IsAllowed>
+        }
+
+        {onRemove &&
+        <IsAllowed to={permissions ? permissions.delete : ALLOW_ALWAYS}>
+          <Button
+            onClick={showConfirmation? () => this.setState({showConfirmation: true}) :onRemove}
+            className="btn btn-icon remove-button"
+            disabled={removeDisabled}>
+            <IconClose/>
+          </Button>
+        </IsAllowed>
+        }
+
+        {showConfirmation &&
+          <ReactCSSTransitionGroup
+            component="div"
+            className="confirmation-transition"
+            transitionName="confirmation-transition"
+            transitionEnterTimeout={10}
+            transitionLeaveTimeout={500}
+            transitionAppear={true}
+            transitionAppearTimeout={10}>
+            {this.state.showConfirmation &&
+            <Confirmation
+              cancelText={intl.formatMessage({id: 'portal.button.no'})}
+              confirmText={intl.formatMessage({id: 'portal.button.yes'})}
+              handleConfirm={onDelete? onDelete: onRemove}
+              handleCancel={() => this.setState({ showConfirmation: false })}>
+              <FormattedMessage id="portal.common.delete.disclaimer.text"/>
+            </Confirmation>}
+          </ReactCSSTransitionGroup>
+        }
+      </div>
+    )
+  }
 }
 
 ActionButtons.displayName = "ActionButtons"
@@ -81,13 +128,15 @@ ActionButtons.propTypes = {
   arrowUpDisabled: PropTypes.bool,
   className: PropTypes.string,
   deleteDisabled: PropTypes.bool,
+  intl: PropTypes.object,
   onArrowDown: PropTypes.func,
   onArrowUp: PropTypes.func,
   onDelete: PropTypes.func,
   onEdit: PropTypes.func,
   onRemove: PropTypes.func,
   permissions: PropTypes.object,
-  removeDisabled: PropTypes.bool
+  removeDisabled: PropTypes.bool,
+  showConfirmation: PropTypes.bool
 }
 
-export default ActionButtons
+export default injectIntl(ActionButtons)
