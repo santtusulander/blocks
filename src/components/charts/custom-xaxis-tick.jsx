29376abfb651a86aee7b824c4e6e63ff374a107e
Nizar Rahme <nizar.rahme@nordsoftware.com>
2017-02-06 13:26:14 +0200

    Modify the PropType

--- src/components/charts/custom-xaxis-tick.jsx
+++ src/components/charts/custom-xaxis-tick.jsx
@@ -4,7 +4,8 @@ const CustomXAxisTick = ({x, y, payload, secondaryXAxisTick}) =>
   <g transform={`translate(${x},${y})`}>
     <text x="0" y="0"  textAnchor="end" >
       <tspan x={0} dy={16}>{payload.value}</tspan>
-      <tspan x={0} dy={16}>{payload.index === 0 ?
+      <tspan x={0} dy={16}>
+      {payload.index === 0 ?
          secondaryXAxisTick[payload.index] :
          secondaryXAxisTick[payload.index -1] !== secondaryXAxisTick[payload.index] &&
          secondaryXAxisTick[payload.index]
@@ -16,7 +17,7 @@ const CustomXAxisTick = ({x, y, payload, secondaryXAxisTick}) =>
 CustomXAxisTick.displayName = "CustomLegend"
 CustomXAxisTick.propTypes = {
   payload: PropTypes.object,
-  secondaryXAxisTick: PropTypes.array.isRequired,
+  secondaryXAxisTick: PropTypes.arrayOf(PropTypes.string).isRequired,
   x: PropTypes.number,
   y: PropTypes.number
 }
