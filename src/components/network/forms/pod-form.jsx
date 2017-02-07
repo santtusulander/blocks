68ab19ac8a7670ef471ac75701a99a376b86997d
Ville Ristimäki <ville.ristimaki@idean.com>
2017-02-07 10:59:29 +0200

    Remove redundand args

--- src/components/network/forms/pod-form.jsx
+++ src/components/network/forms/pod-form.jsx
@@ -356,7 +356,7 @@ const PodForm = ({
                      icon={true}
                      addNew={true}
                      disabled={hasBGPRoutingDaemon}
-                     onClick={() => onShowRoutingDaemonModal(false)}>
+                     onClick={onShowRoutingDaemonModal}>
             <IconAdd/>
           </UDNButton>
         </label>
@@ -371,7 +371,7 @@ const PodForm = ({
               <Col xs={4} className="action-buttons">
                 <Button
                   className="btn btn-icon edit-button"
-                  onClick={() => onShowRoutingDaemonModal(true)}>
+                  onClick={onShowRoutingDaemonModal}>
                   <IconEdit/>
                 </Button>
 
