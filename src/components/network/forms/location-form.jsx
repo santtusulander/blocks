eb86710b782c480d3868358f0573b5d56caed30f
Ville Ristimäki <ville.ristimaki@idean.com>
2017-02-08 11:35:32 +0200

    Change location-form to stateful component

--- src/components/network/forms/location-form.jsx
+++ src/components/network/forms/location-form.jsx
@@ -14,16 +14,14 @@ import LoadingSpinnerSmall from '../../loading-spinner/loading-spinner-sm'
 import { locationReverseGeoCodingLookup } from '../../../util/network-helpers'
 import { isValidLatitude, isValidLongtitude, isValidTextField } from '../../../util/validators.js'
 
-import { isValidLatitude, isValidLongtitude , isValidTextField} from '../../../util/validators.js'
-
-import {LOCATION_NAME_MIN_LENGTH,
-        LOCATION_NAME_MAX_LENGTH,
-        CLOUD_PROVIDER_REGION_MIN_LENGTH,
-        CLOUD_PROVIDER_REGION_MAX_LENGTH,
-        CLOUD_PROVIDER_LOCATION_ID_MIN_LENGTH,
-        CLOUD_PROVIDER_LOCATION_ID_MAX_LENGTH
-      } from '../../../constants/network.js'
-
+import {
+  LOCATION_NAME_MIN_LENGTH,
+  LOCATION_NAME_MAX_LENGTH,
+  CLOUD_PROVIDER_REGION_MIN_LENGTH,
+  CLOUD_PROVIDER_REGION_MAX_LENGTH,
+  CLOUD_PROVIDER_LOCATION_ID_MIN_LENGTH,
+  CLOUD_PROVIDER_LOCATION_ID_MAX_LENGTH
+} from '../../../constants/network.js'
 
 const validate = ({
   name = '',
@@ -31,7 +29,8 @@ const validate = ({
   latitude = '',
   longitude = '',
   cloudProviderRegion,
-  cloudProviderLocationId = '' }) => {
+  cloudProviderLocationId = ''
+}) => {
   const customConditions = {
     name: [
       {
@@ -46,20 +45,20 @@ const validate = ({
     ],
     latitude: [
       {
-        condition: ! isValidLatitude(latitude),
+        condition: !isValidLatitude(latitude),
         errorText: (
           <div>
-            <FormattedMessage id='portal.network.locationForm.latitude.invalid.error' />
+            <FormattedMessage id='portal.network.locationForm.latitude.invalid.error'/>
           </div>
         )
       }
     ],
     longitude: [
       {
-        condition: ! isValidLongtitude(longitude),
+        condition: !isValidLongtitude(longitude),
         errorText: (
           <div>
-            <FormattedMessage id='portal.network.locationForm.longitude.invalid.error' />
+            <FormattedMessage id='portal.network.locationForm.longitude.invalid.error'/>
           </div>
         )
       }
@@ -105,146 +104,236 @@ const validate = ({
 
 }
 
-const NetworkLocationForm = (props) => {
-  const {
-    addressFetching,
-    cloudProvidersOptions,
-    cloudProvidersIdOptions,
-    error,
-    submitting,
-    initialValues,
-    iataCodes,
-    intl,
-    invalid,
-    onCancel,
-    onDelete,
-    handleSubmit
-  } = props;
+class NetworkLocationForm extends React.Component {
+  constructor(props) {
+    super(props)
 
-  const edit = !!initialValues.name
+    const { intl } = this.props
 
-  const actionButtonTitle = submitting ? <FormattedMessage id="portal.button.saving"/> :
-                              edit ? <FormattedMessage id="portal.button.save"/> :
-                              <FormattedMessage id="portal.button.add"/>
-
-  return (
-    <form className="sp-location-form" onSubmit={handleSubmit}>
-
-    {
-      error &&
-      <p className='has-error'>
-        <span className='help-block'>{error}</span>
-      </p>
+    this.state = {
+      isFetchingLocation: false,
+      addressLine: intl.formatMessage({ id: 'portal.network.locationForm.latLongFields.helperTextHint.address' }),
+      latLng: {
+        latitude: null,
+        longitude: null
+      }
     }
 
-      <Row>
-        <Col md={7}>
-          <Field
-            name="name"
-            disabled={edit}
-            type="text"
-            placeholder={intl.formatMessage({id: 'portal.network.locationForm.name.placeholder'})}
-            component={FieldFormGroup}
-            label={<FormattedMessage id="portal.common.name" />}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={7}>
-          <Field
-            name="iataCode"
-            options={iataCodes}
-            emptyLabel={intl.formatMessage({id: 'portal.analytics.dropdownMenu.noResults'})}
-            filterBy={['iata', 'city', 'country']}
-            labelKey={'iata'}
-            placeholder={intl.formatMessage({id: 'portal.network.locationForm.iataCode.placeholder'})}
-            component={FieldFormGroupTypeahead}
-            label={<FormattedMessage id="portal.network.locationForm.iataCode.label" />}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={5}>
-          <Field
-            name="latitude"
-            type="text"
-            component={FieldFormGroup}
-            placeholder={intl.formatMessage({id: 'portal.network.locationForm.latitude.placeholder'})}
-            label={<FormattedMessage id="portal.network.locationForm.latitude.label" />}
-          />
-        </Col>
-        <Col md={5}>
-          <Field
-            name="longitude"
-            type="text"
-            component={FieldFormGroup}
-            placeholder={intl.formatMessage({id: 'portal.network.locationForm.longitude.placeholder'})}
-            label={<FormattedMessage id="portal.network.locationForm.longitude.label" />}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={4}>
-          <div>
-            <FormattedMessage id="portal.network.locationForm.latLongFields.helperText.address" />
-          </div>
-          <div>
-            <FormattedMessage id="portal.network.locationForm.latLongFields.helperTextHint.address" />
-          </div>
-          { addressFetching && <LoadingSpinnerSmall /> }
-        </Col>
-      </Row>
-      <Row>
-        <Col md={7}>
-          <Field
-            name="cloudName"
-            className="input-select"
-            type="select"
-            options={cloudProvidersOptions}
-            component={FieldFormGroupSelect}
-            label={intl.formatMessage({id: 'portal.network.locationForm.cloudProvider.label'})}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={7}>
-          <Field
-            name="cloudProvider"
-            className="input-select"
-            type="select"
-            options={cloudProvidersIdOptions}
-            required={false}
-            component={FieldFormGroupSelect}
-            label={intl.formatMessage({id: 'portal.network.locationForm.cloudProviderId.label'})}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={7}>
-          <Field
-            name="cloudProviderRegion"
-            required={false}
-            type="text"
-            component={FieldFormGroup}
-            placeholder={intl.formatMessage({id: 'portal.network.locationForm.cloudProviderRegion.placeholder'})}
-            label={<FormattedMessage id="portal.network.locationForm.cloudProviderRegion.label" />}
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={7}>
-          <Field
-            name="cloudProviderLocationId"
-            type="text"
-            component={FieldFormGroup}
-            placeholder={intl.formatMessage({id: 'portal.network.locationForm.cloudProviderLocationId.placeholder'})}
-            label={<FormattedMessage id="portal.network.locationForm.cloudProviderLocationId.label" />}
-          />
-        </Col>
-      </Row>
+    this.onSubmit = this.onSubmit.bind(this)
+    this.fetchLocation = this.fetchLocation.bind(this)
+    this.processLatLngFields = this.processLatLngFields.bind(this)
+    this.shouldFetchLocation = this.shouldFetchLocation.bind(this)
+  }
 
-      <FormFooterButtons>
-        { edit &&
+  componentWillMount() {
+    const { initialValues } = this.props;
+    const edit = !!initialValues.name
+
+    if (edit) {
+      this.setState({
+        latLng: {
+          latitude: initialValues.latitude,
+          longitude: initialValues.longitude
+        }
+      })
+    }
+  }
+
+  fetchLocation({ target: { value } }, fieldName) {
+    this.processLatLngFields(fieldName, value)
+      .then(() => {
+        if (this.shouldFetchLocation()) {
+          this.setState({
+            isFetchingLocation: true,
+            latLng: this.props.latLng
+          })
+          const { latLng } = this.state
+          locationReverseGeoCodingLookup(latLng.longitude, latLng.latitude)
+            .then(({ features }) => {
+              this.setState({
+                addressLine: features[0].place_name,
+                isFetchingLocation: false
+              })
+            })
+            .catch(() => {
+              this.setState({
+                addressLine: <FormattedMessage id="portal.network.locationForm.latLongFields.addressNotFound"/>,
+                isFetchingLocation: false
+              })
+            })
+        }
+      })
+  }
+
+  shouldFetchLocation() {
+    const latLngProps = this.props.latLng
+    const latLngState = this.state.latLng
+    return !!latLngProps.latitude && !!latLngProps.longitude &&
+      isValidLatitude(latLngProps.latitude) &&
+      isValidLongtitude(latLngProps.longitude) &&
+      (
+        parseFloat(latLngProps.latitude) !== parseFloat(latLngState.latitude) ||
+        parseFloat(latLngProps.longitude) !== parseFloat(latLngState.longitude)
+      )
+  }
+
+  // Needed as we're using custom listeners on redux-form Field
+  processLatLngFields(fieldName, value) {
+    const { changeField, touchField } = this.props
+    return new Promise(resolve => {
+      changeField(fieldName, value)
+      touchField(fieldName)
+
+      return resolve()
+    })
+  }
+
+  onSubmit(values) {
+    this.props.onSubmit(values)
+  }
+
+  render() {
+    const {
+      cloudProvidersOptions,
+      cloudProvidersIdOptions,
+      error,
+      submitting,
+      initialValues,
+      iataCodes,
+      intl,
+      invalid,
+      onCancel,
+      onDelete,
+      handleSubmit
+    } = this.props;
+
+    const { addressLine, isFetchingLocation } = this.state
+    const edit = !!initialValues.name
+
+    const actionButtonTitle = submitting ? <FormattedMessage id="portal.button.saving"/> :
+      edit ? <FormattedMessage id="portal.button.save"/> :
+        <FormattedMessage id="portal.button.add"/>
+
+    return (
+      <form className="sp-location-form" onSubmit={handleSubmit}>
+
+        {
+          error &&
+          <p className='has-error'>
+            <span className='help-block'>{error}</span>
+          </p>
+        }
+
+        <Row>
+          <Col md={7}>
+            <Field
+              name="name"
+              disabled={edit}
+              type="text"
+              placeholder={intl.formatMessage({ id: 'portal.network.locationForm.name.placeholder' })}
+              component={FieldFormGroup}
+              label={<FormattedMessage id="portal.common.name"/>}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={7}>
+            <Field
+              name="iataCode"
+              options={iataCodes}
+              emptyLabel={intl.formatMessage({ id: 'portal.analytics.dropdownMenu.noResults' })}
+              filterBy={['iata', 'city', 'country']}
+              labelKey={'iata'}
+              placeholder={intl.formatMessage({ id: 'portal.network.locationForm.iataCode.placeholder' })}
+              component={FieldFormGroupTypeahead}
+              label={<FormattedMessage id="portal.network.locationForm.iataCode.label"/>}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={5}>
+            <Field
+              name="latitude"
+              type="text"
+              component={FieldFormGroup}
+              placeholder={intl.formatMessage({ id: 'portal.network.locationForm.latitude.placeholder' })}
+              label={<FormattedMessage id="portal.network.locationForm.latitude.label"/>}
+              onBlur={(e) => this.fetchLocation(e, 'latitude')}
+            />
+          </Col>
+          <Col md={5}>
+            <Field
+              name="longitude"
+              type="text"
+              component={FieldFormGroup}
+              placeholder={intl.formatMessage({ id: 'portal.network.locationForm.longitude.placeholder' })}
+              label={<FormattedMessage id="portal.network.locationForm.longitude.label"/>}
+              onBlur={(e) => this.fetchLocation(e, 'longitude')}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            {isFetchingLocation ?
+              <div>
+                <LoadingSpinnerSmall/>
+                <FormattedMessage id="portal.network.locationForm.latLongFields.helperTextLoading.address"/>
+              </div> :
+              <p>{addressLine}</p>
+            }
+          </Col>
+        </Row>
+        <Row>
+          <Col md={7}>
+            <Field
+              name="cloudName"
+              className="input-select"
+              type="select"
+              options={cloudProvidersOptions}
+              component={FieldFormGroupSelect}
+              label={intl.formatMessage({ id: 'portal.network.locationForm.cloudProvider.label' })}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={7}>
+            <Field
+              name="cloudProvider"
+              className="input-select"
+              type="select"
+              options={cloudProvidersIdOptions}
+              required={false}
+              component={FieldFormGroupSelect}
+              label={intl.formatMessage({ id: 'portal.network.locationForm.cloudProviderId.label' })}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={7}>
+            <Field
+              name="cloudProviderRegion"
+              required={false}
+              type="text"
+              component={FieldFormGroup}
+              placeholder={intl.formatMessage({ id: 'portal.network.locationForm.cloudProviderRegion.placeholder' })}
+              label={<FormattedMessage id="portal.network.locationForm.cloudProviderRegion.label"/>}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={7}>
+            <Field
+              name="cloudProviderLocationId"
+              type="text"
+              component={FieldFormGroup}
+              placeholder={intl.formatMessage({ id: 'portal.network.locationForm.cloudProviderLocationId.placeholder' })}
+              label={<FormattedMessage id="portal.network.locationForm.cloudProviderLocationId.label"/>}
+            />
+          </Col>
+        </Row>
+
+        <FormFooterButtons>
+          { edit &&
           <Button
             className="btn-danger pull-left"
             disabled={submitting}
@@ -252,23 +341,25 @@ const NetworkLocationForm = (props) => {
           >
             <FormattedMessage id="portal.button.delete"/>
           </Button>
-        }
-        <Button
-          className="btn-secondary"
-          onClick={onCancel}
-        >
-          <FormattedMessage id="portal.button.cancel"/>
-        </Button>
-        <Button
-          type="submit"
-          bsStyle="primary"
-          disabled={invalid || submitting}
-        >
-          {actionButtonTitle}
-        </Button>
-      </FormFooterButtons>
-    </form>
-  )};
+          }
+          <Button
+            className="btn-secondary"
+            onClick={onCancel}
+          >
+            <FormattedMessage id="portal.button.cancel"/>
+          </Button>
+          <Button
+            type="submit"
+            bsStyle="primary"
+            disabled={invalid || submitting}
+          >
+            {actionButtonTitle}
+          </Button>
+        </FormFooterButtons>
+      </form>
+    )
+  }
+}
 
 NetworkLocationForm.displayName = 'NetworkLocationEditForm';
 NetworkLocationForm.propTypes = {
@@ -283,7 +374,23 @@ NetworkLocationForm.propTypes = {
   ...reduxFormPropTypes
 };
 
-export default reduxForm({
+const form = reduxForm({
   form: 'networkLocationForm',
   validate
-})(injectIntl(NetworkLocationForm))
+})(NetworkLocationForm)
+
+const mapStateToProps = (state) => {
+  const selector = formValueSelector('networkLocationForm')
+  return {
+    latLng: selector(state, 'latitude', 'longitude')
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    touchField: (field) => dispatch(touch('networkLocationForm', field)),
+    changeField: (field, val) => dispatch(change('networkLocationForm', field, val))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(injectIntl(form))
