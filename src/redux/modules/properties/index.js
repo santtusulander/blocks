6de151dc8bbebfe57bd22afe0ce3704f191640ab
santtu-idean <santtu.sulander@idean.com>
2017-02-09 15:01:09 +0200

    remove unused redux logic - this module will soon be deprecated and removed anyway

--- src/redux/modules/properties/index.js
+++ src/redux/modules/properties/index.js
@@ -1,10 +1,7 @@
 import {combineReducers} from 'redux'
 
 import properties from './actions'
-import * as actionTypes from './actionTypes'
-import mapActionsToFetchingReducers from '../fetching/actions'
 
 export default combineReducers({
-  properties,
-  fetching: mapActionsToFetchingReducers( actionTypes )
+  properties
 })
