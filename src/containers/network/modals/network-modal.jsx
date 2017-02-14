aa1c8fa4fa8d2933c362062fa76acdf676478782
anhnguyen-idean <niklas@omnipartners.fi>
2017-02-14 20:39:38 +0200

    remove duplicate networkID

--- src/containers/network/modals/network-modal.jsx
+++ src/containers/network/modals/network-modal.jsx
@@ -211,8 +211,8 @@ NetworkFormContainer.defaultProps = {
 
 
 const mapStateToProps = (state, ownProps) => {
-  const network = ownProps.networkId && getNetworkById(state, buildReduxId(ownProps.groupId, ownProps.networkId))
   const networkId = buildReduxId(ownProps.groupId, ownProps.networkId)
+  const network = ownProps.networkId && getNetworkById(state, networkId)
   const pops = ownProps.networkId && getPopsByNetwork(state, networkId)
   const edit = !!ownProps.networkId
 
