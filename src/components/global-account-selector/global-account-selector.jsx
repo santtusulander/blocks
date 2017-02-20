318650aac0a10c09edc19bc7567803413261d157
santtu-idean <santtu.sulander@idean.com>
2017-02-20 18:03:08 +0200

    remove redux flow from presentational component

--- src/components/global-account-selector/global-account-selector.jsx
+++ src/components/global-account-selector/global-account-selector.jsx
@@ -1,22 +1,12 @@
 import React, { PropTypes, Component } from 'react'
-import { connect } from 'react-redux'
 import { Dropdown } from 'react-bootstrap'
 
 import ToggleElement from './toggle-element'
 
-import propertyActions from '../../redux/modules/entities/properties/actions'
-import { getByGroup } from '../../redux/modules/entities/properties/selectors'
-
-import groupActions from '../../redux/modules/entities/groups/actions'
-import { getByAccount } from '../../redux/modules/entities/groups/selectors'
-
-import accountActions from '../../redux/modules/entities/accounts/actions'
-import { getByBrand } from '../../redux/modules/entities/accounts/selectors'
-
 import SelectorItems from './selector-items'
 import SelectorHeader from './selector-header'
 
-class Selector extends Component {
+export default class Selector extends Component {
 
   static emptyArray = []
 
@@ -24,6 +14,10 @@ class Selector extends Component {
     open: false,
     activeNode: this.props.activeNode,
     search: ''
+  }
+
+  componentWillMount() {
+    this.props.fetchData()
   }
 
   componentWillReceiveProps({ activeNode }) {
@@ -34,179 +28,73 @@ class Selector extends Component {
     this.setState({ search: e.target.value })
   }
 
+  handleCaretClick = (fetchChildren, nodeId) => {
+    this.props.dispatch(fetchChildren())
+      .then(() => this.changeTier(nodeId))
+  }
+
   changeTier = activeNode => {
     this.setState({ activeNode, search: '' })
   }
 
-  toggleMenu = () => this.setState({ open: !this.state.open })
+  toggleMenu = () => this.setState({ open: !this.state.open, search: '' })
 
   renderDropdown = (tree = Selector.emptyArray, parentId) => {
-    console.log('menee tänne');
-    let found = false
-    let selectorNodes = []
-    let recurse = undefined
-    let activeNodeName = undefined
 
+    let found, selectorNodes, menu, activeNodeName, viewParentPermission = undefined
     for (const node of tree) {
-
-      const { idKey = 'id', labelKey = 'name', nodes } = node
+      const { labelKey = 'name', idKey = 'id', nodes, nodeInfo } = node
       const nodeId = node[idKey]
 
       if (nodeId == this.state.activeNode) {
 
         found = true
+        viewParentPermission = nodeInfo.viewParentPermission
         activeNodeName = node[labelKey]
         selectorNodes = nodes
         break
 
       } else {
-        recurse = this.renderDropdown(nodes, nodeId)
+
+        menu = this.renderDropdown(nodes, nodeId)
+
+        if (menu) {
+          return menu
+        }
       }
     }
+    if (found) {
+      return (
+        <Dropdown.Menu>
+          <SelectorHeader
+            parentId={parentId}
+            viewParentPermission={viewParentPermission}
+            goToParent={this.changeTier}
+            searchValue={this.state.search}
+            onSearchChange={this.onSearchChange}
+            activeNodeName={activeNodeName} />
 
-    if (!found) {
-      return recurse
+          <SelectorItems
+            handleEntityClick={this.props.handleEntityClick}
+            handleCaretClick={this.handleCaretClick}
+            goToChild={this.changeTier}
+            selectorNodes={selectorNodes}
+            searchValue={this.state.search} />
+        </Dropdown.Menu>
+      )
     }
-
-    return (
-      <Dropdown.Menu>
-        <SelectorHeader
-          parentId={parentId}
-          goToParent={this.changeTier}
-          searchValue={this.state.search}
-          onSearchChange={this.onSearchChange}
-          activeNodeName={activeNodeName} />
-
-        <SelectorItems
-          goToChild={this.changeTier}
-          selectorNodes={selectorNodes}
-          searchValue={this.state.search} />
-      </Dropdown.Menu>
-    )
   }
 
   render() {
     const { props: { tree, children }, state: { open } } = this
-    const menu = this.renderDropdown(tree)
 
     return (
       <Dropdown id="" open={open} onToggle={() => {/*noop*/}} className="selector-component">
         <ToggleElement bsRole="toggle" toggle={this.toggleMenu}>
           <span>{children}</span>
         </ToggleElement>
-        {menu}
+        {this.renderDropdown(tree) || <Dropdown.Menu/>}
       </Dropdown>
     )
   }
 }
-
-const mapStateToProps = () => ({
-
-  activeNode: 6,
-  tree: [{
-    id: 'udn',
-    name: 'UDN Admin',
-    fetchChildren: () => Promise.resolve(console.log('fetch accs fron brand udn')),
-    nodes: [
-      {
-        id: 1,
-        name: 'acc1',
-        fetchChildren: () => Promise.resolve(console.log('fetch grps for acc 1')),
-        nodes: []
-      },
-      {
-        id: 3,
-        name: 'acc3',
-        fetchChildren: () => Promise.resolve(console.log('fetch grps for acc 3')),
-        nodes: []
-      },
-      {
-        id: 2,
-        name: 'acc2',
-        fetchChildren: () => Promise.resolve(console.log('fetch grps for acc id 2')),
-        nodes: [
-          {
-            id: 6,
-            name: 'grp1',
-            fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 1')),
-            nodes: [
-              {
-                id: 7,
-                name: 'prop1',
-                fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 1'))
-              },
-              {
-                id: 8,
-                name: 'prop2',
-                fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 2'))
-              },
-              {
-                id: 9,
-                name: 'prop3',
-                fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 3'))
-              }
-            ]
-          },
-          {
-            id: 5,
-            name: 'grp2',
-            fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 2')),
-            nodes: [
-              {
-                id: 3454,
-                name: 'prop1',
-                fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 1'))
-              },
-              {
-                id: 843545,
-                name: 'prop2',
-                fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 2'))
-              },
-              {
-                id: 9345543,
-                name: 'prop3',
-                fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 3'))
-              }
-            ]
-          },
-          {
-            id: 4,
-            name: 'grp3',
-            fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 3')),
-            nodes: [
-              {
-                id: 10,
-                name: 'prop1',
-                fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 1'))
-              },
-              {
-                id: 16,
-                name: 'prop2',
-                fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 2'))
-              },
-              {
-                id: 123,
-                name: 'prop3',
-                fetchChildren: () => Promise.resolve(console.log('fetch grps for grp 3'))
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }]
-
-  // accounts: brand => getByBrand(state, brand),
-  // groups: account => getByAccount(state, account),
-  // properties: group => getByGroup(state, group)
-})
-
-const mapDispatchToProps = (dispatch, { params: { brand } }) => {
-  return {
-    fetchAccounts: () => dispatch(accountActions.fetchAll({ brand })),
-    fetchGroups: (accountId) => dispatch(groupActions.fetchAll({ brand, account: accountId })),
-    fetchProperties: (accountId) => (groupId) => dispatch(propertyActions.fetchAll({ brand, account: accountId, group: groupId }))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Selector)
