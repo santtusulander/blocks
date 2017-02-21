841c135086dcd27cbad5170b92bfc9494bcc7400
Pouya Adib <pouya.adib@nordsoftware.com>
2017-02-21 14:37:27 +0330

    render component

---
+++ src/components/__tests__/file-upload-status-container.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+jest.unmock('../file-upload-status-container')
+import FileUploadStatus from '../file-upload-status-container'
+
+const subject = shallow(
+  <FileUploadStatus />
+)
+
+describe('FileUploadStatus', () => {
+  it('should exist', () => {
+    expect(subject.length).toBe(1)
+  })
+})
---
+++ src/components/file-upload-status-container.jsx
@@ -0,0 +1,7 @@
+const FileUploadStatus = () => {
+  return null
+}
+
+FileUploadStatus.displayName = "FileUploadStatus"
+
+export default FileUploadStatus
