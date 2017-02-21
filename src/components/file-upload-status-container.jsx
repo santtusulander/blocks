7760515ab020502f4056f7378eacb0b32873f81a
Pouya Adib <pouya.adib@nordsoftware.com>
2017-02-21 15:31:33 +0330

    render header

--- src/components/file-upload-status-container.jsx
+++ src/components/file-upload-status-container.jsx
@@ -1,5 +1,17 @@
+import React from 'react'
+import IconHeaderCaret from './icons/icon-header-caret'
+
 const FileUploadStatus = () => {
-  return null
+  return (
+    <div className='file-upload-status-wrapper'>
+      <div className='file-upload-status-header'>
+        <span>Uploading...</span>
+        <span className='file-upload-status-header-caret'>
+          <IconHeaderCaret />
+        </span>
+      </div>
+    </div>
+  )
 }
 
 FileUploadStatus.displayName = "FileUploadStatus"
