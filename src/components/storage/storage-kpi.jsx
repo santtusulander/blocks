a9811afea03232f9ce987df398fe43f8d0210e4f
Pouya Adib <pouya.adib@nordsoftware.com>
2017-02-22 15:44:48 +0330

    show kpi items

--- src/components/storage/storage-kpi.jsx
+++ src/components/storage/storage-kpi.jsx
@@ -1,5 +1,23 @@
+import React from 'react'
+
 const StorageKPI = () => {
-  return null
+  return (
+    <div>
+      <div className='storage-kpi-item'>
+        <h5>Current</h5>
+      </div>
+      <div className='storage-kpi-item'>
+        <h5>Peak</h5>
+      </div>
+      <div className='storage-kpi-item'>
+        <h5>Gain</h5>
+      </div>
+      <div className='storage-kpi-item'>
+        <h5>Location</h5>
+      </div>
+    </div>
+  )
 }
 
+StorageKPI.displayName = "StorageKPI"
 export default StorageKPI
