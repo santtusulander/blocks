7f616226f60a7b0492eed0b634e95d735843d114
Pouya Adib <pouya.adib@nordsoftware.com>
2017-02-23 12:52:52 +0330

    read titles from translations

--- src/components/storage/storage-kpi.jsx
+++ src/components/storage/storage-kpi.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+import { injectIntl, intlShape} from 'react-intl'
 import MiniChart from '../mini-chart'
 import ComparisonBars from './comparison-bars'
 
@@ -7,6 +8,7 @@ const StorageKPI = ({
   chartDataKey,
   currentValue,
   gainPercentage,
+  intl,
   locations,
   peakValue,
   referenceValue,
@@ -15,32 +17,46 @@ const StorageKPI = ({
   return (
     <div>
       <div className='storage-kpi-item'>
-        <span className='storage-kpi-item-title'>Current</span>
+        <span className='storage-kpi-item-title'>
+          {intl.formatMessage({id: 'portal.storage.kpi.current.title'})}
+        </span>
         <div className='storage-kpi-item-content'>
           <div className='storage-kpi-number'>
             <span className='value'>{currentValue}</span>
-            <span className='suffix'>/ {referenceValue} {valuesUnit.toUpperCase()}</span>
+            <span className='suffix'>
+              {`/ ${referenceValue} ${valuesUnit.toUpperCase()}`}
+            </span>
           </div>
           <div className='storage-kpi-comparison-bars'>
-            <ComparisonBars referenceValue={referenceValue} currentValue={currentValue} />
+            <ComparisonBars
+              referenceValue={referenceValue}
+              currentValue={currentValue} />
           </div>
         </div>
       </div>
       <div className='storage-kpi-item'>
-        <span className='storage-kpi-item-title'>Peak</span>
+        <span className='storage-kpi-item-title'>
+          {intl.formatMessage({id: 'portal.storage.kpi.peak.title'})}
+        </span>
         <div className='storage-kpi-item-content'>
           <div className='storage-kpi-number'>
             <span className='value'>{peakValue}</span>
             <span className='suffix'>{valuesUnit.toUpperCase()}</span>
           </div>
         </div>
-        <span className='storage-kpi-item-note'>(this month)</span>
+        <span className='storage-kpi-item-note'>
+          {intl.formatMessage({id: 'portal.storage.kpi.note.thisMonth'})}
+        </span>
       </div>
       <div className='storage-kpi-item'>
-        <span className='storage-kpi-item-title'>Gain</span>
+        <span className='storage-kpi-item-title'>
+          {intl.formatMessage({id: 'portal.storage.kpi.gain.title'})}
+        </span>
         <div className='storage-kpi-item-content'>
           <div className='storage-kpi-number'>
-            <span className='value'>{`${gainPercentage >= 0 ? '+' : '-'} ${Math.abs(gainPercentage)}%`}</span>
+            <span className='value'>
+              {`${(gainPercentage > 0) ? '+' : ''}${gainPercentage.toFixed(1)}%`}
+            </span>
           </div>
           <div className='storage-kpi-chart'>
             <MiniChart
@@ -49,10 +65,14 @@ const StorageKPI = ({
               />
           </div>
         </div>
-        <span className='storage-kpi-item-note'>(this month)</span>
+        <span className='storage-kpi-item-note'>
+          {intl.formatMessage({id: 'portal.storage.kpi.note.thisMonth'})}
+        </span>
       </div>
       <div className='storage-kpi-item'>
-        <span className='storage-kpi-item-title'>Location</span>
+        <span className='storage-kpi-item-title'>
+          {intl.formatMessage({id: 'portal.storage.kpi.location.title'})}
+        </span>
         <div className='storage-kpi-item-content'>
           <div className='storage-kpi-text'>{locations.join(', ')}</div>
         </div>
@@ -67,9 +87,10 @@ StorageKPI.propTypes = {
   chartDataKey: PropTypes.string,
   currentValue: PropTypes.number,
   gainPercentage: PropTypes.number,
+  intl: intlShape,
   locations: PropTypes.array,
   peakValue: PropTypes.number,
   referenceValue: PropTypes.number,
   valuesUnit: PropTypes.string
 }
-export default StorageKPI
+export default injectIntl(StorageKPI)
