d254c03327c764bf70871e6f16155d2f56b22d12
Nizar Rahme <nizar.rahme@nordsoftware.com>
2017-02-24 15:05:32 +0200

    Move the color rules of charts to CSS

--- src/components/content/storage-item-chart.jsx
+++ src/components/content/storage-item-chart.jsx
@@ -2,13 +2,12 @@ import React, { PropTypes } from 'react'
 import { PieChart, Pie } from 'recharts'
 import { ButtonToolbar, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { Link } from 'react-router'
-import { FormattedMessage } from 'react-intl'
+import classNames from 'classnames'
 
+import StorageItemTooltip from './storage-item-tooltip'
 import IconConfiguration from '../icons/icon-configuration'
 import IconChart from '../icons/icon-chart'
 import { formatBytes, separateUnit } from '../../util/helpers'
-
-import { black, paleblue, yellow } from '../../constants/colors'
 
 const FORMAT = '0,0.0'
 
@@ -23,34 +22,26 @@ const StorageItemChart = (
     lastMonthPeak }) => {
 
   const dataSets = [
-    [{value: currentUsage, fill: paleblue },
-     {value: estimate - currentUsage, fill: black }],
-    [{value: peak - (estimate / 360), fill:"transparent"},
-     {value: estimate / 360, fill: peak < estimate ? paleblue : yellow },
-     {value: estimate - peak, fill:"transparent" }],
-    [{value: lastMonthUsage, fill: paleblue, className: 'last-month' },
-     {value: lastMonthEstimate - lastMonthUsage, fill: "transparent" }],
-    [{value: lastMonthPeak - (estimate / 360), fill:"transparent"},
-     {value: lastMonthEstimate / 360, fill: lastMonthPeak < lastMonthEstimate ? paleblue : yellow, className: 'last-month'},
-     {value: lastMonthEstimate - lastMonthPeak, fill:"transparent" }]]
+    [{value: currentUsage, className: 'current-month current-month-usage' },
+     {value: estimate - currentUsage, className: 'current-month current-month-background' }],
+    [{value: peak - (estimate / 360), className: 'current-month'},
+     {value: estimate / 360, className: classNames('current-month current-month-peak', {exceeded: peak >= estimate}) },
+     {value: estimate - peak, className: 'current-month' }],
+    [{value: lastMonthUsage, className: 'last-month last-month-usage' },
+     {value: lastMonthEstimate - lastMonthUsage, className: 'last-month' }],
+    [{value: lastMonthPeak - (estimate / 360), className: 'last-month'},
+     {value: lastMonthEstimate / 360, className: classNames('last-month last-month-peak', {exceeded: lastMonthPeak >= lastMonthEstimate})},
+     {value: lastMonthEstimate - lastMonthPeak, className: 'last-month' }]]
 
-  const tooltip = (<Tooltip className="storage-item-tooltip"
-    id={'tooltip-' + 'name'}>
-      <h3>{name}</h3>
-      <div className="storage-month-info">
-        <FormattedMessage id="portal.account.storage.tooltip.currentUsage"/>
-        <span className="tooltip-storage-value">{formatBytes(currentUsage, null, FORMAT)}</span>
-        <br />
-        <FormattedMessage id="portal.account.storage.tooltip.peakThisMonth"/>
-        <span className="tooltip-storage-value">{formatBytes(peak, null, FORMAT)}</span>
-      </div>
-      <div className="storage-month-info">
-        <FormattedMessage id="portal.account.storage.tooltip.endOfLastMonth"/>
-        <span className="tooltip-storage-value">{formatBytes(currentUsage, null, FORMAT)}</span>
-        <br />
-        <FormattedMessage id="portal.account.storage.tooltip.peakLastMonth"/>
-        <span className="tooltip-storage-value">{formatBytes(lastMonthPeak, null, FORMAT)}</span>
-      </div>
+  const tooltip = (<Tooltip id='tooltip-storage' className="storage-item-tooltip">
+      <StorageItemTooltip
+        name={name}
+        currentUsage={currentUsage}
+        peak={peak}
+        lastMonthUsage={lastMonthUsage}
+        lastMonthPeak={lastMonthPeak}
+        lastMonthEstimate={lastMonthEstimate}
+        valuesFormat={FORMAT} />
     </Tooltip>)
 
   const pies = dataSets.map((dataSet, i) =>
@@ -77,10 +68,10 @@ const StorageItemChart = (
           <div className="title">{name}</div>
           <div className="usage">
             <span className="usage-value">
-              {separateUnit(formatBytes(currentUsage, null, FORMAT)).value}
+              {currentUsage && separateUnit(formatBytes(currentUsage, null, FORMAT)).value}
             </span>
             <span className="usage-unit">
-              {separateUnit(formatBytes(currentUsage, null, FORMAT)).unit}
+              {currentUsage && separateUnit(formatBytes(currentUsage, null, FORMAT)).unit}
             </span>
           </div>
           <div className="usage-estimate">of 250</div>
