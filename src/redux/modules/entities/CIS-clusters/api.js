c351f189b84340f98f0c7c6e701ce25afc845569
Mikko Alander <mikko.alander@idean.com>
2017-02-27 16:17:44 +0200

    use mock data for clusters

--- src/redux/modules/entities/CIS-clusters/api.js
+++ src/redux/modules/entities/CIS-clusters/api.js
@@ -1,22 +1,22 @@
+/*TODO: UDNP-2837 remove lint disable */
+/*eslint-disable no-unused-vars */
 import axios from 'axios'
 import {normalize, schema} from 'normalizr'
 
 import { BASE_URL_CIS_SOUTH } from '../../../util'
 
 
-/*
-{
-    "geolocation": "us",
-    "storage_type": "ssd",
-    "name": "strg0"
+const mockData = {
+  "geolocation": "us",
+  "storage_type": "ssd",
+  "name": "strg0"
 }
- */
 
 const baseUrl = () => {
   return `${BASE_URL_CIS_SOUTH}/clusters`
 }
 
-const clusterSchema = new schema.Entity('cluster', {},{
+const clusterSchema = new schema.Entity('clusters', {},{
   idAttribute: 'name'
 })
 
@@ -29,13 +29,11 @@ const clusterSchema = new schema.Entity('cluster', {},{
  * @return {[type]}         [description]
  */
 export const fetchAll = ({}) => {
-  return axios.get(baseUrl(), {
-    headers: {
-      'Content-Type': 'application/json',
-      'testHeader': 'testValue'
-    }
-  })
-  .then( ({data}) => {
-    return normalize(data, [ clusterSchema ])
-  })
+  return Promise.resolve( normalize([mockData], [ clusterSchema ]) )
+
+  // TODO: UDNP-2873 Uncomment when API is fixed
+  // return axios.get(baseUrl())
+  // .then( ({data}) => {
+  //   return normalize(data, [ clusterSchema ])
+  // })
 }
