7ca4f8029c04bef4802fbda4c77379e662636e43
Nizar Rahme <nizar.rahme@nordsoftware.com>
2017-02-27 14:07:17 +0200

    Use the correct id forms for formatted messages

--- src/components/content/storage-item-tooltip.jsx
+++ src/components/content/storage-item-tooltip.jsx
@@ -7,17 +7,17 @@ const StorageItemTooltip = ({ name, valuesFormat, currentUsage, peak, lastMonthU
   <div className="storage-item-inner-tooltip">
     <h3>{name}</h3>
     <div className="storage-month-info">
-      <FormattedMessage id="portal.account.storages.tooltip.currentUsage"/>
+      <FormattedMessage id="portal.account.storage.tooltip.currentUsage"/>
       <span className="tooltip-storage-value">{formatBytes(currentUsage, null, valuesFormat)}</span>
       <br />
-      <FormattedMessage id="portal.account.storages.tooltip.peakThisMonth"/>
+      <FormattedMessage id="ortal.account.storage.tooltip.currentUsage"/>
       <span className="tooltip-storage-value">{formatBytes(peak, null, valuesFormat)}</span>
     </div>
     <div className="storage-month-info">
-      <FormattedMessage id="portal.account.storages.tooltip.endOfLastMonth"/>
+      <FormattedMessage id="ortal.account.storage.tooltip.currentUsage"/>
       <span className="tooltip-storage-value">{formatBytes(lastMonthUsage, null, valuesFormat)}</span>
       <br />
-      <FormattedMessage id="portal.account.storages.tooltip.peakLastMonth"/>
+      <FormattedMessage id="ortal.account.storage.tooltip.currentUsage"/>
       <span className="tooltip-storage-value">{formatBytes(lastMonthPeak, null, valuesFormat)}</span>
     </div>
   </div>
