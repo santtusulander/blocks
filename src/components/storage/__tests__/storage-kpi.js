ea333fcada91e3d97bec2aa5860b4c782dcb32c7
Pouya Adib <pouya.adib@nordsoftware.com>
2017-02-27 14:10:17 +0330

    update test code

--- src/components/storage/__tests__/storage-kpi.js
+++ src/components/storage/__tests__/storage-kpi.js
@@ -4,8 +4,26 @@ import { shallow } from 'enzyme'
 jest.unmock('../storage-kpi')
 import StorageKPI from '../storage-kpi'
 
+const intlMaker = () => {
+  return {
+    formatMessage: jest.fn()
+  }
+}
+
 const subject = shallow(
-  <StorageKPI />
+  <StorageKPI
+    intl={intlMaker()}
+    chartData={[
+      {bytes: 45000, timestamp: new Date('Thu May 26 2016 11:17:01 GMT-0700 (PDT)')}
+    ]}
+    chartDataKey='bytes'
+    currentValue={112}
+    gainPercentage={0.2}
+    locations={['San Jose', 'Frankfurt']}
+    peakValue={120}
+    referenceValue={100}
+    valuesUnit='tb'
+  />
 )
 
 describe('StorageKPI', () => {
