afae215166ff046801fc5fc2e69750d1dd3487b9
Olga Metlyakova <ometlyakova@vidscale.com>
2017-02-28 15:16:07 +0200

    UDNP-2882 Fix listing data model for SSL certs

--- src/redux/modules/security.js
+++ src/redux/modules/security.js
@@ -171,15 +171,12 @@ export const fetchSSLCertificates = createAction(SECURITY_SSL_CERTIFICATES_FETCH
   }
 
   return axios.get(`${BASE_URL_NORTH}/brands/${brand}/accounts/${account}/groups/${group}/certs`)
-    .then(action => Promise.all(action.data.map(
-      cn => axios.get(`${BASE_URL_NORTH}/brands/${brand}/accounts/${account}/groups/${group}/certs/${cn}`)
-    )))
-    .then(resp => resp.map(certificate => {
+    .then(resp => resp.data.data.map(certificate => {
       return {
         group,
-        cn: certificate.data.cn,
-        title: certificate.data.title,
-        date_not_valid_after: certificate.data.date_not_valid_after,
+        cn: certificate.cn,
+        title: certificate.title,
+        date_not_valid_after: certificate.date_not_valid_after,
         account
       }
     }))
