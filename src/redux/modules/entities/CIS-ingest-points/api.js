cf64644985825e23635b70ce1f8ad030bf30bb0e
Mikko Alander <mikko.alander@idean.com>
2017-03-01 16:45:03 +0200

    fix const

--- src/redux/modules/entities/CIS-ingest-points/api.js
+++ src/redux/modules/entities/CIS-ingest-points/api.js
@@ -29,7 +29,7 @@ const mockArray = [
 
 
 const baseUrl = ({ group, id }) => {
-  return `${BASE_URL_CIS_SOUTH}/ingest_points/${id}?group_id=${group}`
+  return `${BASE_URL_CIS_NORTH}/ingest_points/${id}?group_id=${group}`
 }
 
 /* We only need profile_id -> set entity name to workflowsDummy so workflow doesn't go into redux */
