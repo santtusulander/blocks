b5d59a4875f5a2cd4d818f5d970df448f55353c3
Pouya Adib <pouya.adib@nordsoftware.com>
2017-03-03 19:42:28 +0330

    add dropzone header

--- src/components/storage/storage-dropzone.jsx
+++ src/components/storage/storage-dropzone.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
+import { Button, Col, FormGroup } from 'react-bootstrap'
 
-import SectionContainer from '../../components/layout/section-container'
-import SectionHeader from '../../components/layout/section-header'
-import AsperaUpload from '../../components/storage/aspera-upload'
+import SectionContainer from '../layout/section-container'
+import SectionHeader from '../layout/section-header'
+import AsperaUpload from '../storage/aspera-upload'
+import Toggle from '../toggle'
+import IconAdd from '../icons/icon-add'
 
 const StorageDropzone = () => {
   return (
     <SectionContainer>
       <SectionHeader
-        sectionHeaderTitle={<FormattedMessage id='portal.storage.summaryPage.dropzone.noFiles.title' />} />
+        sectionHeaderTitle={<FormattedMessage id='portal.storage.summaryPage.dropzone.noFiles.title' />}>
+        <FormGroup className="search-input-group">
+          <Col className="pull-left">
+            <FormattedMessage id='portal.storage.summaryPage.dropzone.asperaToggle.title' />
+          </Col>
+          <Col xs={6} className="pull-right">
+            <Toggle
+              value={true}
+              onText='ON'
+              offText='OFF'
+              valueChange={() => {}}
+            />
+          </Col>
+        </FormGroup>
+        <Button
+          className="btn-icon btn-success pull-right"
+          bsStyle="success"
+          icon={true}
+          onClick={() => {}}>
+          <IconAdd />
+        </Button>
+      </SectionHeader>
       <AsperaUpload />
     </SectionContainer>
   )
