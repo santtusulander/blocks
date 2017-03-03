e42b3a57f83424cceaa756b939426f7c485347cd
Pouya Adib <pouya.adib@nordsoftware.com>
2017-03-03 17:28:00 +0330

    show aspera upload in storage dropzone component

--- src/components/storage/storage-dropzone.jsx
+++ src/components/storage/storage-dropzone.jsx
@@ -1,5 +1,20 @@
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+
+import SectionContainer from '../../components/layout/section-container'
+import SectionHeader from '../../components/layout/section-header'
+import AsperaUpload from '../../components/storage/aspera-upload'
+
 const StorageDropzone = () => {
-  return null
+  return (
+    <SectionContainer>
+      <SectionHeader
+        sectionHeaderTitle={<FormattedMessage id='portal.storage.summaryPage.dropzone.noFiles.title' />} />
+      <AsperaUpload />
+    </SectionContainer>
+  )
 }
+
+StorageDropzone.displayName = 'StorageDropzone'
 
 export default StorageDropzone
