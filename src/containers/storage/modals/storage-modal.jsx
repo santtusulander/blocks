f16aa540acf42e633457387d72aaa0cf114bfd33
Mykhailo Prochkaruk <mprochkaruk@vidscale.com>
2017-03-03 11:17:18 +0200

    UDNP-2832 - initial redux integration

--- src/containers/storage/modals/storage-modal.jsx
+++ src/containers/storage/modals/storage-modal.jsx
@@ -4,34 +4,20 @@ import { FormattedMessage } from 'react-intl'
 import { SubmissionError, formValueSelector } from 'redux-form'
 import { Map } from 'immutable'
 
-import networkActions from '../../../redux/modules/entities/networks/actions'
+import { buildReduxId } from '../../../redux/util'
+import accountActions from '../../../redux/modules/entities/accounts/actions'
+import storageActions from '../../../redux/modules/entities/CIS-ingest-points/actions'
+import clusterActions from '../../../redux/modules/entities/CIS-clusters/actions'
+import groupActions from '../../../redux/modules/entities/groups/actions'
+
 import { getById as getAccountById } from '../../../redux/modules/entities/accounts/selectors'
 import { getById as getGroupById } from '../../../redux/modules/entities/groups/selectors'
+import { getById as getStorageById } from '../../../redux/modules/entities/CIS-ingest-points/selectors'
+import { getLocationOptions, getSelectedLocationOptions } from '../../../redux/modules/entities/CIS-clusters/selectors'
 
 import SidePanel from '../../../components/side-panel'
 import ModalWindow from '../../../components/modal'
 import StorageForm from '../../../components/storage/forms/storage-form'
-
-/*
-  TODO: remove this in scope of
-  UDNP-2832 - Integrate CIS Storage configuration form with the redux
-*/
-const storage_mock = {
-  get(cmd) {
-    switch (cmd) {
-      case 'name':
-        return 'Name'
-      case 'locations':
-        return '1'
-      case 'estimate':
-        return '10000000000000000'
-      case 'abr':
-        return true
-      case 'abrProfile':
-        return 'abr_tv_16_9_high'
-    }
-  }
-}
 
 class StorageFormContainer extends React.Component {
   constructor(props) {
@@ -44,6 +30,17 @@ class StorageFormContainer extends React.Component {
     this.onSave = this.onSave.bind(this)
     this.onDelete = this.onDelete.bind(this)
     this.onToggleDeleteModal = this.onToggleDeleteModal.bind(this)
+  }
+
+  componentWillMount() {
+    const { accountId, brand, groupId, storageId } = this.props
+
+    accountId && this.props.fetchAccount({brand, id: accountId})
+    groupId && this.props.fetchGroup({brand, account: accountId, id: groupId})
+    if (storageId) {
+      this.props.fetchStorage({ group: groupId, id: storageId })
+      this.props.fetchClusters({})
+    }
   }
 
   onToggleDeleteModal(showDeleteModal) {
@@ -69,14 +66,14 @@ class StorageFormContainer extends React.Component {
     const save = edit ? this.props.onUpdate : this.props.onCreate
 
     return save(params)
-      .then(() => {
+      .then( (resp) => {
+        if (resp.error) {
+          // Throw error => will be shown inside form
+          throw new SubmissionError({'_error': resp.error.data.message})
+        }
 
         // Close modal
         this.props.onCancel();
-      }).catch(resp => {
-
-        throw new SubmissionError({'_error': resp.data.message})
-
       })
   }
 
@@ -93,27 +90,27 @@ class StorageFormContainer extends React.Component {
     }
 
     return this.props.onDelete(params)
-      .then(() => {
+      .then((resp) => {
+        if (resp.error) {
+          // Throw error => will be shown inside form
+          throw new SubmissionError({'_error': resp.error.data.message})
+        }
 
         // Close modal
         this.props.onCancel()
-      }).catch(resp => {
-
-        throw new SubmissionError({'_error': resp.data.message})
-
       })
   }
 
   render() {
-    const { account, group, storage, initialValues, onCancel, abrToggle, show } = this.props
+    const { account, group, storage, initialValues, onCancel, abrToggle, show, locationOptions } = this.props
     const { showDeleteModal } = this.state
-    // simple way to check if editing -> no need to pass 'edit' - prop
+
     const edit = !!initialValues.name
 
     const title = edit ? <FormattedMessage id="portal.storage.storageForm.edit.title"/>
                        : <FormattedMessage id="portal.storage.storageForm.add.title"/>
 
-    const subTitle = edit ? `${account.get('name')} / ${group.get('name')} / ${storage.get('name')}`
+    const subTitle = edit ? `${account.get('name')} / ${group.get('name')} / ${storage.get('id')}`
                           : `${account.get('name')} / ${group.get('name')}`
 
     return (
@@ -125,6 +122,7 @@ class StorageFormContainer extends React.Component {
             onDelete={() => this.onToggleDeleteModal(true)}
             onCancel={onCancel}
             abrToggle={abrToggle}
+            locationOptions={locationOptions}
           />
         </SidePanel>
 
@@ -150,9 +148,15 @@ StorageFormContainer.propTypes = {
   abrToggle: PropTypes.bool,
   account: PropTypes.instanceOf(Map),
   accountId: PropTypes.string,
+  brand: PropTypes.string,
+  fetchAccount: PropTypes.func,
+  fetchClusters: PropTypes.func,
+  fetchGroup: PropTypes.func,
+  fetchStorage: PropTypes.func,
   group: PropTypes.instanceOf(Map),
   groupId: PropTypes.string,
   initialValues: PropTypes.object,
+  locationOptions: PropTypes.array,
   onCancel: PropTypes.func,
   onCreate: PropTypes.func,
   onDelete: PropTypes.func,
@@ -174,35 +178,40 @@ const formSelector = formValueSelector('storageForm')
 const mapStateToProps = (state, ownProps) => {
   const edit = !!ownProps.storageId
   const isABRSelected = formSelector(state, 'abr')
-  /*
-    TODO: change initialValues to fetch proper data
-    UDNP-2832 - Integrate CIS Storage configuration form with the redux
-  */
-  const storage = storage_mock
+
+  const storageId = ownProps.storageId && buildReduxId(ownProps.groupId, ownProps.storageId)
+  const storage = ownProps.storageId && getStorageById(state, storageId)
+
+  const storageWorkflow = storage && storage.get('workflow')
+  const clusters = storage && storage.get('clusters')
 
   return {
     abrToggle: isABRSelected,
     account: ownProps.accountId && getAccountById(state, ownProps.accountId),
     group: ownProps.groupId && getGroupById(state, ownProps.groupId),
+    storage: storage,
+    locationOptions: getLocationOptions(state),
+
     initialValues: {
-      name: edit && storage ? storage.get('name') : '',
-      locations: edit && storage ? storage.get('locations') : '',
-      estimate: edit && storage ? storage.get('estimate') : '',
-      abr: edit && storage ? storage.get('abr') : '',
-      abrProfile: edit && storage ? storage.get('abrProfile') : ''
+      name: edit ? ownProps.storageId : '',
+      locations: edit && clusters ? getSelectedLocationOptions(state, clusters) : [],
+      estimate: edit && storage ? storage.get('estimated_usage') : '',
+      abr: edit && storageWorkflow ? true : false,
+      abrProfile: edit && storageWorkflow ? storageWorkflow : ''
     }
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  /*
-    TODO: change networkActions to storageActions in scope of
-    UDNP-2832 - Integrate CIS Storage configuration form with the redux
-  */
   return {
-    onCreate: (params, data) => dispatch( networkActions.create( {...params, data } )),
-    onUpdate: (params, data) => dispatch( networkActions.update( {...params, data } )),
-    onDelete: (params) => dispatch( networkActions.remove( {...params } ))
+    onCreate: (params, data) => dispatch( storageActions.create( {...params, data } )),
+    onUpdate: (params, data) => dispatch( storageActions.update( {...params, data } )),
+    onDelete: (params) => dispatch( storageActions.remove( {...params } )),
+
+    fetchAccount: (params) => dispatch( accountActions.fetchOne(params) ),
+    fetchGroup: (params) => dispatch( groupActions.fetchOne(params) ),
+    fetchStorage: (params) => dispatch( storageActions.fetchOne(params) ),
+    fetchClusters: (params) => dispatch( clusterActions.fetchAll(params) )
   }
 }
 
