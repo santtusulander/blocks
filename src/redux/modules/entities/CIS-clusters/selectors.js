a7c13940506f0b5557bdb2d90ce670b93d06441f
Mykhailo Prochkaruk <mprochkaruk@vidscale.com>
2017-03-03 11:15:37 +0200

    UDNP-2832 - add location selectors

--- src/redux/modules/entities/CIS-clusters/selectors.js
+++ src/redux/modules/entities/CIS-clusters/selectors.js
@@ -19,3 +19,48 @@ export const getById = (state, id) => {
 export const getAll = (state) => {
   return state.entities.CISClusters.toList()
 }
+
+/***
+ * Get Locations options for MultiOptionSelector
+ *
+ * @param  {} state from redux
+ *
+ * @return []
+ */
+export const getLocationOptions = (state) => {
+  let locationOptions = []
+
+  state.entities.CISClusters.forEach((cluster) => {
+    locationOptions.push({
+      label: cluster.get('description'),
+      value: cluster.get('geolocation'),
+      options: []
+    })
+  })
+
+  return locationOptions
+}
+
+/***
+ * Get selected locations options for MultiOptionSelector
+ *
+ * @param {} state from redux
+ * @param [] List of clusters
+ *
+ * @return []
+ */
+export const getSelectedLocationOptions = (state, clusters) => {
+  let locationOptions = []
+
+  clusters.forEach((cluster) => {
+    const clusterInfo = getById(state, cluster)
+    if (clusterInfo) {
+      locationOptions.push({
+        id: clusterInfo.get('geolocation'),
+        options: []
+      })
+    }
+  })
+
+  return locationOptions
+}
