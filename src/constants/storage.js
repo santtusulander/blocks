d52d19e4795e31fdade9361ae7d681df12df595a
Mykhailo Prochkaruk <mprochkaruk@vidscale.com>
2017-03-03 11:48:36 +0200

    UDNP-2832 - remove mock constants

--- src/constants/storage.js
+++ src/constants/storage.js
@@ -1,9 +1,3 @@
-export const STORAGE_LOCATIONS = [
-  {label: "San Jose, California, USA", value: 1, options: []},
-  {label: "Frankfurkt, Germany", value: 2, options: []},
-  {label: "Hong Kong, China", value: 3, options: []}
-]
-
 export const STORAGE_ABR_DEFAULT = false
 export const STORAGE_ESTIMATE_DEFAULT = 100
 export const STORAGE_ESTIMATE_MIN = 1
@@ -15,16 +9,4 @@ export const STORAGE_ESTIMATE_UNITS = [
   ['mb', 'MB'],
   ['kb', 'KB'],
   ['b', 'B']
-]
-
-/*
-  TODO: change this to be fetched from the API.
-  UDNP-2832 - Integrate CIS Storage configuration form with the redux
-*/
-export const STORAGE_ABR_PROFILES = [
-    ["abr_tv_16_9_high", "ABR dynamic packager - Smart TV - 16x9, high bitrate"],
-    ["abr_pc_16_9_mid", "ABR dynamic packager - PC Player - 16x9, middle bitrate"],
-    ["abr_mobile_16_9_low", "ABR dynamic packager - Mobile Player - 16x9, low bitrate"],
-    ["abr_tablet_4_3_high", "ABR dynamic packager - Tablet Player - 4x3, high bitrate"],
-    ["abr_sd_4_3_low", "ABR dynamic packager - SD - 4x3, low bitrate"]
 ]
