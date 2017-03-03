8931e65bc2ca29306e23ee8c2c3b057ee1847cd9
Pouya Adib <pouya.adib@nordsoftware.com>
2017-03-03 16:39:15 +0330

    show kpi in summary page

--- src/containers/storage/storage.jsx
+++ src/containers/storage/storage.jsx
@@ -3,7 +3,9 @@ import { connect } from 'react-redux'
 import { Map } from 'immutable'
 
 import Content from '../../components/layout/content'
+import PageContainer from '../../components/layout/page-container'
 import StorageHeader from '../../components/storage/storage-header'
+import StorageKPI from '../../components/storage/storage-kpi'
 
 const Storage = ({ params, currentUser }) => {
   return (
@@ -12,6 +14,28 @@ const Storage = ({ params, currentUser }) => {
         currentUser={currentUser}
         params={params}
       />
+
+      <PageContainer>
+        <StorageKPI
+          chartData={[
+          {bytes: 45000, timestamp: new Date('Thu May 26 2016 11:17:01 GMT-0700 (PDT)')},
+          {bytes: 65000, timestamp: new Date('Thu May 26 2016 12:17:01 GMT-0700 (PDT)')},
+          {bytes: 45000, timestamp: new Date('Thu May 26 2016 13:17:01 GMT-0700 (PDT)')},
+          {bytes: 105000, timestamp: new Date('Thu May 26 2016 14:17:01 GMT-0700 (PDT)')},
+          {bytes: 115000, timestamp: new Date('Thu May 26 2016 15:17:01 GMT-0700 (PDT)')},
+          {bytes: 190000, timestamp: new Date('Thu May 26 2016 16:17:01 GMT-0700 (PDT)')},
+          {bytes: 125000, timestamp: new Date('Thu May 26 2016 17:17:01 GMT-0700 (PDT)')},
+          {bytes: 155000, timestamp: new Date('Thu May 26 2016 18:17:01 GMT-0700 (PDT)')}
+          ]}
+          chartDataKey='bytes'
+          currentValue={112}
+          gainPercentage={0.2}
+          locations={['San Jose', 'Frankfurt']}
+          peakValue={120}
+          referenceValue={100}
+          valuesUnit='tb'
+        />
+      </PageContainer>
     </Content>
   )
 }
