d80f3f8db6ea425e565477099df21dc70d575675
Olga Metlyakova <ometlyakova@vidscale.com>
2017-03-06 14:33:34 +0200

    Added to styleguid

--- src/containers/styleguide.jsx
+++ src/containers/styleguide.jsx
@@ -55,6 +55,7 @@ import AsperaUpload from '../components/storage/aspera-upload'
 import Typeahead from '../components/typeahead'
 import StorageKPI from '../components/storage/storage-kpi'
 import StorageItemChart from '../components/content/storage-item-chart'
+import SortableMultiSelector from '../components/sortable-multi-selector'
 
 import IconAccount       from '../components/icons/icon-account'
 import IconAdd           from '../components/icons/icon-add'
@@ -145,7 +146,8 @@ class Styleguide extends React.Component {
         'link9'
       ]),
       multiOptionValues: Immutable.List([ {id: 1, options: [1, 2]} ]),
-      numberInputValue: 100
+      numberInputValue: 100,
+      sortableMultiSelectorItems: Immutable.List([1, 2])
     }
   }
 
@@ -1103,6 +1105,28 @@ class Styleguide extends React.Component {
             peakValue={120}
             referenceValue={100}
             valuesUnit='tb'
+          />
+
+          <h1 className="page-header">Sortable Multi Selector</h1>
+
+          <SortableMultiSelector
+            label={'Items'}
+            options={[
+              {
+                label: 'Item 1',
+                value: 1
+              },
+              {
+                label: 'Item 2',
+                value: 2
+              },
+              {
+                label: 'Item 3',
+                value: 3
+              }
+            ]}
+            onChange={val => this.setState({ sortableMultiSelectorItems: Immutable.List(val) })}
+            value={this.state.sortableMultiSelectorItems}
           />
 
           <h1 className="page-header">Icons</h1>
