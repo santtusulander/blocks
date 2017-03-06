d3ab6fd3009fc4f52a2686e75a54c8e294272e08
santtu-idean <santtu.sulander@idean.com>
2017-03-06 12:05:46 +0200

    fix jsdoc, remove selector callbacks

--- src/components/global-account-selector/menu-selectors.js
+++ src/components/global-account-selector/menu-selectors.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
 
+import {
+  VIEW_CONTENT_GROUPS,
+  VIEW_CONTENT_ACCOUNTS,
+  VIEW_CONTENT_PROPERTIES,
+  VIEW_CONTENT_STORAGES } from '../../constants/permissions'
+
 import propertyActions from '../../redux/modules/entities/properties/actions'
 import { getByGroup as getPropertiesByGroup } from '../../redux/modules/entities/properties/selectors'
 
@@ -13,28 +19,25 @@ import { accountIsServiceProviderType } from '../../util/helpers'
 
 const getStoragesByGroup = () => ([
   {
-    "id": 'storage123',
+    "id": 'A Mock Storage',
     "estimated_usage": 999999,
     "usage": 2932224,
     "clusters": ["strg0"]
   }
 ])
 
-const getGroupsForAccount = (state, parents, callBack) => getByAccount(state, parents.account).toJS().map(callBack)
-const getAccountsForBrand = (state, parents, callBack) => getByBrand(state, parents.brand).toJS().map(callBack)
-
 /**
- * get groups from state, setting child nodes and defining a function to fetch child nodes for each one.
+ * get groups from state, set child nodes and defining a function to fetch child nodes for each one.
  * @param  {[type]} state     [description]
  * @param  {[type]} brandId   [description]
  * @param  {[type]} accountId [description]
  * @return {[type]}           [description]
  */
-export const getGroups = (state, parents, levels, callBack = getGroupsForAccount) => {
+export const getGroups = (state, parents, canView) => {
 
-  return callBack(state, parents, (group) => {
+  return getByAccount(state, parents.account).toJS().map(group => {
 
-    const { nodes, headerSubtitle } = getStoragesAndProperties(state, levels, { ...parents, group: group.id })
+    const { nodes, headerSubtitle } = getStoragesAndProperties(state, canView, { ...parents, group: group.id })
 
     return {
       ...group,
@@ -96,16 +99,17 @@ export const getStorages = (state, parents) => {
 }
 
 /**
- * get accounts from state, setting child nodes and defining a function to fetch child nodes for each one.
+ * get accounts from state, set child nodes and defining a function to fetch child nodes for each one.
  * @param  {[type]} state     [description]
  * @param  {[type]} parents   [description]
  * @param  {Object} [hide={}] [description]
  * @return {[type]}           [description]
  */
-export const getAccounts = (state, parents, levels, callBack = getAccountsForBrand) => {
-  return callBack(state, parents, account => {
+export const getAccounts = (state, parents, canView) => {
 
-    const nodes = levels.includes("account") && getGroups(state, { ...parents, account: account.id }, levels)
+  return getByBrand(state, parents.brand).toJS().map(account => {
+
+    const nodes = canView(VIEW_CONTENT_GROUPS) && getGroups(state, { ...parents, account: account.id }, canView)
 
     const headerSubtitle = <FormattedMessage id="portal.common.group.multiple" values={{numGroups: nodes.length || 0}}/>
 
@@ -122,9 +126,9 @@ export const getAccounts = (state, parents, levels, callBack = getAccountsForBra
   })
 }
 
-export const getBrands = (state, brand, levels) => {
+export const getBrands = (state, brand, canView) => {
 
-  const nodes = levels.includes('brand') && getAccounts(state, { brand }, levels)
+  const nodes = canView(VIEW_CONTENT_ACCOUNTS) && getAccounts(state, { brand }, canView)
   const headerSubtitle = <FormattedMessage id="portal.common.account.multiple" values={{numAccounts: nodes.length || 0}}/>
   return [{
     id: brand,
@@ -138,22 +142,23 @@ export const getBrands = (state, brand, levels) => {
 
 /**
  * If active account is of service provider type, it should not have a group level.
+ * Otherwise get storages and properties from store
  * @param  {[type]} state   [description]
- * @param  {[type]} levels  [description]
+ * @param  {[type]} canView [description]
  * @param  {[type]} parents [description]
  * @return {[type]}         [description]
  */
-const getStoragesAndProperties = (state, levels, parents) => {
+const getStoragesAndProperties = (state, canView, parents) => {
 
   let nodes, headerSubtitle = undefined
   let activeAccount = getAccountById(state, parents.account)
 
   if (!accountIsServiceProviderType(activeAccount)) {
 
-    const properties = getProperties(state, parents)
-    const storages = getStorages(state, parents)
+    const properties = canView(VIEW_CONTENT_PROPERTIES) && getProperties(state, parents)
+    const storages = canView(VIEW_CONTENT_STORAGES) && getStorages(state, parents)
 
-    nodes = levels.includes("group") && [
+    nodes = [
       ...properties,
       ...storages
     ]
@@ -165,7 +170,6 @@ const getStoragesAndProperties = (state, levels, parents) => {
     )
 
   }
-
 
   return { nodes, headerSubtitle }
 }
