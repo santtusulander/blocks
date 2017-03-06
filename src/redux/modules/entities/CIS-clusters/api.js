7b7cbf1289bfae44c8e35de9337134793a6fd3fb
Mikko Alander <mikko.alander@idean.com>
2017-03-06 13:04:26 +0200

    remove mock

--- src/redux/modules/entities/CIS-clusters/api.js
+++ src/redux/modules/entities/CIS-clusters/api.js
@@ -5,34 +5,13 @@ import {normalize, schema} from 'normalizr'
 
 import { BASE_URL_CIS_SOUTH } from '../../../util'
 
-
-const mock = {
-  "geolocation": "us",
-  "storage_type": "ssd",
-  "name": "strg0"
-}
-
-const mockArray = [
-  {
-    ...mock,
-    name: "strg0"
-  },
-  {
-    ...mock,
-    name: "strg1"
-  }
-]
-
-
 const baseUrl = () => {
-  return `${BASE_URL_CIS_SOUTH}/clusters`
+  return `${BASE_URL_CIS_SOUTH}/clusters?format=brief`
 }
 
 const clusterSchema = new schema.Entity('clusters', {},{
   idAttribute: 'name'
 })
-
-
 
 /**
  * Fetch list of Clusters (Locations)
@@ -41,11 +20,11 @@ const clusterSchema = new schema.Entity('clusters', {},{
  * @return {[type]}         [description]
  */
 export const fetchAll = ({}) => {
-  return Promise.resolve( normalize(mockArray, [ clusterSchema ]) )
+  //return Promise.resolve( normalize(mockArray, [ clusterSchema ]) )
 
   // TODO: UDNP-2873 Uncomment when API is fixed
-  // return axios.get(baseUrl())
-  // .then( ({data}) => {
-  //   return normalize(data, [ clusterSchema ])
-  // })
+  return axios.get(baseUrl())
+  .then( ({data}) => {
+    return normalize(data, [ clusterSchema ])
+  })
 }
