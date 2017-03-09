c226fd9e4428fd22c886a80020df9b1cba702a50
Olga Metlyakova <ometlyakova@vidscale.com>
2017-03-09 13:02:08 +0200

    UDNP-2936 Time picker component

--- src/components/time-picker.jsx
+++ src/components/time-picker.jsx
@@ -7,46 +7,41 @@ import { Dropdown } from 'react-bootstrap'
 import IconSelectCaret from './icons/icon-select-caret'
 import IconClock from './icons/icon-clock'
 
-export class TimePicker extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const TimePicker = ({ time, onChange, isUTC }) => {
+  const value = `${time.format('hh:mma')} ${isUTC ? 'UTC' : ''}`
 
-  render() {
-    const { time, onChange } = this.props
-    const value = `${time.format('hh:mma')} UTC`
-
-    return (
-      <Dropdown className="dropdown-select">
-          <Dropdown.Toggle
-            className="has-left-icon"
-            noCaret={true}
-          >
-            <IconClock className="left"/>
-            {value}
-            <IconSelectCaret/>
-          </Dropdown.Toggle>
-          <Dropdown.Menu className="dropdown-menu">
-            <Datetime
-              className="time-picker"
-              dateFormat={false}
-              value={time}
-              onChange={onChange}
-              input={false}
-              utc={true}
-            />
-          </Dropdown.Menu>
-        </Dropdown>
-    )
-  }
+  return (
+    <Dropdown className="dropdown-select">
+        <Dropdown.Toggle
+          className="has-left-icon"
+          noCaret={true}
+        >
+          <IconClock className="left"/>
+          {value}
+          <IconSelectCaret/>
+        </Dropdown.Toggle>
+        <Dropdown.Menu className="dropdown-menu">
+          <Datetime
+            className="time-picker"
+            dateFormat={false}
+            value={time}
+            onChange={onChange}
+            input={false}
+            utc={isUTC}
+          />
+        </Dropdown.Menu>
+      </Dropdown>
+  )
 }
 
 TimePicker.displayName = 'TimePicker'
 TimePicker.propTypes = {
+  isUTC: React.PropTypes.bool,
   onChange: React.PropTypes.func,
   time: React.PropTypes.instanceOf(moment)
 }
 TimePicker.defaultProps = {
+  isUTC: true,
   time: moment().utc()
 }
 
