70159b030c87bc5d0028993b0287f1c9d59eb019
Santtu Sulander <santtu.sulander@idean.com>
2017-03-15 14:40:29 +0200

    remove TODO

--- src/redux/modules/entities/storage-metrics/api.js
+++ src/redux/modules/entities/storage-metrics/api.js
@@ -7,7 +7,6 @@ const URL = (params, getOverView = true) => {
   return `${analyticsBase({ legacy: false })}/storage/${getOverView ? 'get-overview/' : 'get-by-region/'}${qsBuilder(params)}`
 }
 
-//TODO: return actual response data when API returns a similar data set than this placeholder
 export const fetch = (urlParams) =>
   axios.get(URL(urlParams)).then(({ data }) => {
     return { storageMetrics: data.data }
