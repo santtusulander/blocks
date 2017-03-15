db3a76ceae38b44701d519ec702ccf2ec8dda19d
Nizar Rahme <nizar.rahme@nordsoftware.com>
2017-03-15 20:20:02 +0200

    Connect Usage-By-Time chart to the api

--- src/components/analysis/storage.jsx
+++ src/components/analysis/storage.jsx
@@ -1,58 +1,83 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
-import { FormattedMessage } from 'react-intl'
+import { injectIntl, FormattedMessage } from 'react-intl'
+import { List } from 'immutable'
 
 import SectionHeader from '../layout/section-header'
 import SectionContainer from '../layout/section-container'
 import LineAreaComposedChart from '../../components/charts/line-area-composed-chart'
-import { formatBitsPerSecond } from '../../util/helpers'
 
-//TODO: remove mock data when integrating with redux in UDNP-2954
-import {composedChartData } from '../../containers/__mocks__/chart-data'
+const AnalysisStorage = ({
+  dateRangeLabel,
+  storageType,
+  peakStorage,
+  avgStorage,
+  lowStorage,
+  chartData,
+  valueFormatter,
+  intl
+  }) => {
 
-const AnalysisStorage = (props) => {
   return (
     <div className="analysis-storage">
       <SectionHeader
-        sectionHeaderTitle={props.storageType === 'bytes'
-          ? <FormattedMessage id="portal.analytics.storage.usage.text"/>
-          : <FormattedMessage id="portal.analytics.storage.files.text"/>} />
+        sectionHeaderTitle={storageType === 'bytes'
+          ? intl.formatMessage({id: "portal.analytics.storage.usage.text"})
+          : intl.formatMessage({id: "portal.analytics.storage.files.text"})} />
       <SectionContainer className="analysis-data-box wide">
         <Row>
           <Col xs={4} className="right-separator">
             <h4><FormattedMessage id="portal.analytics.peak.text"/></h4>
-            <p>{props.peakStorage && props.peakStorage}</p>
+            <p>{peakStorage}</p>
           </Col>
           <Col xs={4} className="right-separator">
             <h4><FormattedMessage id="portal.analytics.average.text"/></h4>
-            <p>{props.avgStorage && props.avgStorage}</p>
+            <p>{avgStorage}</p>
           </Col>
           <Col xs={4}>
             <h4><FormattedMessage id="portal.analytics.low.text"/></h4>
-            <p>{props.lowStorage && props.lowStorage}</p>
+            <p>{lowStorage}</p>
           </Col>
         </Row>
       </SectionContainer>
 
       <SectionHeader
-        sectionHeaderTitle={<FormattedMessage id="portal.analytics.storage.usageByTime.text"/>} />
+        sectionHeaderTitle={intl.formatMessage({id: "portal.analytics.storage.usageByTime.text"})} />
       <SectionContainer className="analysis-by-time">
         <LineAreaComposedChart
-          chartLabel="Oct 2016 Month To Date"
-          data={composedChartData}
-          valueFormatter={formatBitsPerSecond}
+          chartLabel={intl.formatMessage({id: dateRangeLabel})}
+          data={chartData.toJS()}
+          dataKey={storageType}
+          keyLabel={storageType === 'bytes'
+            ? intl.formatMessage({id: "portal.analysis.filters.storageType.usage.title"})
+            : intl.formatMessage({id: "portal.analysis.filters.storageType.files.title"})}
+          valueFormatter={valueFormatter}
         />
       </SectionContainer>
     </div>
   )
 }
 
+AnalysisStorage.defaultProps   = {
+  avgStorage: 0,
+  lowStorage: 0,
+  peakStorage: 0
+}
+
+AnalysisStorage.defaultProps   = {
+  chartData: List()
+}
+
 AnalysisStorage.displayName = 'AnalysisStorage'
 AnalysisStorage.propTypes   = {
   avgStorage: React.PropTypes.string,
+  chartData: React.PropTypes.instanceOf(List),
+  dateRangeLabel: React.PropTypes.string,
+  intl: React.PropTypes.object,
   lowStorage: React.PropTypes.string,
   peakStorage: React.PropTypes.string,
-  storageType: React.PropTypes.string
+  storageType: React.PropTypes.string,
+  valueFormatter: React.PropTypes.func
 }
 
-export default AnalysisStorage
+export default injectIntl(AnalysisStorage)
