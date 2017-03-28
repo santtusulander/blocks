7054af4780f8d086214a242725a2da3a816361cd
Nizar Rahme <nizar.rahme@nordsoftware.com>
2017-03-28 16:58:12 +0300

    Make the custom legend accepts an optional array prop that specifies the order of the legend items depending of the data key

--- src/components/charts/custom-legend.jsx
+++ src/components/charts/custom-legend.jsx
@@ -1,8 +1,11 @@
 import React, { PropTypes } from 'react'
 
-const CustomLegend = ({ data = [], payload }) => {
+const CustomLegend = ({ data = [], payload, order }) => {
   if(!data.length && payload.length) {
     data = payload.map((pl) => pl.payload )
+  }
+  if(order) {
+    data.sort((a, b) => order.indexOf(a.dataKey) - order.indexOf(b.dataKey))
   }
   return (
     <div className="bar-chart-legend">
@@ -21,7 +24,8 @@ const CustomLegend = ({ data = [], payload }) => {
 CustomLegend.displayName = "CustomLegend"
 CustomLegend.propTypes = {
   data: PropTypes.array,
-  payload: PropTypes.array
+  payload: PropTypes.array,
+  order: PropTypes.array
 }
 
 export default CustomLegend
