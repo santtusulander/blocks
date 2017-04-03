efacd7b7c4a24cee1a9ef38dc4c091de87945b3d
Pouya Adib <pouya.adib@nordsoftware.com>
2017-04-03 11:01:07 +0300

    don't destruct jsSHA constructor

--- src/util/token-authentication.js
+++ src/util/token-authentication.js
@@ -1,5 +1,5 @@
 import md5 from 'blueimp-md5'
-import { jsSHA1, jsSHA256 } from 'jssha'
+import jsSHA from 'jssha'
 import moment from 'moment'
 
 import { SCHEMA_LABEL_MAP, STATIC_TOKEN_SAMPLE_VALUES } from '../constants/configuration'
@@ -36,12 +36,12 @@ export const generateTokenHash = (method, key, string) => {
     case 'HMAC_MD5':
       return md5(string, key)
     case 'HMAC_SHA1':
-      const sha1Obj = new jsSHA1('SHA-1', "TEXT")
+      const sha1Obj = new jsSHA('SHA-1', "TEXT")
       sha1Obj.setHMACKey(key, "TEXT")
       sha1Obj.update(string)
       return sha1Obj.getHMAC("HEX")
     case 'HMAC_SHA256':
-      const sha256Obj = new jsSHA256('SHA-256', "TEXT")
+      const sha256Obj = new jsSHA('SHA-256', "TEXT")
       sha256Obj.setHMACKey(key, "TEXT")
       sha256Obj.update(string)
       return sha256Obj.getHMAC("HEX")
