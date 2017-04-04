606b137ec8ed025f07e85e8c3bc9472e21a6f4cc
Mikko Alander <mikko.alander@idean.com>
2017-04-04 14:25:50 +0300

    new module for metadata

---
+++ src/redux/modules/entities/metadata/api.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import { BASE_URL_NORTH, PAGINATION_MOCK }  from '../../../util.js'
+import { normalize, schema } from 'normalizr'
+
+const baseURL = (brand, account, group) => `${BASE_URL_NORTH}/brands/${brand}/accounts/${account}/groups/${group}/published_hosts`
+
+const publishedName = ({ services }) => services[0].configurations[0].edge_configuration.published_name
+
+const propertySchema = new schema.Entity('properties', {},
+  {
+    idAttribute: 'published_host_id',
+    processStrategy: (value, parent) => {
+      return { ...value, parentId: parent.id }
+    }
+  }
+)
+
+const groupPropertiesSchema = new schema.Entity('grpProperties', { properties: [ propertySchema ] })
+
+export const fetch = ({ brand, account, group, id }) => {
+  return axios.get(`${baseURL(brand, account, group)}/${id}/custom_policy_configs`)
+    .then(({ data }) => {
+      return normalize({ id: group, properties: [ data.data ] }, groupPropertiesSchema)
+    })
+}
+
+export const fetchAll = ({ brand, account, group }) => {
+  return axios.get(baseURL(brand, account, group), PAGINATION_MOCK)
+    .then(({data}) => {
+      return normalize({ id: group, properties: data.data }, groupPropertiesSchema)
+    })
+}
+
+export const fetchIds = ({ brand, account, group }) => {
+  return axios.get(baseURL(brand, account, group))
+    .then(({ data }) => data)
+}
+
+export const create = ({ brand, account, group, payload }) =>
+  axios.post(`${baseURL(brand, account, group)}/${publishedName(payload)}/custom_policy_configs`, payload, { headers: { 'Content-Type': 'application/json' } })
+    .then(({ data }) => {
+      return normalize({ id: group, properties: [ data ] }, groupPropertiesSchema)
+    })
+
+export const update = ({ brand, account, group, payload }) =>
+  axios.put(`${baseURL(brand, account, group)}/${publishedName(payload)}/custom_policy_configs`, payload, { headers: { 'Content-Type': 'application/json' } })
+    .then(({ data }) => {
+      return normalize({ id: group, properties: [ data ] }, groupPropertiesSchema)
+    })
+
+export const remove = ({ brand, account, group, id }) =>
+  axios.delete(`${baseURL(brand, account, group)}/${id}/custom_policy_configs`)
+    .then(() => ({ id }))
---
+++ src/redux/modules/entities/metadata/selectors.js
@@ -0,0 +1,125 @@
+import { List, fromJS } from 'immutable'
+import {getEntityById, getEntitiesByParent, getEntityIdsByParent} from '../../entity/selectors'
+import { getConfiguredName } from '../../../../util/helpers'
+/**
+ * Get property by ID
+ * @param  {} state from redux
+ * @param  String id of the item
+ * @return {} property
+ */
+export const getById = (state, id) => {
+  return getEntityById(state, 'properties', id)
+}
+
+/**
+ * Get Properties By Group
+ * @param  {} state
+ * @param  {String} brand [description]
+ * @return List
+ */
+export const getByGroup = (state, group) => {
+  return getEntitiesByParent(state, 'properties', group)
+}
+
+/**
+ * Get Properties By Account
+ * @param  {} state
+ * @param  {String} account ID
+ * @return List
+ */
+export const getByAccount = (state, account) => {
+  const groups = getEntitiesByParent(state, 'groups', account)
+
+  if (groups && groups.size > 0) {
+    return groups.reduce((acc, g) => {
+      const groupProperties = getByGroup(state, g.get('id'))
+      return acc.merge(groupProperties)
+    }, List())
+  }
+}
+
+/**
+ * Get Properties By Groups
+ * @param  {} state
+ * @param  {List} list of groups
+ * @return List
+ */
+export const getByGroups = (state, groups) => {
+  if (groups && groups.size > 0) {
+    const properties = []
+    groups.forEach(group => {
+      const groupProperties = getByGroup(state, group.get('id'))
+      groupProperties.forEach(property => {
+        properties.push(property)
+      })
+    })
+    return fromJS(properties)
+  }
+}
+
+/**
+ * Get Property IDs by Group
+ * @param  {[type]} state   [description]
+ * @param  {[type]} groupId [description]
+ * @return {[type]}         [description]
+ */
+export const getIdsByGroup = (state, groupId) => {
+  return getEntityIdsByParent(state, 'properties', groupId)
+}
+
+
+/**
+ * [getPropertyMetricsById description]
+ * @param  {[type]} state      [description]
+ * @param  {[type]} propertyId [description]
+ * @return {[type]}            [description]
+ */
+export const getPropertyMetricsById = (state, propertyId) => {
+
+  const entity = getById(state, propertyId)
+  const configuredName = getConfiguredName(entity)
+
+  return state.metrics.get('hostMetrics').find(metric => metric.get('property') === configuredName)
+}
+
+/**
+ * [getByGroupWithTotalTraffic description]
+ * @param  {[type]} state [description]
+ * @param  {[type]} group [description]
+ * @return {[type]}       [description]
+ */
+export const getByGroupWithTotalTraffic = (state, group) => {
+  const properties = getByGroup(state, group)
+  const result = properties.map(property => {
+
+    const metrics = getPropertyMetricsById(state, property.get('published_host_id'))
+    const totalTraffic = metrics ? metrics.get('totalTraffic') : 0
+
+    return property.set('totalTraffic', totalTraffic)
+  })
+
+  return result
+}
+
+/**
+ * getPropertyDailyTrafficById
+ * @param  {Object} redux state
+ * @param  {String|Number} propertyId
+ * @return {Map} dailyTraffic of a property
+ */
+export const getPropertyDailyTrafficById = (state, propertyId) => {
+
+  const entity = getById(state, propertyId)
+  const configuredName = getConfiguredName(entity)
+
+  return state.metrics.get('hostDailyTraffic').find(metric => metric.get('property') === configuredName)
+}
+
+/**
+ * getTotalTraffics
+ * @param  {Object} state
+ * @return {List} List of totalTraffics
+ */
+export const getTotalTraffics = (state) => {
+  return state.metrics.get('hostMetrics').map(property => property.get('totalTraffic'))
+}
