060dfd98f4eff446ce648475a566c9f1380bc6ee
Mikko Alander <mikko.alander@idean.com>
2017-04-05 15:50:44 +0300

    take only first config

--- src/redux/modules/entities/metadata/api.js
+++ src/redux/modules/entities/metadata/api.js
@@ -2,31 +2,32 @@ import axios from 'axios'
 import { BASE_URL_NORTH, PAGINATION_MOCK, buildReduxId }  from '../../../util.js'
 import { normalize, schema } from 'normalizr'
 
-const baseURL = (brand, account, group, property, serviceType) => `${BASE_URL_NORTH}/brands/${brand}/accounts/${account}/groups/${group}/published_hosts/${property}/services/${serviceType}/custom_policy_configs?force=true`
+const baseURL = (brand, account, group, property, serviceType) => `${BASE_URL_NORTH}/brands/${brand}/accounts/${account}/groups/${group}/published_hosts/${property}/services/${serviceType}/custom_policy_configs`
 
 const metadataSchema = new schema.Entity('propertyMetadata', {}, {})
 
 export const fetchAll = ({ brand, account, group, property, serviceType }) => {
-  return axios.get(`${baseURL(brand, account, group, property, serviceType)}`, PAGINATION_MOCK)
+  return axios.get(`${baseURL(brand, account, group, property, serviceType)}?force=true`, PAGINATION_MOCK)
     .then(({data}) => {
 
-      return normalize({ id: buildReduxId(property, serviceType), customPolicyConfigs: data.data }, metadataSchema)
+      const [customPolicyConfig] = data.data
+      return normalize({ id: buildReduxId(property, serviceType), customPolicyConfig }, metadataSchema)
     })
 }
 
 export const create = ({ brand, account, group, property, serviceType, payload }) => {
   return axios.get(`${baseURL(brand, account, group, property, serviceType)}`,  payload, { headers: { 'Content-Type': 'application/json' } })
     .then(({data}) => {
-
-      return normalize({ id: buildReduxId(property, serviceType), customPolicyConfigs: data.data }, metadataSchema)
+      const [customPolicyConfig] = data.data
+      return normalize({ id: buildReduxId(property, serviceType), customPolicyConfig }, metadataSchema)
     })
 }
 
 export const update = ({ brand, account, group, property, serviceType, payload }) => {
   return axios.get(`${baseURL(brand, account, group, property, serviceType)}`,  payload, { headers: { 'Content-Type': 'application/json' } })
     .then(({data}) => {
-
-      return normalize({ id: buildReduxId(property, serviceType), customPolicyConfigs: data.data }, metadataSchema)
+      const [customPolicyConfig] = data.data
+      return normalize({ id: buildReduxId(property, serviceType), customPolicyConfig}, metadataSchema)
     })
 }
 
