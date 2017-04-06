5536402d02bb87f77a976258a72ddc8ff4cd0469
Mykhailo Prochkaruk <mprochkaruk@vidscale.com>
2017-04-06 15:33:05 +0300

    UDNP-3326 - divide styleguide and styleguide-charts into separete components

--- src/containers/styleguide/styleguide.jsx
+++ src/containers/styleguide/styleguide.jsx
@@ -31,12 +31,9 @@ import {
 } from 'react-bootstrap';
 
 import StyleguideIcons from './styleguide-icons'
-import SectionContainer from '../../components/shared/layout/section-container'
+import StyleguideCharts from './styleguide-charts'
+
 import SelectWrapper from '../../components/shared/form-elements/select-wrapper'
-import BarChart from '../../components/charts/bar-chart'
-import StackedAreaChart from '../../components/charts/stacked-area-chart'
-import LineAreaComposedChart from '../../components/charts/line-area-composed-chart'
-import LineChart from '../../components/charts/line-chart'
 import FilterChecklistDropdown from '../../components/shared/form-elements/filter-checklist-dropdown.jsx'
 import SelectorComponent from '../../components/global-account-selector/selector-component'
 import Tabs from '../../components/shared/page-elements/tabs'
@@ -67,17 +64,8 @@ import IconAdd from '../../components/shared/icons/icon-add'
 import IconCaretDown from '../../components/shared/icons/icon-caret-down'
 import IconQuestionMark from '../../components/shared/icons/icon-question-mark'
 
-import { formatBytes, formatBitsPerSecond, separateUnit } from '../../util/helpers'
-import { stackedBarChartData,
-         singleBarChartData,
-         lineChartData,
-         twoStackedComparisonAreaData as AreaChartData,
-         nonStackedAreaDataset,
-         stackedAreaDataset,
-         comparisonAreaDataset,
-         twoStackedAreaDataset,
-         composedChartData
-  } from '../__mocks__/chart-data'
+import { formatBytes, separateUnit } from '../../util/helpers'
+
 import DateRanges from '../../constants/date-ranges'
 import country_list from '../../constants/country-list'
 
@@ -294,114 +282,8 @@ class Styleguide extends React.Component {
 
 
           <h1 className="page-header">Charts</h1>
-          <Row>
-            <label>Stacked barchart</label>
-            <SectionContainer className="analysis-chart-container">
-              {<BarChart
-                chartLabel="Month to Date"
-                chartData={stackedBarChartData}
-                barModels={[
-                  { dataKey: 'offNetHttps', name: 'Off-Net HTTPS', className: 'line-3' },
-                  { dataKey: 'offNetHttp', name: 'Off-Net HTTP', className: 'line-2' },
-                  { dataKey: 'onNetHttps', name: 'On-Net HTTPS', className: 'line-1' },
-                  { dataKey: 'onNetHttp', name: 'On-Net HTTP', className: 'line-0' }
-                ]}/>}
-            </SectionContainer>
-            </Row>
+          <StyleguideCharts />
 
-            <hr />
-
-            <Row>
-              <label>Normal barchart</label>
-              <SectionContainer className="analysis-chart-container">
-                <BarChart
-                  chartLabel="This Week"
-                  chartData={singleBarChartData}
-                  barModels={[{ dataKey: 'onNetHttp', name: 'On-Net HTTP', className: 'line-0' }]}/>
-              </SectionContainer>
-            </Row>
-
-            <hr/>
-
-            <Row>
-              <label>Line chart</label>
-              <SectionContainer className="analysis-by-time">
-                <LineChart
-                  data={lineChartData}
-                  dataKey="bits_per_second"
-                />
-              </SectionContainer>
-            </Row>
-
-            <hr/>
-
-            <Row>
-              <label>Area chart</label>
-              <SectionContainer className="analysis-by-time">
-                <StackedAreaChart
-                  chartLabel="Oct 2016 Month To Date"
-                  areas={nonStackedAreaDataset}
-                  data={AreaChartData}
-                  valueFormatter={formatBitsPerSecond}
-                />
-              </SectionContainer>
-            </Row>
-
-            <hr/>
-
-            <Row>
-              <label>Stacked area chart</label>
-              <SectionContainer className="analysis-by-time">
-                <StackedAreaChart
-                  chartLabel="Oct 2016 Month To Date"
-                  areas={stackedAreaDataset}
-                  data={AreaChartData}
-                  valueFormatter={formatBitsPerSecond}
-                />
-              </SectionContainer>
-            </Row>
-
-            <hr/>
-            <Row>
-              <label>Comparison area chart</label>
-              <SectionContainer className="analysis-by-time">
-                <StackedAreaChart
-                  chartLabel="Oct 2016 Month To Date"
-                  areas={comparisonAreaDataset}
-                  data={AreaChartData}
-                  valueFormatter={formatBitsPerSecond}
-                />
-              </SectionContainer>
-            </Row>
-
-            <hr/>
-            <Row>
-              <label>Comparison chart for two stacked area</label>
-              <SectionContainer className="analysis-by-time">
-                <StackedAreaChart
-                  chartLabel="Oct 2016 Month To Date"
-                  areas={twoStackedAreaDataset}
-                  data={AreaChartData}
-                  valueFormatter={formatBitsPerSecond}
-                />
-              </SectionContainer>
-            </Row>
-
-            <hr/>
-
-            <Row>
-              <label>Comparison chart composed with line chart </label>
-              <SectionContainer className="analysis-by-time">
-                <LineAreaComposedChart
-                  chartLabel="Oct 2016 Month To Date"
-                  data={composedChartData}
-                  keyLabel="Storage"
-                  valueFormatter={formatBitsPerSecond}
-                  comparisonKeyLabel="Comparison Storage"
-                  isComparison={true}
-                />
-              </SectionContainer>
-            </Row>
           <h1 className="page-header">Tabs</h1>
 
           <Tabs
