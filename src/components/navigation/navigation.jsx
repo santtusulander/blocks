1fa30a6824f6d0f41be81aacade273db7bd7c44e
Anh Nguyen <anh.nguyen@nordsoftware.com>
2017-04-07 11:30:37 +0300

    fix active class of dashboard and content

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -74,7 +74,7 @@ const Navigation = ({ activeAccount, currentUser, params, roles, router }) => {
         { isUDNCore &&
           <IsAllowed to={VIEW_CONTENT_SECTION}>
             <li>
-              <Link to={getDashboardUrlFromParams(params)} activeClassName="active" className={dashboardSPActive}>
+              <Link to={getDashboardUrlFromParams(params)} activeClassName="active">
                 <IconDashboard />
                 <FormattedMessage id="portal.navigation.dashboard.text"/>
               </Link>
