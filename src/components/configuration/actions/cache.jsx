7294c835bfd0839f76315126e53984eedeed4cec
Olga Metlyakova <ometlyakova@vidscale.com>
2017-04-10 16:21:52 +0300

    UDNP-1831 Cache screen can set incompatible cache rules at the same time

--- src/components/configuration/actions/cache.jsx
+++ src/components/configuration/actions/cache.jsx
@@ -1,183 +1,174 @@
 import React from 'react'
-import { Button, Col, ControlLabel, FormControl, Modal, Row } from 'react-bootstrap'
+import { Button, Col, ControlLabel, Modal, Row } from 'react-bootstrap'
 import Immutable from 'immutable'
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl'
 
-import FormFooterButtons from '../../shared/form-elements/form-footer-buttons'
-import Toggle from '../../shared/form-elements/toggle'
-import Select from '../../shared/form-elements/select'
+import { connect } from 'react-redux'
+import { Field, reduxForm, formValueSelector, propTypes as reduxFormPropTypes } from 'redux-form'
 
-import { secondsToUnit, secondsFromUnit } from '../helpers'
+import FieldFormGroupNumber from '../../shared/form-fields/field-form-group-number'
+import FieldFormGroupToggle from '../../shared/form-fields/field-form-group-toggle'
+import FieldFormGroupSelect from '../../shared/form-fields/field-form-group-select'
+import FormFooterButtons from '../../shared/form-elements/form-footer-buttons'
+
+import { secondsToUnit, unitFromSeconds, secondsFromUnit } from '../helpers'
+
+const cacheControlEtagOptions = [
+  { value: 'strong', label: <FormattedMessage id="portal.policy.edit.defaults.enableEtagStrong.text"/>},
+  { value: 'weak', label: <FormattedMessage id="portal.policy.edit.defaults.enableEtagWeak.text"/>},
+  { value: 'false', label: <FormattedMessage id="portal.policy.edit.defaults.enableEtagFalse.text"/>}
+]
+const ttlUnitOptions = [
+  {value: 'seconds', label: <FormattedMessage id="portal.units.seconds"/>},
+  {value: 'minutes', label: <FormattedMessage id="portal.units.minutes"/>},
+  {value: 'hours', label: <FormattedMessage id="portal.units.hours"/>},
+  {value: 'days', label: <FormattedMessage id="portal.units.days"/>}
+]
 
 class Cache extends React.Component {
   constructor(props) {
     super(props)
-    const setKey = props.set
-    let maxAge = setKey.get('max_age')
 
-    if (!maxAge) {
-      maxAge = secondsFromUnit(7, 'days')
-    } else {
-      maxAge = Number(maxAge)
-    }
-
-    let ttlType = 'seconds'
-    if (maxAge / 86400 >= 1) {
-      maxAge = secondsToUnit(maxAge, 'days')
-      ttlType = 'days'
-    } else if (maxAge / 3600 >= 1) {
-      maxAge = secondsToUnit(maxAge, 'hours')
-      ttlType = 'hours'
-    } else if (maxAge / 60 >= 1) {
-      maxAge = secondsToUnit(maxAge, 'minutes')
-      ttlType = 'minutes'
-    }
-
-    this.state = {
-      checkEtag: setKey.get('check_etag', 'false'),
-      honorOrigin: setKey.get('honor_origin', false),
-      maxAge: maxAge,
-      noStore: setKey.get('no_store', false),
-      ttlType: ttlType
-    }
-
-    this.handleChange = this.handleChange.bind(this)
-    this.handleEtagChange = this.handleEtagChange.bind(this)
-    this.handleTTLTypeChange = this.handleTTLTypeChange.bind(this)
-    this.handleToggleChange = this.handleToggleChange.bind(this)
-    this.getMaxAge = this.getMaxAge.bind(this)
+    this.updateProps = this.updateProps.bind(this)
     this.saveChanges = this.saveChanges.bind(this)
   }
-  handleChange(key) {
-    return e => {
-      const stateObj = {}
-      stateObj[key] = e.target.value
-      this.setState(stateObj)
-    }
-  }
-  handleEtagChange(value) {
-    this.setState({checkEtag: value})
-  }
-  handleTTLTypeChange(value) {
-    this.setState({ttlType: value})
-  }
-  handleToggleChange(key) {
-    return value => {
-      const stateObj = {}
-      stateObj[key] = value
-      this.setState(stateObj)
-    }
-  }
-  getMaxAge() {
-    let maxAge = Number(this.state.maxAge)
 
-    switch (this.state.ttlType) {
-      case 'minutes':
-        maxAge = secondsFromUnit(maxAge, 'minutes')
-        break
-      case 'hours':
-        maxAge = secondsFromUnit(maxAge, 'hours')
-        break
-      case 'days':
-        maxAge = secondsFromUnit(maxAge, 'days')
-        break
-    }
-
-    return maxAge;
+  componentWillMount() {
+    this.updateProps(this.props.set)
   }
-  saveChanges() {
+
+  componentWillReceiveProps(nextProps) {
+    const { set } = nextProps
+
+    if (this.props.set !== set) {
+      this.updateProps(set)
+    }
+  }
+
+  updateProps(set) {
+    this.props.change('noStore', set.get('no_store', false))
+    this.props.change('checkEtag', set.get('check_etag', 'false'))
+    this.props.change('honorOrigin', set.get('honor_origin', false))
+
+    const ttl = set.get('max_age', 0)
+    const ttlUnit = unitFromSeconds(ttl)
+    const ttlValue = secondsToUnit(ttl, ttlUnit)
+
+    this.props.change('ttlValue', ttlValue)
+    this.props.change('ttlUnit', ttlUnit)
+  }
+
+  saveChanges(values) {
+    const { noStore, checkEtag, honorOrigin, ttlValue, ttlUnit } = values
+
     this.props.saveAction(this.props.path, this.props.setKey, {
-      check_etag: this.state.checkEtag,
-      max_age: this.getMaxAge(),
-      no_store: this.state.noStore,
-      honor_origin: this.state.honorOrigin
+      check_etag: checkEtag,
+      max_age: secondsFromUnit(ttlValue, ttlUnit),
+      no_store: noStore,
+      honor_origin: honorOrigin
     })
   }
+
   render() {
+    const { disabled, handleSubmit, close } = this.props
     return (
       <div>
         <Modal.Header>
           <h1><FormattedMessage id="portal.policy.edit.cache.cache.text"/></h1>
         </Modal.Header>
         <Modal.Body>
+          <form onSubmit={handleSubmit(this.saveChanges)}>
 
-          <Row className="no-gutters">
-            <Col xs={8} className="toggle-label">
-              <label><FormattedMessage id="portal.policy.edit.cache.noStore.text"/></label>
-            </Col>
-            <Col xs={4}>
-              <Toggle className="pull-right"
-                value={this.state.noStore}
-                changeValue={this.handleToggleChange('noStore')}/>
-            </Col>
-          </Row>
+            <Row className="form-group">
+              <Col lg={4} xs={6} className="toggle-label">
+                <ControlLabel>
+                  <FormattedMessage id="portal.policy.edit.cache.noStore.text"/>
+                </ControlLabel>
+              </Col>
+              <Col lg={8} xs={6}>
+                <Field
+                  name="noStore"
+                  component={FieldFormGroupToggle}
+                />
+              </Col>
+            </Row>
 
-          <hr />
+            <hr />
 
-          <Row className="no-gutters">
-            <Col xs={8} className="toggle-label">
-              <label><FormattedMessage id="portal.policy.edit.cache.honorCacheControl.text"/></label>
-            </Col>
-            <Col xs={4}>
-              <Toggle className="pull-right"
-                value={this.state.honorOrigin}
-                changeValue={this.handleToggleChange('honorOrigin')}/>
-            </Col>
-          </Row>
+            <Row className="form-group">
+              <Col lg={4} xs={6} className="toggle-label">
+                <ControlLabel>
+                  <FormattedMessage id="portal.policy.edit.cache.honorCacheControl.text"/>
+                </ControlLabel>
+              </Col>
+              <Col lg={8} xs={6}>
+                <Field
+                  name="honorOrigin"
+                  component={FieldFormGroupToggle}
+                  readonly={disabled}
+                />
+              </Col>
+            </Row>
 
-          <hr />
+            <hr />
 
-          <Row className="no-gutters">
-            <Col xs={8} className="toggle-label">
-              <label><FormattedMessage id="portal.policy.edit.cache.honorEtag.text"/></label>
-            </Col>
-            <Col xs={4}>
-              <Select className="input-select small-select"
-                onSelect={this.handleEtagChange}
-                value={this.state.checkEtag}
-                options={[
-                  ['strong', <FormattedMessage id="portal.policy.edit.cache.honorEtagStrong.text"/>],
-                  ['weak', <FormattedMessage id="portal.policy.edit.cache.honorEtagWeak.text"/>],
-                  ['false', <FormattedMessage id="portal.policy.edit.cache.honorEtagFalse.text"/>]]}/>
-            </Col>
-          </Row>
+            <Row className="form-group">
+              <Col lg={4} xs={6} className="toggle-label">
+                <ControlLabel>
+                  <FormattedMessage id="portal.policy.edit.cache.honorEtag.text"/>
+                </ControlLabel>
+              </Col>
+              <Col lg={8} xs={6}>
+                <Field
+                  name="checkEtag"
+                  component={FieldFormGroupSelect}
+                  options={cacheControlEtagOptions}
+                  disabled={disabled}
+                />
+              </Col>
+            </Row>
+            
+            <hr/>
 
-          <hr />
+            <Row className="form-group">
+              <Col lg={4} xs={6} className="toggle-label">
+                <ControlLabel>
+                  <FormattedMessage id="portal.policy.edit.cache.ttlValue.text"/>
+                </ControlLabel>
+              </Col>
+              <Col lg={2} xs={3}>
+                <Field
+                  name="ttlValue"
+                  component={FieldFormGroupNumber}
+                  min={0}
+                  disabled={disabled}
+                />
+              </Col>
+              <Col xs={3}>
+                <Field
+                  name="ttlUnit"
+                  component={FieldFormGroupSelect}
+                  options={ttlUnitOptions}
+                  disabled={disabled}
+                />
+              </Col>
+            </Row>
 
-          <ControlLabel>
-            <FormattedMessage id="portal.policy.edit.cache.ttlValue.text" />
-          </ControlLabel>
-
-          <Row>
-            <Col xs={6}>
-              <FormControl
-                type="number"
-                id="actions_ttl-value-number"
-                placeholder={this.props.intl.formatMessage({id: 'portal.policy.edit.cache.ttlValue.placeholder'})}
-                value={this.state.maxAge || 0}
-                onChange={this.handleChange('maxAge')}/>
-            </Col>
-            <Col xs={6}>
-              <Select className="input-select"
-                onSelect={this.handleTTLTypeChange}
-                value={this.state.ttlType}
-                options={[
-                  ['seconds', <FormattedMessage id="portal.policy.edit.cache.ttlValue.seconds"/>],
-                  ['minutes', <FormattedMessage id="portal.policy.edit.cache.ttlValue.minutes"/>],
-                  ['hours', <FormattedMessage id="portal.policy.edit.cache.ttlValue.hours"/>],
-                  ['days', <FormattedMessage id="portal.policy.edit.cache.ttlValue.days"/>]]}/>
-            </Col>
-          </Row>
-
-          <FormFooterButtons>
-            <Button className="btn-secondary" onClick={this.props.close}>
-              <FormattedMessage id="portal.button.cancel"/>
-            </Button>
-            <Button bsStyle="primary" onClick={this.saveChanges}>
-              <FormattedMessage id="portal.button.saveAction"/>
-            </Button>
-          </FormFooterButtons>
-
+            <FormFooterButtons>
+              <Button
+                className="btn-secondary"
+                onClick={close}
+              >
+                <FormattedMessage id="portal.button.cancel"/>
+              </Button>
+              <Button
+                type="submit"
+                bsStyle="primary"
+              >
+                <FormattedMessage id="portal.button.saveAction"/>
+              </Button>
+            </FormFooterButtons>
+          </form>
         </Modal.Body>
       </div>
     )
@@ -191,7 +182,20 @@ Cache.propTypes = {
   path: React.PropTypes.instanceOf(Immutable.List),
   saveAction: React.PropTypes.func,
   set: React.PropTypes.instanceOf(Immutable.Map),
-  setKey: React.PropTypes.string
+  setKey: React.PropTypes.string,
+  ...reduxFormPropTypes
 }
 
-module.exports = injectIntl(Cache)
+const cacheFormSelector = formValueSelector('cache-form')
+
+const mapStateToProps = (state) => (
+  {
+    disabled: cacheFormSelector(state, 'no_store')
+  }
+)
+
+const form = reduxForm({
+  form: 'cache-form'
+})(Cache)
+
+export default connect(mapStateToProps)(injectIntl(form))
