ef33c321e8a10a571ed46775beb8af1dd49a0291
Mykhailo Prochkaruk <mprochkaruk@vidscale.com>
2017-04-10 12:00:35 +0300

    UDNP-2103 - remove topo module

--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -2,7 +2,6 @@ export const account = require('./account').default;
 export const form = require('redux-form').reducer;
 export const group = require('./group').default;
 export const host = require('./host').default;
-export const topo = require('./topo').default;
 export const content = require('./content').default;
 export const traffic = require('./traffic').default;
 export const ui = require('./ui').default;
