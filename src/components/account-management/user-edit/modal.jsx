13ecda6f674adad0bd70c5185394962c11d116b0
Nizar Rahme <nizar.rahme@nordsoftware.com>
2017-04-12 15:53:07 +0300

    Flatten phone value

--- src/components/account-management/user-edit/modal.jsx
+++ src/components/account-management/user-edit/modal.jsx
@@ -35,7 +35,9 @@ class UserEditModal extends React.Component {
       groups: user.get('group_id'),
       first_name: user.get('first_name'),
       last_name: user.get('last_name'),
-      phone: {phone_number: user.get('phone_number'), phone_country_code: user.get('phone_country_code') }
+      full_phone_number: ` ${user.get('phone_country_code')}${user.get('phone_number')}`,
+      phone_country_code: user.get('phone_country_code'),
+      phone_number: user.get('phone_number')
     } : {}
 
     const title = <FormattedMessage id='portal.account.editUser.title' />
