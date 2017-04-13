85cec11db28e160b00d2f33e5de12c04f0f8fc8d
Mikko Alander <mikko.alander@idean.com>
2017-04-13 19:06:01 +0300

    add instanbul rules

--- src/containers/account-management/tabs/accounts.jsx
+++ src/containers/account-management/tabs/accounts.jsx
@@ -237,6 +237,7 @@ AccountList.defaultProps = {
   accounts: List()
 }
 
+/* istanbul ignore next */
 const mapStateToProps = (state, ownProps) => {
   const {brand} = ownProps.params
 
@@ -248,6 +249,7 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
+/* istanbul ignore next */
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchServiceInfo: () => dispatch(serviceInfofetchAll()),
