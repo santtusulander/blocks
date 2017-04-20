93bc3db43285d5c93b3fb1b7d8b581e9e6ce25fa
Anh Nguyen <anh.nguyen@nordsoftware.com>
2017-04-20 22:04:36 +0300

    add link for API documentation

--- src/containers/support/tabs/documentation.jsx
+++ src/containers/support/tabs/documentation.jsx
@@ -9,6 +9,8 @@ import {
   ACCOUNT_TYPE_CLOUD_PROVIDER
 } from '../../../constants/account-management-options'
 
+import './documentation.scss'
+
 class SupportTabDocumentation extends React.Component {
   render() {
     const { currentUserRole } = this.props
@@ -29,16 +31,33 @@ class SupportTabDocumentation extends React.Component {
         break
     }
 
+    const APIdocumentPath = "https://doc-0s-6k-docs.googleusercontent.com/docs/securesc/m2osprpdkpsvri3c87mvo1tsj6nmtp71/50n2iuocdlrcgckea2g2thjg8f9tf7ch/1492711200000/05635225967415231415/07254079547932312855/0B9BiKzBEVL3wTHl1eHBMb3hzT2M?e=pdf"
+
     return (
-      <div className="account-support-documentation text-center">
-        <p className="lead"><FormattedMessage id="portal.support.documentation.body.text" values={{br: <br/>}} /></p>
-        <p>
-          <a href={documentPath}
-            target="_blank"
-            className="btn btn-primary">
-            <FormattedMessage id="portal.support.documentation.body.link" />
-          </a>
-        </p>
+      <div className="account-support-documentation">
+        <div className="section">
+          <h3><FormattedMessage id="portal.support.documentation.userGuide.header" /></h3>
+          <p className="section-body"><FormattedMessage id="portal.support.documentation.body.text" values={{br: <br/>}} /></p>
+          <p>
+            <a href={documentPath}
+              target="_blank"
+              className="btn btn-primary">
+              <FormattedMessage id="portal.support.documentation.body.link" />
+            </a>
+          </p>
+        </div>
+
+        <div className="section">
+          <h3><FormattedMessage id="portal.support.documentation.APIGuide.header" /></h3>
+          <p className="section-body"><FormattedMessage id="portal.support.API.documentation.body.text" values={{br: <br/>}} /></p>
+          <p>
+            <a href={APIdocumentPath}
+              target="_blank"
+              className="btn btn-primary">
+              <FormattedMessage id="portal.support.API.documentation.body.link" />
+            </a>
+          </p>
+        </div>
       </div>
     )
   }
