2bdb4d2057bb5d1b418d666f727c769858655a1f
Olga Metlyakova <ometlyakova@vidscale.com>
2017-04-20 11:18:44 +0300

    Fixing tests

--- src/components/configuration/actions/__tests__/cache.js
+++ src/components/configuration/actions/__tests__/cache.js
@@ -42,42 +42,20 @@ describe('Cache', () => {
     expect(component).toBeTruthy();
   })
 
-//TODO-2277
+  it('should save changes', () => {
+    const changeValue = jest.genMockFunction()
+    const close = jest.genMockFunction()
+    const values = { noStore, checkEtag, honorOrigin, ttlValue, ttlUnit } 
+    const expectedSave = Immutable.fromJS({
+      check_etag: 'aaa',
+      honor_origin: true,
+      max_age: 123,
+      no_store: false
+    })
 
-  // it('should update the state as changes happen', () => {
-  //   let changeValue = jest.genMockFunction()
-  //   let cache = shallow(
-  //     <Cache changeValue={changeValue} set={fakeConfig} path={fakePath}
-  //       intl={intlMaker()}/>
-  //   )
-  //   const inputs = cache.find('FormControl')
-  //   inputs.at(0).simulate('change', {target: {value: 'new'}})
-  //   expect(cache.state('maxAge')).toEqual('new')
-  // })
+    component.instance().saveChanges()
 
-  // it('should save changes', () => {
-  //   const changeValue = jest.genMockFunction()
-  //   const close = jest.genMockFunction()
-  //   const expectedSave = Immutable.fromJS({
-  //     cases: [[ "foo" ]],
-  //     check_etag: 'aaa',
-  //     honor_origin: 'bbb',
-  //     max_age: 123,
-  //     no_store: 'ddd'
-  //   })
-  //   let cache = shallow(
-  //     <Cache changeValue={changeValue} set={fakeConfig} path={fakePath}
-  //       close={close} intl={intlMaker()}/>
-  //   )
-  //   cache.setState({
-  //     checkEtag: 'aaa',
-  //     honorOrigin: 'bbb',
-  //     maxAge: '123',
-  //     noStore: 'ddd'
-  //   })
-  //   cache.instance().saveChanges()
-  //   expect(changeValue.mock.calls[0][0].toJS()).toEqual(['foo', 'bar'])
-  //   expect(Immutable.is(changeValue.mock.calls[0][1], expectedSave)).toBeTruthy()
-  //   expect(close.mock.calls.length).toBe(1)
-  // })
+    expect(changeValue.mock.calls[0][0].toJS()).toEqual(['foo', 'bar'])
+    expect(Immutable.is(changeValue.mock.calls[0][1], expectedSave)).toBeTruthy()
+  })
 })
