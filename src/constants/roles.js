579643d3c6ab5d24b3b39d435e442355db273ee2
Mykhailo Prochkaruk <mprochkaruk@vidscale.com>
2017-04-21 15:06:53 +0300

    UDNP-3379 - fix services

--- src/constants/roles.js
+++ src/constants/roles.js
@@ -1,5 +1,3 @@
 export const ROLE_UDN = 'role-udn'
 export const ROLE_CONTENT_PROVIDER = 'role-content-provider'
 export const ROLE_SERVICE_PROVIDER = 'role-service-provider'
-
-export const UDN_ADMIN_ROLE_ID = 1
