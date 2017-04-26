987118af1a4f7e404f3bf69a90490166d761c2ff
Anh Nguyen <anh.nguyen@nordsoftware.com>
2017-04-26 16:11:51 +0300

    format labels

--- src/components/network/forms/add-node-form.jsx
+++ src/components/network/forms/add-node-form.jsx
@@ -173,7 +173,7 @@ class NetworkAddNodeForm extends React.Component {
   }
 
   render() {
-    const { handleSubmit, error } = this.props
+    const { handleSubmit, error, intl } = this.props
     const footerButtons = this.getFooterButtons()
 
     return (
@@ -220,7 +220,7 @@ class NetworkAddNodeForm extends React.Component {
             name="node_role"
             className="input-select"
             component={FieldFormGroupSelect}
-            options={NODE_ROLE_OPTIONS}
+            options={NODE_ROLE_OPTIONS.map(role => ({...role, label: intl.formatMessage({id: role.label})}))}
             label={<FormattedMessage id="portal.network.addNodeForm.role.title" />}
             addonAfter={
               <HelpTooltip
@@ -235,7 +235,7 @@ class NetworkAddNodeForm extends React.Component {
             name="node_env"
             className="input-select"
             component={FieldFormGroupSelect}
-            options={NODE_ENVIRONMENT_OPTIONS}
+            options={NODE_ENVIRONMENT_OPTIONS.map(env => ({...env, label: intl.formatMessage({id: env.label})}))}
             label={<FormattedMessage id="portal.network.addNodeForm.environment.title" />}
             addonAfter={
               <HelpTooltip
