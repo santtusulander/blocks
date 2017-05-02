c3614860f1d0c39d2e6928c58f992227d8ae3217
Santtu Sulander <santtu.sulander@idean.com>
2017-05-02 15:25:29 +0300

    use netaquity IDs as continent IDs

--- src/constants/continents.js
+++ src/constants/continents.js
@@ -1,9 +1,9 @@
 export default [
-  { id: "af", labelId: "portal.common.continent.africa" },
-  { id: "an", labelId: "portal.common.continent.antarctica" },
-  { id: "as", labelId: "portal.common.continent.asia" },
-  { id: "eu", labelId: "portal.common.continent.europe" },
-  { id: "na", labelId: "portal.common.continent.northAmerica" },
-  { id: "oc", labelId: "portal.common.continent.oceania" },
-  { id: "sa", labelId: "portal.common.continent.southAmerica" }
+  { id: "1", labelId: "portal.common.continent.africa" },
+  { id: "2", labelId: "portal.common.continent.antarctica" },
+  { id: "3", labelId: "portal.common.continent.oceania" },
+  { id: "4", labelId: "portal.common.continent.asia" },
+  { id: "5", labelId: "portal.common.continent.europe" },
+  { id: "6", labelId: "portal.common.continent.northAmerica" },
+  { id: "7", labelId: "portal.common.continent.southAmerica" }
 ]
