194aef10a98698c5b32c286306261cc74e878ef7
Mykhailo Prochkaruk <mprochkaruk@vidscale.com>
2017-05-04 15:09:10 +0300

    UDNP-3621 - fix dashboard-panels UTs

--- src/components/dashboard/__tests__/dashboard-panels.js
+++ src/components/dashboard/__tests__/dashboard-panels.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
+jest.unmock('classnames')
 jest.unmock('../dashboard-panels.jsx')
 import DashboardPanels from '../dashboard-panels.jsx'
 
@@ -9,5 +10,15 @@ describe('DashboardPanel', () => {
   it('should exist', () => {
     const dashboardPanels = shallow(<DashboardPanels />)
     expect(dashboardPanels.length).toBe(1)
+  })
+
+  it('should reflect className', () => {
+    const dashboardPanels = shallow(<DashboardPanels className="testClass"/>)
+    expect(dashboardPanels.find('.testClass').length).toBe(1)
+  })
+
+  it('should reflect children', () => {
+    const dashboardPanels = shallow(<DashboardPanels className="testClass"> <div className="children">test</div></DashboardPanels>)
+    expect(dashboardPanels.find('.children').length).toBe(1)
   })
 })
