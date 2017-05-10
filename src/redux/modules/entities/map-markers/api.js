184b7d68363701b19ca2e91c15162300b52d5ba7
Mikko Alander <mikko.alander@idean.com>
2017-05-10 18:28:47 +0300

    processStrategy fix

--- src/redux/modules/entities/map-markers/api.js
+++ src/redux/modules/entities/map-markers/api.js
@@ -3,47 +3,15 @@ import {normalize, schema} from 'normalizr'
 
 import { BASE_URL_NORTH, PAGINATION_MOCK } from '../../../util'
 
-// const markerMockData = [
-//   {
-//     account_id: 1,
-//     boundingbox: [
-//       50.027758,
-//       50.057758,
-//       8.5420492,
-//       8.5720492
-//     ],
-//     brand_id: 'udn',
-//     city_name: 'Frankfurt',
-//     cloud_location_id: 'fra01',
-//     cloud_name: 'UDN-Core',
-//     cloud_provider: 'os',
-//     cloud_region: 'EU',
-//     country_code: 'de',
-//     group_id: 6,
-//     iata_code: 'fra',
-//     id: 'fra01',
-//     lat: 50.042758,
-//     lon: 8.5570492,
-//     postalcode: '123123',
-//     state: 'Hesse',
-//     street: 'some street address'
-//   }
-// ]
-
 const markerSchema = new schema.Entity('mapMarkers', {},{
   processStrategy: (value) => {
-    if (value.account_id === 1) {
-      return {
-        ...value,
-        latlng: [value.lat, value.lon],
-        type: 'core'
-      }
-    } else {
-      return {
-        ...value,
-        latlng: [value.lat, value.lon],
-        type: 'edge'
-      }
+
+    const type = (value.account_id === 1) ? 'core' : 'edge'
+
+    return {
+      ...value,
+      latlng: [value.lat, value.lon],
+      type
     }
   }
 })
