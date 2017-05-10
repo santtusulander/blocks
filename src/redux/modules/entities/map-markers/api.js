489bc97544359898bf775733639f33114759be08
Mikko Alander <mikko.alander@idean.com>
2017-05-10 17:31:56 +0300

    remove mock data - use real api

--- src/redux/modules/entities/map-markers/api.js
+++ src/redux/modules/entities/map-markers/api.js
@@ -1,42 +1,40 @@
-//import axios from 'axios'
+import axios from 'axios'
 import {normalize, schema} from 'normalizr'
 
-const markerMockData = [
-  {
-    account_id: 1,
-    boundingbox: [
-      50.027758,
-      50.057758,
-      8.5420492,
-      8.5720492
-    ],
-    brand_id: 'udn',
-    city_name: 'Frankfurt',
-    cloud_location_id: 'fra01',
-    cloud_name: 'UDN-Core',
-    cloud_provider: 'os',
-    cloud_region: 'EU',
-    country_code: 'de',
-    group_id: 6,
-    iata_code: 'fra',
-    id: 'fra01',
-    lat: 50.042758,
-    lon: 8.5570492,
-    postalcode: '123123',
-    state: 'Hesse',
-    street: 'some street address'
-  }
-]
+import { BASE_URL_NORTH } from '../../../util'
+
+// const markerMockData = [
+//   {
+//     account_id: 1,
+//     boundingbox: [
+//       50.027758,
+//       50.057758,
+//       8.5420492,
+//       8.5720492
+//     ],
+//     brand_id: 'udn',
+//     city_name: 'Frankfurt',
+//     cloud_location_id: 'fra01',
+//     cloud_name: 'UDN-Core',
+//     cloud_provider: 'os',
+//     cloud_region: 'EU',
+//     country_code: 'de',
+//     group_id: 6,
+//     iata_code: 'fra',
+//     id: 'fra01',
+//     lat: 50.042758,
+//     lon: 8.5570492,
+//     postalcode: '123123',
+//     state: 'Hesse',
+//     street: 'some street address'
+//   }
+// ]
 
 const markerSchema = new schema.Entity('mapMarkers', {},{})
 
-// TODO: remove mock data when api is ready
-//
-// import { BASE_URL_CIS_SOUTH } from '../../../util'
-//
-// const baseUrl = () => {
-//   return `${BASE_URL_CIS_SOUTH}/clusters?format=brief`
-// }
+const baseUrl = () => {
+  return `${BASE_URL_NORTH}/locations`
+}
 
 /**
  * Fetch list of Markers
@@ -44,12 +42,8 @@ const markerSchema = new schema.Entity('mapMarkers', {},{})
  */
 export const fetchAll = () => {
 
-  const data = markerMockData
-
-  return normalize(data, [markerSchema])
-
-  // return axios.get(baseUrl())
-  // .then(({data}) => {
-  //   return normalize(data, [ markerSchema ])
-  // })
+  return axios.get(baseUrl())
+  .then(({data}) => {
+    return normalize(data.data, [ markerSchema ])
+  })
 }
