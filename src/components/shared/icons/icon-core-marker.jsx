a1b48684a771314c26baaa3ac97361332ba00371
Mykhailo Prochkaruk <mprochkaruk@vidscale.com>
2017-05-12 16:38:06 +0300

    UDNP-3651 - add new icons for maps

---
+++ src/components/shared/icons/icon-core-marker.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Icon from '../icon.jsx'
+
+const IconCoreMarker = (props) => {
+  const {className, height, width} = props
+  return (
+    <Icon className={className} width={width} height={height} viewbox="0 0 24 24">
+      <path fill="#00a9d4" transform="translate(2 2)" d="M7.5,0C5.0676,0,2.2297,1.4865,2.2297,5.2703 C2.2297,7.8378,6.2838,13.5135,7.5,15c1.0811-1.4865,5.2703-7.027,5.2703-9.7297C12.7703,1.4865,9.9324,0,7.5,0z"/>
+    </Icon>
+  )
+}
+
+IconCoreMarker.displayName = "IconUser"
+IconCoreMarker.propTypes = {
+  className: React.PropTypes.string,
+  height: React.PropTypes.number,
+  width: React.PropTypes.number
+}
+
+export default IconCoreMarker
