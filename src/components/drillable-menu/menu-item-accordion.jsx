a55ebe3667a25b705560ac5b7ede06c960521f39
Santtu Sulander <santtu.sulander@idean.com>
2017-05-12 15:12:18 +0300

    styles

--- src/components/drillable-menu/menu-item-accordion.jsx
+++ src/components/drillable-menu/menu-item-accordion.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import classnames from 'classnames'
+
+import CaretDown from '../shared/icons/icon-caret-down'
 
 class Accordion extends Component {
   constructor(props) {
@@ -9,25 +12,29 @@ class Accordion extends Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    this.toggle(!!nextProps.searchActive)
+    this.toggle(!!nextProps.items.length && !!nextProps.searchActive)
   }
 
   toggle(open = !this.state.open) {
     this.setState({ open })
   }
 
+  /*eslint-disable react-intl/string-is-marked-for-translation*/
   render() {
 
     const { headerTitle, items } = this.props
+    const active = this.state.open
 
     return (
-      <li>
-        <ul className="scrollable-menu">
-          <li>
-            <a className="name-container" onClick={() => this.toggle()}><h4>{headerTitle}</h4></a>
-          </li>
-          {this.state.open && items}
-        </ul>
+      <li className={classnames('accordion', { active })}>
+        <a onClick={() => this.toggle()} className="accordion-header">
+          <h4 className="name-container">{headerTitle}<CaretDown /></h4>
+          <h4 className="entity-count-number">({items.length})</h4>
+        </a>
+          {this.state.open &&
+            <ul className='scrollable-menu'>
+              {items}
+            </ul>}
       </li>
     )
   }
