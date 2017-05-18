97f1dd78bb9af01a70ac70c498ff08a9697a6fbe
Pouya Adib <pouya.adib@nordsoftware.com>
2017-05-19 00:06:12 +0300

    fix current folder name bug

--- src/components/storage/storage-contents.jsx
+++ src/components/storage/storage-contents.jsx
@@ -253,7 +253,7 @@ class StorageContents extends Component {
     const renderDropZone = !hasContents || !hasFiles
     const highlightZoneOnDrag = this.state.isDragging && this.state.draggingOver === null
 
-    const uploadDestinationFolder = this.state.draggingOver ? this.state.draggingOver : (isRootDirectory ? storageId : params.splat.slice(-1).shift())
+    const uploadDestinationFolder = this.state.draggingOver ? this.state.draggingOver : (isRootDirectory ? storageId : params.splat.split('/').slice(-1).shift())
 
     return (
       <SectionContainer>
