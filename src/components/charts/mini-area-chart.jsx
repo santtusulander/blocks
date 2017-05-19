49c35d044de79a9d4fe26508ee2104af07ad9c30
Chi Nguyen <chi.nguyen.idean@gmail.com>
2017-05-19 12:06:03 +0300

    add mini chart to styleguide and mock

--- src/components/charts/mini-area-chart.jsx
+++ src/components/charts/mini-area-chart.jsx
@@ -1,12 +1,16 @@
 import React, {PropTypes, Component} from 'react';
-import Immutable from 'immutable'
 import { ResponsiveContainer, Area, AreaChart } from 'recharts'
+
+import { paleblue, orange } from '../../constants/colors'
+
+const colors = [ paleblue, orange ]
 
 export default class MiniAreaChart extends Component {
   constructor() {
     super()
 
     this.renderAreas = this.renderAreas.bind(this)
+    this.renderGradients = this.renderGradients.bind(this)
   }
 
   renderAreas() {
@@ -14,10 +18,31 @@ export default class MiniAreaChart extends Component {
       <Area
         key={i}
         isAnimationActive={false}
-        fillOpacity={0}
+        stroke={colors[i]}
+        strokeWidth="2"
+        fillOpacity="1"
+        fill={`url(#color-${area.dataKey})`}
         {...area}
       />
     ).reverse()
+  }
+
+  renderGradients() {
+    const { areas, noGradient } = this.props
+
+    return areas.map((area,i) => {
+      const opacity = 0.5
+      const {dataKey} = area
+
+      return (
+        <linearGradient key={i} id={`color-${dataKey}`}
+            x1="0" y1="0"
+            x2="0" y2="100%">
+          <stop offset="0%" stopColor={colors[i]} stopOpacity={noGradient ? 0 : opacity}/>
+          <stop offset="100%" stopColor={colors[i]} stopOpacity={0}/>
+        </linearGradient>
+      )
+    })
   }
 
   render() {
@@ -28,6 +53,9 @@ export default class MiniAreaChart extends Component {
         <span className="mini-area-chart-label">{chartLabel}</span>
         <ResponsiveContainer minHeight={50} aspect={2}>
           <AreaChart data={data} className={className}>
+            <defs>
+              { this.renderGradients() }
+            </defs>
             { this.renderAreas() }
           </AreaChart>
       </ResponsiveContainer>
@@ -36,9 +64,13 @@ export default class MiniAreaChart extends Component {
 }
 
 MiniAreaChart.displayName = 'MiniAreaChart'
+MiniAreaChart.defaultProps = {
+  noGradient: false
+}
 MiniAreaChart.propTypes = {
-  areas: PropTypes.object,
+  areas: PropTypes.array,
   chartLabel: PropTypes.string,
   className: PropTypes.string,
-  data: PropTypes.instanceOf(Immutable.List)
+  data: PropTypes.array,
+  noGradient: PropTypes.bool
 };
