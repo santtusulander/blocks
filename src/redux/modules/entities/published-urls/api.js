a81fa0799e62814e94b952f46296616b599e0b4d
Mikko Alander <mikko.alander@idean.com>
2017-05-22 18:21:04 +0300

    extract downloadUrls and publishedUrls from api response and normalize

--- src/redux/modules/entities/published-urls/api.js
+++ src/redux/modules/entities/published-urls/api.js
@@ -1,24 +1,62 @@
 import axios from 'axios'
-//import {normalize, schema} from 'normalizr'
+import {normalize, schema} from 'normalizr'
+import { Map } from 'immutable'
 
-import { BASE_URL_NORTH } from '../../../util'
+import { BASE_URL_NORTH, buildReduxId } from '../../../util'
 
-// const serviceTitleSchema = new schema.Entity('serviceTitles', {}, {
-//   idAttribute: 'name'
-// })
+const publishedUrlSchema = new schema.Entity('publishedUrls', {}, {})
 
-export const create = ({payloadX, brand, account, group}) => {
-  const payload = {
-    ingest_points: [
-      {
-        ingest_point_id: "video",
-        ingest_paths: ["fold/subfold/mike-third-test.png"]
-      }
-    ]
-  }
+const ALLOWED_SCHEMAS = ['http', 'https']
 
-  return axios.post(`${BASE_URL_NORTH}/brands/${brand}/accounts/${account}/group/${group}/published_urls`, payload, { headers: { 'Content-Type': 'application/json' }})
+export const create = ({payload, brand, account, group}) => {
+  
+  //Shchema of payload
+  //  const payload = {
+  //   ingest_points: [
+  //     {
+  //       ingest_point_id: "video",
+  //       ingest_paths: ["/2982_GTM_v4asdas.pdf"]
+  //     }
+  //   ]
+  // }
+
+  return axios.post(`${BASE_URL_NORTH}/brands/${brand}/accounts/${account}/groups/${group}/published_urls`, payload, { headers: { 'Content-Type': 'application/json' }})
     .then((resp) => {
-      console.log('publishedUrls', resp);
+
+      const [ingest_point] = resp.data.ingest_points
+
+      //get downloadUrls
+      /* NOTE: Casting to Map to be able to map object-keys easily */
+      const downloadUrls = Map(ingest_point.ingest_urls.protocols).reduce((acc, val, key) => {
+        if (ALLOWED_SCHEMAS.includes(key)) {
+          acc.push(...val.urls)
+        }
+        return acc
+      }, [])
+
+      //get publishedUrls
+      /* NOTE: Casting to Map to be able to map object-keys easily */
+      const publishedUrls = ingest_point.published_urls.reduce((acc, val) => {
+        const urls = Map(val.services).reduce((urlAcc, service) => {
+          if (service.urls.length > 0) {
+            urlAcc.push(...service.urls)
+          }
+
+          return urlAcc
+        }, [])
+
+        if (urls.length > 0) {
+          acc.push(...urls)
+        }
+
+        return acc
+      }, [])
+
+
+      const id = buildReduxId(brand,account,group,ingest_point.ingest_point_id, ingest_point.ingest_paths.join(''))
+
+      const normalized = normalize({id, downloadUrls, publishedUrls }, publishedUrlSchema)
+
+      return normalized
     })
 }
