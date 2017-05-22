7c755fbdb03daa4f191f417f62102d5126d13d7d
Mikko Alander <mikko.alander@idean.com>
2017-05-22 18:22:11 +0300

    selector

--- src/redux/modules/entities/published-urls/selectors.js
+++ src/redux/modules/entities/published-urls/selectors.js
@@ -1,21 +1,21 @@
 import {getEntityById} from '../../entity/selectors'
 
 /**
- * Get serviceTitle by ID
+ * Get publishedUrls by ID
  * @param  {} state from redux
  * @param  String id of the item
  * @return {} Map
  */
 export const getById = (state, id) => {
-  return getEntityById(state, 'serviceTitles', id)
+  return getEntityById(state, 'publishedUrls', id)
 }
 
 /**
- * Get list of serviceTitles
+ * Get list of publishedUrls
  * @param  {} state
  * @param  {String} brand [description]
  * @return Map
  */
 export const getAll = (state) => {
-  return state.entities.serviceTitles
+  return state.entities.publishedUrls
 }
