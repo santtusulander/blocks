4625bf8e5197634062974a0d2fa87a7da7e494c0
Nizar Rahme <nizar.rahme@nordsoftware.com>
2017-05-23 17:09:17 +0300

    Add constants to mapbox

--- src/constants/mapbox.js
+++ src/constants/mapbox.js
@@ -1,12 +1,12 @@
 export const MAPBOX_DARK_THEME = 'mapbox://styles/ericssonudn/cixtc24am001h2qqpz566a3td'
 export const MAPBOX_LIGHT_THEME = 'mapbox://styles/ericssonudn/cixtceqrk001k2sqwck38fico'
-export const MAPBOX_ZOOM_MIN = 1
 export const MAPBOX_ZOOM_MAX = 13
 export const MAPBOX_SCROLL_TIMEOUT = 500
 export const MAPBOX_MAX_CITIES_FETCHED = 50
 export const MAPBOX_BOUNDS_CHANGE_PERCENTAGE = 3
 export const MAPBOX_CITY_LEVEL_ZOOM = 7
 export const MAPBOX_CITY_RADIUS_DIVIDER = 10
+export const MAPBOX_CENTER = [ 0, 15 ]
 export const MAPBOX_HEAT_MAP_COLORS = [
   '#7b0663',
   '#8f2254',
@@ -14,6 +14,10 @@ export const MAPBOX_HEAT_MAP_COLORS = [
   '#ba5f32',
   '#d4851d',
   '#f9ba01'
+]
+export const MAPBOX_MAXBOUNDS = [
+  [-180, -83],
+  [180, 85]
 ]
 export const MAPBOX_HEAT_MAP_DEFAULT_COLOR = MAPBOX_HEAT_MAP_COLORS[5]
 
