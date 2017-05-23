744bab1547bc6d53ca6a8950c4e9a39a0a559312
Pouya Adib <pouya.adib@nordsoftware.com>
2017-05-23 16:57:45 +0300

    move copy to clipboard functions to item component

--- src/components/storage/storage-item-properties.jsx
+++ src/components/storage/storage-item-properties.jsx
@@ -1,6 +1,10 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 import { FormattedMessage } from 'react-intl'
-import { Button, FormControl } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import * as uiActionCreators from '../../redux/modules/ui'
 
 import { formatDate, formatBytes } from '../../util/helpers'
 
@@ -9,63 +13,102 @@ const mockUrls = [
   "http://pub_name1/path4"
 ]
 
-const StorageItemProperties = ({
-  copyToClipboard,
-  created,
-  dateFormat,
-  isDirectory,
-  lastModified,
-  location,
-  size
-}) => {
-  return (
-    <div className='storage-item-properties-container'>
-      <div className='storage-item-properties-column left'>
-        <div className='info'>
-          <FormattedMessage id='portal.storage.summaryPage.itemProperties.created.label' />
-          <div className='text'>{formatDate(created, dateFormat)}</div>
-        </div>
-        <div className='info'>
-          <FormattedMessage id='portal.storage.summaryPage.itemProperties.lastModified.label' />
-          <div className='text'>{formatDate(lastModified, dateFormat)}</div>
-        </div>
-        <div className='info'>
-          {isDirectory
-            ?
-              <FormattedMessage id='portal.storage.summaryPage.itemProperties.location.folder.label' />
-            :
-              <FormattedMessage id='portal.storage.summaryPage.itemProperties.location.file.label' />
-          }
-          <div className='text'>{location}</div>
-        </div>
-      </div>
-      {!isDirectory &&
-        <div className='storage-item-properties-column'>
-          <div className='info'>
-            <FormattedMessage id='portal.storage.summaryPage.itemProperties.size.label' />
-            <div className='text'>{formatBytes(size)}</div>
-          </div>
-          <div className='info'>
-            <FormattedMessage id='portal.storage.summaryPage.itemProperties.url.label' />
-            {mockUrls.map((url, index) => (
-                <div key={index} className='url'>
-                  <div className='url-text'>
-                    {url}
-                  </div>
-                  <Button
-                    className='url-copy-button'
-                    bsStyle="link"
-                    onClick={() => copyToClipboard(url)}>
-                    <FormattedMessage id="portal.storage.summaryPage.itemProperties.copyLink.label" />
-                  </Button>
-                </div>
-              ))
-            }
-          </div>
-        </div>
-      }
-    </div>
-  )
+class StorageItemProperties extends Component {
+  constructor(props) {
+    super(props)
+
+    this.notificationTimeout = null
+  }
+
+  copyToClipboard(text) {
+    const textArea = document.createElement('textarea')
+    textArea.style.left = '-1000px'
+    textArea.style.position = 'absolute'
+    textArea.value = text
+    document.body.appendChild(textArea)
+    textArea.select()
+
+    try {
+      const result = document.execCommand('copy')
+      document.body.removeChild(textArea)
+      result
+        ?
+        this.showNotification(<FormattedMessage id="portal.storage.summaryPage.itemProperties.copyToClipboard.successful.label" />)
+        :
+        this.showNotification(<FormattedMessage id="portal.storage.summaryPage.itemProperties.copyToClipboard.failed.label" />)
+    } catch (e) {
+      document.body.removeChild(textArea)
+      this.showNotification(<FormattedMessage id="portal.storage.summaryPage.itemProperties.copyToClipboard.failed.label" />)
+    }
+
+  }
+
+  showNotification(message) {
+    clearTimeout(this.notificationTimeout)
+    this.props.uiActions.changeSidePanelNotification(message)
+    this.notificationTimeout = setTimeout(this.props.uiActions.changeSidePanelNotification, 10000)
+  }
+
+  render() {
+    const {
+      copyToClipboard,
+      created,
+      dateFormat,
+      isDirectory,
+      lastModified,
+      location,
+      size
+    } = this.props
+
+    return (
+     <div className='storage-item-properties-container'>
+       <div className='storage-item-properties-column left'>
+         <div className='info'>
+           <FormattedMessage id='portal.storage.summaryPage.itemProperties.created.label' />
+           <div className='text'>{formatDate(created, dateFormat)}</div>
+         </div>
+         <div className='info'>
+           <FormattedMessage id='portal.storage.summaryPage.itemProperties.lastModified.label' />
+           <div className='text'>{formatDate(lastModified, dateFormat)}</div>
+         </div>
+         <div className='info'>
+           {isDirectory
+             ?
+               <FormattedMessage id='portal.storage.summaryPage.itemProperties.location.folder.label' />
+             :
+               <FormattedMessage id='portal.storage.summaryPage.itemProperties.location.file.label' />
+           }
+           <div className='text'>{location}</div>
+         </div>
+       </div>
+       {!isDirectory &&
+         <div className='storage-item-properties-column'>
+           <div className='info'>
+             <FormattedMessage id='portal.storage.summaryPage.itemProperties.size.label' />
+             <div className='text'>{formatBytes(size)}</div>
+           </div>
+           <div className='info'>
+             <FormattedMessage id='portal.storage.summaryPage.itemProperties.url.label' />
+             {mockUrls.map((url, index) => (
+                 <div key={index} className='url'>
+                   <div className='url-text'>
+                     {url}
+                   </div>
+                   <Button
+                     className='url-copy-button'
+                     bsStyle="link"
+                     onClick={() => copyToClipboard(url)}>
+                     <FormattedMessage id="portal.storage.summaryPage.itemProperties.copyLink.label" />
+                   </Button>
+                 </div>
+               ))
+             }
+           </div>
+         </div>
+       }
+     </div>
+    )
+  }
 }
 
 StorageItemProperties.displayName = "StorageItemProperties"
@@ -76,7 +119,15 @@ StorageItemProperties.propTypes = {
   isDirectory: PropTypes.bool,
   lastModified: PropTypes.number,
   location: PropTypes.string,
-  size: PropTypes.number
+  size: PropTypes.number,
+  uiActions: PropTypes.objext
 }
 
-export default StorageItemProperties
+/* istanbul ignore next */
+const mapDispatchToProps = (dispatch) => (
+  {
+    uiActions: bindActionCreators(uiActionCreators, dispatch)
+  }
+)
+
+export default connect(null, mapDispatchToProps)(StorageItemProperties)
