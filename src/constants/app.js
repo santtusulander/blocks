805c42773f313fb4b0f3757f56ad35fd429dd82f
Mykhailo Prochkaruk <mprochkaruk@vidscale.com>
2017-05-25 10:18:30 +0300

    Enable SMARTLING for dev-portal

--- src/constants/app.js
+++ src/constants/app.js
@@ -12,7 +12,8 @@ export const SENTRY_DSN = 'https://75534db319c14707b6dd711ee3c804e2@sentry.io/11
   List of hostnames where Smartling will be used
  */
 export const SMARTLING_HOSTNAMES = [
-  'staging-portal.ericssonudn.com'
+  'staging-portal.ericssonudn.com',
+  'dev-portal.ericssonudn.com'
 ]
 
 export const SMARTLING_ORG_ID = 'KOdvArOY-sCmyD7hmxyx9g'
