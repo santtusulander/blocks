af8be8ae04de51aa9a150826abfd40da050cfc71
Chi Nguyen <chi.nguyen.idean@gmail.com>
2017-05-30 11:48:05 +0300

    replace formatDate

--- src/components/support/support-ticket-comment.jsx
+++ src/components/support/support-ticket-comment.jsx
@@ -1,7 +1,8 @@
 import React, { PropTypes } from 'react'
 import classNames from 'classnames'
 import { Map } from 'immutable'
-import { formatDate } from '../../util/helpers'
+import { FormattedDate } from 'react-intl'
+import { DATE_FORMATS } from '../../constants/date-formats'
 
 /* TODO: uncomment when this tab will be implemented */
 /* eslint-disable react-intl/string-is-marked-for-translation */
@@ -11,7 +12,7 @@ const SupportTicketComment = (props) => {
     <div className={classNames('support-ticket-comment', props.className)}>
       <div className="support-ticket-comment__header">
         <span className="support-ticket-comment__author">{props.comment.get('author_id')}</span>
-        <span className="support-ticket-comment__created"> - {formatDate(props.comment.get('created_at'), 'MM/DD/YYYY hh:mm a')}</span>
+        <span className="support-ticket-comment__created"> - <FormattedDate value={props.comment.get('created_at')} format={DATE_FORMATS.DATE_HOUR_12}/></span>
       </div>
       <div className="support-ticket-comment__body">
         {props.comment.get('body')}
